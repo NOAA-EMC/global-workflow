e897547890d5ab25c9aed62afaca9c7734892ac7
xiujuan.su@noaa.gov <xiujuan.su@noaa.gov>
2014-08-14 15:08:50 +0000

    modified to increase wall time

--- scripts/global_T62_regression.sh
+++ scripts/global_T62_regression.sh
@@ -11,7 +11,7 @@ if [[ "$arch" = "Linux" ]]; then
 
    # Submit jobs using sub_zeus wrapper.
 
-   /bin/sh sub_zeus -j $global_T62_updat_exp1 -t 0:15:00 -p 6/6/0 $scripts/global_T62.sh
+   /bin/sh sub_zeus -j $global_T62_updat_exp1 -t 0:30:00 -p 6/6/0 $scripts/global_T62.sh
 
    while [[ $(grep -c '+ rc=0' ${global_T62_updat_exp1}.out) -ne 1 ]]; do
       grep '+ rc=' ${global_T62_updat_exp1}.out > return_code_global_3dvar.out
@@ -30,7 +30,7 @@ if [[ "$arch" = "Linux" ]]; then
 
    rm -f return_code_global_3dvar.out
 
-   /bin/sh sub_zeus -j $global_T62_updat_exp2 -t 0:12:00 -p 8/8/0 $scripts/global_T62.sh
+   /bin/sh sub_zeus -j $global_T62_updat_exp2 -t 0:30:00 -p 8/8/0 $scripts/global_T62.sh
 
    while [[ $(grep -c '+ rc=0' ${global_T62_updat_exp2}.out) -ne 1 ]]; do
       grep '+ rc=' ${global_T62_updat_exp2}.out > return_code_global_3dvar.out
@@ -49,7 +49,7 @@ if [[ "$arch" = "Linux" ]]; then
 
    rm -f return_code_global_3dvar.out
 
-   /bin/sh sub_zeus -j $global_T62_contrl_exp1 -t 0:15:00 -p 6/6/0 $scripts/global_T62.sh
+   /bin/sh sub_zeus -j $global_T62_contrl_exp1 -t 0:30:00 -p 6/6/0 $scripts/global_T62.sh
 
    while [[ $(grep -c '+ rc=0' ${global_T62_contrl_exp1}.out) -ne 1 ]]; do
       grep '+ rc=' ${global_T62_contrl_exp1}.out > return_code_global_3dvar.out
@@ -68,7 +68,7 @@ if [[ "$arch" = "Linux" ]]; then
 
    rm -f return_code_global_3dvar.out
 
-   /bin/sh sub_zeus -j $global_T62_contrl_exp2 -t 0:12:00 -p 8/8/0 $scripts/global_T62.sh
+   /bin/sh sub_zeus -j $global_T62_contrl_exp2 -t 0:30:00 -p 8/8/0 $scripts/global_T62.sh
 
    while [[ $(grep -c '+ rc=0' ${global_T62_contrl_exp2}.out) -ne 1 ]]; do
       grep '+ rc=' ${global_T62_contrl_exp2}.out > return_code_global_3dvar.out
