01a678db6e9405b3112e2b9383435b4c226b8d41
russ.treadon <russ.treadon@noaa.gov>
2014-12-09 19:38:29 +0000

    GDAS-4DEnVar:  generalize refactor_4nco_global.sh (see #429)
    scripts/
     * refactor_4nco_global.sh - modify to accept command line options

--- scripts/refactor_4nco_global.sh
+++ scripts/refactor_4nco_global.sh
@@ -1,81 +1,104 @@
+#!/bin/ksh
+
 set -x
 
-# Set svn source
-SVNROOT=https://svnemc.ncep.noaa.gov/projects
-SVNGSI=gsi/branches/GDAS-4DEnVar
-
-# Set version numbers
-gfs_ver=v1.0.0
-gdas_ver=v1.0.0
-global_shared_ver=v1.0.0
+options=${1:-""}
+source_path=${2:-$LOGNAME/source_path}
+target_path=${3:-$LOGNAME/target_path}
+gfs_ver=${4:-v1.0.1}
+gdas_ver=${5:-v1.0.0}
+global_shared_ver=${6:-v1.0.0}
 
 
-# Set directory pathnames
-target_path=/global/save/$LOGNAME/TEST
+# Set target directory pathnames
 target_gfs=$target_path/gfs.$gfs_ver
 target_gdas=$target_path/gdas.$gdas_ver
 target_shared=$target_path/global_shared.$global_shared_ver
 
 
-# Create directories
-mkdir -p $target_path
-mkdir -p $target_gfs
-mkdir -p $target_gdas
-mkdir -p $target_shared
+# Set commands
+if [[ "$options" = "svn" ]]; then
+  svnmkdir="svn mkdir"
+  svnmkdirp="svn mkdir --parents"
+  svncopy="svn copy"
+else
+   svnmkdir="mkdir"
+   svnmkdirp="mkdir -p"
+   svncopy="cp"
+fi
 
 
-# Checkout gfs and gdas jobs
-mkdir -p $target_gfs/jobs
-svn checkout $SVNROOT/$SVNGSI/scripts $target_gfs/jobs
-list="arw EnKF exglobal Get gfs global JGDAS hwrf link llsub multi nmm refactor regress rtma run sub"
-for type in $list; do
-   rm -rf $target_gfs/jobs/*${type}*
+# Create base directory
+$svnmkdir  $target_path
+$svnmkdirp $target_shared
+
+
+# Create and populate gfs directory
+$svnmkdirp $target_gfs
+$svnmkdirp $target_gfs/jobs
+flist="JGFS_ANALYSIS"
+for file in $flist; do
+   $svncopy $source_path/scripts/$file $target_gfs/jobs/
 done
 
-mkdir -p $target_gdas/jobs
-svn checkout $SVNROOT/$SVNGSI/scripts $target_gdas/jobs
-mv $target_gdas/jobs/EnKF/scripts_ncep/JGDAS* $target_gdas/jobs
-list="arw EnKF exglobal Get gfs global JGFS hwrf link llsub multi nmm refactor regress rtma run sub"
-for type in $list; do
-   rm -rf $target_gdas/jobs/*${type}*
+
+# Create and populate gdas directory
+$svnmkdirp $target_gdas
+$svnmkdirp $target_gdas/jobs
+flist="JGDAS_ANALYSIS_HIGH"
+for file in $flist; do
+   $svncopy $source_path/scripts/$file $target_gdas/jobs/
+done
+flist="JGDAS_ENKF_FCST JGDAS_ENKF_INFLATE_RECENTER JGDAS_ENKF_INNOVATE_OBS JGDAS_ENKF_POST JGDAS_ENKF_SELECT_OBS JGDAS_ENKF_UPDATE"
+for file in $flist; do
+   $svncopy $source_path/scripts/EnKF/scripts_ncep/$file $target_gdas/jobs/
 done
 
 
-# Checkout global_shared fix
+# Create and populate global_shared directory
+$svnmkdirp $target_shared
+
+
+# Create and populate global_shared fix
 mkdir -p $target_shared/fix
-svn checkout $SVNROOT/$SVNGSI/fix $target_shared/fix
-list="aod arw bufrtab cmaq gsd hwrf ndas nems rtma example ozonly global_ch4 global_co2 global_co_esrl reg_test global_n2o Little_Endian nam prepobs_prep rtma rap"
-for type in $list; do
-   rm -rf $target_shared/fix/*${type}*
+flist="atms_beamwidth.txt global_aeroinfo.txt global_anavinfo.l64.txt global_convinfo.txt global_hybens_locinfo.l64.txt global_hybens_smoothinfo.l64.txt global_insituinfo.txt global_ozinfo.txt global_pcpinfo.txt global_satangbias.txt global_satinfo.txt global_scaninfo.txtk prepobs_errtable.global"
+for file in $flist; do
+   $svncopy $source_path/fix/$file $target_shared/fix/
+done
+$svnmkdirp $target_shared/fix/Big_Endian
+flist="global_berror.l64y1154.f77 global_berror.l64y130.f77 global_berror.l64y192.f77 global_berror.l64y258.f77 global_berror.l64y290.f77 global_berror.l64y386.f77 global_berror.l64y578.f77 global_berror.l64y882.f77 global_berror.l64y96.f77"
+for file in $flist; do
+   $svncopy $source_path/fix/Big_Endian/$file $target_shared/fix/Big_Endian/
 done
 
 
-# Checkout global_shared scripts
-mkdir -p $target_shared/scripts
-svn checkout $SVNROOT/$SVNGSI/scripts $target_shared/scripts
-mv $target_shared/scripts/EnKF/scripts_ncep/exglobal* $target_shared/scripts
-list="arw EnKF Get gfs JGDAS JGFS hwrf link llsub multi nmm refactor regress rtma run sub"
-for type in $list; do
-   rm -rf $target_shared/scripts/*${type}*
+# Create and populate global_shared scripts
+$svnmkdirp $target_shared/scripts
+flist="exglobal_analysis.sh.ecf exglobal_enkf_innovate_obs.sh.ecf"
+for file in $flist; do
+   $svncopy $source_path/scripts/$file $target_shared/scripts/
 done
-rm -rf $target_shared/scripts/global_*
-
-
-# Checkout global_shared sorc
-mkdir -p $target_shared/sorc/gsi.fd
-svn checkout $SVNROOT/$SVNGSI/sorc $target_shared/sorc/gsi.fd
-
-list="adderrspec_nmcmeth_spec.fd getsfcensmeanp.fd getsigensmeanp_smooth_ncep.fd recentersigp.fd"
-for code in $list; do
-   mkdir -p $target_shared/sorc/$code
-   svn checkout $SVNROOT/$SVNGSI/util/EnKF/gfs/src/$code $target_shared/sorc/$code
+flist="exglobal_enkf_fcst.sh.ecf exglobal_enkf_inflate_recenter.sh.ecf exglobal_enkf_post.sh.ecf exglobal_enkf_update.sh.ecf"
+for file in $flist; do
+   $svncopy $source_path/scripts/EnKF/scripts_ncep/$file $target_shared/scripts/
 done
 
 
-# Checkout global_shared ush
-mkdir -p $target_shared/ush
-svn checkout $SVNROOT/$SVNGSI/scripts $target_shared/ush
-list="arw EnKF Get global JGDAS JGFS hwrf link llsub multi nmm refactor regress rtma run_arw rungsi sub"
-for type in $list; do
-   rm -rf $target_shared/ush/*${type}*
+# Create and populate global_shared sorc
+$svnmkdirp $target_shared/sorc/gsi.fd
+$svncopy $source_path/src/* $target_shared/sorc/gsi.fd/
+
+$svnmkdirp $target_shared/sorc/gsi.fd/enkf
+$svncopy $source_path/src/enkf/* $target_shared/sorc/gsi.fd/enkf/
+
+flist="adderrspec_nmcmeth_spec.fd getsfcensmeanp.fd getsigensmeanp_smooth_ncep.fd recentersigp.fd"
+for code in $flist; do
+   $svnmkdirp $target_shared/sorc/$code
+   $svncopy $source_path/util/EnKF/gfs/src/$code/* $target_shared/sorc/$code/
 done
+
+
+# Create and populate global_shared ush
+$svnmkdirp $target_shared/ush
+$svncopy $source_path/scripts/gfs_truncate_enkf.sh $target_shared/ush/
+
