bbf980fab4baf18bbb1dfbb146131d593a2d3c12
yanqiu.zhu@noaa.gov <yanqiu.zhu@noaa.gov>
2015-10-13 14:01:30 +0000

    cloudy_radiance: bug fixes and small minor changes.

--- scripts/regression_nl_update.sh
+++ scripts/regression_nl_update.sh
@@ -12,7 +12,7 @@ if [[ `expr substr $exp 1 6` = "global" ]]; then
       export SETUP_enkf=""
    else
       export SETUP_update="newpc4pred=.true.,adp_anglebc=.true.,angord=4,passive_bc=.true.,use_edges=.false.,diag_precon=.true.,step_start=1.0e-3,emiss_bc=.true.,cwoption=3,print_diag_pcg=.true.,"
-      export SETUP_enkf="univaroz=.true.,adp_anglebc=.true.,angord=4,use_edges=.false.,emiss_bc=.true.,cwoption=3,"
+      export SETUP_enkf="univaroz=.true.,adp_anglebc=.true.,angord=4,use_edges=.false.,emiss_bc=.true.,"
    fi
 fi
 export GRIDOPTS_update=""
