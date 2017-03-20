21bea2b420a461e3f2da7e9edfef2e6d2c466da7
mark.potts@noaa.gov <mark.potts@noaa.gov>
2017-03-20 16:59:56 +0000

    Fixed regression scripts and old makefile dependency files

--- scripts/multi_regression.sh
+++ scripts/multi_regression.sh
@@ -15,17 +15,6 @@ regtests_all="global_T62
               hwrf_nmm_d3
               rtma
               global_enkf_T62"
-                global_4dvar_T62
-                global_hybrid_T126
-                global_lanczos_T62
-                global_nemsio_T62
-                arw_netcdf
-                arw_binary
-                nmm_binary
-                nmm_netcdf
-                nmmb_nems_4denvar
-                hwrf_nmm_d2
-                hwrf_nmm_d3"
 
 regtests_debug="global_T62
                 global_4dvar_T62
