c44c6301d6e83282283d1ad779a442fa1d0f2403
Michael.Lueken <Michael.Lueken@noaa.gov>
2017-08-02 13:51:55 +0000

    tic608_nsst_rt: While running the regression tests on Gyre, it was noted that the new global_4denvar_T126 regression test was failing due to non-reproducible results.  Looking at the stdout files for the loproc_updat and loproc_contrl runs, the loproc_updat stdout had instances of:
    Doing nothing in dummy routine NST_INIT_
    
    and similar messages, while the loproc_contrl did not have these.  This message is found in stub_nstmod.f90, which shouldn't be compiled.  To correct this issue, I removed stub_nstmod.f90 from the rest of the source files in src/CMakeLists.txt.
    
    I also modified the resources to run the new script for WCOSS in regression_param.sh.

--- scripts/regression_param.sh
+++ scripts/regression_param.sh
@@ -129,8 +129,8 @@ case $regtest in
            topts[2]="0:15:00" ; popts[2]="6/10/" ; ropts[2]="/2"
            sub_cmd="sub_zeus"
         elif [[ "$machine" = "WCOSS" ]]; then
-           topts[1]="0:35:00" ; popts[1]="16/2/" ; ropts[1]="/1"
-           topts[2]="0:35:00" ; popts[2]="16/4/" ; ropts[2]="/2"
+           topts[1]="0:35:00" ; popts[1]="6/8/" ; ropts[1]="/1"
+           topts[2]="0:35:00" ; popts[2]="6/10/" ; ropts[2]="/2"
            sub_cmd="sub_wcoss -a GDAS-T2O -d $PWD"
         elif [[ "$machine" = "WCOSS_C" ]]; then
            topts[1]="0:35:00" ; popts[1]="48/8/" ; ropts[1]="1024/1"  # sub_wcoss_c popts are "#tasks/#nodes/"
