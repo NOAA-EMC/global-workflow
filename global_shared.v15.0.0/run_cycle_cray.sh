427db278a5dec2822567eb545a7d5ae93d8eb8fc
george.gayno@noaa.gov <george.gayno@noaa.gov>
2017-09-22 19:03:30 +0000

    cycle_fv3 branch:
    /ush/global_cycle.sh - Add variables 'z1' and 'z2' which
    define the depth over which the NSST heating/cooling is applied.
    
    Update build process to remove unneeded nemsio library.
    
    read_write_data.f90 - Update to "read_gsi_data" to read
    GSI tref increments and mask from a netcdf file.
    
    chgres.f90 - Updates to "adjust_nsst" to compute the change
    in tref and SST based on the tref increment from the GSI.
    The SST update is computed by new routine "dtzm_point".
    New routine "nsst_water_reset" to reset all nsst records
    at points that were formally sea ice.
    
    Ticket #20.

--- global_shared.v15.0.0/run_cycle_cray.sh
+++ global_shared.v15.0.0/run_cycle_cray.sh
@@ -19,16 +19,20 @@ export machine=WCOSS_C
 export APRUNCY="aprun -n 1 -N 1 -j 1 -d $OMP_NUM_THREADS_CY -cc depth"
 
 export global_shared_ver="v15.0.0"
-export BASEDIR=/gpfs/hps/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3
+export BASEDIR=/gpfs/hps3/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3
 export HOMEglobal=$BASEDIR/global_shared.${global_shared_ver}
 
 export CYCLEXEC=$HOMEglobal/exec/global_cycle
 
-export CDATE=2016092900
+export CDATE=2016100118
 export FHOUR=00
 
-export SFCGES=/gpfs/hps/ptmp/George.Gayno/control/C384/gfs.20160929/00/RESTART/sfc_data.tile1.nc
-export SFCANL="./gdas1.t00z.sfcanl"
+TILE=tile6
+export SFCGES=/gpfs/hps3/emc/da/noscrub/Xu.Li/data/fv3test/gdas.20161001/18/RESTART/20161001.180000.sfc_data.${TILE}.nc
+export SFCANL="./sfcanl.$TILE"
+
+export NST_ANL=.true.
+export GSI_FILE=/gpfs/hps3/emc/da/noscrub/Xu.Li/data/fv3test/gdas.20161001/18/gdas.t18z.dtfanl
 
 export FNTSFA=/gpfs/gp1/emc/globaldump/$CDATE/gdas/rtgssthr.grb.gdas.$CDATE
 export FNSNOA=' '
@@ -39,10 +43,10 @@ export JCAP=1534
 LONB=3072
 LATB=1536
 export CRES=384
-export FNGRID=/gpfs/hps/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_fv3/C${CRES}/C${CRES}_grid.tile1.nc
-export FNOROG=/gpfs/hps/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_fv3/C${CRES}/C${CRES}_oro_data.tile1.nc
+export FNGRID=/gpfs/hps3/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_fv3/C${CRES}/C${CRES}_grid.${TILE}.nc
+export FNOROG=/gpfs/hps3/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_fv3/C${CRES}/C${CRES}_oro_data.${TILE}.nc
 
-export FIXgsm=/gpfs/hps/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_am
+export FIXgsm=/gpfs/hps3/emc/global/noscrub/George.Gayno/fv3gfs/branches/cycle_fv3/global_shared.v15.0.0/fix/fix_am
 export FNABSC=$FIXgsm/global_mxsnoalb.uariz.t$JCAP.$LONB.$LATB.rg.grb
 export FNALBC=$FIXgsm/global_snowfree_albedo.bosu.t$JCAP.$LONB.$LATB.rg.grb
 export FNVETC=$FIXgsm/global_vegtype.igbp.t$JCAP.$LONB.$LATB.rg.grb
@@ -58,7 +62,8 @@ export NWPROD=/nwprod
 export FILESTYLE=L
 export use_ufo=.true.
 export VERBOSE=YES
-export CYCLVARS=FSNOL=99999.,FSNOS=99999.,
+# turn off sst update to isolate nsst changes
+export CYCLVARS=FSNOL=99999.,FSNOS=99999.,FTSFS=99999.,
 export ISOT=1
 export IVEGSRC=1
 export JCAP=1534
