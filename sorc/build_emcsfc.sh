d61af9583521e458f57c8c1c59ea6354b6cb1abd
George <George.Gayno@noaa.gov>
2018-03-09 17:58:52 +0000

    Vlab issue #47065.  Modify EMCSFC_SFC_PREP jobs for flat directory structure.
    Change-Id: Ie23ffd62437b993cc042a901bc7f36b6d16b33b2

--- sorc/build_emcsfc.sh
+++ sorc/build_emcsfc.sh
@@ -1,18 +1,5 @@
-#!/bin/sh
-
-#------------------------------------------------------------
-# Build all "emcsfc" programs.
-# 
-# For more details, see the documentation in each
-# program sub-directory:
-#
-#  ./sorc/emcsfc_ice_blend.fd
-#  ./sorc/emcsfc_snow2mdl.fd
-#
-# To run, type "build_emcsfc.sh" from the command line.
-#------------------------------------------------------------
-
-set -x
+#! /usr/bin/env bash
+set -eux
 
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
@@ -24,17 +11,21 @@ fi
 
 for prog in emcsfc_ice_blend emcsfc_snow2mdl
 do
+  module purge
   USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
   if [ $USE_PREINST_LIBS = true ]; then
-    export MOD_PATH=/scratch3/NCEPDEV/nwprod/lib/modulefiles
+    source ../modulefiles/modulefile.global_${prog}.${target}             > /dev/null 2>&1
   else
     export MOD_PATH=${cwd}/lib/modulefiles
+    if [ $target = wcoss_cray ]; then
+      source ../modulefiles/modulefile.global_${prog}.${target}_userlib   > /dev/null 2>&1
+    else
+      source ../modulefiles/modulefile.global_${prog}.${target}           > /dev/null 2>&1
+    fi
   fi
-
-  cd ${prog}.fd
-  make clean
-  sh make.sh
   module list
+  cd ${cwd}/${prog}.fd
+  ./make.sh
   cd $cwd
 done
 
