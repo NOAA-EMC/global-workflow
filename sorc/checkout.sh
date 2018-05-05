b6723eb9ff3e8394d5a36db3901ada8bc3cd455d
fanglin.yang <fanglin.yang@noaa.gov>
2018-05-05 01:39:31 +0000

    Vlab Issue #49439: update config.base, sorc/checkout.sh. add env/WCOSS_DELL_P3.env for Dell

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -35,7 +35,8 @@ if [[ ! -d gfs_post.fd ]] ; then
     rm -f ${topdir}/checkout-gfs_post.log
     git clone --recursive gerrit:EMC_post gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     cd gfs_post.fd
-    git checkout ncep_post.v8.0.7
+    #git checkout ncep_post.v8.0.7
+    git checkout post_fv3gfs_dell 
     cd ${topdir}
 else
     echo 'Skip.  Directory gfs_post.fd already exists.'
