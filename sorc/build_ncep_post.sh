58b1c45ffb3713ea653ff18e06818e0a4a413c31
Terry McGuinness <terry.mcguinness@noaa.gov>
2018-05-10 09:03:39 -0400

    added module file for gaea post (temporarily) in build branch for gaea

--- sorc/build_ncep_post.sh
+++ sorc/build_ncep_post.sh
@@ -16,8 +16,9 @@ if [ ! -d "../exec" ]; then
   mkdir ../exec
 fi
 
-# Cheat until moving through commit process with post
+# Cheats until moving through commit process with post
 cp build_ncep_post_native.sh gfs_post.fd/sorc/build_ncep_post.sh
+cp ../modulefiles/fv3gfs/post/v7.0.0-gaea gfs_post.fd/modulefiles/post
 
 cd gfs_post.fd/sorc
 sh build_ncep_post.sh
