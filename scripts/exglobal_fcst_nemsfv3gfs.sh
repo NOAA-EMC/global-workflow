4e9faff3256b83ceb5a0e35d18c53eabaf609c32
fanglin.yang <fanglin.yang@noaa.gov>
2018-05-12 15:00:15 +0000

    update atmos_model_nml in exglobal_fcst_nemsfv3gfs.sh following the latest model change

--- scripts/exglobal_fcst_nemsfv3gfs.sh
+++ scripts/exglobal_fcst_nemsfv3gfs.sh
@@ -552,10 +552,6 @@ cat > input.nml <<EOF
   chksum_debug = $chksum_debug
   dycore_only = $dycore_only
   fdiag = $FDIAG
-  fhmax = $FHMAX
-  fhout = $FHOUT
-  fhmaxhf = $FHMAX_HF
-  fhouthf = $FHOUT_HF
   $atmos_model_nml
 /
 
