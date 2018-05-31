01e2528c750f60f21f12d5f4e1e76ed8ac3c3e00
fanglin.yang <fanglin.yang@noaa.gov>
2018-05-31 04:39:23 +0000

    Squashed commit of the following:
    1.   Update UPP to tag ncep_post.v8.0.9 -- fv3gfs occasionally produces unphysically thin or even negative pressure layers (dpres) at isolatted points in non-hydrostatic mode.  This causes UPP to fail in the vertical interpolation from model native grid to standard pressure levels.  A protection is added to UPP to force dpres to be larger than 5 hPa and delz larger than 5 meter (by Wen Meng)
    
    2.  enable variable length forecast hours (by Rahul Mahajan)
    
     	modified:   jobs/rocoto/post.sh
     	modified:   parm/config/config.base
     	modified:   sorc/checkout.sh
     	modified:   ush/rocoto/setup_workflow.py

--- jobs/rocoto/post.sh
+++ jobs/rocoto/post.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh -x
 
 ###############################################################
-## NCEP post driver script 
+## NCEP post driver script
 ## FHRGRP : forecast hour group to post-process (e.g. 0, 1, 2 ...)
 ## FHRLST : forecast hourlist to be post-process (e.g. anl, f000, f000_f001_f002, ...)
 ###############################################################
@@ -13,13 +13,21 @@ status=$?
 
 
 if [ $FHRGRP -eq 0 ]; then
-    fhrlst="anl"             
+    fhrlst="anl"
+    restart_file=$ROTDIR/${CDUMP}.${PDY}/${cyc}/${CDUMP}.t${cyc}z.atm
 else
     fhrlst=$(echo $FHRLST | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
+    restart_file=$ROTDIR/${CDUMP}.${PDY}/${cyc}/${CDUMP}.t${cyc}z.logf
 fi
+
 
 #---------------------------------------------------------------
 for fhr in $fhrlst; do
+
+    if [ ! -f $restart_file${fhr}.nemsio ]; then
+        echo "Nothing to process for FHR = $fhr, cycle"
+        continue
+    fi
 
     export post_times=$fhr
     $HOMEgfs/jobs/JGLOBAL_NCEPPOST
