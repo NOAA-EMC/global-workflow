56446d6e177d937cd4288eccaa12e56d7f870813
Terry McGuinness <Terry.McGuinness@noaa.gov>
2018-06-08 18:53:54 +0000

    updated NEMSfv3gfs branch to beta-tjet-ujet in checkout.sh to be aligned with parallel

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone --recursive gerrit:NEMSfv3gfs fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    git checkout tjet-ujet
+    git checkout beta-tjet-ujet
     git submodule update --init --recursive
     cd ${topdir}
 else
