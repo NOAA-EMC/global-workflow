a6a14f022643604d4f9f471b0fcefdf3c6bec022
fanglin.yang <fanglin.yang@noaa.gov>
2018-09-20 00:50:20 +0000

    add experiment-prod.h for running ecflow in EMC environment

---
+++ ecflow/ecf/include/experiment-prod.h
@@ -0,0 +1,16 @@
+# These are development overrides.  They are intended to set
+# variables that would otherwise be set to operational values, and
+# cause your workflow to fail.
+
+# EMC experiment configuration file
+export HOMEgfs="/gpfs/dell2/emc/modeling/noscrub/Fanglin.Yang/git/fv3gfs/port2dell2"
+export HOMEobsproc_global="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/obsproc_global.iss-46886.fv3gfs"
+export HOMEobsproc_network="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/obsproc_global.iss-46886.fv3gfs"
+export HOMEobsproc_prep="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/obsproc_prep.iss-46886.fv3gfs"
+export DATAROOT="/gpfs/dell2/ptmp/emc.glopara/tmpnwprd"
+export jlogfile="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/com/gfs/prod/jlogfile"
+export COMROOT="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/com/gfs/prod"
+export CDUMP=$model
+export cycle=t%CYC%z
+export EXPDIR="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/para_fv3gfs/myexp"
+
