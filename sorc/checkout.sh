9390adcb4d4c27398b83c20fc6e21672c58400b3
russ.treadon <Russ.Treadon@noaa.gov>
2018-11-09 16:09:41 +0000

    Incorporate changes / requests from NCO
     * env/JET.env - add variables job and jobid
     * env/THEIA.env - add variables job and jobid
     * env/WCOSS_C.env - add variables job and jobid
     * env/WCOSS_DELL_P3.env - add variables job and jobid
     * scripts/exglobal_fcst_nemsfv3gfs.sh - remove "." between enkf and dump
     * sorc/checkout.sh - update to fv3da.v1.0.34
     * ush/fv3gfs_chgres.sh - remove "." between enkf and gdas
     * ush/hpssarch_gen.sh - remove "." between enkf and (gdas,gfs)

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,7 +21,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
     cd gsi.fd
-    git checkout fv3da.v1.0.33
+    git checkout fv3da.v1.0.34
     git submodule update
     cd ${topdir}
 else
