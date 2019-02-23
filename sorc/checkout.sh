2a69eb89196083ed28c746eb3ec0051cbd5e198d
russ.treadon <Russ.Treadon@noaa.gov>
2019-02-23 22:04:37 +0000

    Update DA tag to fv3da.v1.0.40 (change factqmax)

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,7 +21,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
     cd gsi.fd
-    git checkout fv3da.v1.0.39
+    git checkout fv3da.v1.0.40
     git submodule update
     cd ${topdir}
 else
