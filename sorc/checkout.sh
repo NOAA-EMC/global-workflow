a6927f431ae5743fb5abe38d7e1629500a67535d
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-04-02 03:30:18 +0000

    update fv3gfs to 1.0.18

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone --recursive gerrit:NEMSfv3gfs fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    git checkout nemsfv3gfs_beta_v1.0.17
+    git checkout nemsfv3gfs_beta_v1.0.18
     git submodule update --init --recursive
     cd ${topdir}
 else
