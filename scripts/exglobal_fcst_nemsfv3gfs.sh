d28e57d88a64f17c1e86af5dca8c6f911ea87053
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-04-14 17:42:48 +0000

    add output_1st_timestep so it works with NEMSFV3gfs master

--- scripts/exglobal_fcst_nemsfv3gfs.sh
+++ scripts/exglobal_fcst_nemsfv3gfs.sh
@@ -558,6 +558,7 @@ RUN_CONTINUE:            ${RUN_CONTINUE:-".false."}
 ENS_SPS:                 ${ENS_SPS:-".false."}
 
 dt_atmos:                $DELTIM
+output_1st_tstep_rst:    .false.
 calendar:                ${calendar:-'julian'}
 cpl:                     ${cpl:-".false."}
 memuse_verbose:          ${memuse_verbose:-".false."}
