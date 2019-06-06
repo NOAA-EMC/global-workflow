77f8b292b1880815d7a8971dd686fa8e87b11619
Lin.Gan <lin.gan@noaa.gov>
2019-06-06 14:10:03 +0000

    Add missing line in config.fv3

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -48,7 +48,6 @@ if [[ ! -d gfs_post.fd ]] ; then
     git clone --recursive gerrit:EMC_post gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     #git clone --recursive gerrit:EMC_post_gtg gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     #cd gfs_post.fd
-    #git checkout gfsv16_fyang
     #git checkout ncep_post.v8.0.27e
     #git checkout ncep_post_gtg.v1.0.6c
     cd ${topdir}
