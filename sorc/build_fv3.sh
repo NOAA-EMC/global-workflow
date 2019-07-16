01b12587ae6567677d95e15e990397f142c53633
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2019-07-16 17:49:44 +0000

    Fix clean arguments for building fv3
    The arguments used when building the FV3-GSDCHEM-WW3 app used named
    arguments, but the script expects arguments without names.
    
    Change-Id: Ibc98a4c883f4f28c361104d03e4b17fb070dc7f8
    Refs: #58418

--- sorc/build_fv3.sh
+++ sorc/build_fv3.sh
@@ -21,6 +21,6 @@ if [ $target = theia ]; then target=theia.intel ; fi
 cd fv3gfs.fd/
 FV3=$( pwd -P )/FV3
 cd tests/
-./compile.sh "$FV3" "$target" "NCEP64LEV=Y HYDRO=N 32BIT=Y" 1 "clean_before=YES" "clean_after=NO"
+./compile.sh "$FV3" "$target" "NCEP64LEV=Y HYDRO=N 32BIT=Y" 1 "YES" "NO"
 ##mv -f fv3_1.exe ../NEMS/exe/fv3_gfs_nh.prod.32bit.x
 mv -f fv3_1.exe ../NEMS/exe/global_fv3gfs.x
