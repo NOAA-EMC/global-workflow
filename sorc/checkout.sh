9fd2726d64d62b7fc6196fb74060dc2ec15b3dab
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2019-08-05 18:24:21 +0000

    Update FV3-GSDCHEM-WW3 version
    Updated the checkout script to fetch a newer commit of the
    FV3-GSDCHEM-WW3 app. This newer version uses ESMF v8.0.0bs42, updates
    GSDCHEM to 0.87, WW3 to an OMP-enabled version, and FV3 to the latest
    develop commit.
    
    Change-Id: I7c16511e2d5d183abd96371afd0d0b237e5349aa
    Refs: #58418, #62104

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone --recursive gerrit:EMC_FV3-GSDCHEM-WW3 fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    git checkout 59f7725
+    git checkout d94e32a
     git submodule update --init --recursive
     cd ${topdir}
 else
