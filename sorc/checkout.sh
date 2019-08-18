24bcacb621073dcbad0fdd457f1d48cdb7daf53a
fanglin.yang <fanglin.yang@noaa.gov>
2019-08-18 03:55:36 +0000

    add back checkout.sh

---
+++ sorc/checkout.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+set -xue
+
+topdir=$(pwd)
+echo $topdir
+
+echo fv3gfs checkout ...
+if [[ ! -d fv3gfs.fd ]] ; then
+    rm -f ${topdir}/checkout-fv3gfs.log
+    git clone gerrit:NEMSfv3gfs fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
+    cd fv3gfs.fd
+    git checkout gfs.v16_tag_v2.0.0
+    git submodule update --init --recursive
+    cd ${topdir}
+else
+    echo 'Skip.  Directory fv3gfs.fd already exists.'
+fi
+
+echo gsi checkout ...
+if [[ ! -d gsi.fd ]] ; then
+    rm -f ${topdir}/checkout-gsi.log
+    git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
+    cd gsi.fd
+    git checkout DA_GFSv16
+    git submodule update
+    cd ${topdir}
+else
+    echo 'Skip.  Directory gsi.fd already exists.'
+fi
+
+echo ufs_utils checkout ...
+if [[ ! -d ufs_utils.fd ]] ; then
+    rm -f ${topdir}/checkout-ufs_utils.log
+    git clone --recursive gerrit:UFS_UTILS ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
+    cd ufs_utils.fd
+    git checkout develop
+    cd ${topdir}
+else
+    echo 'Skip.  Directory ufs_utils.fd already exists.'
+fi
+
+echo EMC_post checkout ...
+if [[ ! -d gfs_post.fd ]] ; then
+    rm -f ${topdir}/checkout-gfs_post.log
+    git clone --recursive gerrit:EMC_post gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
+    cd ${topdir}
+else
+    echo 'Skip.  Directory gfs_post.fd already exists.'
+fi
+
+echo EMC_gfs_wafs checkout ...
+if [[ ! -d gfs_wafs.fd ]] ; then
+    rm -f ${topdir}/checkout-gfs_wafs.log
+    git clone --recursive gerrit:EMC_gfs_wafs gfs_wafs.fd >> ${topdir}/checkout-gfs_wafs.log 2>&1
+    cd gfs_wafs.fd
+    git checkout gfs_wafs.v5.0.9
+    cd ${topdir}
+else
+    echo 'Skip.  Directory gfs_wafs.fd already exists.'
+fi
+
+exit 0
