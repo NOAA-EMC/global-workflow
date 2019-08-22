f2674fc739747bfea1d0488db42865555d113836
Jian Kuang <jian.kuang@noaa.gov>
2019-08-22 19:56:08 +0000

    bug fixes

--- scripts/exglobal_fcst_nemsfv3gfs.sh
+++ scripts/exglobal_fcst_nemsfv3gfs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ksh
 ################################################################################
 # UNIX Script Documentation Block
 # Script name:         exglobal_fcst_nemsfv3gfs.sh.ecf
@@ -997,7 +997,7 @@ fi
 
 #------------------------------------------------------------------
 # Clean up before leaving
-if [ $KEEPDATA = "NO" ]; then rm -rf $DATA; fi
+if [ $mkdata = "YES" ]; then rm -rf $DATA; fi
 
 #------------------------------------------------------------------
 set +x
