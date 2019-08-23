57f74c842ba30e40f6e2c3ba1bd4802388aa068f
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2019-08-23 16:34:57 +0000

    Update UFS_utils version to work with GEFS
    The GEFS init system requires changes to global_chgres_driver.sh that
    are not present in v1.0.0. The UFS_utils version is updated to the
    current tip of the develop branch, which includes the necessary changes.
    
    Change-Id: I097b16342ca081e867786410a6ac9916fd5576fb
    Refs: #58418

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -42,7 +42,8 @@ if [[ ! -d ufs_utils.fd ]] ; then
     rm -f ${topdir}/checkout-ufs_utils.log
     git clone --recursive gerrit:UFS_UTILS ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
     cd ufs_utils.fd
-    git checkout v1.0.0
+    # git checkout v1.0.0
+    git checkout e7eb676
     cd ${topdir}
 else
     echo 'Skip.  Directory ufs_utils.fd already exists.'
