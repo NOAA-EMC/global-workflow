4c7d8d8abdeb6c01a8d33bc09763a3ccc27d191d
Mallory Row <mallory.row@noaa.gov>
2019-08-26 19:00:52 +0000

    Update EMC_verif-global tag number to 1.1.3

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -77,7 +77,7 @@ if [[ ! -d verif-global.fd ]] ; then
     rm -f ${topdir}/checkout-verif-global.log
     git clone --recursive gerrit:EMC_verif-global verif-global.fd >> ${topdir}/checkout-verif-global.log 2>&1
     cd verif-global.fd
-    git checkout verif_global_v1.1.2
+    git checkout verif_global_v1.1.3
     cd ${topdir}
 else
     echo 'Skip. Directory verif-global.fd already exist.'
