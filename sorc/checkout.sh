0d853b7c0df9e1f9901062b84b659685470fd559
fanglin.yang <fanglin.yang@noaa.gov>
2019-09-16 23:32:51 -0400

    change ufs_util repo back to the master version

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -37,7 +37,7 @@ fi
 echo ufs_utils checkout ...
 if [[ ! -d ufs_utils.fd ]] ; then
     rm -f ${topdir}/checkout-ufs_utils.log
-    git clone https://github.com/yangfanglin/UFS_UTILS.git  ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
+    git clone https://github.com/NOAA-EMC/UFS_UTILS  ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
     cd ufs_utils.fd
     git checkout develop
     cd ${topdir}
