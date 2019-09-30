20757f505d0c5c01e1b01d095a554b3c3ba670ac
kate.friedman <kate.friedman@noaa.gov>
2019-09-30 17:56:29 +0000

    VLab Issue #67188 - adding missing tropcy module for Dell and added target renaming to build_ufs_util.sh for WCOSSes

--- sorc/build_ufs_utils.sh
+++ sorc/build_ufs_utils.sh
@@ -4,6 +4,9 @@ set -eux
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
 
+if [ $target=wcoss_dell_p3 ]; then target=dell; fi
+if [ $target=wcoss_cray ]; then target=cray; fi
+
 cd ufs_utils.fd/sorc
 
 ./build_all_ufs_utils.sh
