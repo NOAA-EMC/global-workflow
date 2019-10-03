25a564c5ea37275e1c4605b2532d19adccd6d42c
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-10-03 14:55:20 +0000

    Change checkout of GSI to be feature/fv3_ncio

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,19 +21,8 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
     cd gsi.fd
-    #git checkout fv3da.v1.0.43
-    git checkout hera-build
+    git checkout feature/fv3_ncio 
     git submodule update
-    # Workaround for missing GSI file on lowres parallel runs
-    cd fix/Big_Endian/
-    git checkout ae3bc2538f34c7cdb6a533a14c4880f6970a6724^ global_berror.l64y98.f77
-    git checkout ae3bc2538f34c7cdb6a533a14c4880f6970a6724^ global_berror.l64y194.f77
-    # Check out historical ozinfo files for use in retrospective parallels
-    cd ${topdir}/gsi.fd/fix/fv3_historical
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  0readme.ozinfo
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  global_ozinfo.txt.2015110500
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  global_ozinfo.txt.2018110700
-    cd ${topdir}
 else
     echo 'Skip.  Directory gsi.fd already exists.'
 fi
