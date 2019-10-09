519a99c671db60eb8d994190fa75102c8a4b01e4
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2019-10-09 19:10:26 +0000

    Update NEMS app version for Hera port
    Change-Id: I19cdf40e4c0a147d138cc59fc720b7608b51554a
    Refs: #58418

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone --recursive gerrit:EMC_FV3-GSDCHEM-WW3 fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    git checkout 09328501
+    git checkout 750ecb06
     git submodule update --init --recursive
     cd ${topdir}
 else
