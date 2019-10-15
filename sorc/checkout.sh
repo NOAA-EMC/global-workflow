1ed46e56340c9fec8d58bcaef7a1f19aaf76e9f1
Kate.Friedman <Kate.Friedman@noaa.gov>
2019-10-15 17:40:36 +0000

    Updates to checkout.sh for NEMSfv3gfs tag and ProdGSI master, also increased gfs vrfy walltime to 8hrs on Hera

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone gerrit:NEMSfv3gfs fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    #git checkout nemsfv3gfs_beta_v1.0.18
+    git checkout gfs.v16_PhysicsUpdate
     git submodule update --init --recursive
     cd ${topdir}
 else
@@ -22,17 +22,7 @@ if [[ ! -d gsi.fd ]] ; then
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
     cd gsi.fd
     #git checkout fv3da.v1.0.43
-    git checkout hera-build
     git submodule update
-    # Workaround for missing GSI file on lowres parallel runs
-    cd fix/Big_Endian/
-    git checkout ae3bc2538f34c7cdb6a533a14c4880f6970a6724^ global_berror.l64y98.f77
-    git checkout ae3bc2538f34c7cdb6a533a14c4880f6970a6724^ global_berror.l64y194.f77
-    # Check out historical ozinfo files for use in retrospective parallels
-    cd ${topdir}/gsi.fd/fix/fv3_historical
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  0readme.ozinfo
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  global_ozinfo.txt.2015110500
-    git checkout cd0847ee5c67115113f61c79e7d8bc6b1b7095ba  global_ozinfo.txt.2018110700
     cd ${topdir}
 else
     echo 'Skip.  Directory gsi.fd already exists.'
