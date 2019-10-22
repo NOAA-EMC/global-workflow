7fee101302a547c525fba9df6cac5c42f66d35cb
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-10-22 14:44:58 +0000

    Added enkf_chgres_recenter_nc to gfsv16 workflow

--- sorc/build_enkf_chgres_recenter_nc.sh
+++ sorc/build_enkf_chgres_recenter_nc.sh
@@ -7,13 +7,13 @@ cwd=`pwd`
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH=/scratch3/NCEPDEV/nwprod/lib/modulefiles
-  source ../modulefiles/fv3gfs/chgres_nc_gauss.$target             > /dev/null 2>&1
+  source ../modulefiles/fv3gfs/enkf_chgres_recenter_nc.$target             > /dev/null 2>&1
 else
   export MOD_PATH=${cwd}/lib/modulefiles
   if [ $target = wcoss_cray ]; then
-    source ../modulefiles/fv3gfs/chgres_nc_gauss.${target}_userlib > /dev/null 2>&1
+    source ../modulefiles/fv3gfs/enkf_chgres_recenter_nc.${target}_userlib > /dev/null 2>&1
   else
-    source ../modulefiles/fv3gfs/chgres_nc_gauss.$target           > /dev/null 2>&1
+    source ../modulefiles/fv3gfs/enkf_chgres_recenter_nc.$target           > /dev/null 2>&1
   fi
 fi
 module list
@@ -23,7 +23,7 @@ if [ ! -d "../exec" ]; then
   mkdir ../exec
 fi
 
-cd ${cwd}/chgres_nc_gauss.fd
+cd ${cwd}/enkf_chgres_recenter_nc
 
 export FFLAGS="-O3 -qopenmp -traceback -fp-model precise"
 export FV3GFS_NCIO_LIB="${cwd}/gsi.fd/build/lib/libfv3gfs_ncio.a"
