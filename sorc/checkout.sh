4d2ff3432b962a2efad32c4e0557e2776827466c
Kate.Friedman <Kate.Friedman@noaa.gov>
2019-10-23 16:17:56 +0000

    VLab Issue #67188 - pinning EMC_post to a revision as a pseudo tag

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -45,6 +45,7 @@ if [[ ! -d gfs_post.fd ]] ; then
     git clone --recursive https://github.com/NOAA-EMC/EMC_post.git gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     #git clone --recursive gerrit:EMC_post_gtg gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     cd gfs_post.fd
+    git checkout ba7e59b290c8149ff1c2fee98d01e99e4ef92ee6
     #git checkout ncep_post.v8.0.27e
     #git checkout ncep_post_gtg.v1.0.6c
     cd ${topdir}
