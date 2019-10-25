38c7e97f63b056266fe436de41c2b0dd2b8f284a
Kate.Friedman <Kate.Friedman@noaa.gov>
2019-10-25 17:08:01 +0000

    VLab Issue #67188 - updated UFS_UTILS checkout to new v1.1.0 tag

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -33,7 +33,7 @@ if [[ ! -d ufs_utils.fd ]] ; then
     rm -f ${topdir}/checkout-ufs_utils.log
     git clone https://github.com/NOAA-EMC/UFS_UTILS.git ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
     cd ufs_utils.fd
-    #git checkout v1.1.0
+    git checkout v1.1.0
     cd ${topdir}
 else
     echo 'Skip.  Directory ufs_utils.fd already exists.'
