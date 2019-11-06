034363975e5d97d27cdf67b0ff13e40f1ad02075
kate.friedman <kate.friedman@noaa.gov>
2019-11-06 19:03:21 +0000

    VLab Issue #66132 - GFS v15.2.1 changes

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -9,7 +9,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rm -f ${topdir}/checkout-fv3gfs.log
     git clone gerrit:NEMSfv3gfs fv3gfs.fd >> ${topdir}/checkout-fv3gfs.log 2>&1
     cd fv3gfs.fd
-    git checkout nemsfv3gfs_beta_v1.0.18
+    git checkout nemsfv3_gfsv15.2.1
     git submodule update --init --recursive
     cd ${topdir}
 else
@@ -19,9 +19,9 @@ fi
 echo gsi checkout ...
 if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
-    git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.fd.log 2>&1
+    git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
     cd gsi.fd
-    git checkout fv3da.v1.0.43
+    git checkout fv3da_gfsv15.2.0
     git submodule update
     # Workaround for missing GSI file on lowres parallel runs
     cd fix/Big_Endian/
@@ -54,8 +54,8 @@ if [[ ! -d gfs_post.fd ]] ; then
     git clone --recursive gerrit:EMC_post gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     #git clone --recursive gerrit:EMC_post_gtg gfs_post.fd >> ${topdir}/checkout-gfs_post.log 2>&1
     cd gfs_post.fd
-    git checkout ncep_post.v8.0.27e
-    #git checkout ncep_post_gtg.v1.0.6c
+    git checkout ncep_post.v8.1.2
+    #git checkout ncep_post_gtg.v1.1.7
     cd ${topdir}
 else
     echo 'Skip.  Directory gfs_post.fd already exists.'
@@ -66,7 +66,7 @@ if [[ ! -d gfs_wafs.fd ]] ; then
     rm -f ${topdir}/checkout-gfs_wafs.log
     git clone --recursive gerrit:EMC_gfs_wafs gfs_wafs.fd >> ${topdir}/checkout-gfs_wafs.log 2>&1
     cd gfs_wafs.fd
-    git checkout gfs_wafs.v5.0.9
+    git checkout gfs_wafs.v5.0.10
     cd ${topdir}
 else
     echo 'Skip.  Directory gfs_wafs.fd already exists.'
