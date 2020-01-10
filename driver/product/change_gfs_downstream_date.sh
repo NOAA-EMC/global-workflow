e292d96010dc1e63f28d2c9d95c5e61776abe0dd
BoiVuong-NOAA <Boi.Vuong@noaa.gov>
2020-01-10 21:12:36 +0000

    GitHub Issue Updated AWIPS parm card grid#211

--- driver/product/change_gfs_downstream_date.sh
+++ driver/product/change_gfs_downstream_date.sh
@@ -7,8 +7,8 @@ export dir=` pwd `
 export PDY=`date -u +%Y%m%d`
 export PDY1=`expr $PDY - 1`
 
-export var=20200110
-export varold=20200109
+export newdate=20191230
+export olddate=20200110
 
 export gdas=/gpfs/dell1/nco/ops/com/gfs/prod/gdas.${PDY}/${cyc}
 export gdasgp=/gpfs/dell1/nco/ops/com/gfs/prod/gdas.${PDY}/${cyc}/gempak
@@ -21,14 +21,14 @@ export gdastestmeta=/gpfs/dell2/ptmp/Boi.Vuong/com/gfs/prod/gdas.${PDY}/${cyc}/g
 for cyc in 00 06 12 18
 # for cyc in 00
 do
-sed -i "s/${varold}/${var}/g"  run_JGDAS_GEMPAK_dell.sh_${cyc}
-sed -i "s/${varold}/${var}/g"   run_JGDAS_GEMPAK_META_NCDC_dell.sh_${cyc}
-sed -i "s/${varold}/${var}/g"  run_JGFS_GEMPAK_dell.sh_${cyc}
-sed -i "s/${varold}/${var}/g"  run_JGFS_AWIPS_20KM_1P0DEG_dell.sh_${cyc} 
-sed -i "s/${varold}/${var}/g"   run_JGFS_AWIPS_G2_dell.sh_${cyc}   
-sed -i "s/${varold}/${var}/g"  run_JGFS_FBWIND_dell.sh_${cyc} 
-sed -i "s/${varold}/${var}/g"  run_JGFS_GEMPAK_META_dell.sh_${cyc}
-sed -i "s/${varold}/${var}/g"  run_JGFS_GEMPAK_NCDC_UPAPGIF_dell.sh_${cyc} 
-sed -i "s/${varold}/${var}/g"  run_JGFS_PGRB2_SPEC_NPOESS_dell.sh_${cyc}
-sed -i "s/${varold}/${var}/g"  run_JGFS_PGRB2_SPEC_GEMPAK_dell.sh_${cyc} 
+sed -i "s/${olddate}/${newdate}/g"  run_JGDAS_GEMPAK_dell.sh_${cyc}
+sed -i "s/${olddate}/${newdate}/g"   run_JGDAS_GEMPAK_META_NCDC_dell.sh_${cyc}
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_GEMPAK_dell.sh_${cyc}
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_AWIPS_20KM_1P0DEG_dell.sh_${cyc} 
+sed -i "s/${olddate}/${newdate}/g"   run_JGFS_AWIPS_G2_dell.sh_${cyc}   
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_FBWIND_dell.sh_${cyc} 
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_GEMPAK_META_dell.sh_${cyc}
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_GEMPAK_NCDC_UPAPGIF_dell.sh_${cyc} 
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_PGRB2_SPEC_NPOESS_dell.sh_${cyc}
+sed -i "s/${olddate}/${newdate}/g"  run_JGFS_PGRB2_SPEC_GEMPAK_dell.sh_${cyc} 
 done
