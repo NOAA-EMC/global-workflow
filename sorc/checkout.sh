f89bafe55aa0b15ae306dbaff8d97cbd17dc27c5
russ.treadon <Russ.Treadon@noaa.gov>
2020-02-03 14:09:33 +0000

    Issue #16:  update DA checkout to feature/parallel_ncio

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -22,7 +22,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
     cd gsi.fd
-    git checkout feature/fv3_ncio 
+    git checkout feature/parallel_ncio
     git submodule update
     cd ${topdir}
 else
