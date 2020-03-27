cda06e33b3d3c06d532822d4bd6f1bbceaaa59bb
JianKuang-NOAA <51758200+JianKuang-NOAA@users.noreply.github.com>
2020-03-27 10:13:33 -0400

    Update checkout.sh

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -52,15 +52,17 @@ if [ $model != "coupled" ]; then
     fi
 fi
 
-echo ufs_utils checkout ...
-if [[ ! -d ufs_utils.fd ]] ; then
-    rm -f ${topdir}/checkout-ufs_utils.log
-    git clone https://github.com/NOAA-EMC/UFS_UTILS.git ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
-    cd ufs_utils.fd
-    #git checkout v1.1.0
-    cd ${topdir}
-else
-    echo 'Skip.  Directory ufs_utils.fd already exists.'
+if [ $model != "coupled" ]; then
+    echo ufs_utils checkout ...
+    if [[ ! -d ufs_utils.fd ]] ; then
+        rm -f ${topdir}/checkout-ufs_utils.log
+        git clone https://github.com/NOAA-EMC/UFS_UTILS.git ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
+        cd ufs_utils.fd
+        #git checkout v1.1.0
+        cd ${topdir}
+    else
+        echo 'Skip.  Directory ufs_utils.fd already exists.'
+    fi
 fi
 
 echo EMC_post checkout ...
@@ -79,26 +81,30 @@ else
     echo 'Skip.  Directory gfs_post.fd already exists.'
 fi
 
-echo EMC_gfs_wafs checkout ...
-if [[ ! -d gfs_wafs.fd ]] ; then
-    rm -f ${topdir}/checkout-gfs_wafs.log
-    git clone --recursive https://github.com/NOAA-EMC/EMC_gfs_wafs.git gfs_wafs.fd >> ${topdir}/checkout-gfs_wafs.log 2>&1
-    cd gfs_wafs.fd
-    git checkout gfs_wafs.v5.0.11
-    cd ${topdir}
-else
-    echo 'Skip.  Directory gfs_wafs.fd already exists.'
+if [ $model != "coupled" ]; then
+    echo EMC_gfs_wafs checkout ...
+    if [[ ! -d gfs_wafs.fd ]] ; then
+        rm -f ${topdir}/checkout-gfs_wafs.log
+        git clone --recursive https://github.com/NOAA-EMC/EMC_gfs_wafs.git gfs_wafs.fd >> ${topdir}/checkout-gfs_wafs.log 2>&1
+        cd gfs_wafs.fd
+        git checkout gfs_wafs.v5.0.11
+        cd ${topdir}
+    else
+        echo 'Skip.  Directory gfs_wafs.fd already exists.'
+    fi
 fi
 
-echo EMC_verif-global checkout ...
-if [[ ! -d verif-global.fd ]] ; then
-    rm -f ${topdir}/checkout-verif-global.log
-    git clone --recursive gerrit:EMC_verif-global verif-global.fd >> ${topdir}/checkout-verif-global.log 2>&1
-    cd verif-global.fd
-    git checkout verif_global_v1.2.2
-    cd ${topdir}
-else
-    echo 'Skip. Directory verif-global.fd already exist.'
+if [ $model != "coupled" ]; then
+    echo EMC_verif-global checkout ...
+    if [[ ! -d verif-global.fd ]] ; then
+        rm -f ${topdir}/checkout-verif-global.log
+        git clone --recursive gerrit:EMC_verif-global verif-global.fd >> ${topdir}/checkout-verif-global.log 2>&1
+        cd verif-global.fd
+        git checkout verif_global_v1.2.2
+        cd ${topdir}
+    else
+        echo 'Skip. Directory verif-global.fd already exist.'
+    fi
 fi
 
 exit 0
