2a84d99215a3a77c8f47c23c12c6bb7f6d67aa98
kate.friedman <kate.friedman@noaa.gov>
2020-04-14 13:06:32 +0000

    Issue #52 - updates for GFSv15.2.11 from RFC 6745

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,7 +21,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
     cd gsi.fd
-    git checkout fv3da_gfsv15.2.9
+    git checkout fv3da_gfsv15.2.11
     git submodule update
     cd ${topdir}
 else
