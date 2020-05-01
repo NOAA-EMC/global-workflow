c90ea4b242b2a895c323fb1240d8c87c3d51e7b0
russ.treadon <Russ.Treadon@noaa.gov>
2020-05-01 19:23:17 +0000

    Issue #63:  update ProdGSI tag to gfsda.v15.3.0

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -21,7 +21,7 @@ if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
     git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
     cd gsi.fd
-    git checkout fv3da_gfs.v15.2.12
+    git checkout gfsda.v15.3.0
     git submodule update
     cd ${topdir}
 else
