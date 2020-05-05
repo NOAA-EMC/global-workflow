d8ebf59cadb1f75fb66790b001a2e19d0899e857
kate.friedman <kate.friedman@noaa.gov>
2020-05-05 19:05:24 +0000

    Issue #63 - updating EMC_gfs_wafs tag to gfs_wafs.v5.0.11

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -46,7 +46,7 @@ if [[ ! -d gfs_wafs.fd ]] ; then
     rm -f ${topdir}/checkout-gfs_wafs.log
     git clone --recursive gerrit:EMC_gfs_wafs gfs_wafs.fd >> ${topdir}/checkout-gfs_wafs.log 2>&1
     cd gfs_wafs.fd
-    git checkout gfs_wafs.v5.0.10
+    git checkout gfs_wafs.v5.0.11
     cd ${topdir}
 else
     echo 'Skip.  Directory gfs_wafs.fd already exists.'
