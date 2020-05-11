a471e298057b785b53d36e6a2bc06192942a6aa8
kate.friedman <kate.friedman@noaa.gov>
2020-05-11 11:51:00 -0500

    Issue #5 - update verif_global tag

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -82,7 +82,7 @@ if [[ ! -d verif-global.fd ]] ; then
     rm -f ${topdir}/checkout-verif-global.log
     git clone --recursive https://github.com/NOAA-EMC/EMC_verif-global.git verif-global.fd >> ${topdir}/checkout-verif-global.log 2>&1
     cd verif-global.fd
-    git checkout verif_global_v1.7.0
+    git checkout verif_global_v1.7.1
     cd ${topdir}
 else
     echo 'Skip. Directory verif-global.fd already exist.'
