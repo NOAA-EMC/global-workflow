84ab8c87e88a81b2af3bc8de382169986186ecda
Jessica.Meixner <Jessica.Meixner@noaa.gov>
2020-05-28 18:28:53 +0000

    more updates for ICs

--- jobs/rocoto/coupled_ic.sh
+++ jobs/rocoto/coupled_ic.sh
@@ -40,21 +40,27 @@ CPL_OCNIC=CPC3Dvar
 CPL_ICEIC=CPC
 OCNRES=025
 ICERES=025
+ICERES=${ICERES:-"025"}
 
 # Create ICSDIR if needed
 [[ ! -d $ICSDIR/$CDATE ]] && mkdir -p $ICSDIR/$CDATE
 [[ ! -d $ICSDIR/$CDATE/ocn ]] && mkdir -p $ICSDIR/$CDATE/ocn
 [[ ! -d $ICSDIR/$CDATE/ice ]] && mkdir -p $ICSDIR/$CDATE/ice
 
+if [ $ICERES = '025' ]; then
+  ICERESdec="0.25"
+else if [ $ICERES = '050' ]; then         
+ ICERESdec="0.50"        
+fi 
 
 # Setup ATM initial condition files
 cp -r $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP  $ICSDIR/$CDATE/
 
 # Setup Ocean IC files 
-cp -r $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/*  $ICSDIR/$CDATE/ocn/
+cp -r $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/MOM*.nc  $ICSDIR/$CDATE/ocn/
 
 #Setup Ice IC files 
-cp $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/* $ICSDIR/$CDATE/ice/
+cp $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/cice5_model_${ICERESdec}.res_$CDATE.nc $ICSDIR/$CDATE/ice/
 
 #TODO for wave coupling
 #if cplwav=true 
