0acee674759a5e2c8fbd4ec7b1cdd0459f95e2bd
russ.treadon <Russ.Treadon@noaa.gov>
2020-06-03 20:10:09 +0000

    Issue #1:  remove redundant entry from config.ediag; update checkout.sh to pull GFS v16 DA from github

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -19,10 +19,9 @@ fi
 echo gsi checkout ...
 if [[ ! -d gsi.fd ]] ; then
     rm -f ${topdir}/checkout-gsi.log
-    git clone --recursive gerrit:ProdGSI gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
+    git clone --recursive https://github.com/NOAA-EMC/GSI.git gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
     cd gsi.fd
-#   git checkout gfsda.v16.0.0
-    git checkout feature/parallel_ncio
+    git checkout release/gfsda.v16.0.0
     git submodule update
     cd ${topdir}
 else
