37e0f57d948fa98c66fcc9d9695de2769a4a38d2
kate.friedman <kate.friedman@noaa.gov>
2020-07-13 17:26:52 +0000

    Issue #1 - add mod_icec.sh to UPP ush script symlinking in link_fv3gfs.sh

--- sorc/link_fv3gfs.sh
+++ sorc/link_fv3gfs.sh
@@ -63,8 +63,8 @@ cd ${pwd}/../scripts            ||exit 8
     $LINK ../sorc/ufs_utils.fd/scripts/exemcsfc_global_sfc_prep.sh.ecf .
     $LINK ../sorc/gldas.fd/scripts/exgdas_gldas.sh.ecf .             
 cd ${pwd}/../ush                ||exit 8
-    for file in fv3gfs_downstream_nems.sh  fv3gfs_dwn_nems.sh  gfs_nceppost.sh  \
-        gfs_transfer.sh  link_crtm_fix.sh  trim_rh.sh fix_precip.sh; do
+    for file in fv3gfs_downstream_nems.sh fv3gfs_dwn_nems.sh gfs_nceppost.sh  \
+        gfs_transfer.sh mod_icec.sh link_crtm_fix.sh trim_rh.sh fix_precip.sh; do
         $LINK ../sorc/gfs_post.fd/ush/$file                  .
     done
     for file in emcsfc_ice_blend.sh  fv3gfs_driver_grid.sh  fv3gfs_make_orog.sh  global_cycle_driver.sh \
