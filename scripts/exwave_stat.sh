6b450acba72c5f59204063ce2545a925f4eca7a6
wx21rph <Roberto.Padilla@m72a2.ncep.noaa.gov>
2020-07-15 17:01:37 +0000

    Ensuring that DBN alert names are in capital letters

--- scripts/exwave_stat.sh
+++ scripts/exwave_stat.sh
@@ -466,7 +466,8 @@
 # 2.g Alert DBN
           if [ "$SENDDBN" = 'YES' ]
           then
-           $DBNROOT/bin/dbn_alert MODEL ${NET}_${COMPONENT}_GB2 $job $COMOUT/gridded/${fcopy}
+           MODCOM=$(echo ${NET}_${COMPONENT} | tr '[a-z]' '[A-Z]')
+           $DBNROOT/bin/dbn_alert MODEL ${MODCOM}_GB2 $job $COMOUT/gridded/${fcopy}
           fi
         fi
       else
@@ -676,8 +677,9 @@
 #
   if [ "$SENDDBN" = 'YES' ]
   then
-       $DBNROOT/bin/dbn_alert MODEL ${NET}_${COMPONENT}_GB2 $job $COMOUT/gridded/${WAV_MOD_TAG}.t${cyc}z.bull_tar
-       $DBNROOT/bin/dbn_alert MODEL ${NET}_${COMPONENT}_GB2 $job $COMOUT/station/${WAV_MOD_TAG}.t${cyc}z.station_tar
+       MODCOM=$(echo ${NET}_${COMPONENT} | tr '[a-z]' '[A-Z]')
+       $DBNROOT/bin/dbn_alert MODEL ${MODCOM}_GB2 $job $COMOUT/gridded/${WAV_MOD_TAG}.t${cyc}z.bull_tar
+       $DBNROOT/bin/dbn_alert MODEL ${MODCOM}_GB2 $job $COMOUT/station/${WAV_MOD_TAG}.t${cyc}z.station_tar
   fi
 #
   if [ "$exit_code" -ne '0' ]
