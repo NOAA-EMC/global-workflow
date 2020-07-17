71d55f1c1b88d735e043244d0940ae382f505ac2
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2020-07-17 17:11:30 +0000

    Update NEMS app version for NCO ESMF
    The NEMS app version is updated to a new tag that uses the NCO-
    installed location of the ESMF 8.0.0 module instead of a user area.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -30,7 +30,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rc=$?
     ((err+=$rc))
     cd fv3gfs.fd
-	git checkout gefs_v12.0.0
+	git checkout gefs_v12.0.1
 	git submodule update --init --recursive
     rc=$?
     ((err+=$rc))
