7e1bae71a948bf7cde2d4b2b3fba21e56ad11457
JessicaMeixner-NOAA <Jessica.Meixner@noaa.gov>
2020-09-09 18:55:49 -0500

    add building of WW3 pre and prep jobs currently requires extra module files (can be removed when base uses hpc-stack) uses the last commit of ufs-s2s-model pre-CICE6 and modules are made to be consistent with this commit

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -16,7 +16,7 @@ rm -f ${topdir}/checkout-fv3_coupled.log
 if [[ ! -d fv3_coupled.fd ]] ; then
     git clone https://github.com/ufs-community/ufs-s2s-model fv3_coupled.fd >> ${topdir}/checkout-fv3_coupled.log 2>&1
     cd fv3_coupled.fd
-    git checkout ufss2s_cmeps_v0.7 
+    git checkout d7b7081f410eb84902a43c7479e14d44dfed0144 
     git submodule update --init --recursive
     cd ${topdir}
 else
