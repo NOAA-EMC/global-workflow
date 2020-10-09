3ee525ba32c7f06a9b7b8e5b0af716de4c3597a7
Jian Kuang <jkuang@login1.stampede2.tacc.utexas.edu>
2020-10-09 08:24:41 -0500

    link and case file update for stampede2

--- sorc/link_fv3gfs.sh
+++ sorc/link_fv3gfs.sh
@@ -55,7 +55,7 @@ elif [ $target == "hera" ]; then
     FIX_DIR="/scratch2/NCEPDEV/climate/climpara/S2S/FIX/fix_UFSp4"
 elif [ $target == "orion" ]; then
     FIX_DIR="/work/noaa/marine/jmeixner/tempFixICdir/fix_UFSp4"
-elif [ $target == "stampede2" ]; then
+elif [ $target == "stampede" ]; then
     FIX_DIR="/work/07738/jkuang/stampede2/tempFixICdir/fix_UFSp4"
 else
     echo 'CRITICAL: links to fix files not set'
