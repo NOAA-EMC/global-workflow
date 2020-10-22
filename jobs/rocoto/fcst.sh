cbe5ddff8013a03a9391eead40796f9c30d7ca28
Lin.Gan <lin.gan@noaa.gov>
2020-10-22 18:35:49 +0000

    Cycled warm start run using develop ufs-weather-model branch and feature/ccpp using merged branch on the 10/13 (4da0f) with feature/gfsv16b (31563). CPP_SUITE="FV3_GFS_v16beta" build_fv3 option: CCPP=Y 32BIT=Y SUITES=FV3_GFS_v15,FV3_GSD_v0,FV3_GSD_noah,FV3_GFS_v16beta wave is turned off for this test because it's for atmospheric physics and comparison.

--- jobs/rocoto/fcst.sh
+++ jobs/rocoto/fcst.sh
@@ -2,7 +2,8 @@
 
 ###############################################################
 # Source FV3GFS workflow modules
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+#### . $HOMEgfs/ush/load_fv3gfs_modules.sh
+. $HOMEgfs/ush/load_fv3gfs_modules_fcst.sh
 status=$?
 [[ $status -ne 0 ]] && exit $status
 
---
+++ ush/load_fv3gfs_modules_fcst.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#set -x
+
+###############################################################
+# Setup runtime environment by loading modules
+ulimit_s=$( ulimit -S -s )
+ulimit -S -s 10000
+
+# Find module command and purge:
+source "$HOMEgfs/modulefiles/module-setup.sh.inc" 
+
+# Load our modules:
+module use "$HOMEgfs/modulefiles" 
+
+if [[ -d /lfs3 ]] ; then
+    # We are on NOAA Jet
+	module load module_base.jet 
+elif [[ -d /scratch1 ]] ; then
+    # We are on NOAA Hera
+	module load module_base.hera
+elif [[ -d /gpfs/hps && -e /etc/SuSE-release ]] ; then
+    # We are on NOAA Luna or Surge
+	module load module_base.wcoss_c 
+elif [[ -L /usrx && "$( readlink /usrx 2> /dev/null )" =~ dell ]] ; then
+    # We are on NOAA Mars or Venus
+	module load module_base.wcoss_dell_p3_fcst 
+elif [[ -d /dcom && -d /hwrf ]] ; then
+    # We are on NOAA Tide or Gyre
+	module load module_base.wcoss 
+elif [[ -d /glade ]] ; then
+    # We are on NCAR Yellowstone
+	module load module_base.cheyenne 
+elif [[ -d /lustre && -d /ncrc ]] ; then
+    # We are on GAEA.
+	module load module_base.gaea 
+else
+    echo WARNING: UNKNOWN PLATFORM 
+fi
+
+# Restore stack soft limit:
+ulimit -S -s "$ulimit_s"
+unset ulimit_s
