6151aaeec4a0e5c7bc22d774cfeb9345a29d1cb3
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2020-10-31 01:53:22 +0000

    Update component versions for newer bufr
    The version of bufr used in some components will stop working after
    2020 Dec 31, so the component versions are updated to ones that use
    a newer version of bufr.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -30,7 +30,7 @@ if [[ ! -d fv3gfs.fd ]] ; then
     rc=$?
     ((err+=$rc))
     cd fv3gfs.fd
-	git checkout gefs_v12.0.1
+	git checkout gefs_v12.1.0
 	git submodule update --init --recursive
     rc=$?
     ((err+=$rc))
@@ -46,7 +46,7 @@ if [[ ! -d gsi.fd ]] ; then
     rc=$?
     ((err+=$rc))
     cd gsi.fd
-    git checkout gefs_v12.0.1
+    git checkout gefs_v12.0.2
     git submodule update
     cd ${topdir}
 else
