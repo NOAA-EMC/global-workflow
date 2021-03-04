be0ed38a53d2422923ccd5380d610785e61deafa
Kate.Friedman <Kate.Friedman@noaa.gov>
2021-03-04 18:06:24 +0000

    Consolidate/cleanup init config and script
    Refs: #178

--- jobs/rocoto/init.sh
+++ jobs/rocoto/init.sh
@@ -36,50 +36,18 @@ status=$?
 ###############################################################
 # Set script and dependency variables
 
-yy=$(echo $CDATE | cut -c1-4)
-mm=$(echo $CDATE | cut -c5-6)
-dd=$(echo $CDATE | cut -c7-8)
-hh=${cyc:-$(echo $CDATE | cut -c9-10)}
+export yy=$(echo $CDATE | cut -c1-4)
+export mm=$(echo $CDATE | cut -c5-6)
+export dd=$(echo $CDATE | cut -c7-8)
+export hh=${cyc:-$(echo $CDATE | cut -c9-10)}
 
 export DATA=${DATA:-${DATAROOT}/init}
-
 export EXTRACT_DIR=${EXTRACT_DIR:-$ROTDIR}
 export WORKDIR=${WORKDIR:-$DATA}
 export OUTDIR=${OUTDIR:-$ROTDIR}
-
-COMPONENT="atmos"
-
-gfs_ver=v16
-RUNICSH=${GDASINIT_DIR}/run_v16.chgres.sh
-
-RETRO=${RETRO:-"NO"}
-if [ $RETRO = "YES" ]; then
-  GETICSH=${GDASINIT_DIR}/run_v16retro.chgres.sh
-fi
-
-if [ $RETRO = "NO" ]; then # Operational input
-  # No ENKF data prior to 2012/05/21/00z
-  if [ $yy$mm$dd$hh -lt 2012052100 ]; then
-    set +x
-    echo FATAL ERROR: SCRIPTS DO NOT SUPPORT OLD GFS DATA
-    exit 2
-  elif [ $yy$mm$dd$hh -lt 2016051000 ]; then
-    gfs_ver=v12
-    RUNICSH=${GDASINIT_DIR}/run_pre-v14.chgres.sh
-  elif [ $yy$mm$dd$hh -lt 2017072000 ]; then
-    gfs_ver=v13
-    RUNICSH=${GDASINIT_DIR}/run_pre-v14.chgres.sh
-  elif [ $yy$mm$dd$hh -lt 2019061200 ]; then
-    gfs_ver=v14
-    RUNICSH=${GDASINIT_DIR}/run_${gfs_ver}.chgres.sh
-  elif [ $yy$mm$dd$hh -lt 2021031700 ]; then
-    gfs_ver=v15
-    RUNICSH=${GDASINIT_DIR}/run_${gfs_ver}.chgres.gfs.sh
-  fi
-fi
-
-export EXTRACT_DIR yy mm dd hh UFS_DIR OUTDIR CRES_HIRES CRES_ENKF
-export LEVS gfs_ver
+export COMPONENT="atmos"
+export gfs_ver=${gfs_ver:-v16}
+export RUNICSH=${RUNICSH:-${GDASINIT_DIR}/run_v16.chgres.sh}
 
 # Check if init is needed and run if so
 if [[ $gfs_ver = "v16" && $EXP_WARM_START = ".true." && $CASE = "C768" ]]; then
