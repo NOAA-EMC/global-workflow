41c19e93f4b327b98a3e8b44245cbbfcde8f855b
kate.friedman <kate.friedman@noaa.gov>
2021-03-16 19:37:44 +0000

    Component tag updates for nwprod/gfsv16.0.7
    - Final nwprod implementation component tag versions set in Externals.cfg and checkout.sh
    - GLDAS: roll back to gldas_gfsv16_release.v1.12.0
    - EMC_verif-global: roll back to verif_global_v1.11.0
    - WAFS: update in Externals.cfg to gfs_wafs.v6.0.19
    
    Notes about local nwprod install changes:
    
    - EMC_post: nwprod install shows upp_gfsv16_release.v1.1.1 tag but contains
    local updates found in upp_gfsv16_release.v1.1.3 tag
    - WAFS: nwprod install shows gfs_wafs.v6.0.17 tag but contains local updates
    found in gfs_wafs.v6.0.19 tag
    
    Refs: #1,#238

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -50,7 +50,7 @@ if [[ ! -d gldas.fd ]] ; then
     rm -f ${topdir}/checkout-gldas.log
     git clone https://github.com/NOAA-EMC/GLDAS  gldas.fd >> ${topdir}/checkout-gldas.fd.log 2>&1
     cd gldas.fd
-    git checkout gldas_gfsv16_release.v1.13.0
+    git checkout gldas_gfsv16_release.v1.12.0
     cd ${topdir}
 else
     echo 'Skip.  Directory gldas.fd already exists.'
@@ -106,7 +106,7 @@ if [[ ! -d verif-global.fd ]] ; then
     rm -f ${topdir}/checkout-verif-global.log
     git clone --recursive https://github.com/NOAA-EMC/EMC_verif-global.git verif-global.fd >> ${topdir}/checkout-verif-global.log 2>&1
     cd verif-global.fd
-    git checkout verif_global_v1.13.4
+    git checkout verif_global_v1.11.0
     cd ${topdir}
 else
     echo 'Skip. Directory verif-global.fd already exist.'
