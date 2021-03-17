19d2360effda9a29df3a0efcad8e0c30b870f95d
Jessica.Meixner <Jessica.Meixner@noaa.gov>
2021-03-17 18:55:06 +0000

    update build_ww3prepost.sh for updates in ufs-weather-model/WW3

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -42,7 +42,7 @@ else
   if [[ ! -d ufs_coupled.fd ]] ; then
     git clone https://github.com/ufs-community/ufs-weather-model ufs_coupled.fd >> ${topdir}/checkout-ufs_coupled.log 2>&1
     cd ufs_coupled.fd
-    git checkout Prototype-6.0beta 
+    git checkout 3be851d55572221d81a807d1c53dc909a4866154 
     git submodule update --init --recursive
     cd ${topdir} 
   else 
