5c77500bc4742a840a26f05aedffe2aca3e15ec7
Kate.Friedman <Kate.Friedman@noaa.gov>
2021-03-22 15:44:40 +0000

    Add exit in init.sh when C768 v16 warm starts detected
    Exit init.sh and init job when high res C768 v16 warm starts are detected
    and running chgres_cube is not needed.
    
    Refs: #178

--- jobs/rocoto/init.sh
+++ jobs/rocoto/init.sh
@@ -52,6 +52,7 @@ export RUNICSH=${RUNICSH:-${GDASINIT_DIR}/run_v16.chgres.sh}
 # Check if init is needed and run if so
 if [[ $gfs_ver = "v16" && $EXP_WARM_START = ".true." && $CASE = "C768" ]]; then
   echo "Detected v16 C768 warm starts, will not run init. Exiting..."
+  exit 0
 else
   # Run chgres_cube
   if [ ! -d $OUTDIR ]; then mkdir -p $OUTDIR ; fi
