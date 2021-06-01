127723adfde2b7b7c79bd4d346cafcb1f265dc1c
Kate.Friedman <Kate.Friedman@noaa.gov>
2021-06-01 16:44:37 +0000

    getic job updates to add OPS_RES variable and pull from operational tarballs for warm starting
    - add OPS_RES variable to define current operational resolution; use variable where needed now
    - updated getic job script to use OPS_RES variable in if-block check
    - updated getic job script with new operational tarballs to pull for warm starting high res runs
    
    Refs: #178

--- jobs/rocoto/getic.sh
+++ jobs/rocoto/getic.sh
@@ -40,6 +40,11 @@ export yy=$(echo $CDATE | cut -c1-4)
 export mm=$(echo $CDATE | cut -c5-6)
 export dd=$(echo $CDATE | cut -c7-8)
 export hh=${cyc:-$(echo $CDATE | cut -c9-10)}
+export GDATE=$($NDATE -${assim_freq:-"06"} $CDATE)
+export gyy=$(echo $GDATE | cut -c1-4)
+export gmm=$(echo $GDATE | cut -c5-6)
+export gdd=$(echo $GDATE | cut -c7-8)
+export ghh=$(echo $GDATE | cut -c9-10)
 
 export DATA=${DATA:-${DATAROOT}/init}
 export EXTRACT_DIR=${EXTRACT_DIR:-$ROTDIR}
@@ -47,16 +52,17 @@ export WORKDIR=${WORKDIR:-$DATA}
 export OUTDIR=${OUTDIR:-$ROTDIR}
 export PRODHPSSDIR=${PRODHPSSDIR:-/NCEPPROD/hpssprod/runhistory}
 export COMPONENT="atmos"
-export gfs_ver=${gfs_ver:-v16}
+export gfs_ver=${gfs_ver:-"v16"}
+export OPS_RES=${OPS_RES:-"C768"}
 export GETICSH=${GETICSH:-${GDASINIT_DIR}/get_v16.data.sh}
 
 # Run get data script
 if [ ! -d $EXTRACT_DIR ]; then mkdir -p $EXTRACT_DIR ; fi
 
 # Check if init is needed and run if so
-if [[ $gfs_ver = "v16" && $EXP_WARM_START = ".true." && $CASE = "C768" ]]; then
-  # Pull RESTART files off HPSS
+if [[ $gfs_ver = "v16" && $EXP_WARM_START = ".true." && $CASE = $OPS_RES ]]; then
 
+  # Pull RESTART files off HPSS
   if [ ! -d $ROTDIR ]; then mkdir $ROTDIR ; fi
   cd $ROTDIR
 
@@ -85,8 +91,13 @@ if [[ $gfs_ver = "v16" && $EXP_WARM_START = ".true." && $CASE = "C768" ]]; then
     status=$?
     [[ $status -ne 0 ]] && exit $status
 
-# else # Opertional input
-#   # ADD AFTER IMPLEMENTATION
+  else # Opertional input - warm starts
+
+    # Pull CDATE gfs restart tarball
+    htar -xvf ${PRODHPSSDIR}/rh${yy}/${yy}${mm}/${yy}${mm}${dd}/com_gfs_prod_gfs.${yy}${mm}${dd}_${hh}.gfs_restart.tar
+    # Pull GDATE gdas restart tarball
+    htar -xvf ${PRODHPSSDIR}/rh${gyy}/${gyy}${gmm}/${gyy}${gmm}${gdd}/com_gfs_prod_gfs.${gyy}${gmm}${gdd}_${ghh}.gfs_restart.tar
+
   fi
 
 else
