35ab5a591b355f33cc88e1b593bb0ddead53b2ce
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-06-25 16:39:04 -0400

    Port sorc/build_gsd_prep_chem.sh to acorn
     On branch feature/gefs_v12_github_port2acorn
    	modified:   sorc/build_gsd_prep_chem.sh
    
    Refs: #345

--- sorc/build_gsd_prep_chem.sh
+++ sorc/build_gsd_prep_chem.sh
@@ -19,6 +19,8 @@ elif [ $target = wcoss_dell_p3 ]; then
   gpctarget=wcoss-dell-p3
 elif [ $target = wcoss ]; then
   gpctarget=wcoss12
+elif [ $target = acorn ]; then
+  gpctarget=acorn
 else
   gpctarget=$target
 fi
