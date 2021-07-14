970964135d147c41af0547ea176e67989fbc3596
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2021-07-14 20:23:57 +0000

    Initial implementation of mass-conserving aerosol cycling based on rescaling of mixing ratios according to pressure layer thickness.

--- scripts/exgfs_chem_init_aerosol.py
+++ scripts/exgfs_chem_init_aerosol.py
@@ -12,8 +12,11 @@ from functools import partial
 # Constants
 atm_base_pattern = "{ics_dir}/%Y%m%d%H/atmos/{case}/INPUT"                    # Location of atmosphere ICs
 atm_file_pattern = "{path}/gfs_data.{tile}.nc"                                # Atm IC file names
-tracer_base_pattern = "{rot_dir}/{cdump}.%Y%m%d/%H/atmos/RERUN_RESTART"       # Location of restart files (time of previous run)
-tracer_file_pattern = "{tracer_base}/{timestamp}fv_tracer.res.{tile}.nc"      # Name of restart files (time when restart is valid)
+atm_ctrl_pattern = "{path}/gfs_ctrl.nc"                                       # Atm IC control file name
+restrt_base_pattern = "{rot_dir}/{cdump}.%Y%m%d/%H/atmos/RERUN_RESTART"       # Location of restart files (time of previous run)
+restrt_file_pattern = "{file_base}/{timestamp}fv_core.res.{tile}.nc"          # Name of restart data files (time when restart is valid)
+tracer_file_pattern = "{file_base}/{timestamp}fv_tracer.res.{tile}.nc"        # Name of restart tracer files (time when restart is valid)
+dycore_file_pattern = "{file_base}/{timestamp}fv_core.res.nc"                 # Name of restart dycore file (time when restart is valid)
 tracer_list_file_pattern = "{parm_gfs}/chem/gocart_tracer.list"               # Text list of tracer names to copy
 merge_script_pattern = "{ush_gfs}/merge_fv3_chem_tile.py"
 n_tiles = 6
@@ -54,11 +57,11 @@ def main() -> None:
 		for var in ['merge_script', 'tracer_list_file', 'atm_source_path']:
 			print(f'{var} = {f"{var}"}')
 
-	atm_files = get_atm_files(atm_source_path)
-	tracer_files = get_tracer_files(time, incr, max_lookback, fcst_length, rot_dir, cdump)
+	atm_files, ctrl_files = get_atm_files(atm_source_path)
+	tracer_files, rest_files, core_files = get_restart_files(time, incr, max_lookback, fcst_length, rot_dir, cdump)
 
 	if (tracer_files is not None):
-		merge_tracers(merge_script, atm_files, tracer_files, tracer_list_file)
+		merge_tracers(merge_script, atm_files, tracer_files, rest_files, core_files[0], ctrl_files[0], tracer_list_file)
 
 	return
 
@@ -84,17 +87,55 @@ def get_env_var(varname: str, fail_on_missing: bool = True) -> str:
 def get_atm_files(path: str) -> typing.List[str]:
 	print(f'Checking for atm files in {path}')
 
-	files = list(map(lambda tile: atm_file_pattern.format(tile=tile, path=path), tiles))
-	for file_name in files:
-		if(debug):
-			print(f"\tChecking for {file_name}")
-		if(not os.path.isfile(file_name)):
-			print("FATAL: Atmosphere file {file_name} not found".format(file_name=file_name))
-			exit(101)
-		elif(debug):
-			print(f"\t\tFound {file_name}")
-	return files
+	file_list = []
+	for file_pattern in atm_file_pattern, atm_ctrl_pattern:
+		files = list(map(lambda tile: file_pattern.format(tile=tile, path=path), tiles))
+		for file_name in files:
+			if(debug):
+				print(f"\tChecking for {file_name}")
+			if(not os.path.isfile(file_name)):
+				print("FATAL: Atmosphere file {file_name} not found".format(file_name=file_name))
+				exit(101)
+			elif(debug):
+				print(f"\t\tFound {file_name}")
+		file_list = file_list + [ files ]
+	return file_list
 
+# Find last cycle with restart files available
+def get_restart_files(time: datetime, incr: int, max_lookback: int, fcst_length: int, rot_dir: str, cdump: str) -> typing.List[str]:
+	print(f"Looking for restart tracer files in {rot_dir}")
+	for lookback in map(lambda i: incr * (i + 1), range(max_lookback)):
+		if(lookback > fcst_length):
+			# Trying to look back farther than the length of a forecast
+			break
+		elif(lookback == fcst_length):
+			# Restart files at the end of the cycle don't have a timestamp
+			timestamp = ""
+		else:
+			timestamp = time.strftime("%Y%m%d.%H0000.")
+
+		last_time = time - timedelta(hours=lookback)
+
+		if(debug):
+			print(f"\tChecking {last_time}")
+		file_list = []
+		file_base = last_time.strftime(restrt_base_pattern.format(**locals()))
+		for file_pattern in tracer_file_pattern, restrt_file_pattern, dycore_file_pattern:
+			files = list(map(lambda tile: file_pattern.format(timestamp=timestamp, file_base=file_base, tile=tile), tiles))
+			if(debug):
+				print(f"\t\tLooking for files {files} in directory {file_base}")
+			found = [file for file in files if os.path.isfile(file)]
+			if(found):
+				file_list = file_list + [ files ]
+			else:
+				print(last_time.strftime("Restart files not found for %Y%m%d_%H"))
+				break
+
+		if(found):
+			return file_list
+		else:
+			print("WARNING: Unable to find restart files, will use zero fields")
+			return None
 
 # Find last cycle with tracer data available via restart files
 def get_tracer_files(time: datetime, incr: int, max_lookback: int, fcst_length: int, rot_dir: str, cdump: str) -> typing.List[str]:
@@ -131,16 +172,16 @@ def get_tracer_files(time: datetime, incr: int, max_lookback: int, fcst_length: 
 
 
 # Merge tracer data into atmospheric data
-def merge_tracers(merge_script: str, atm_files: typing.List[str], tracer_files: typing.List[str], tracer_list_file: str) -> None:
+def merge_tracers(merge_script: str, atm_files: typing.List[str], tracer_files: typing.List[str], rest_files: typing.List[str], core_file: str, ctrl_file: str, tracer_list_file: str) -> None:
 	print(f"Merging tracers")
 	if(len(atm_files) != len(tracer_files)):
 		print("FATAL: atmosphere file list and tracer file list are not the same length")
 		exit(102)
 
-	for atm_file, tracer_file in zip(atm_files, tracer_files):
+	for atm_file, tracer_file, rest_file in zip(atm_files, tracer_files, rest_files):
 		if(debug):
 			print(f"\tMerging tracers from {tracer_file} into {atm_file}")
-		subprocess.run([merge_script, atm_file, tracer_file, tracer_list_file], check=True)
+		subprocess.run([merge_script, atm_file, tracer_file, core_file, ctrl_file, rest_file, tracer_list_file], check=True)
 
 
 if __name__ == "__main__":
