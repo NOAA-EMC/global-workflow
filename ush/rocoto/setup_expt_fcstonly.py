b34c581c3b19ecdcdb4eff463e855597e5c08d80
Kate.Friedman <Kate.Friedman@noaa.gov>
2021-07-14 19:15:31 +0000

    Adjust how EXP_WARM_START is set from setup_expt_fcstonly.py step
    - remove default value for start (EXP_WARM_START)
    - if user provides start flag then use it for EXP_WARM_START
    - if user doesn't provide start flag then make a decision for them based on resolution
    - if start=None and res=768 then start=warm
    - if start=None and res!=768 then start=cold
    
    Refs: #353

--- ush/rocoto/setup_expt_fcstonly.py
+++ ush/rocoto/setup_expt_fcstonly.py
@@ -118,7 +118,7 @@ Create COMROT experiment directory structure'''
     parser.add_argument('--configdir', help='full path to directory containing the config files', type=str, required=False, default=None)
     parser.add_argument('--gfs_cyc', help='GFS cycles to run', type=int, choices=[0, 1, 2, 4], default=1, required=False)
     parser.add_argument('--partition', help='partition on machine', type=str, required=False, default=None)
-    parser.add_argument('--start', help='restart mode: warm or cold', type=str, choices=['warm', 'cold'], required=False, default='cold')
+    parser.add_argument('--start', help='restart mode: warm or cold', type=str, choices=['warm', 'cold'], required=False)
 
     args = parser.parse_args()
 
@@ -139,13 +139,14 @@ Create COMROT experiment directory structure'''
     start = args.start
 
     # Set restart setting in config.base
-    if start == 'cold':
+    if start is None:
+      if res == 768:
+        exp_warm_start = '.true.'
+      else:
+        exp_warm_start = '.false.'
+    elif start == 'cold':
       exp_warm_start = '.false.'
     elif start == 'warm':
-      exp_warm_start = '.true.'
-
-    # Check resolution for start setting - assume warm start if ops res
-    if res == 768:
       exp_warm_start = '.true.'
 
     # Set FDATE (first full cycle)
