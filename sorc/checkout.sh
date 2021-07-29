e9be461ab65faca250cf7f0af877d5a5e677bda4
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2021-07-29 20:49:34 +0000

    Update UPP to 10.0.8
    Updates UPP to 10.0.8 to be consistent with UFS. This also adds
    consistency between the moduleload and checked out version.
    
    Refs: #366, #374

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -77,7 +77,7 @@ if [[ ! -d gfs_post.fd ]] ; then
     rm -f ${topdir}/checkout-gfs_post.log
     git clone https://github.com/NOAA-EMC/EMC_post.git gfs_post.fd >> ${logdir}/checkout-gfs_post.log 2>&1
     cd gfs_post.fd
-    git checkout upp_v10.0.3
+    git checkout upp_v10.0.8
     git submodule update --init CMakeModules
     ################################################################################
     # checkout_gtg
