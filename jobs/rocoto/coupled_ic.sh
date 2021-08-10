97a40ad4d5df352813a7361b017f7705ccf8a74e
Jessica Meixner <jessica.meixner@noaa.gov>
2021-08-10 18:32:48 -0400

    coupled-crow: catch copy errors and bail out if ICs do not exist (#395)
    * catch copy errors and bail out if ICs do not exist
    
    * update error handling
    
    * remove exit

--- jobs/rocoto/coupled_ic.sh
+++ jobs/rocoto/coupled_ic.sh
@@ -19,6 +19,7 @@ set -x
 . $HOMEgfs/ush/load_fv3gfs_modules.sh
 status=$?
 [[ $status -ne 0 ]] && exit $status
+err=0
 
 export DATAROOT="$RUNDIR/$CDATE/$CDUMP"
 [[ ! -d $DATAROOT ]] && mkdir -p $DATAROOT
@@ -53,18 +54,39 @@ fi
 
 # Setup ATM initial condition files
 cp -r $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP/*  $ICSDIR/$CDATE/atmos/
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo "FATAL: Unable to copy $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP/* to $ICSDIR/$CDATE/atmos/ (Error code $rc)" 
+fi
+((err+=$rc))
+
 
 # Setup Ocean IC files 
 cp -r $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/MOM*.nc  $ICSDIR/$CDATE/ocn/
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo "FATAL: Unable to copy $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/MOM*.nc to $ICSDIR/$CDATE/ocn/ (Error code $rc)"
+fi
+((err+=$rc))
 
 #Setup Ice IC files 
 cp $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/cice5_model_${ICERESdec}.res_$CDATE.nc $ICSDIR/$CDATE/ice/cice_model_${ICERESdec}.res_$CDATE.nc
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo "FATAL: Unable to copy $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/cice5_model_${ICERESdec}.res_$CDATE.nc to $ICSDIR/$CDATE/ice/cice_model_${ICERESdec}.res_$CDATE.nc (Error code $rc)"
+fi
+((err+=$rc))
 
 if [ $cplwav = ".true." ]; then
   [[ ! -d $ICSDIR/$CDATE/wav ]] && mkdir -p $ICSDIR/$CDATE/wav
   for grdID in $waveGRD
   do
     cp $ORIGIN_ROOT/$CPL_WAVIC/$CDATE/wav/$grdID/*restart.$grdID $ICSDIR/$CDATE/wav/
+    rc=$?
+    if [[ $rc -ne 0 ]] ; then
+      echo "FATAL: Unable to copy $ORIGIN_ROOT/$CPL_WAVIC/$CDATE/wav/$grdID/*restart.$grdID to $ICSDIR/$CDATE/wav/ (Error code $rc)" 
+    fi
+    ((err+=$rc))
   done
 fi
 
@@ -75,6 +97,12 @@ COMOUT="$ROTDIR/$CDUMP.$PDY/$cyc/atmos"
 cd $COMOUT || exit 99
 rm -rf INPUT
 $NLN $OUTDIR .
+
+
+if  [[ $err -ne 0 ]] ; then 
+  echo "Fatal Error: ICs are not properly set-up" 
+  exit $err 
+fi 
 
 ##############################################################
 # Exit cleanly
