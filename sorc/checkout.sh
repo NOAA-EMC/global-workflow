36b4cf01cc75bdcb6dfcbfbe23751016878d6401
kate.friedman <kate.friedman@noaa.gov>
2021-10-01 14:45:11 +0000

    Update UFS_UTILS checkout and build
    - update checkout.sh to use test version of UFS_UTILS for WCOSS2
    - update build_ufs_utils.sh for new build_all.sh script
    
    Refs: #399

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -58,9 +58,9 @@ fi
 echo ufs_utils checkout ...
 if [[ ! -d ufs_utils.fd ]] ; then
     rm -f ${topdir}/checkout-ufs_utils.log
-    git clone https://github.com/NOAA-EMC/UFS_UTILS.git ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
+    git clone https://github.com/GeorgeGayno-NOAA/UFS_UTILS.git ufs_utils.fd >> ${topdir}/checkout-ufs_utils.fd.log 2>&1
     cd ufs_utils.fd
-    git checkout ops-gfsv16.0.0
+    git checkout feature/wcoss2
     cd ${topdir}
 else
     echo 'Skip.  Directory ufs_utils.fd already exists.'
