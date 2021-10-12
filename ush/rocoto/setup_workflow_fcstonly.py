9698508413a12f703d6bd27f3515ef6e6ff149df
jikuang <jikuang@umd.edu>
2021-10-12 09:09:57 -0500

    update metp dependency of forecast only workflows

--- ush/rocoto/setup_workflow_fcstonly.py
+++ ush/rocoto/setup_workflow_fcstonly.py
@@ -668,8 +668,6 @@ def get_workflow(dict_configs, cdump='gdas'):
         deps = []
         dep_dict = {'type':'metatask', 'name':f'{cdump}post'}
         deps.append(rocoto.add_dependency(dep_dict))
-        dep_dict = {'type':'task', 'name':f'{cdump}arch', 'offset':'-&INTERVAL;'}
-        deps.append(rocoto.add_dependency(dep_dict))
         dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
         sdate_gfs = rocoto.create_envar(name='SDATE_GFS', value='&SDATE;')
         metpcase = rocoto.create_envar(name='METPCASE', value='#metpcase#')
