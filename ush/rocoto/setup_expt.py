332bf0148d7772135b1c13ac1ad43a69b5298208
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2021-10-18 09:28:16 -0500

    Make switching to coupled defaults easier
    In order to facilitate running coupled experiments without having to
    modify several config files, some updates are made to the config files.
    
    A new file, config.defaults.s2sw is created to contain the alternate
    defaults for the coupled model, based on prototype 7. This file is
    sourced whenever the app is S2SW.
    
    The UFS app is made into a variable that is now set by the setup scripts
    via the --app switch. Allowed values are ATM, ATMW, S2S, S2SW. If no
    argument is provided, the default is ATM (so it doesn't change existing
    behavior).
    
    The section in config.base that sets all the components based on the app
    is moved earlier in order to allow the new defaults to be set.
    
    Settings in some config files have been modifyied to now accept values
    that have been set previously to allow the alternate settings for the
    coupled model. In the future, this could be extended to additional config
    files to allow more settings to be overridden.
    
    waveGRDN and waveGRDG are updated to be based on waveGRD.
    
    Updated instructions to setup prototype analog:
    
    1. Clone and run checkout, build_all, and link as normal for coupled:
    ```
    ./checkout.sh
    ./build_all.sh -c
    ./link_workflow emc orion coupled
    ```
    
    2. In ush/rocoto, run
    ```
    ./setup_expt_fcstonly.py --app S2SW --res 384 --pslot $PSLOT --comrot $COMROT --expdir $EXPDIR --idate 2013040100 --edate 2013040100 --configdir ../../parm/config
    ./setup_workflow_fcstonly.py --expdir $EXPDIR/$PSLOT
    ```
    
    3. In $EXPDIR/$PSLOT, modify $PSLOT.xml as follows:
    - Remove or comment out the entire gfsinit task
    - Remove or comment out gfsinit as a dependency for gfswaveprep
    - Optionally reduce the walltime for the forecast job to 1 hour to move through the queue faster
    -- The forecast job uses too many nodes for the debug queue, though you can use the debug queue for the wave prep jobs
    
    4. In $ROTDIR, do the following:
    ```
    cd $ROTDIR
    ln -s /work/noaa/global/wkolczyn/noscrub/global-workflow/FV3ICS FV3ICS
    
    mkdir -p $ROTDIR/$PSLOT/gfs.20130401/00/atmos
    cd $ROTDIR/$PSLOT/gfs.20130401/00/atmos
    ln -s $ROTDIR/FV3ICS/2013040100/atmos/C384/INPUT INPUT
    
    mkdir -p $ROTDIR/$PSLOT/gdas.20130331/18/wave/restart
    cd $ROTDIR/$PSLOT/gdas.20130331/18/wave/restart
    ln -s $ROTDIR/FV3ICS/2013040100/wav/20130401.000000.restart.gwes_30m .
    ```
    
    Refs: #368

--- ush/rocoto/setup_expt.py
+++ ush/rocoto/setup_expt.py
@@ -109,7 +109,8 @@ def edit_baseconfig():
                     .replace('@CHGRP_RSTPROD@', chgrp_rstprod) \
                     .replace('@CHGRP_CMD@', chgrp_cmd) \
                     .replace('@HPSSARCH@', hpssarch) \
-                    .replace('@gfs_cyc@', f'{gfs_cyc}')
+                    .replace('@gfs_cyc@', f'{gfs_cyc}') \
+                    .replace('@APP@', f'{app}')
                 if expdir is not None:
                     line = line.replace('@EXPDIR@', os.path.dirname(expdir))
                 if comrot is not None:
@@ -135,6 +136,7 @@ Create COMROT experiment directory structure,
 link initial condition files from $ICSDIR to $COMROT'''
 
     parser = ArgumentParser(description=description, formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--app', help='UFS application', type=str, choices=['ATM', 'ATMW', 'S2S', 'S2SW'], required=False, default='ATM')
     parser.add_argument('--pslot', help='parallel experiment name', type=str, required=False, default='test')
     parser.add_argument('--resdet', help='resolution of the deterministic model forecast', type=int, required=False, default=384)
     parser.add_argument('--resens', help='resolution of the ensemble model forecast', type=int, required=False, default=192)
@@ -158,6 +160,7 @@ link initial condition files from $ICSDIR to $COMROT'''
     if not configdir:
         configdir = os.path.abspath(os.path.dirname(__file__) + '/../parm/config')
 
+    app = args.app
     pslot = args.pslot
     idate = datetime.strptime(args.idate, '%Y%m%d%H')
     edate = datetime.strptime(args.edate, '%Y%m%d%H')
