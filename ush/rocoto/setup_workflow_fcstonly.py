d51dae88cc653715d65b136f58cdfea6ad349dbb
Mallory Row <mallory.row@noaa.gov>
2021-11-16 15:47:53 +0000

    Update gfsmetp and gfsarch dependencies
    gfsmetp had a dependency on the previous cycle's
    gfsarch, as gfsmetp uses the data moved to ARCDIR
    by gfsarch. This caused problems in cycled runs where
    the previous gfsarch cycle did not exist in the first
    run of the gfs cycles. This removes that dependency.
    
    A dependency gfsarch on gfsmetp was added. This was
    added the running of the gfsmetp jobs will now use
    data in ROTDIR, which gfsarch will possibly remove
    as a part of clean up.
    
    Refs: #437

--- ush/rocoto/setup_workflow_fcstonly.py
+++ ush/rocoto/setup_workflow_fcstonly.py
@@ -668,8 +668,6 @@ def get_workflow(dict_configs, cdump='gdas'):
         deps = []
         dep_dict = {'type':'metatask', 'name':f'{cdump}post'}
         deps.append(rocoto.add_dependency(dep_dict))
-        dep_dict = {'type':'task', 'name':f'{cdump}arch', 'offset':'-&INTERVAL;'}
-        deps.append(rocoto.add_dependency(dep_dict))
         dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
         sdate_gfs = rocoto.create_envar(name='SDATE_GFS', value='&SDATE;')
         metpcase = rocoto.create_envar(name='METPCASE', value='#metpcase#')
@@ -687,6 +685,9 @@ def get_workflow(dict_configs, cdump='gdas'):
     deps.append(rocoto.add_dependency(dep_dict))
     if do_vrfy in ['Y', 'YES']:
         dep_dict = {'type':'task', 'name':f'{cdump}vrfy'}
+        deps.append(rocoto.add_dependency(dep_dict))
+    if do_metp in ['Y', 'YES']:
+        dep_dict = {'type':'metatask', 'name':f'{cdump}metp'}
         deps.append(rocoto.add_dependency(dep_dict))
     dep_dict = {'type':'streq', 'left':'&ARCHIVE_TO_HPSS;', 'right':f'{hpssarch}'}
     deps.append(rocoto.add_dependency(dep_dict))
