0bd758137d79d33bde30b59a49c4597b3a55b9c5
Guang.Ping.Lou <Guang.Ping.Lou@noaa.gov>
2021-11-30 18:37:56 +0000

    issue #399 remove .lua in module_base.wcoss2.sh

--- ush/load_fv3gfs_modules.sh
+++ ush/load_fv3gfs_modules.sh
@@ -34,7 +34,7 @@ elif [[ -L /usrx && "$( readlink /usrx 2> /dev/null )" =~ dell ]] ; then
 	module load module_base.wcoss_dell_p3 
 elif [[ -d /lfs/h2 ]]; then
     # We are on WCOSS2 (Cactus or Dogwood)
-	module load module_base.wcoss2.lua
+	module load module_base.wcoss2
 elif [[ -d /dcom && -d /hwrf ]] ; then
     # We are on NOAA Tide or Gyre
 	module load module_base.wcoss 
