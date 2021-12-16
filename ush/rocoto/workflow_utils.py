04e997d8b2e5da19d0183fd7c38a5b234e4aa335
kate.friedman <kate.friedman@noaa.gov>
2021-12-16 14:38:12 +0000

    Add NODESIZE to setup scripts
    - add nodesize/NODESIZE to setup scripts for rocoto xml
    - hardcode for just WCOSS2 for now; set to 128ppn value
    - update to support other machines later
    
    Refs: #399

--- ush/rocoto/workflow_utils.py
+++ ush/rocoto/workflow_utils.py
@@ -195,6 +195,7 @@ def create_wf_task(task, cdump='gdas', cycledef=None, envar=None, dependency=Non
                  'walltime': f'&WALLTIME_{task.upper()}_{cdump.upper()};', \
                  'native': f'&NATIVE_{task.upper()}_{cdump.upper()};', \
                  'memory': f'&MEMORY_{task.upper()}_{cdump.upper()};', \
+                 'nodesize': '&NODESIZE;', \
                  'resources': f'&RESOURCES_{task.upper()}_{cdump.upper()};', \
                  'log': f'&ROTDIR;/logs/@Y@m@d@H/{taskstr}.log', \
                  'envar': envar, \
@@ -243,6 +244,7 @@ def create_firstcyc_task(cdump='gdas'):
                  'queue': '&QUEUE_SERVICE;', \
                  'walltime': f'&WALLTIME_ARCH_{cdump.upper()};', \
                  'native': f'&NATIVE_ARCH_{cdump.upper()};', \
+                 'nodesize': '&NODESIZE;', \
                  'resources': f'&RESOURCES_ARCH_{cdump.upper()};', \
                  'log': f'&ROTDIR;/logs/@Y@m@d@H/{taskstr}.log', \
                  'dependency': dependencies}
