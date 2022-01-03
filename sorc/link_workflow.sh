bb861013c67321bc706c5bbecef3e632f0188119
Cory Martin <cory.r.martin@noaa.gov>
2022-01-03 12:12:50 -0600

    Fix for orion env file and add link to j-job

--- sorc/link_workflow.sh
+++ sorc/link_workflow.sh
@@ -188,6 +188,13 @@ cd ${pwd}/../ush                ||exit 8
 
 
 #------------------------------
+#--add UFS DA/JEDI files
+#------------------------------
+cd ${pwd}/../jobs               ||exit 8
+    $LINK ../sorc/ufs_da.fd/UFS-DA/jobs/JUFSDA_GLOBAL_ATMOS_ANALYSIS        .
+
+
+#------------------------------
 #--add DA Monitor file (NOTE: ensure to use correct version)
 #------------------------------
 cd ${pwd}/../fix                ||exit 8
