9f4b151ecb420b6ed2c72ae54c162c83ea58c67b
kate.friedman <kate.friedman@noaa.gov>
2022-01-03 15:25:43 +0000

    Convert gfs_bufr build to LUA
    - convert gfs_bufr WCOSS2 modulefile to LUA format
    - update build_gfs_bufrsnd.sh for loading module
    
    Refs: #399

--- sorc/build_gfs_bufrsnd.sh
+++ sorc/build_gfs_bufrsnd.sh
@@ -4,17 +4,8 @@ set -eux
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
 
-USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
-if [ $USE_PREINST_LIBS = true ]; then
-	    source ../modulefiles/gfs_bufr.$target             > /dev/null 2>&1
-	else
-	  export MOD_PATH=${cwd}/lib/modulefiles
-	    if [ $target = wcoss_cray ]; then
-	    source ../modulefiles/gfs_bufr.${target}_userlib > /dev/null 2>&1
-	   else
-        source ../modulefiles/gfs_bufr.$target           > /dev/null 2>&1
-	    fi
-	fi
+module use ${cwd}/../modulefiles
+module load gfs_bufr.$target             > /dev/null 2>&1
 
 # Check final exec folder exists
 if [ ! -d "../exec" ]; then
