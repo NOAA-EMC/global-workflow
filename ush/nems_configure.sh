c24c02965a1da77e029c490d9604e34348d8e4b5
Jessica Meixner <Jessica.Meixner@noaa.gov>
2022-01-04 15:17:27 +0000

    use existing RESTILE variable

--- ush/nems_configure.sh
+++ ush/nems_configure.sh
@@ -48,8 +48,6 @@ else
   EPS_IMESH='1.0e-1'
 fi
 
-ATMTILESIZE=$(echo $CASE | cut -c2-)
-
 rm -f $DATA/nems.configure
 
 med_petlist_bounds=${med_petlist_bounds:-"0 $(( $MEDPETS-1 ))"}
@@ -88,7 +86,7 @@ if [ $cplflx = .true. ]; then
   sed -i -e "s;@\[RESTART_N\];$restart_interval_nems;g" tmp1
   sed -i -e "s;@\[use_mommesh\];$USE_MOMMESH;g" tmp1
   sed -i -e "s;@\[eps_imesh\];$EPS_IMESH;g" tmp1
-  sed -i -e "s;@\[ATMTILESIZE\];$ATMTILESIZE;g" tmp1
+  sed -i -e "s;@\[ATMTILESIZE\];$RESTILE;g" tmp1
 fi
 if [ $cplwav = .true. ]; then
   sed -i -e "s;@\[wav_model\];ww3;g" tmp1
