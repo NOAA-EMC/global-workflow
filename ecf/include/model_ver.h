abeedc4e6a33acba844ed6c1a0213cc978230740
Lin.Gan <lin.gan@noaa.gov>
2022-01-06 01:19:46 +0000

    As directed by Rahul Mahajan this ecflow PR will include "PBS -l place" from the Google sheet column L (snapshot at 5:00p January 5, 2022) and "PBS -l walltime" from current operation.  This PR will NOT be tested at time of submit.
    The snapshot URL https://docs.google.com/spreadsheets/d/1sef7gAq5OZAP_ioySKKwcMmmOkhlM0RMO67XTIoMlDg/edit?usp=sharing
    
    This PR is only for update under the $HOMEgfs/ecf location for wcoss2 GFS ecflow workflow transition.

--- ecf/include/model_ver.h
+++ ecf/include/model_ver.h
@@ -1,2 +1,5 @@
-. ${NWROOT:?}/versions/${model:?}.ver
-eval export HOME${model}=${NWROOT}/${model}.\${${model}_ver:?}
+# . ${NWROOT:?}/versions/${model:?}.ver
+# eval export HOME${model}=${NWROOT}/${model}.\${${model}_ver:?}
+
+. /lfs/h2/emc/global/noscrub/Lin.Gan/git/feature-ops-wcoss2/ecf/versions/${model:?}.ver
+export HOMEgfs=/lfs/h2/emc/global/noscrub/Lin.Gan/git/feature-ops-wcoss2
