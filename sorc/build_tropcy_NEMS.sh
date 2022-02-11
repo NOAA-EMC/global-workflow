40253b9f3798878fe0f078afc38b26dabd13cd61
kate.friedman <kate.friedman@noaa.gov>
2022-02-11 09:52:34 -0600

    Update SIGIO library variables in build_tropcy_NEMS.sh
    - replace SIGIO_LIB4 with SIGIO_LIB
    - replace SIGIO_INC4 with SIGIO_INC
    - remove two lines that set "4" variables to non-"4" variables
    
    Refs: #639

--- sorc/build_tropcy_NEMS.sh
+++ sorc/build_tropcy_NEMS.sh
@@ -24,9 +24,6 @@ fi
 module use ${cwd}/../modulefiles
 module load modulefile.storm_reloc_v6.0.0.$target
 
-SIGIO_LIB4=$SIGIO_LIB
-SIGIO_INC4=$SIGIO_INC
-
 export FC=$myFC
 export JASPER_LIB=${JASPER_LIB:-$JASPER_LIBRARIES/libjasper.a}
 
@@ -36,8 +33,8 @@ export LIBS_SUP="${W3EMC_LIBd} ${W3NCO_LIBd}"
 echo lset
 echo lset
  export LIBS_REL="${W3NCO_LIB4}"
-export LIBS_REL="${NEMSIOGFS_LIB} ${NEMSIO_LIB} ${LIBS_REL} ${SIGIO_LIB4} ${BACIO_LIB4} ${SP_LIBd}"
-export LIBS_SIG="${SIGIO_INC4}"
+export LIBS_REL="${NEMSIOGFS_LIB} ${NEMSIO_LIB} ${LIBS_REL} ${SIGIO_LIB} ${BACIO_LIB4} ${SP_LIBd}"
+export LIBS_SIG="${SIGIO_INC}"
 export LIBS_SYN_GET="${W3NCO_LIB4}"
 export LIBS_SYN_MAK="${W3NCO_LIB4} ${BACIO_LIB4}"
 export LIBS_SYN_QCT="${W3NCO_LIB8}"
