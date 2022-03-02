0e90a2d98b487c138706da53ff49ebf20b4ec852
kate.friedman <kate.friedman@noaa.gov>
2022-03-02 13:32:31 -0600

    Remove png module from modulefile.storm_reloc_v6.0.0.orion.lua
    - no longer needed, now called libpng
    
    Refs: #665

--- modulefiles/modulefile.storm_reloc_v6.0.0.orion.lua
+++ modulefiles/modulefile.storm_reloc_v6.0.0.orion.lua
@@ -10,7 +10,6 @@ load(pathJoin("hpc-impi", os.getenv("hpc_impi_ver")))
 
 load(pathJoin("jasper", os.getenv("jasper_ver")))
 load(pathJoin("libpng", os.getenv("libpng_ver")))
-load(pathJoin("png", os.getenv("1.6.35")))
 load(pathJoin("zlib", os.getenv("zlib_ver")))
 
 load(pathJoin("bacio", os.getenv("bacio_ver")))
