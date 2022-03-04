a4fb28697394a691fd04b30bfc0b1133066b062e
Kyle Nevins <kyle.nevins@noaa.gov>
2022-03-04 16:30:28 -0500

    Updated scripts for new build structure
    This is the new setup and definitions setup using the new ecflow
    build structure vetted by the team. Still requires inclusion of
    triggers and events.

--- workflow_generator/ecflow_setup/ecflow_definitions.py
+++ workflow_generator/ecflow_setup/ecflow_definitions.py
@@ -7,7 +7,6 @@
         Kyle Nevins
         kyle.nevins@noaa.gov
     FILE DEPENDENCIES:
-        1. configuration file config.json
         2. config files for the parallel; e.g. config.base, config.fcst[.gfs], etc.
         Without this dependency, the script will fail
     OUTPUT:
@@ -17,26 +16,25 @@
 
 import sys
 import os
-import json
 import re
+import shutil
 
 try:
-    from ecflow import (Defs, Suite, Family, Variable, Task)
     import ecflow
 except ImportError as err:
     raiseException("Error: Could not import ecflow module: %s" % err)
 
 class Ecflowsuite:
 
-    def __init__(self,ecfsuite,ecfconf,env_configs):
-        self.suitename = ecfsuite
-        self.ecfsuite = self.add_suite(ecfsuite)
-        self.ecfconf = ecfconf
-        self.env_configs = env_configs
-        self.ecfhome = "%s/scripts" % env_configs['base']['ECFgfs']
+    def __init__(self,ecfsuite,ecfhome,build_tree=True):
+
+        # Initialize environment
         self.ecfnodes = {}
-        #self.taskskip = arguments.taskskip if arguments.taskskip else []
-        #self.nodeskip = arguments.nodeskip if arguments.nodeskip else []
+        self.ecfhome = ecfhome
+        self.build_tree = build_tree
+
+        # Create initial suite
+        self.ecfsuite = self.add_suite(ecfsuite)
 
     def raiseException(self,e):
         print(e)
@@ -46,169 +44,144 @@ class Ecflowsuite:
         raise Exception("Node is not definied")
 
     def add_suite(self, suite):
-        new_suite = Suite("%s" % suite)
+        new_suite = ecfSuite("%s" % suite)
+        if self.build_tree:
+            new_suite.generate_folders(self.ecfhome)
         return new_suite
 
     def get_suite(self):
         return self.ecfsuite
 
-    def add_suite_edits(self):
+    def get_suite_name(self):
+        return self.ecfsuite.name()
 
-        def verify_edit(edit):
-            edit = edit.upper()
-            if (
-                edit in self.env_configs['base'].keys() and
-                self.env_configs['base'][edit] is not None
-                ):
-                self.ecfsuite += ecflow.Edit({edit : self.env_configs['base'][edit]})
-            elif (
-                edit.lower() in self.env_configs['base'].keys() and
-                self.env_configs['base'][edit.lower()] is not None
-            ):
-                self.ecfsuite += ecflow.Edit({edit : self.env_configs['base'][edit.lower()]})
+    def get_base_name(self,node):
+        return re.search("(.*)\{.*\}",node).group(1).strip()
 
-        default_edits = [
-            'ACCOUNT',
-            'queue',
-            'machine',
-            'RUN_ENVIR',
-        ]
+    def get_range(self,node,initial_count=0,increment=1,max_value=1):
+        count_string = re.search(".*\{(.*)\}.*",node).group(1).strip().split(',')
+        for item in count_string:
+            if 'initial_count' in item and initial_count==0:
+                initial_count = int(item.split(':')[1].strip())
+            if 'increment' in item and increment==1:
+                increment = int(item.split(':')[1].strip())
+            if item.strip().isdigit() and max_value==1:
+                max_value = int(item.strip())
+        max_value = max_value * increment
+        if initial_count > 0:
+            max_value += initial_count
+        return range(initial_count,max_value,increment)
 
-        for edit in default_edits:
-            verify_edit(edit)
-
-        # Setup ECF Home
-        self.ecfsuite += ecflow.Edit(ECF_HOME=self.ecfhome, ECF_INCLUDE=self.ecfhome)
-
-        # Baseline edits
-        if 'edits' in self.ecfconf['suites'].keys():
-            edits = ecflow.Edit(self.ecfconf['suites']['edits'])
-            self.ecfsuite += edits
-
-        # Setup sutite specific edits
-        if (
-            type(self.ecfconf['suites'][self.suitename]) is dict and
-            'edits' in self.ecfconf['suites'][self.suitename].keys()
-        ):
-            edits = ecflow.Edit(self.ecfconf['suites'][self.suitename]['edits'])
-            self.ecfsuite += edits
+    def add_edit(self,edit_dict,parent=None):
+        if parent:
+            self.ecfnodes[parent] += ecflow.Edit(edit_dict)
+        else:
+            self.ecfsuite += ecflow.Edit(edit_dict)
 
     def add_family(self,family,parents=None):
         family_name = "%s_%s" % (parents, family) if parents else family
 
         # If the name already exists, the family already exists
         if family_name not in self.ecfnodes.keys():
-            self.ecfnodes[family_name] = Family(family)
+            self.ecfnodes[family_name] = ecfFamily(family)
+            if self.build_tree:
+                self.ecfnodes[family_name].generate_folders(self.ecfhome,self.get_suite_name(),parents)
 
         if parents:
             self.ecfnodes[parents] += self.ecfnodes[family_name]
         else:
             self.ecfsuite += self.ecfnodes[family_name]
 
-    def add_task(self,task,parents):
-        if task not in self.ecfnodes.keys():
-            self.ecfnodes[task] = Task(task)
-            self.ecfnodes[parents] += self.ecfnodes[task]
+    def add_task(self,task,parents,scriptrepo,template=None):
+        if re.search(r"\{.*\}",task):
+            task_base_name = self.get_base_name(task)
+            counter = self.get_range(task)
+            for task_number in counter:
+                task_name = "%s%03d" %( task_base_name, task_number )
+                if task_name not in self.ecfnodes.keys():
+                    self.ecfnodes[task_name] = ecfTask(task_name)
+                    self.ecfnodes[task_name].set_scriptrepo(scriptrepo)
+                    if self.build_tree:
+                        self.ecfnodes[task_name].generate_ecflow_task(self.ecfhome,self.get_suite_name(),parents)
+                    self.ecfnodes[parents] += self.ecfnodes[task_name]
+        else:
+            if task not in self.ecfnodes.keys():
+                self.ecfnodes[task] = ecfTask(task)
+                self.ecfnodes[task].set_scriptrepo(scriptrepo)
+                if self.build_tree:
+                    self.ecfnodes[task].generate_ecflow_task(self.ecfhome,self.get_suite_name(),parents)
+                self.ecfnodes[parents] += self.ecfnodes[task]
 
-    def add_trigger(self,task,trigger_dict, counter=None):
-        trigger_statement = []
-        for trigger in trigger_dict:
-            if re.search("\{.*\}",trigger['task']) and counter is not None:
-                trigger_task_base_name = re.search("(.*)\{.*\}",trigger['task']).group(1).strip()
-                if re.search("initial_count",trigger['task']):
-                    initial_count = re.search(".*\{(.*)\}",trigger['task']).group(1).strip().split(':')[1]
-                    trigger_task = "%s%03d" %( trigger_task_base_name, counter+int(initial_count) )
-                elif re.search("increment",trigger['task']):
-                    increment = re.search(".*\{(.*)\}",trigger['task']).group(1).strip().split(':')[1]
-                    trigger_task = "%s%03d" %( trigger_task_base_name, counter*int(increment) )
-                else:
-                    trigger_task = "%s%03d" %( trigger_task_base_name, counter )
-            else:
-                trigger_task = trigger['task']
-            trigger_path = self.ecfnodes[trigger_task].get_abs_node_path()
-            if 'state' in trigger.keys():
-                trigger_statement.append("%s == %s" %( trigger_path, trigger['state']))
-            elif 'event' in trigger.keys():
-                if re.search("\{.*\}",trigger['event']) and counter is not None:
-                    trigger_event_base_name = re.search("(.*)\{.*\}",trigger['event']).group(1).strip()
-                    if re.search("initial_count",trigger['event']):
-                        initial_count = re.search(".*\{(.*)\}",trigger['event']).group(1).strip().split(':')[1]
-                        trigger_event = "%s%03d" %( trigger_event_base_name, counter+int(initial_count) )
-                    elif re.search("increment",trigger['event']):
-                        increment = re.search(".*\{(.*)\}",trigger['event']).group(1).strip().split(':')[1]
-                        trigger_event = "%s%03d" %( trigger_event_base_name, counter*int(increment) )
-                    else:
-                        trigger_event = "%s%03d" %( trigger_event_base_name, counter )
-                else:
-                    trigger_event = trigger['event']
-                trigger_task_event_name = "%s_%s" %( trigger_task, trigger_event )
-                trigger_statement.append("%s:%s" %( trigger_path, trigger_event ))
-            else:
-                trigger_statement.append(self.ecfnodes[trigger_task])
-        self.ecfnodes[task].add( ecflow.Trigger( trigger_statement ))
-
-    def add_event(self,task,event_dict, counter=None):
-        for event in event_dict:
-            if re.search(r"\{.*\}",event):
-                if counter is None:
-                    loop_check = re.search("\{(.*)\}",event).group(1).strip()
-                    if isinstance(loop_check,str) and 'env.' in loop_check:
-                        if loop_check.split('.')[1] in os.environ:
-                            loop = os.environ[loop_check.split('.')[1]]
-                        else:
-                            loop = self.env_configs['base'][loop_check.split('.')[1]]
-                    elif loop_check.isdigit():
-                        loop = int(loop_check)
-                    else:
-                        loop = loop_check
-                    for counter in range(loop):
-                        base_event_name = re.search("(.*)\{.*\}",event).group(1).strip()
-                        event_name = "%s_%s" %( task, base_event_name )
-                        ecfnodes_name = "%s%03d" % ( event_name, counter )
-                        event_item = "%s%03d" %( base_event_name, counter)
-                        self.ecfnodes[ecfnodes_name] = ecflow.Event(event_item)
-                        self.ecfnodes[task].add(self.ecfnodes[ecfnodes_name])
-            else:
-                event_name = "%s_%s" %( task, event )
-                ecfnodes_name = "%s" % ( event_name )
-                event_item = "%s" %( event )
-                self.ecfnodes[ecfnodes_name] = ecflow.Event(event_item)
-                self.ecfnodes[task].add(self.ecfnodes[ecfnodes_name])
-
-    def add_edit(self,task,edit_dict, counter=None):
+    def add_task_edits(self,task,edit_dict):
         edits_to_add = {}
-        for edit in edit_dict:
-            if re.search("\{.*\}",edit_dict[edit]) and counter is not None:
-                edit_name = edit
-                edit_base_value = re.search("(.*)\{.*\}",edit_dict[edit]).group(1).strip()
-                if re.search("initial_count",edit_dict[edit]):
-                    initial_count = re.search(".*\{(.*)\}",edit_dict[edit]).group(1).strip().split(':')[1]
-                    edits_to_add[edit] = "%s%03d" %( edit_base_value, counter+int(initial_count) )
-                elif re.search("increment",edit_dict[edit]):
-                    increment = re.search(".*\{(.*)\}",edit_dict[edit]).group(1).strip().split(':')[1]
-                    edits_to_add[edit] = "%s%03d" %( edit_base_value, counter*int(increment) )
-                else:
-                    edits_to_add[edit] = "%s%03d" %( edit_base_value, counter )
-            else:
-                edits_to_add[edit] = edit_dict[edit]
-        self.ecfnodes[task].add( ecflow.Edit( edits_to_add ))
+        if re.search(r"\{.*\}",task):
+            task_base_name = self.get_base_name(task)
+            task_counter = self.get_range(task)
+            loop_index = 0
+            for task_number in task_counter:
+                task_name = "%s%03d" %( task_base_name, task_number )
+                for edit in edit_dict:
+                    if re.search(r"\{.*\}",edit_dict[edit]):
+                        edit_base_name = self.get_base_name(edit_dict[edit])
+                        keywords = ['initial_count','increment']
+                        if any(match in edit_dict[edit] for match in keywords):
+                            total_tasks=len(task_counter)
+                            edit_range = self.get_range(edit_dict[edit],max_value=total_tasks)
+                            edit_count = [*edit_range]
+                            edit_value = "%s%03d" %(edit_base_name, edit_count[loop_index])
+                        else:
+                            edit_value = "%s%03d" %(edit_base_name, task_number)
+                        self.add_edit({edit:edit_value},task_name)
+                    else:
+                        self.add_edit({edit:edit_dict[edit]},task_name)
+                loop_index+=1
+        else:
+            for edit in edit_dict:
+                self.add_edit({edit:edit_dict[edit]},task)
+
+
+class ecfRoot():
+
+    def get_base_name():
+        return re.search("(.*)\{.*\}",self.name()).group(1).strip()
+
+class ecfSuite(ecflow.Suite,ecfRoot):
+
+    def generate_folders(self,ecfhome):
+        folder_path = "%s/%s" %(ecfhome,self.name())
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
 #class ecfEvent()
 
-#class ecfTask(ecflow.Task):
-#
-#    def __init(self,name):
-#        self.ecftask = Task(name)
-#
-#    def get_node():
-#        return self.ecftask
-#
-#class ecfFamily(ecflow.Family):
-#
-#    def __init(self,name):
-#        self.ecffamily = Family(name)
-#
-#    def get_node():
-#        return self.ecffamily
-#
-#    def
+class ecfFamily(ecflow.Family,ecfRoot):
+
+    def generate_folders(self,ecfhome,suite,parents):
+        if parents:
+            folder_path = "%s/%s/%s/%s" %(ecfhome,suite,parents.replace('_','/'),self.name())
+        else:
+            folder_path = "%s/%s/%s" %(ecfhome,suite,self.name())
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+class ecfTask(ecflow.Task,ecfRoot):
+
+    def set_scriptrepo(self,repopath):
+        self.scriptrepo = repopath
+
+    def generate_ecflow_task(self,ecfhome,suite,parents):
+        script_name = "%s.ecf" % self.name()
+        ecfscript = None
+        if parents:
+            script_path = "%s/%s/%s/%s.ecf" %(ecfhome,suite,parents.replace('_','/'),script_name)
+        else:
+            script_path = "%s/%s/%s.ecf" %(ecfhome,suite,script_name)
+        for root,dirs,files in os.walk(self.scriptrepo):
+            if script_name in files and ecfscript is None:
+                ecfscript = os.path.join(root, script_name)
+            elif script_name in files:
+                print("More than one script named %s. Using the first one found." % script_name )
+        if ecfscript is not None:
+            shutil.copyfile(ecfscript, script_path, follow_symlinks=True)
+        else:
+            raise Exception("Could not find the script %s. Exiting build." % script_name)
--- workflow_generator/ecflow_setup/ecflow_setup.py
+++ workflow_generator/ecflow_setup/ecflow_setup.py
@@ -40,21 +40,41 @@ class Ecflowsetup:
         base_ecflowconfig = load_ecflow_config('%s' % args.ecflow_config )
         self.ecfconf = update_ecflow_config(base_ecflowconfig,env_configs)
 
+        self.ecfhome = env_configs['base']['ECFgfs']
+
+        if 'scriptrepo' in self.ecfconf.keys():
+            self.env_configs['base']['scriptrepo'] = self.ecfconf['scriptrepo']
+        elif 'scriptrepo' not in self.env_configs['base'].keys():
+            self.env_configs['base']['scriptrepo'] = "%s/scripts" % self.ecfhome
+        self.scriptrepo = self.env_configs['base']['scriptrepo']
+
+        # Setup the default edits from the environment
+        self.environment_edits = [
+            'ACCOUNT',
+            'queue',
+            'machine',
+            'RUN_ENVIR',
+        ]
+
     def generate_workflow(self):
         # Add in extern headers
         self.process_definition_header()
 
         # Process each of the suites
         for suite in self.ecfconf['suites'].keys():
-            new_suite = Ecflowsuite(suite,self.ecfconf,self.env_configs)
-            new_suite.add_suite_edits()
-            self.setup_datastructure(new_suite)
-            self.DEFS += new_suite.get_suite()
+            if suite not in {'externs','edits'}:
+                new_suite = Ecflowsuite(suite,self.env_configs['base']['ECFgfs'])
+                self.add_environment_edits(new_suite)
+                self.add_suite_edits(new_suite)
+                self.add_families(new_suite,self.ecfconf['suites'][suite]['nodes'])
+                self.add_tasks_and_edits(new_suite,self.ecfconf['suites'][suite]['nodes'])
+                self.add_triggers_and_events(new_suite,self.ecfconf['suites'][suite]['nodes'])
+                self.DEFS += new_suite.get_suite()
+
 
     def raiseException(self,e):
         print(e)
         sys.exit(1)
-
 
     def save(self):
         print("Saving definition File")
@@ -63,88 +83,113 @@ class Ecflowsetup:
         print(self.DEFS.check())
         print(self.DEFS)
 
-    def setup_datastructure(self,suite):
-        ecfhome = "%s/scripts" % self.env_configs['base']['ECFgfs']
+    def add_environment_edits(self,suite):
 
-        def find(name, path):
-            for root, dirs, files in os.walk(path):
-                if name in files:
-                    return root.replace(ecfhome,'')
+        # Add in the ECF Home and ECF Include edits.
+        suite.add_edit({'ECF_HOME':self.ecfhome, 'ECF_INCLUDE':self.ecfhome})
 
-        # Setup the family and task structure
-        for job in self.ecfconf['jobs']:
-            if re.search(r"\{.*\}",job):
-                loop_check = re.search("\{(.*)\}",job).group(1).strip()
-                if isinstance(loop_check,str) and 'env.' in loop_check:
-                    if loop_check.split('.')[1] in os.environ:
-                        loop = os.environ[loop_check.split('.')[1]]
-                    else:
-                        loop = self.env_configs['base'][loop_check.split('.')[1]]
-                elif loop_check.isdigit():
-                    loop = int(loop_check)
-                else:
-                    loop = 0
-                for counter in range(loop):
-                    job_base_name = re.search("(.*)\{.*\}",job).group(1).strip()
-                    job_name = "%s%03d" %( job_base_name, counter )
-                    familypath = find("%s.ecf" % job_name, "%s/%s" %(ecfhome,suite.get_suite().name()))
-                    families = familypath.split('/')[2:]
-                    for family in families:
-                        parents = ('_'.join(families[:families.index(family)]))
-                        suite.add_family(family,parents)
-                    suite.add_task(job_name,'_'.join(familypath.split('/')[2:]))
-            else:
-                familypath = find("%s.ecf" % job, "%s/%s" %(ecfhome,suite.get_suite().name()))
-                families = familypath.split('/')[2:]
-                for family in families:
-                    parents = ('_'.join(families[:families.index(family)]))
-                    suite.add_family(family,parents)
-                suite.add_task(job,'_'.join(familypath.split('/')[2:]))
+        # Add in the edits for the environment.
+        for edit in self.environment_edits:
+            edit = edit.upper()
+            if (
+                edit in self.env_configs['base'].keys() and
+                self.env_configs['base'][edit] is not None
+                ):
+                edit_dict = {edit : self.env_configs['base'][edit]}
+            elif (
+                edit.lower() in self.env_configs['base'].keys() and
+                self.env_configs['base'][edit.lower()] is not None
+            ):
+                edit_dict = {edit : self.env_configs['base'][edit.lower()]}
+            suite.add_edit(edit_dict)
 
-        # Add in triggers. We have to run through this twice because in creating
-        # the references, some tasks might not be setup so we need them all setup
-        # before adding in triggers or events.
-        for job in self.ecfconf['jobs']:
-            if re.search("\{.*\}",job):
-                loop_check = re.search("\{(.*)\}",job).group(1).strip()
-                if isinstance(loop_check,str) and 'env.' in loop_check:
-                    if loop_check.split('.')[1] in os.environ:
-                        loop = os.environ[loop_check.split('.')[1]]
-                    else:
-                        loop = self.env_configs['base'][loop_check.split('.')[1]]
-                elif loop_check.isdigit():
-                    loop = int(loop_check)
-                else:
-                    loop = 1
-                for counter in range(loop):
-                    if isinstance(self.ecfconf['jobs'][job],dict):
-                        job_base_name = re.search("(.*)\{.*\}",job).group(1).strip()
-                        job_name = "%s%03d" %( job_base_name, counter )
-                        if 'triggers' in self.ecfconf['jobs'][job].keys():
-                            suite.add_trigger(job_name,self.ecfconf['jobs'][job]['triggers'],counter)
-                        if 'events' in self.ecfconf['jobs'][job].keys():
-                            suite.add_event(job_name,self.ecfconf['jobs'][job]['events'])
-                        if 'edits' in self.ecfconf['jobs'][job].keys():
-                            suite.add_edit(job_name,self.ecfconf['jobs'][job]['edits'],counter)
+    def add_suite_edits(self,suite):
 
-            else:
-                if isinstance(self.ecfconf['jobs'][job],dict):
-                    if 'triggers' in self.ecfconf['jobs'][job].keys():
-                        suite.add_trigger(job,self.ecfconf['jobs'][job]['triggers'])
-                    if 'events' in self.ecfconf['jobs'][job].keys():
-                        suite.add_event(job,self.ecfconf['jobs'][job]['events'])
-                    if 'edits' in self.ecfconf['jobs'][job].keys():
-                        suite.add_edit(job,self.ecfconf['jobs'][job]['edits'])
+        # Baseline edits
+        if 'edits' in self.ecfconf['suites'].keys():
+            suite.add_edit(self.ecfconf['suites']['edits'])
+
+        # Setup sutite specific edits
+        suite_name = suite.get_suite_name()
+        if (
+            type(self.ecfconf['suites'][suite_name]) is dict and
+            'edits' in self.ecfconf['suites'][suite_name].keys()
+        ):
+            suite.add_edit(self.ecfconf['suites'][suite_name]['edits'])
 
     def process_definition_header(self):
         if 'externs' in self.ecfconf.keys():
             for extern in self.ecfconf['externs']:
                 self.DEFS.add_extern(extern)
 
+    def invalid_node():
+        raise Exception("Node is not definied")
+
+    def add_families(self,suite,nodes,parents=None):
+        for item in nodes.keys():
+            if ( isinstance(nodes[item],dict) and
+                item not in {'edits','tasks'} ):
+                suite.add_family(item,parents)
+                if parents:
+                    family_path = "%s_%s" %( parents, item)
+                else:
+                    family_path = item
+                if (isinstance(nodes[item],dict) and
+                    'edits' in nodes[item].keys() and
+                    isinstance(nodes[item]['edits'],dict) ):
+                    suite.add_edit(nodes[item]['edits'],family_path)
+                self.add_families(suite,nodes[item],family_path)
+
+    def add_tasks_and_edits(self,suite,nodes,parents=None):
+        for item in nodes.keys():
+            if ( isinstance(nodes[item],dict) and
+                item == 'tasks' ):
+                for task in nodes['tasks'].keys():
+                    if (isinstance(nodes['tasks'][task],dict) and
+                        'template' in nodes['tasks'][task].keys() ):
+                        task_template = nodes['tasks'][task]['template']
+                    else:
+                        task_template = None
+                    updated_task = find_env_param(task,'env.',self.env_configs)
+                    suite.add_task(updated_task,parents,self.scriptrepo,task_template)
+                    if (isinstance(nodes['tasks'][task],dict) and
+                        'edits' in nodes['tasks'][task].keys() and
+                        isinstance(nodes['tasks'][task]['edits'],dict) ):
+                        suite.add_task_edits(updated_task,nodes['tasks'][task]['edits'])
+            elif ( isinstance(nodes[item],dict) and
+                item != 'edits' ):
+                if parents:
+                    family_path = "%s_%s" %( parents, item)
+                else:
+                    family_path = item
+                self.add_tasks_and_edits(suite,nodes[item],family_path)
+
+    def add_triggers_and_events(self,suite,nodes):
+        print("adding triggers and events")
+
+
 def load_ecflow_config(configfile):
     with open(configfile, 'r') as file:
         base_config = yaml.safe_load(file)
     return base_config
+
+def find_env_param(node,value,envconfig):
+    new_node = node
+    if value in node:
+        variable_lookup = re.search(".*%s([\dA-Za-z_]*)" % value,node).group(1).strip()
+        if variable_lookup in os.environ:
+            if isinstance(os.environ[variable_lookup],datetime.datetime):
+                new_variable = os.environ[variable_lookup].strftime("%Y%m%d%H")
+            else:
+                new_variable = os.environ[variable_lookup]
+        else:
+            if isinstance(envconfig['base'][variable_lookup],datetime.datetime):
+                new_variable = envconfig['base'][variable_lookup].strftime("%Y%m%d%H")
+            else:
+                new_variable = envconfig['base'][variable_lookup]
+        search_key = re.search(r"(.*)(env\.[\dA-Za-z_]*)(.*)",node)
+        new_node = "%s %s %s" %(search_key.group(1),new_variable,search_key.group(3))
+    return new_node
 
 def update_ecflow_config(configfile,envconfig):
 
