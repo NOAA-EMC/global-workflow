147ef9be7f476829fe9cb6b6e8e91f1350aed79e
Kyle Nevins <kyle.nevins@noaa.gov>
2022-03-11 15:07:00 -0500

    Restructured classes based one new requirements
    Completion of the work by 3/11/22. Given the new requirements last
    week, this refactors the ecflow_setup script to handle the configuration
    file setup and then all the defintion pieces are handled in the
    ecflow_definitions. This also manages the looping structures much
    better and adds in some exceptions.

--- workflow_generator/ecflow_setup/ecflow_definitions.py
+++ workflow_generator/ecflow_setup/ecflow_definitions.py
@@ -2,16 +2,16 @@
 
 '''
     PROGRAM:
-        Create the ecFlow workflow
+        Manage the ecflow definition structures
     AUTHOR:
         Kyle Nevins
         kyle.nevins@noaa.gov
     FILE DEPENDENCIES:
-        2. config files for the parallel; e.g. config.base, config.fcst[.gfs], etc.
-        Without this dependency, the script will fail
+        1. None
     OUTPUT:
-        1. ecFlow definition file
-        2. ecFlow scripts for post processing
+        1. The ecFlow definition file
+        2. The ecFlow folder structure and scripts
+
 '''
 
 import sys
@@ -55,28 +55,26 @@ class Ecflowsuite:
     def get_suite_name(self):
         return self.ecfsuite.name()
 
-    def get_base_name(self,node):
-        return re.search("(.*)\{.*\}",node).group(1).strip()
-
-    def get_range(self,node,initial_count=0,increment=1,max_value=1):
-        count_string = re.search(".*\{(.*)\}.*",node).group(1).strip().split(',')
-        for item in count_string:
-            if 'initial_count' in item and initial_count==0:
-                initial_count = int(item.split(':')[1].strip())
-            if 'increment' in item and increment==1:
-                increment = int(item.split(':')[1].strip())
-            if item.strip().isdigit() and max_value==1:
-                max_value = int(item.strip())
-        max_value = max_value * increment
-        if initial_count > 0:
-            max_value += initial_count
-        return range(initial_count,max_value,increment)
-
     def add_edit(self,edit_dict,parent=None):
         if parent:
             self.ecfnodes[parent] += ecflow.Edit(edit_dict)
         else:
             self.ecfsuite += ecflow.Edit(edit_dict)
+
+    def add_event(self,event,parent=None):
+        if parent:
+            self.ecfnodes[parent] += ecflow.Event(event)
+
+    def add_trigger(self,trigger,parent,state=None,event=None):
+        if state is None and event is None:
+            self.ecfnodes[parent].add(ecflow.Trigger([self.ecfnodes[trigger]]))
+        elif state is not None and event is None:
+            trigger_path = self.ecfnodes[trigger].get_abs_node_path()
+            self.ecfnodes[parent].add(ecflow.Trigger("%s == %s" %( trigger_path, state)))
+        elif state is None and event is not None:
+            trigger_path = self.ecfnodes[trigger].get_abs_node_path()
+            self.ecfnodes[parent].add(ecflow.Trigger("%s:%s" %( trigger_path, event)))
+
 
     def add_family(self,family,parents=None):
         family_name = "%s_%s" % (parents, family) if parents else family
@@ -93,11 +91,10 @@ class Ecflowsuite:
             self.ecfsuite += self.ecfnodes[family_name]
 
     def add_task(self,task,parents,scriptrepo,template=None):
-        if re.search(r"\{.*\}",task):
-            task_base_name = self.get_base_name(task)
-            counter = self.get_range(task)
-            for task_number in counter:
-                task_name = "%s%03d" %( task_base_name, task_number )
+        taskNode = ecfTaskNode(task)
+        if taskNode.is_loop():
+            for task_number in taskNode.get_range():
+                task_name = "%s%03d" %( taskNode.get_base_name(), task_number )
                 if task_name not in self.ecfnodes.keys():
                     self.ecfnodes[task_name] = ecfTask(task_name)
                     self.ecfnodes[task_name].set_scriptrepo(scriptrepo)
@@ -114,31 +111,299 @@ class Ecflowsuite:
 
     def add_task_edits(self,task,edit_dict):
         edits_to_add = {}
-        if re.search(r"\{.*\}",task):
-            task_base_name = self.get_base_name(task)
-            task_counter = self.get_range(task)
+        taskNode = ecfTaskNode(task)
+        if taskNode.is_loop():
             loop_index = 0
-            for task_number in task_counter:
-                task_name = "%s%03d" %( task_base_name, task_number )
-                for edit in edit_dict:
-                    if re.search(r"\{.*\}",edit_dict[edit]):
-                        edit_base_name = self.get_base_name(edit_dict[edit])
-                        keywords = ['initial_count','increment']
-                        if any(match in edit_dict[edit] for match in keywords):
-                            total_tasks=len(task_counter)
-                            edit_range = self.get_range(edit_dict[edit],max_value=total_tasks)
+            for task_number in taskNode.get_range():
+                task_name = "%s%03d" %( taskNode.get_base_name(), task_number )
+                for edit, editvalue in edit_dict.items():
+                    editNodeValue = ecfEditNode(editvalue)
+                    if editNodeValue.is_loop():
+                        edit_base_name = editNodeValue.get_base_name()
+                        if editNodeValue.has_counter():
+                            total_tasks=len(taskNode.get_range())
+                            edit_range = editNodeValue.get_range(max_value=total_tasks)
                             edit_count = [*edit_range]
-                            edit_value = "%s%03d" %(edit_base_name, edit_count[loop_index])
+                            neweditvalue = "%s%03d" %(edit_base_name, edit_count[loop_index])
                         else:
-                            edit_value = "%s%03d" %(edit_base_name, task_number)
-                        self.add_edit({edit:edit_value},task_name)
+                            neweditvalue = "%s%03d" %(edit_base_name, task_number)
+                        self.add_edit({edit:neweditvalue},task_name)
                     else:
-                        self.add_edit({edit:edit_dict[edit]},task_name)
+                        self.add_edit({edit:editvalue},task_name)
                 loop_index+=1
         else:
             for edit in edit_dict:
                 self.add_edit({edit:edit_dict[edit]},task)
 
+    def add_task_events(self,task,events):
+        taskNode = ecfTaskNode(task)
+        if taskNode.is_loop():
+            for task_number in taskNode.get_range():
+                task_name = "%s%03d" %( taskNode.get_base_name(), task_number )
+                for event in events:
+                    eventNode = ecfEventNode(event)
+                    if eventNode.is_loop():
+                        event_base_name = self.get_base_name(event)
+                        if eventNode.has_counter():
+                            event_counter = self.get_range(event)
+                            for event_number in event_counter:
+                                event_name = "%s%03d" %( event_base_name, event_number )
+                                self.add_event(event_name,task_name)
+                        else:
+                            event_name = "%s%03d" %(event,task_number)
+                            self.add_event(event_name,task_name)
+                    else:
+                        self.add_event(event,task_name)
+        else:
+            for event in events:
+                eventNode = ecfEventNode(event)
+                if eventNode.has_counter():
+                    for event_number in eventNode.get_range():
+                        event_name = "%s%03d" %( eventNode.get_base_name(), event_number )
+                        self.add_event(event_name,task)
+                else:
+                    self.add_event(event,task)
+
+    def add_task_triggers(self,task,triggers):
+
+        def process_trigger(trigger_name,triggerTaskNode,task,task_loop_index=None,total_tasks=None,task_number=None):
+            if triggerTaskNode.has_state():
+                self.add_trigger(trigger_name,task,state=triggerTaskNode.get_state())
+            elif triggerTaskNode.has_event():
+                if triggerTaskNode.has_event_loop():
+                    if triggerTaskNode.has_event_max_value():
+                        for event_count in triggerTaskNode.get_event_range():
+                            event_name = "%s%03d" %( triggerTaskNode.get_event_base_name(), event_count)
+                            self.add_trigger(trigger_name,task,event=event_name)
+                    elif triggerTaskNode.has_event_counter():
+                        event_range = triggerTaskNode.get_event_range(max_value=total_tasks)
+                        event_count = [*event_range]
+                        event_name = "%s%03d" %(triggerTaskNode.get_event_base_name(),event_count[task_loop_index])
+                        self.add_trigger(trigger_name,task,event=event_name)
+                    else:
+                        event_name = "%s%03d" %(triggerTaskNode.get_event_base_name(),task_number)
+                        self.add_trigger(trigger_name,task,event=event_name)
+                else:
+                    self.add_trigger(trigger_name,task,event=triggerTaskNode.get_event())
+
+            else:
+                self.add_trigger(trigger_name,task)
+
+        taskNode = ecfTaskNode(task)
+        if taskNode.is_loop():
+            task_loop_index=0
+            for task_number in taskNode.get_range():
+                task_name = "%s%03d" %( taskNode.get_base_name(), task_number )
+                total_tasks=len(taskNode.get_range())
+                for trigger in triggers:
+                    triggerTaskNode = ecfTriggerNode(trigger)
+                    if triggerTaskNode.is_loop():
+                        if triggerTaskNode.has_max_value():
+                            for trigger_flag in triggerTaskNode.get_range():
+                                trigger_name = "%s%03d" %(triggerTaskNode.get_base_name(),trigger_flag)
+                                process_trigger(trigger_name,triggerTaskNode,task_name,task_loop_index,total_tasks,task_number)
+                        else:
+                            trigger_range = triggerTaskNode.get_range(max_value=total_tasks)
+                            trigger_count = [*trigger_range]
+                            trigger_name = "%s%03d" %(triggerTaskNode.get_base_name(),trigger_count[task_loop_index])
+                            process_trigger(trigger_name,triggerTaskNode,task_name,task_loop_index,total_tasks,task_number)
+                    else:
+                        process_trigger(triggerTaskNode.get_name(),triggerTaskNode,task_name,task_loop_index,total_tasks,task_number)
+                task_loop_index+=1
+        else:
+            for trigger in triggers:
+                triggerTaskNode = ecfTriggerNode(trigger)
+                if triggerTaskNode.is_loop():
+                    if triggerTaskNode.has_max_value():
+                        for trigger_flag in triggerTaskNode.get_range():
+                            trigger_name = "%s%03d" %(triggerTaskNode.get_base_name(),trigger_flag)
+                            process_trigger(trigger_name,triggerTaskNode,task)
+                    else:
+                        self.raiseException("Task: %s - Looping mechanism called without max value in a non looped task." % task)
+                else:
+                    process_trigger(triggerTaskNode.get_name(),triggerTaskNode,task)
+
+class ecfNode():
+
+    def __init__(self,ecfItem):
+        self.name = ecfItem
+
+    def get_name(self):
+        return self.name
+
+    def is_loop(self):
+        if re.search(r"\{.*\}",self.name):
+            return True
+        else:
+            return False
+
+    def get_base_name(self):
+        if re.search(r"\{.*\}",self.name):
+            return re.search("(.*)\{.*\}",self.name).group(1).strip()
+        else:
+            return node
+
+    def has_counter(self):
+        if re.search(r"\{.*\}",self.name):
+            count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+            for item in count_string:
+                if ('initial_count' in item or
+                    'increment' in item or
+                    item.strip().isdigit()):
+                    return True
+            return False
+
+    def has_initial_count(self):
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        for item in count_string:
+            if 'initial_count' in item:
+                return True
+        return False
+
+    def get_initial_count(self):
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        initial_count = 0
+        for item in count_string:
+            if 'initial_count' in item and initial_count==0:
+                initial_count = int(item.split(':')[1].strip())
+        return initial_count
+
+    def has_max_value(self):
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        for item in count_string:
+            if item.strip().isdigit():
+                return True
+        return False
+
+    def get_max_value(self):
+        max_value = 1
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        for item in count_string:
+            if item.strip().isdigit() and max_value==1:
+                max_value = int(item.strip())
+        return max_value
+
+    def get_increment(self):
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        for item in count_string:
+            if 'increment' in item:
+                return True
+        return False
+
+    def get_increment(self):
+        increment = 1
+        count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+        for item in count_string:
+            if 'increment' in item and increment==1:
+                increment = int(item.split(':')[1].strip())
+        return increment
+
+    def get_count_string(self):
+        return re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+
+    def get_range(self,initial_count=0,increment=1,max_value=1):
+        if re.search(r"\{.*\}",self.name):
+            count_string = re.search(".*\{(.*)\}.*",self.name).group(1).strip().split(',')
+            for item in count_string:
+                if 'initial_count' in item and initial_count==0:
+                    initial_count = int(item.split(':')[1].strip())
+                if 'increment' in item and increment==1:
+                    increment = int(item.split(':')[1].strip())
+                if item.strip().isdigit() and max_value==1:
+                    max_value = int(item.strip())
+            max_value = max_value * increment
+            if initial_count > 0:
+                max_value += initial_count
+            return range(initial_count,max_value,increment)
+        else:
+            return range(0,1,1)
+
+class ecfTaskNode(ecfNode):
+
+    def get_type(self):
+        return 'task'
+
+class ecfTriggerNode(ecfNode):
+
+    def __init__(self,ecfItem):
+        self.task_setup = ecfItem
+        self.name = ecfItem['task']
+
+    def get_type(self):
+        return 'trigger'
+
+    def get_state(self):
+        return self.state
+
+    def get_event(self):
+        return self.event_string
+
+    def has_state(self):
+        if 'state' in self.task_setup.keys():
+            self.state = self.task_setup['state']
+            return True
+        else:
+            return False
+
+    def has_event(self):
+        if 'event' in self.task_setup.keys():
+            self.event_string = self.task_setup['event']
+            return True
+        else:
+            return False
+
+    def has_event_max_value(self):
+        if re.search(r"\{.*\}",self.event_string):
+            count_string = re.search(".*\{(.*)\}.*",self.event_string).group(1).strip().split(',')
+            for item in count_string:
+                if item.strip().isdigit():
+                    return True
+        return False
+
+    def has_event_loop(self):
+        if re.search(r"\{.*\}",self.event_string):
+            return True
+        return False
+
+    def get_event_base_name(self):
+        if re.search(r"\{.*\}",self.event_string):
+            return re.search("(.*)\{.*\}",self.event_string).group(1).strip()
+        else:
+            return node
+
+    def has_event_counter(self):
+        if re.search(r"\{.*\}",self.event_string):
+            count_string = re.search(".*\{(.*)\}.*",self.event_string).group(1).strip().split(',')
+            for item in count_string:
+                if ('initial_count' in item or
+                    'increment' in item or
+                    item.strip().isdigit()):
+                    return True
+        return False
+
+    def get_event_range(self,initial_count=0,increment=1,max_value=1):
+        count_string = re.search(".*\{(.*)\}.*",self.event_string).group(1).strip().split(',')
+        for item in count_string:
+            if 'initial_count' in item and initial_count==0:
+                initial_count = int(item.split(':')[1].strip())
+            if 'increment' in item and increment==1:
+                increment = int(item.split(':')[1].strip())
+            if item.strip().isdigit() and max_value==1:
+                max_value = int(item.strip())
+        max_value = max_value * increment
+        if initial_count > 0:
+            max_value += initial_count
+        return range(initial_count,max_value,increment)
+
+class ecfEventNode(ecfNode):
+
+    def get_type(self):
+        return 'edit'
+
+class ecfEditNode(ecfNode):
+
+    def get_type(self):
+        return 'edit'
 
 class ecfRoot():
 
@@ -151,8 +416,6 @@ class ecfSuite(ecflow.Suite,ecfRoot):
         folder_path = "%s/%s" %(ecfhome,self.name())
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-
-#class ecfEvent()
 
 class ecfFamily(ecflow.Family,ecfRoot):
 
