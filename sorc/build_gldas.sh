460957327530ce9abadcfeb83a992914cff16654
lin.gan <lin.gan@noaa.gov>
2022-05-09 14:29:40 +0000

    Remove Hera MOD_PATH from build scripts Implemented in: build_gfs_wafs.sh build_gldas.sh build_gsi.sh build_ncep_post.sh
    REF: 298

--- sorc/build_gldas.sh
+++ sorc/build_gldas.sh
@@ -6,7 +6,7 @@ cwd=$(pwd)
 
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
-  export MOD_PATH=/scratch3/NCEPDEV/nwprod/lib/modulefiles
+  echo "Using default MODULEPATH"
 else
   export MOD_PATH=${cwd}/lib/modulefiles
 fi
