45b199eca7c192e36567a91a04148f253273857b
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2022-06-13 15:40:43 +0000

    Replace --aerosols with new apps
    Removes the `--aerosols` option for setup_expt and replaces it with new apps
    for aerosols to match the idiom for other components. Currently, ATMA and
    S2SWA are supported.

--- ush/rocoto/setup_workflow_fcstonly.py
+++ ush/rocoto/setup_workflow_fcstonly.py
@@ -257,7 +257,7 @@ def get_workflow(dict_configs, cdump='gdas'):
 
     tasks = []
 
-    if app in ['S2S', 'S2SW']:
+    if app[0:3] in ['S2S']:
         # Copy prototype ICs
         deps = []
         base_cplic = dict_configs['coupled_ic']['BASE_CPLIC']
@@ -357,7 +357,7 @@ def get_workflow(dict_configs, cdump='gdas'):
         deps = []
         dep_dict = {'type': 'task', 'name': f'{cdump}waveinit'}
         deps.append(rocoto.add_dependency(dep_dict))
-        if app not in ['S2S', 'S2SW']:
+        if app[0:3] not in ['S2S']:
             dep_dict = {'type': 'task', 'name': f'{cdump}init'}
             deps.append(rocoto.add_dependency(dep_dict))
         dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
