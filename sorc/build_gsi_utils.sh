e3681c631f5912eb79ff38a10ebd2bbab0b53017
AndrewEichmann-NOAA <Andrew.Eichmann@noaa.gov>
2022-07-26 08:27:25 -0500

    backed out changes to build_gsi_utils.sh because it expects allowances for CRTM in gsi-utils

--- sorc/build_gsi_utils.sh
+++ sorc/build_gsi_utils.sh
@@ -21,17 +21,9 @@ while getopts ":dov" option; do
 done
 shift $((OPTIND-1))
 
-UTIL_OPTS="-DBUILD_UTIL_ENKF_GFS=ON -DBUILD_UTIL_NCIO=ON"
-GSIENKF_INSTALL_PREFIX="${cwd}/gsi_enkf.fd/install"
-if [[ -d "${GSIENKF_INSTALL_PREFIX}" ]]; then
-  CMAKE_OPTS="-Dgsi_ROOT=${GSIENKF_INSTALL_PREFIX} -Denkf_ROOT=${GSIENKF_INSTALL_PREFIX}"
-  UTIL_OPTS+=" -DBUILD_UTIL_EFSOI=ON"
-fi
-
 BUILD_TYPE=${BUILD_TYPE:-"Release"} \
 BUILD_VERBOSE=${BUILD_VERBOSE:-"NO"} \
-CMAKE_OPTS="${CMAKE_OPTS:-}" \
-UTIL_OPTS="${UTIL_OPTS:-}" \
+UTIL_OPTS="-DBUILD_UTIL_ENKF_GFS=ON -DBUILD_UTIL_NCIO=ON" \
 ${cwd}/gsi_utils.fd/ush/build.sh
 
 exit
