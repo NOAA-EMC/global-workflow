b415d64b77e697dd56dda8d789c60393d96c7e3b
kate.friedman <kate.friedman@noaa.gov>
2022-07-28 19:49:51 +0000

    Create WCOSS2 build modulefiles
    - Create modulefile.ww3.wcoss2.lua and workflow_utils.wcoss2.lua with
    version variables within for building WW3 and workflow_utils.
    
    Refs: #419

---
+++ modulefiles/modulefile.ww3.wcoss2.lua
@@ -0,0 +1,20 @@
+help([[
+Build environment for WW3 on WCOSS2 
+]])
+
+load(pathJoin("PrgEnv-intel", os.getenv("PrgEnv_intel_ver")))
+load(pathJoin("craype", os.getenv("craype_ver")))
+load(pathJoin("intel", os.getenv("intel_ver")))
+load(pathJoin("cray-mpich", os.getenv("cray_mpich_ver")))
+
+load(pathJoin("cmake", os.getenv("cmake_ver")))
+
+load(pathJoin("jasper", os.getenv("jasper_ver")))
+load(pathJoin("zlib", os.getenv("zlib_ver")))
+load(pathJoin("png", os.getenv("png_ver")))
+
+load(pathJoin("bacio", os.getenv("bacio_ver")))
+load(pathJoin("g2", os.getenv("g2_ver")))
+
+load(pathJoin("hdf5", os.getenv("hdf5_ver")))
+load(pathJoin("netcdf", os.getenv("netcdf_ver")))
