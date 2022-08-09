NOAA-EMC/global-workflow

commit e480093446797e556bc1371f9f80610a7c9a6d4b

Add preamble, convert to bash, and remove env (#929)
This is the first in a wave of commits to improve and standardize the scripts within global workflow. In this commit, all scripts run during execution are converted to bash and a preamble is added to every script that is not sourced by another script.

Every script executed during a forecast cycle is converted to bash. This was mostly straightforward, though there were a couple Korne-shell conventions (primarily using `typeset` to format strings) that had to be replaced with bash-compatable alternatives like `printf`. This in turn required a few modification to prevent zero-padded numbers from being treated as octals (other may have been pre-existing bugs).

The preamble contains a number of feature to standardize code and improve script flow and debugging.

- First, it uses two variables, `$STRICT` and `$TRACE` to control the behavior of `set`. When `$STRICT` is `"YES"`, error on undefined variables (`set -u`) and exit on non-zero return (`set -e`) are turned on. When `$TRACE` is `"YES"`, command trace (`set -x`) is turned on and a useful string is set to `$PS4` that gives the name and line number of the script. Both `$STRICT` and `$TRACE` default to `"YES"`. They also set up commands, `$ERR_EXIT_ON` and `$TRACE_ON`, that will restore the setting of each in the event a script needs to temporarily turn them off.

- Second, the preamble sets up primative timing of scripts using Posix `date`.

- Third, it echos the script is beginning and at what time.

- Finally, it also establishes a postamble function and sets it to run as a trap of EXIT. The postamble will use the end time to calculate the run time of the script, then print that the script has ended at what time, how long has elapsed, and the exit code. By setting this up as a trap instead of just calling it at the end of the script, it ensures the postamble is called even if the script exits early because there is an error.

ush/fv3gfs_downstream_nems.sh
@@ -1,5 +1,4 @@
-#!/bin/ksh
-set -x
+#! /usr/bin/env bash
 
 #-----------------------------------------------------------------------
 #-Hui-Ya Chuang, January 2014:  First version.
@@ -31,8 +30,7 @@ set -x
 #  1. Modify sea icea cover via land-sea mask.
 #-----------------------------------------------------------------------
 
-
-echo "!!!!!CREATING $RUN DOWNSTREAM PRODUCTS FOR FH = $FH !!!!!!"
+source "$HOMEgfs/ush/preamble.sh" "$FH"
 
 export downset=${downset:-1}
 export DATA=${DATA:-/ptmpd2/$LOGNAME/test}
@@ -130,16 +128,27 @@ while [ $nset -le $totalset ]; do
     # if final record of each piece is ugrd, add vgrd
     # copygb will only interpolate u and v together
     #$WGRIB2 -d $end $tmpfile |grep -i ugrd
-    $WGRIB2 -d $end $tmpfile |egrep -i "ugrd|ustm|uflx|u-gwd"
+    # grep returns 1 if no match is found, so temporarily turn off exit on non-zero rc
+    set +e
+    $WGRIB2 -d $end $tmpfile | egrep -i "ugrd|ustm|uflx|u-gwd"
     export rc=$?
+    ${ERR_EXIT_ON:-set -eu}
     if [[ $rc -eq 0 ]] ; then
       export end=$(expr ${end} + 1)
+    elif [[ $rc -gt 1 ]]; then
+      echo "FATAL: WGRIB2 failed with error code ${rc}"
+      exit $rc
     fi
-    # if final record is land, add next record icec
-    $WGRIB2 -d $end $tmpfile |egrep -i "land"
+    # if final record is land, add next record icec 
+    set +e
+    $WGRIB2 -d $end $tmpfile | egrep -i "land"
     export rc=$?
+    ${ERR_EXIT_ON:-set -eu}
     if [[ $rc -eq 0 ]] ; then
       export end=$(expr ${end} + 1)
+    elif [[ $rc -gt 1 ]]; then
+      echo "FATAL: WGRIB2 failed with error code ${rc}"
+      exit $rc
     fi
     if [ $iproc -eq $nproc ]; then
       export end=$ncount
@@ -176,6 +185,7 @@ while [ $nset -le $totalset ]; do
       echo "$nm $line" >> $DATA/poescript_srun
       nm=$((nm+1))
     done
+    nm=$(wc -l < $DATA/poescript_srun)
     ${launcher:-"srun --export=ALL"} -n $nm --multi-prog $DATA/poescript_srun
   else
     $launcher
@@ -282,9 +292,5 @@ while [ $nset -le $totalset ]; do
 
   export nset=$(expr $nset + 1 )
 done
-
-echo "!!!!!!CREATION OF SELECT $RUN DOWNSTREAM PRODUCTS COMPLETED FOR FHR = $FH !!!!!!!"
-#---------------------------------------------------------------
-
 
 exit 0

commit 96b9b5f18aa1cd53647f096fa0c1a066db923b56

Fix post comparison with zero-padded numbers
An error was introduced with PR #929 that was causing 0p50 and 1p00 grib
files to not be produced due to an error in comparing a zero-padded string.
Switching to arithmatic comparison solves the issue. Also updated the method
of the zero-padding to the preferred printf since the code was in close
proximity.

ush/fv3gfs_downstream_nems.sh
@@ -77,10 +77,8 @@ elif [ $FH -eq 0 ] ; then
 else
   export paramlist=${paramlist:-$PARMpost/global_1x1_paramlist_g2}
   export paramlistb=${paramlistb:-$PARMpost/global_master-catchup_parmlist_g2}
-  export fhr3=$(expr $FH + 0 )
-  if [ $fhr3 -lt 100 ]; then export fhr3="0$fhr3"; fi
-  if [ $fhr3 -lt 10 ];  then export fhr3="0$fhr3"; fi
-  if [ $fhr3%${FHOUT_PGB} -eq 0 ]; then
+  export fhr3=$(printf "%03d" ${FH})
+  if (( fhr3%FHOUT_PGB == 0 )); then
     export PGBS=YES
   fi
 fi