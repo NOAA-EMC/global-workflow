91fac1a026c75d4dd24ff7fe7ef0bc83872af7dd
Cory Martin <cory.r.martin@noaa.gov>
2022-08-19 14:27:44 -0500

    Dummy rocoto jobs

---
+++ jobs/rocoto/aeroanlrun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+###############################################################
+# Source FV3GFS workflow modules
+. $HOMEgfs/ush/load_fv3gfs_modules.sh
+status=$?
+[[ $status -ne 0 ]] && exit $status
+
+###############################################################
+echo "Do nothing for now"
