692e3fc891e32d7b7d1a55aca63bb43a3b8ad9b0
Cory Martin <cory.r.martin@noaa.gov>
2022-08-23 16:13:38 -0400

    Add in stubs for aerosol DA tasks + bugfix for setup_expt where cycled and ATMA are used (#990)
    This PR adds in stubs for aerosol DA tasks (jobs/rocoto shell scripts), the ability for setup_expt and setup_xml to include three aerosol DA related tasks, and in the process of adding this capability, fixes a bug in which the combination of ATMA and cycled revealed that @property was being used incorrectly.
    
    Fixes #981

--- workflow/applications.py
+++ workflow/applications.py
@@ -217,6 +217,9 @@ class AppConfig:
         if self.do_wafs:
             configs += ['wafs', 'wafsgrib2', 'wafsblending', 'wafsgcip', 'wafsgrib20p25', 'wafsblending0p25']
 
+        if self.do_aero:
+            configs += ['aeroanlinit', 'aeroanlrun', 'aeroanlfinal']
+
         return configs
 
     @property
@@ -347,6 +350,9 @@ class AppConfig:
             gdas_gfs_common_tasks_before_fcst += ['anal']
 
         gdas_gfs_common_tasks_before_fcst += ['sfcanl', 'analcalc']
+
+        if self.do_aero:
+            gdas_gfs_common_tasks_before_fcst += ['aeroanlinit', 'aeroanlrun', 'aeroanlfinal']
 
         gldas_tasks = ['gldas']
         wave_prep_tasks = ['waveinit', 'waveprep']
