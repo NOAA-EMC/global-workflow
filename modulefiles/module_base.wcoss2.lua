7a23a1008c5de8d2e436419097d191499c51ffdf
kate.friedman <kate.friedman@noaa.gov>
2022-08-26 17:54:59 +0000

    Add para ncdiag module load to module_base.wcoss2.lua
    - Similar to hack in GSI, load the para install of the ncdiag module on
    WCOSS2 for use during runtime.
    
    Refs #419

--- modulefiles/module_base.wcoss2.lua
+++ modulefiles/module_base.wcoss2.lua
@@ -33,6 +33,12 @@ load(pathJoin("crtm", "2.3.0"))
 load(pathJoin("g2tmpl", "1.9.1"))
 load(pathJoin("wgrib2", "2.0.7"))
 
+pushenv("HPC_OPT", "/apps/ops/para/libs")
+prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
+prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
+
+load("ncdiag/1.0.0")
+
 prepend_path("MODULEPATH", pathJoin("/lfs/h2/emc/global/save/emc.global/git/prepobs/v1.0.1/modulefiles"))
 load(pathJoin("prepobs", "1.0.1"))
 
