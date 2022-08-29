91c37cde5470503e19803286f8fc406aecc44b4b
Cory Martin <cory.r.martin@noaa.gov>
2022-08-29 12:29:02 -0500

    Dummy ex-scripts

---
+++ scripts/exgdas_global_aero_analysis_finalize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+################################################################################
+#  UNIX Script Documentation Block
+#                      .                                             .
+# Script name:         exgdas_global_aero_analysis_finalize.py
+# Script description:  Finalizes runtime directory for global aerosol analysis
+#
+# Author: Cory Martin      Org: NCEP/EMC     Date: 2022-8-29
+#
+# Abstract: This script cleans up the runtime directory and moves/archives
+#           necessary output files ffrom FV3-JEDI executable(s)
+#           produced from a UFS Global Aerosol Analysis.
+#
+# $Id$
+#
+# Attributes:
+#   Language: Python3
+#
+################################################################################
+
+print('For now, finalize does nothing!')
---
+++ scripts/exgdas_global_aero_analysis_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "For now, run does nothing"
