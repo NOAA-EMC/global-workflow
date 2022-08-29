81858d7c850a180558c4ed4155914b73c3343653
Cory Martin <cory.r.martin@noaa.gov>
2022-08-29 11:54:50 -0500

    Start of initialize script

---
+++ scripts/exgdas_global_aero_analysis_initialize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+################################################################################
+#  UNIX Script Documentation Block
+#                      .                                             .
+# Script name:         exgdas_global_aero_analysis_initialize.py
+# Script description:  Initializes runtime directory for global aerosol analysis
+#
+# Author: Cory Martin      Org: NCEP/EMC     Date: 2022-8-29
+#
+# Abstract: This script sets up the runtime directory and stages
+#           necessary input files for FV3-JEDI executable(s) needed
+#           to produce a UFS Global Aerosol Analysis.
+#
+# $Id$
+#
+# Attributes:
+#   Language: Python3
+#
+################################################################################
+
+
