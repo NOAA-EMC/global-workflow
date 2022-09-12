1e60ef756bd9e16e605e7ef25485406a4cad1190
Cory Martin <cory.r.martin@noaa.gov>
2022-09-12 13:23:56 -0500

    Update finalize ex script

--- scripts/exgdas_global_aero_analysis_finalize.py
+++ scripts/exgdas_global_aero_analysis_finalize.py
@@ -18,4 +18,28 @@
 #
 ################################################################################
 
-print('For now, finalize does nothing!')
+# import os and sys to add ush to path
+import logging
+import os
+import shutil
+import sys
+
+# set up logger
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+# get absolute path of ush/ directory either from env or relative to this file
+my_dir = os.path.dirname(__file__)
+my_home = os.path.dirname(os.path.dirname(my_dir))
+sys.path.append(os.path.join(os.getenv('HOMEgfs', my_home), 'ush'))
+logging.info(f"sys.path={sys.path}")
+
+# import UFSDA utilities
+import ufsda
+
+# import GFS workflow module
+import gfs.task.analysis
+
+if __name__ == '__main__':
+
+    AeroAnl = gfs.task.analysis.AerosolAnalysis(os.environ)
+    AeroAnl.finalize()
