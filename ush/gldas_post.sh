2e810c36a95961615c1db1b4ef29d3b591595cb2
kate.friedman <kate.friedman@noaa.gov>
2022-09-13 15:05:58 -0500

    Address linter warnings in ush/gldas_archive.sh and ush/gldas_post.sh
    Refs #1014

--- ush/gldas_post.sh
+++ ush/gldas_post.sh
@@ -2,31 +2,33 @@
 
 source "${HOMEgfs:?}/ush/preamble.sh"
 
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 ]]; then
   echo "usage: $0 gldas.gbin gdas.sfcanl"
   err_exit 99
 fi
 
-cd ${RUNDIR}
+cd "${RUNDIR:?}" || exit
 
 export pgm=gldas_post
-  . prep_step
+# shellcheck disable=SC1091
+. prep_step
+# shellcheck disable=
 
 gbin=$1
 sfcanl=$2
 
 rm -f fort.11 fort.12 fort.22
-cp ${gbin} fort.11
-cp ${sfcanl} fort.12
+cp "${gbin}" fort.11
+cp "${sfcanl}" fort.12
 
-${EXECgldas}/gldas_post >>${pgmout} 2>errfile
+"${EXECgldas:?}/gldas_post" >>"${pgmout:?}" 2>errfile
 export err=$?; err_chk
 
 cp fort.22 ./gldas.nemsio
-cp  fort.22 ${sfcanl}.gldas
+cp  fort.22 "${sfcanl}.gldas"
 rm -f fort.11 fort.12 fort.22
 
-echo ${sfcanl}.gldas
+echo "${sfcanl}.gldas"
 
 exit $?
 
