dd6651fe0dca7f11401c5a8a5c83e853b2c271f3
kate.friedman <kate.friedman@noaa.gov>
2022-09-13 12:22:37 -0500

    Address SC2086 shellcheck warnings in GLDAS ush scripts
    Refs #1014

--- ush/gldas_get_data.sh
+++ ush/gldas_get_data.sh
@@ -8,7 +8,7 @@ source "${HOMEgfs:?}/ush/preamble.sh"
 bdate=$1
 edate=$2
 
-if [ ${machine} = "HERA" ] || [ ${machine} = "WCOSS2" ]; then
+if [ "${machine}" = "HERA" ] || [ "${machine}" = "WCOSS2" ]; then
   touch ./cfile
 fi
 
@@ -23,39 +23,39 @@ cycint=${assim_freq:-6}
 # get gdas flux files to force gldas.
 # CPC precipitation is from 12z to 12z. One more day of gdas data is
 # needed to disaggregate daily CPC precipitation values to hourly values
-cdate=$(${NDATE} -12 ${bdate})
+cdate=$(${NDATE} -12 "${bdate}")
 
 iter=0
 
 #-------------------------------
-while [ ${cdate} -lt ${edate} ]; do
+while [ "${cdate}" -lt "${edate}" ]; do
 #-------------------------------
-  ymd=$(echo ${cdate} |cut -c 1-8)
-  cyc=$(echo ${cdate} |cut -c 9-10)
-  [[ ! -d ${fpath}/gdas.${ymd} ]] && mkdir -p ${fpath}/gdas.${ymd}
-  [[ ! -d ${gpath}/gdas.${ymd} ]] && mkdir -p ${gpath}/gdas.${ymd}
+  ymd=$(echo "${cdate}" |cut -c 1-8)
+  cyc=$(echo "${cdate}" |cut -c 9-10)
+  [[ ! -d ${fpath}/gdas.${ymd} ]] && mkdir -p "${fpath}/gdas.${ymd}"
+  [[ ! -d ${gpath}/gdas.${ymd} ]] && mkdir -p "${gpath}/gdas.${ymd}"
 
 f=1
-while [ ${f} -le ${cycint} ]; do
+while [ ${f} -le "${cycint}" ]; do
   rflux=${COMINgdas}/gdas.${ymd}/${cyc}/${COMPONENT}/gdas.t${cyc}z.sfluxgrbf00${f}.grib2
   fflux=${fpath}/gdas.${ymd}/gdas.t${cyc}z.sfluxgrbf0${f}.grib2
   gflux=${gpath}/gdas.${ymd}/gdas1.t${cyc}z.sfluxgrbf0${f}
-  if [ ! -s ${rflux} ];then
+  if [ ! -s "${rflux}" ];then
      echo "WARNING: GLDAS MISSING ${rflux}, WILL NOT RUN."
      exit 2
   fi
-  rm -f ${fflux} ${gflux}
-  touch ${fflux} ${gflux}
+  rm -f "${fflux}" "${gflux}"
+  touch "${fflux}" "${gflux}"
 
   fcsty=anl
   if [ ${f} -ge 1 ]; then fcsty=fcst; fi
 
-  if [ ${machine} = "WCOSS2" ]; then
+  if [ "${machine}" = "WCOSS2" ]; then
     echo "${USHgldas}/gldas_process_data.sh ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
-  elif [ ${machine} = "HERA" ]; then
+  elif [ "${machine}" = "HERA" ]; then
     echo "${iter} ${USHgldas}/gldas_process_data.sh ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
   else
-    ${USHgldas}/gldas_process_data.sh ${rflux} ${fcsty} ${fflux} ${gflux} ${f}
+    "${USHgldas}/gldas_process_data.sh ${rflux} ${fcsty} ${fflux} ${gflux} ${f}"
   fi
 
   iter=$((iter+1))
@@ -63,11 +63,11 @@ while [ ${f} -le ${cycint} ]; do
 done
 
 #-------------------------------
-  cdate=$(${NDATE} +${cycint} ${cdate})
+  cdate=$(${NDATE} +"${cycint}" "${cdate}")
 done
 #-------------------------------
 
-if [ ${machine} = "HERA" ] || [ ${machine} = "WCOSS2" ]; then
+if [ "${machine}" = "HERA" ] || [ "${machine}" = "WCOSS2" ]; then
   ${APRUN_GLDAS_DATA_PROC} ./cfile
 fi
 
--- ush/gldas_liscrd.sh
+++ ush/gldas_liscrd.sh
@@ -3,7 +3,7 @@
 source "${HOMEgfs:?}/ush/preamble.sh"
 
 if [ $# -lt 3 ]; then
-  echo usage $0 yyyymmddhh1 yyyymmddhh2 126/382/574/1534
+  echo usage "$0" yyyymmddhh1 yyyymmddhh2 126/382/574/1534
   exit $?
 fi
 
@@ -11,21 +11,21 @@ date1=$1
 date2=$2
 grid=$3
 
-export yyyy1=$(echo ${date1} | cut -c 1-4)
-export   mm1=$(echo ${date1} | cut -c 5-6)
-export   dd1=$(echo ${date1} | cut -c 7-8)
-export   hh1=$(echo ${date1} | cut -c 9-10)
-export yyyy2=$(echo ${date2} | cut -c 1-4)
-export   mm2=$(echo ${date2} | cut -c 5-6)
-export   dd2=$(echo ${date2} | cut -c 7-8)
-export   hh2=$(echo ${date2} | cut -c 9-10)
+export yyyy1=$(echo "${date1}" | cut -c 1-4)
+export   mm1=$(echo "${date1}" | cut -c 5-6)
+export   dd1=$(echo "${date1}" | cut -c 7-8)
+export   hh1=$(echo "${date1}" | cut -c 9-10)
+export yyyy2=$(echo "${date2}" | cut -c 1-4)
+export   mm2=$(echo "${date2}" | cut -c 5-6)
+export   dd2=$(echo "${date2}" | cut -c 7-8)
+export   hh2=$(echo "${date2}" | cut -c 9-10)
 export  grid=${grid}
 export PARM_LM=${PARMgldas}
 export LISCARD=lis.crd
 
 rm -f ${LISCARD}
 touch ${LISCARD}
-cat ${PARM_LM}/lis.crd.T${grid}.tmp.1 >> ${LISCARD}
+cat "${PARM_LM}/lis.crd.T${grid}.tmp.1" >> ${LISCARD}
 echo "LIS%t%SSS        = 0     "      >> ${LISCARD}
 echo "LIS%t%SMN        = 00    "      >> ${LISCARD}
 echo "LIS%t%SHR        = ${hh1}  "      >> ${LISCARD}
@@ -39,6 +39,6 @@ echo "LIS%t%EHR        = ${hh2}  "      >> ${LISCARD}
 echo "LIS%t%EDA        = ${dd2}  "      >> ${LISCARD}
 echo "LIS%t%EMO        = ${mm2}  "      >> ${LISCARD}
 echo "LIS%t%EYR        = ${yyyy2}"      >> ${LISCARD}
-cat ${PARM_LM}/lis.crd.T${grid}.tmp.2   >> ${LISCARD}
+cat "${PARM_LM}/lis.crd.T${grid}.tmp.2"   >> ${LISCARD}
 
 exit 0
