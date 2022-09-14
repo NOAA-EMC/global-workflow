670bd17c3b863d3789b03ff9c52a533ab2b3b381
AndrewEichmann-NOAA <Andrew.Eichmann@noaa.gov>
2022-09-14 15:35:37 -0500

    linter corrections

--- jobs/rocoto/efsoi.sh
+++ jobs/rocoto/efsoi.sh
@@ -6,7 +6,7 @@ source "${HOMEgfs}/ush/preamble.sh"
 # Source FV3GFS workflow modules
 . "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+[[ "${status}" -ne 0 ]] && exit "${status}"
 
 ###############################################################
 # Execute the JJOB
