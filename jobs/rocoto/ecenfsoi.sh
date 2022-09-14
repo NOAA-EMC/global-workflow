dc1627fbc94ef2b5df1e70208cf1b2ee62d91128
AndrewEichmann-NOAA <Andrew.Eichmann@noaa.gov>
2022-09-14 09:59:40 -0500

    linter corrections

--- jobs/rocoto/ecenfsoi.sh
+++ jobs/rocoto/ecenfsoi.sh
@@ -1,26 +1,26 @@
 #! /usr/bin/env bash
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+. "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ "${status}" -ne 0 ]] && exit "${status}"
 
 ###############################################################
 # Loop over groups to Execute the JJOB
-fhrlst=$(echo $FHRLST | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
-for fhr in $fhrlst; do
+fhrlst=$(echo ${FHRLST} | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
+for fhr in ${fhrlst}; do
 
-    export FHMIN_ECEN=$fhr
-    export FHMAX_ECEN=$fhr
-    export FHOUT_ECEN=$fhr
+    export FHMIN_ECEN=${fhr}
+    export FHMAX_ECEN=${fhr}
+    export FHOUT_ECEN=${fhr}
     export job=ecen${fhr}
 
-    $HOMEgfs/jobs/JGDAS_EFSOI_ECEN
+    "${HOMEgfs}/jobs/JGDAS_EFSOI_ECEN"
     status=$?
-    [[ $status -ne 0 ]] && exit $status
+    [[ ${status} -ne 0 ]] && exit ${status}
 
 done
 
--- jobs/rocoto/eposfsoi.sh
+++ jobs/rocoto/eposfsoi.sh
@@ -1,28 +1,28 @@
 #! /usr/bin/env bash
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+. "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ "${status}" -ne 0 ]] && exit "${status}"
 
 ###############################################################
 # Loop over groups to Execute the JJOB
-fhrlst=$(echo $FHRLST | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
+fhrlst=$(echo ${FHRLST} | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
 
-for fhr in $fhrlst; do
+for fhr in ${fhrlst}; do
     
-    export FHMIN_EPOS=$fhr
-    export FHMAX_EPOS=$fhr
-    export FHOUT_EPOS=$fhr
+    export FHMIN_EPOS=${fhr}
+    export FHMAX_EPOS=${fhr}
+    export FHOUT_EPOS=${fhr}
     export job=epos${fhr}
     
-	$HOMEgfs/jobs/JGDAS_EFSOI_POST
+	"${HOMEgfs}/jobs/JGDAS_EFSOI_POST"
 	
     status=$?
-    [[ $status -ne 0 ]] && exit $status
+    [[ "${status}" -ne 0 ]] && exit "${status}"
 
 done
 
