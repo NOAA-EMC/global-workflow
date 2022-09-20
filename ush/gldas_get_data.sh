2aa8d5118032302d41191aaa681538bae12fb2c5
kate.friedman <kate.friedman@noaa.gov>
2022-09-20 14:38:02 -0500

    Indent while loops in GLDAS scripts
    - Add missing indentation into ush/gldas_forcing.sh and ush/gldas_get_data.sh.
    
    Refs #1014

--- ush/gldas_get_data.sh
+++ ush/gldas_get_data.sh
@@ -29,40 +29,40 @@ iter=0
 
 #-------------------------------
 while [[ "${cdate}" -lt "${edate}" ]]; do
-#-------------------------------
+
   ymd=$(echo "${cdate}" |cut -c 1-8)
   cyc=$(echo "${cdate}" |cut -c 9-10)
   [[ ! -d ${fpath}/gdas.${ymd} ]] && mkdir -p "${fpath}/gdas.${ymd}"
   [[ ! -d ${gpath}/gdas.${ymd} ]] && mkdir -p "${gpath}/gdas.${ymd}"
 
-f=1
-while [[ "${f}" -le "${cycint}" ]]; do
-  rflux=${COMINgdas:?}/gdas.${ymd}/${cyc}/${COMPONENT}/gdas.t${cyc}z.sfluxgrbf00${f}.grib2
-  fflux=${fpath}/gdas.${ymd}/gdas.t${cyc}z.sfluxgrbf0${f}.grib2
-  gflux=${gpath}/gdas.${ymd}/gdas1.t${cyc}z.sfluxgrbf0${f}
-  if [[ ! -s "${rflux}" ]];then
-     echo "WARNING: GLDAS MISSING ${rflux}, WILL NOT RUN."
-     exit 2
-  fi
-  rm -f "${fflux}" "${gflux}"
-  touch "${fflux}" "${gflux}"
-
-  fcsty=anl
-  if [[ "${f}" -ge 1 ]]; then fcsty=fcst; fi
-
-  if [[ "${USE_CFP:-"NO"}" = "YES" ]] ; then
-    if [[ "${CFP_MP:-"NO"}" = "YES" ]]; then
-      echo "${iter} ${USHgldas:?}/gldas_process_data.sh ${iter} ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
-    else
-      echo "${USHgldas:?}/gldas_process_data.sh ${iter} ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
+  f=1
+  while [[ "${f}" -le "${cycint}" ]]; do
+    rflux=${COMINgdas:?}/gdas.${ymd}/${cyc}/${COMPONENT}/gdas.t${cyc}z.sfluxgrbf00${f}.grib2
+    fflux=${fpath}/gdas.${ymd}/gdas.t${cyc}z.sfluxgrbf0${f}.grib2
+    gflux=${gpath}/gdas.${ymd}/gdas1.t${cyc}z.sfluxgrbf0${f}
+    if [[ ! -s "${rflux}" ]];then
+      echo "WARNING: GLDAS MISSING ${rflux}, WILL NOT RUN."
+      exit 2
     fi
-  else
-    "${USHgldas:?}/gldas_process_data.sh" "${iter}" "${rflux}" "${fcsty}" "${fflux}" "${gflux}" "${f}"
-  fi
+    rm -f "${fflux}" "${gflux}"
+    touch "${fflux}" "${gflux}"
 
-  iter=$((iter+1))
-  f=$((f+1))
-done
+    fcsty=anl
+    if [[ "${f}" -ge 1 ]]; then fcsty=fcst; fi
+
+    if [[ "${USE_CFP:-"NO"}" = "YES" ]] ; then
+      if [[ "${CFP_MP:-"NO"}" = "YES" ]]; then
+        echo "${iter} ${USHgldas:?}/gldas_process_data.sh ${iter} ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
+      else
+        echo "${USHgldas:?}/gldas_process_data.sh ${iter} ${rflux} ${fcsty} ${fflux} ${gflux} ${f}" >> ./cfile
+      fi
+    else
+      "${USHgldas:?}/gldas_process_data.sh" "${iter}" "${rflux}" "${fcsty}" "${fflux}" "${gflux}" "${f}"
+    fi
+
+    iter=$((iter+1))
+    f=$((f+1))
+  done
 
 #-------------------------------
   cdate=$(${NDATE} +"${cycint}" "${cdate}")
