5118de85090f8131a504f78bd3dccf8ad446478e
David Huber <david.huber@noaa.gov>
2022-09-21 16:57:14 +0000

    Removed S4 resolution restrictions. #138

--- workflow/setup_expt.py
+++ workflow/setup_expt.py
@@ -274,9 +274,6 @@ if __name__ == '__main__':
     user_inputs = input_args()
     host = Host()
 
-    if host.machine.upper() == "S4" and int(user_inputs.resdet) > 384:
-        raise ValueError("S4 does not support experiments at resolutions finer than C384.")
-
     comrot = os.path.join(user_inputs.comrot, user_inputs.pslot)
     expdir = os.path.join(user_inputs.expdir, user_inputs.pslot)
 
