72d1ea5a38cae935b4e8d92d9a03864932a0a1f1
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-09-23 11:52:12 -0400

    add NWS in the orgs list and a filesystem utility file

---
+++ ush/python/pygw/src/pygw/fsutils.py
@@ -0,0 +1,46 @@
+import os
+import errno
+import shutil
+import contextlib
+
+__all__ = ['mkdir', 'mkdir_p', 'rmdir', 'chdir', 'rm_p']
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise OSError(f"unable to create directory at {path}")
+
+
+mkdir = mkdir_p
+
+
+def rmdir(dir_path):
+    try:
+        shutil.rmtree(dir_path)
+    except OSError as exc:
+        raise OSError(f"unable to remove {dir_path}")
+
+
+@contextlib.contextmanager
+def chdir(path):
+    cwd = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+def rm_p(path):
+    try:
+        os.unlink(path)
+    except OSError as exc:
+        if exc.errno == errno.ENOENT:
+            pass
+        else:
+            raise OSError(f"unable to remove {path}")
