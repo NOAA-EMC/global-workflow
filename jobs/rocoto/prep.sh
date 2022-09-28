97dba45ca2fc9d4f563bc5a6bc42f5a39412d895
Kate.Friedman <Kate.Friedman@noaa.gov>
2022-09-28 19:49:44 +0000

    Update primary obsproc JJOB call in prep.sh
    The updated obsproc package renamed the primary global JJOB script
    and the HOMEobsproc variable is the only home path for the package,
    so update prep.sh to now call:
    $HOMEobsproc/jobs/JOBSPROC_GLOBAL_PREP
    instead of the old:
    $HOMEobsproc_network/jobs/JGLOBAL_PREP
    
    Refs #1033

--- jobs/rocoto/prep.sh
+++ jobs/rocoto/prep.sh
@@ -109,7 +109,7 @@ if [ $DO_MAKEPREPBUFR = "YES" ]; then
         export MAKE_NSSTBUFR="NO"
     fi
 
-    $HOMEobsproc_network/jobs/JGLOBAL_PREP
+    $HOMEobsproc/jobs/JOBSPROC_GLOBAL_PREP
     status=$?
     [[ $status -ne 0 ]] && exit $status
 
