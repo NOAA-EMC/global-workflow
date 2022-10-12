ad67dcadb96f9ff3eb5f6e4d6bc7d9f21179c0cc
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-10-12 14:11:04 -0400

    fix ediag

--- jobs/rocoto/ediag.sh
+++ jobs/rocoto/ediag.sh
@@ -8,6 +8,9 @@ source "$HOMEgfs/ush/preamble.sh"
 status=$?
 [[ $status -ne 0 ]] && exit $status
 
+export job="ediag"
+export jobid="${job}.$$"
+
 ###############################################################
 # Execute the JJOB
 $HOMEgfs/jobs/JGDAS_ENKF_DIAG
