9d54a7019b84a0d0a4e8682aac6abe2f57abeca4
lin.gan <lin.gan@noaa.gov>
2022-11-16 20:06:14 +0000

    Apply shellcheck 2004,2086,2166,2248,2250 to rocoto jobs

--- jobs/rocoto/earc.sh
+++ jobs/rocoto/earc.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+. "${HOMEgfs}"/ush/load_fv3gfs_modules.sh
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit "${status}"
 
 ###############################################################
 # Execute the JJOB
-$HOMEgfs/jobs/JENKFGDAS_ARCHIVE
+"${HOMEgfs}"/jobs/JENKFGDAS_ARCHIVE
 status=$?
 
 
-exit ${status}
+exit "${status}"
