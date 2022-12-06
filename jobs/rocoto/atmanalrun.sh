6ab858ea1e5f7827026655007617730c6bc9eda6
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-12-06 11:04:07 -0500

    First step towards making j-jobs consistent in use from ecflow and rocoto (#1120)
    Several jobs run in development mode do not adhere to EE2 standards.  Sometimes these development jobs do get used for operations at which time there is a rush in getting them to be standardized.
    These jobs are also used to run parallel retrospectives and near-real time parallels.
    
    This work is performed with GFSv17 and GEFSv13 as the target implementation.
    
    This PR attempts to standardize the use of several variables as defined in the NCO EE2 standards document.
    Specifically;
    The following are defined in the job-card:
    `DATAROOT`
    `cyc`
    `HOMEgfs`
    `job`
    `jobid=$job/$$` or `jobid=$PBS_JOBNAME/$PBS_JOBID` depending on Rocoto/Ops
    
    The following are defined in the j-job:
    `pid`
    `DATA=DATAROOT/jobid`
    `cycle`
    
    This PR also fixes a bug in `exgfs_atmos_post.sh` that is only encountered when `WAFSF=YES` and in the `gdaspostanl` job.  An overzealous linter added extra white-space and put quotes around an argument for `wgrib2`.

--- jobs/rocoto/atmanalrun.sh
+++ jobs/rocoto/atmanalrun.sh
@@ -1,17 +1,20 @@
 #! /usr/bin/env bash
 
 export STRICT="NO"
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 export STRICT="YES"
 
 ###############################################################
 # Source UFSDA workflow modules
-. $HOMEgfs/ush/load_ufsda_modules.sh
+. ${HOMEgfs}/ush/load_ufsda_modules.sh
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit ${status}
+
+export job="atmanalrun"
+export jobid="${job}.$$"
 
 ###############################################################
 # Execute the JJOB
-$HOMEgfs/jobs/JGDAS_GLOBAL_ATMOS_ANALYSIS_RUN
+${HOMEgfs}/jobs/JGDAS_GLOBAL_ATMOS_ANALYSIS_RUN
 status=$?
-exit $status
+exit ${status}
--- jobs/rocoto/waveinit.sh
+++ jobs/rocoto/waveinit.sh
@@ -1,20 +1,23 @@
 #! /usr/bin/env bash
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 echo
 echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit ${status}
+
+export job="waveinit"
+export jobid="${job}.$$"
 
 ###############################################################
 echo
 echo "=============== START TO RUN WAVE INIT ==============="
 # Execute the JJOB
-$HOMEgfs/jobs/JGLOBAL_WAVE_INIT
+${HOMEgfs}/jobs/JGLOBAL_WAVE_INIT
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit ${status}
 
 exit 0
