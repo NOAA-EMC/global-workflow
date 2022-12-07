d49e61255d8faa0b86f2de4cda2ef718177f4602
Cory Martin <cory.r.martin@noaa.gov>
2022-12-07 14:54:55 -0500

    Remove py header docstring and change to pygfs

--- scripts/exgdas_global_aero_analysis_finalize.py
+++ scripts/exgdas_global_aero_analysis_finalize.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python3
-################################################################################
-#  UNIX Script Documentation Block
-#                      .                                             .
-# Script name:         exgdas_global_aero_analysis_finalize.py
-# Script description:  Finalizes runtime directory for global aerosol analysis
-#
-# Author: Cory Martin      Org: NCEP/EMC     Date: 2022-8-29
-#
-# Abstract: This script cleans up the runtime directory and moves/archives
-#           necessary output files ffrom FV3-JEDI executable(s)
-#           produced from a UFS Global Aerosol Analysis.
-#
-# $Id$
-#
-# Attributes:
-#   Language: Python3
-#
-################################################################################
-
-import gfs.task.aero_analysis
+import pygfs.task.aero_analysis
 import logging
 import os
 
@@ -27,5 +8,5 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=loggin
 
 if __name__ == '__main__':
 
-    AeroAnl = gfs.task.aero_analysis.AerosolAnalysis(os.environ)
+    AeroAnl = pygfs.task.aero_analysis.AerosolAnalysis(os.environ)
     AeroAnl.finalize()
--- scripts/exgdas_global_aero_analysis_initialize.py
+++ scripts/exgdas_global_aero_analysis_initialize.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python3
-################################################################################
-#  UNIX Script Documentation Block
-#                      .                                             .
-# Script name:         exgdas_global_aero_analysis_initialize.py
-# Script description:  Initializes runtime directory for global aerosol analysis
-#
-# Author: Cory Martin      Org: NCEP/EMC     Date: 2022-09-06
-#
-# Abstract: This script sets up the runtime directory and stages
-#           necessary input files for FV3-JEDI executable(s) needed
-#           to produce a UFS Global Aerosol Analysis.
-#
-# $Id$
-#
-# Attributes:
-#   Language: Python3
-#
-################################################################################
-
-import gfs.task.aero_analysis
+import pygfs.task.aero_analysis
 import logging
 import os
 
@@ -28,5 +9,5 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=loggin
 
 if __name__ == '__main__':
 
-    AeroAnl = gfs.task.aero_analysis.AerosolAnalysis(os.environ)
+    AeroAnl = pygfs.task.aero_analysis.AerosolAnalysis(os.environ)
     AeroAnl.initialize()
