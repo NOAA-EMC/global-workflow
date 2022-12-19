7dd922bdcd139195a0a3c6ee70e967b4fa221e70
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-12-19 10:07:55 -0500

    execute in the bash shell

--- ush/python/pygw/src/tests/test_configuration.py
+++ ush/python/pygw/src/tests/test_configuration.py
@@ -155,14 +155,14 @@ def test_find_config(tmp_path, create_configs):
     assert str(tmp_path / 'config.file0') == file0
 
 
-#@pytest.mark.skip(reason="fails in GH runner, passes on localhost")
+#  @pytest.mark.skip(reason="fails in GH runner, passes on localhost")
 def test_parse_config1(tmp_path, create_configs):
     cfg = Configuration(tmp_path)
     f0 = cfg.parse_config('config.file0')
     assert file0_dict == f0
 
 
-#@pytest.mark.skip(reason="fails in GH runner, passes on localhost")
+#  @pytest.mark.skip(reason="fails in GH runner, passes on localhost")
 def test_parse_config2(tmp_path, create_configs):
     cfg = Configuration(tmp_path)
     ff = cfg.parse_config(['config.file0', 'config.file1'])
