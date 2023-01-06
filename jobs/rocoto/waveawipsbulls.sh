95a4d9c7bf326a4dfcce5ebd42958cdc7d280653
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2023-01-05 23:39:41 -0600

    Abstract common j-job tasks
    Takes all of the tasks that are common to all j-jobs and abstracts
    them out into a shared script that is sourced by each job:
    - Set and create $DATA
    - Call setpy and set $cycle
    - Set pid, pgmout, and pgmerr
    - Source config files
    - Source machine environment file
    
    The common j-job header script is called by passing a list of config
    files to source:
    ```
    ${HOMEgfs}/ush/jjob_header.sh [config1 [config2 [...]]]
    ```
    
    Some pre j-job rocoto entry scripts (`jobs/rocoto/*`) are currently
    doing much more than they should be. These sometimes required extra
    finagling, usually pre-calling the jjob header in the rocoto script
    before it does something.

--- jobs/rocoto/waveawipsbulls.sh
+++ jobs/rocoto/waveawipsbulls.sh
@@ -3,34 +3,15 @@
 source "$HOMEgfs/ush/preamble.sh"
 
 ###############################################################
-echo
-echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+# Source FV3GFS workflow modules
+source ${HOMEgfs}/ush/load_fv3gfs_modules.sh
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit ${status}
+
+export job="waveawipsbulls"
+export jobid="${job}.$$"
 
 ###############################################################
-echo
-echo "=============== BEGIN TO SOURCE RELEVANT CONFIGS ==============="
-configs="base waveawipsbulls"
-for config in $configs; do
-    . $EXPDIR/config.${config}
-    status=$?
-    [[ $status -ne 0 ]] && exit $status
-done
-
-###############################################################
-echo
-echo "=============== BEGIN TO SOURCE MACHINE RUNTIME ENVIRONMENT ==============="
-. $BASE_ENV/${machine}.env waveawipsbulls
-status=$?
-[[ $status -ne 0 ]] && exit $status
-
-export DBNROOT=/dev/null
-
-###############################################################
-echo
-echo "=============== START TO RUN WAVE PRDGEN BULLS ==============="
 # Execute the JJOB
 $HOMEgfs/jobs/JGLOBAL_WAVE_PRDGEN_BULLS
 status=$?
