399b66871d9154b74e1e64240ee190aaf7c46163
Jessica Meixner <Jessica.Meixner@noaa.gov>
2023-01-27 21:32:58 +0000

    update how 32 bit is built

--- sorc/build_ufs.sh
+++ sorc/build_ufs.sh
@@ -4,7 +4,7 @@ set -eux
 cwd=$(pwd)
 
 # Default settings
-APP="MS2SWA"
+APP="S2SWA"
 CCPP_SUITES="FV3_GFS_v16,FV3_GFS_v16_no_nsst,FV3_GFS_v16_ugwpv1,FV3_GFS_v17_p8,FV3_GFS_v16_coupled_nsstNoahmpUGWPv1,FV3_GFS_v17_coupled_p8"
 
 export RT_COMPILER="intel"
