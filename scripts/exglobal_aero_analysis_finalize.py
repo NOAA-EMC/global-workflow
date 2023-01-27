b48383af3edbec31e0259dff593f469c46897991
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-01-27 16:18:20 -0500

    no need for the configure method

--- scripts/exglobal_aero_analysis_finalize.py
+++ scripts/exglobal_aero_analysis_finalize.py
@@ -22,5 +22,4 @@ if __name__ == '__main__':
 
     # Instantiate the aerosol analysis task
     AeroAnl = AerosolAnalysis(config)
-    AeroAnl.configure()  # TODO: clarify, how do you configure before initializing?  These should be swapped.
     AeroAnl.initialize()
