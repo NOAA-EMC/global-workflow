1040216d8a4efb9955efecebf59775e91d8845e2
Cory Martin <cory.r.martin@noaa.gov>
2023-02-10 17:16:44 -0500

    Add in initial 3DVar aerosol DA cycling capability (#1106)
    This PR adds an initial 3DVar aerosol DA cycling capability to support scientific development and testing towards an operationally viable candidate system for aerosol DA for GFSv17/GEFSv13.
    
    This PR includes the following:
    - Three new j-jobs and ex-scripts for aeroanlinit, aeroanlrun, and aeroanlfinal
    - modifies the rocoto scripts that call the above j-jobs to actually call them
    - makes modification to the config.resources and config.aeroanl files
    - Introduces an object-oriented python structure to initialize/finalize the aerosol analysis with the intention of eventually using the top-level classes for other analysis jobs (soca, atm, land)
    
    Closes #982

--- jobs/rocoto/aeroanlinit.sh
+++ jobs/rocoto/aeroanlinit.sh
@@ -1,15 +1,24 @@
 #! /usr/bin/env bash
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
-# Source FV3GFS workflow modules
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
+# Source UFSDA workflow modules
+. "${HOMEgfs}/ush/load_ufsda_modules.sh"
 status=$?
-[[ $status -ne 0 ]] && exit $status
+[[ ${status} -ne 0 ]] && exit "${status}"
 
 export job="aeroanlinit"
 export jobid="${job}.$$"
 
 ###############################################################
-echo "Do nothing for now"
+# setup python path for workflow utilities and tasks
+pygwPATH="${HOMEgfs}/ush/python:${HOMEgfs}/ush/python/pygw/src"
+PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${pygwPATH}"
+export PYTHONPATH
+
+###############################################################
+# Execute the JJOB
+"${HOMEgfs}/jobs/JGLOBAL_AERO_ANALYSIS_INITIALIZE"
+status=$?
+exit "${status}"
