fb2f8fbb6b9913a86ba74eda1904500dbc2c525c
Jiarui Dong <Jiarui.Dong@noaa.gov>
2023-02-28 19:25:46 -0500

    Add initial land DA cycling.

---
+++ jobs/rocoto/landanlrun.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+source "${HOMEgfs}/ush/preamble.sh"
+
+###############################################################
+# Source UFSDA workflow modules
+. "${HOMEgfs}/ush/load_ufsda_modules.sh"
+status=$?
+[[ ${status} -ne 0 ]] && exit "${status}"
+
+export job="landanlrun"
+export jobid="${job}.$$"
+
+###############################################################
+# Execute the JJOB
+"${HOMEgfs}/jobs/JGLOBAL_LAND_ANALYSIS_RUN"
+status=$?
+exit "${status}"
