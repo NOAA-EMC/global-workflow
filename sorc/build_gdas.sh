d9a79968bf202a896aa8bc0d3a1dc8b265be4a21
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-03-05 01:15:55 -0500

    update build_gdas.sh for missing COMPILER

--- sorc/build_gdas.sh
+++ sorc/build_gdas.sh
@@ -4,14 +4,17 @@ set -eux
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 cd "${script_dir}" || exit 1
 
-source gfs_utils.fd/ush/detect_machine.sh
+export COMPILER="intel"
+source gfs_utils.fd/ush/detect_machine.sh  # TODO: this script should not source something from another repo
+set +x
 source gfs_utils.fd/ush/module-setup.sh
+set -x
 
 # detect_machine now includes the compiler on some machines
 #   but the GDAS build script does not want it
-MACHINE_ID=$(echo "${MACHINE_ID}" | cut -d "." -f 1)
+TARGET=$(echo "${MACHINE_ID}" | cut -d "." -f 1)
 
-case "${MACHINE_ID}" in
+case "${TARGET}" in
     hera|orion) build_jobs=10 ;;
     *)          build_jobs=4 ;;
 esac
@@ -23,7 +26,7 @@ fi
 
 cd gdas.cd
 
-WORKFLOW_BUILD="ON" BUILD_JOBS="${build_jobs}" ./build.sh -t "${MACHINE_ID}"
+WORKFLOW_BUILD="ON" BUILD_JOBS="${build_jobs}" ./build.sh -t "${TARGET}"
 
 exit $?
 
