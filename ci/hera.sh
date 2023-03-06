32b0d2bf74fabf5b46dfca27f5fe206fa00cf253
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-03-06 17:41:03 +0000

    Removed redundent GFS_CI_HOST in sourced bash config file

--- ci/hera.sh
+++ ci/hera.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 export GFS_CI_ROOT=/scratch1/NCEPDEV/global/Terry.McGuinness/GFS_CI_ROOT
-export GFS_CI_HOST='Hera'
 export GFS_MODULE_USE="${GFS_CI_ROOT}/repo/modulefiles"
 export SLURM_ACCOUNT=fv3-cpu
 export SALLOC_ACCOUNT="${SLURM_ACCOUNT}"
 export SBATCH_ACCOUNT="${SLURM_ACCOUNT}"
 export SLURM_QOS=debug
 export repo_url="https://github.com/NOAA-EMC/global-workflow.git"
-#export repo_url="https://github.com/TerrenceMcGuinness-NOAA/global-workflow.git"
