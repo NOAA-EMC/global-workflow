c796911a5ba15411a4a22344145ddc65e0dc0aae
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-03-08 15:17:08 -0500

    use full nodes on all machines for the forecast task. load ufs-weather-model modules for fcst/efcs jobs

--- jobs/rocoto/efcs.sh
+++ jobs/rocoto/efcs.sh
@@ -4,9 +4,17 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
-status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ ${status} -ne 0 ]] && exit ${status}
+
+set +x
+module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
+module load modules.ufs_model.lua
+# Workflow needs utilities from prod_util (setPDY.sh, ndate, etc.)
+module load prod_util
+module list
+set_trace
 
 export job="efcs"
 export jobid="${job}.$$"
--- jobs/rocoto/fcst.sh
+++ jobs/rocoto/fcst.sh
@@ -4,9 +4,16 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
-status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ ${status} -ne 0 ]] && exit ${status}
+
+set +x
+module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
+module load modules.ufs_model.lua
+module load prod_util
+module list
+set_trace
 
 export job="fcst"
 export jobid="${job}.$$"
