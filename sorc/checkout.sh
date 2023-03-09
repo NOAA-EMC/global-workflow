fa89cd11b66a77a827de9fc49b30f82d45c1ce5c
Jiarui Dong <Jiarui.Dong@noaa.gov>
2023-03-09 13:21:15 -0500

    Add to checkout the new GDASApp commit hash.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -165,7 +165,7 @@ if [[ ${checkout_gsi} == "YES" ]]; then
 fi
 
 if [[ ${checkout_gdas} == "YES" ]]; then
-  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "a00b5da"; errs=$((errs + $?))
+  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "563c7ee"; errs=$((errs + $?))
 fi
 
 if [[ ${checkout_gsi} == "YES" || ${checkout_gdas} == "YES" ]]; then
