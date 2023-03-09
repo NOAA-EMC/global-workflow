42bc9aff664debb9f87a864931d6466023139314
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-03-09 21:59:22 +0000

    improved yaml format, added Pathlib, and loggin

--- ci/create_experment.py
+++ ci/create_experment.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
-import os
+import os, sys
 import socket
+from pathlib import Path
 
 from pygw.yaml_file import YAMLFile
-from pygw.logger import logit
+from pygw.logger import Logger
 from pygw.executable import Executable
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 from hosts import Host
 
-
-# @logit(logger)
-
-_here = os.path.dirname(__file__)
-_top = os.path.abspath(os.path.join(os.path.abspath(_here), '..'))
+logger = Logger(level='DEBUG', colored_log=True)
+_here = Path.absolute(Path(__file__)).parents[0]
+_top  = Path.absolute(Path(__file__)).parents[1]
 
 def input_args():
     """
@@ -27,8 +26,7 @@ def input_args():
     parser = ArgumentParser(description=description,
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--yaml', help='Defaults to substitute from', type=str,
-                          required=True, default=os.path.join(_top, 'ci/experment1.yaml'))
+    parser.add_argument('--yaml', help='yaml configuration file per experment', type=str, required=True )
 
     args = parser.parse_args()
     return args
@@ -43,35 +41,33 @@ if __name__ == '__main__':
     except:
         print(socket.gethostname(),"is not specificly supported")
 
-
     setup_expt_args = YAMLFile(path=user_inputs.yaml)
 
-    mode = setup_expt_args.mode
-    icdir = setup_expt_args.icdir
-    rotdir = setup_expt_args.rotdir
-    del setup_expt_args['mode']
-    del setup_expt_args['icdir']
-    del setup_expt_args['rotdir']
+    mode = setup_expt_args.experment.mode
+    icdir = setup_expt_args.environment.icdir
+    rotdir = setup_expt_args.environment.rotdir
 
-    setup_expt_cmd = Executable(os.path.abspath(os.path.join(_top,'workflow/setup_expt.py')))
+    setup_expt_cmd = Executable(Path.absolute(Path.joinpath(_top,'workflow','setup_expt.py')))
     setup_expt_cmd.add_default_arg(mode)
-    for conf,value in setup_expt_args.items():
+    for conf,value in setup_expt_args.arguments.items():
          setup_expt_cmd.add_default_arg(f'--{conf}')
          setup_expt_cmd.add_default_arg(str(value))
 
-    print( setup_expt_cmd.command )
+    logger.info(f'run command: {setup_expt_cmd.command}')
     setup_expt_cmd(output='stdout_expt', error='stderr_expt') 
 
+    # Make symlinks of initial conitions into ROTDIR
     link_ic_cmd = Executable('cp')
     link_ic_cmd.add_default_arg('-as')
     link_ic_cmd.add_default_arg(os.path.abspath(icdir+'/'))
     link_ic_cmd.add_default_arg(os.path.abspath(rotdir))
-    print( link_ic_cmd.command )
+
+    logger.info(f'run command: {link_ic_cmd.command}')
     link_ic_cmd(output='stdout_linkic', error='stderr_linkic')
 
-    setup_xml_cmd = Executable(os.path.abspath(os.path.join(_top,'workflow/setup_xml.py')))
-    setup_xml_cmd.add_default_arg(os.path.abspath(os.path.join(setup_expt_args.expdir,setup_expt_args.pslot)))
-    print( setup_xml_cmd.command )
-    setup_xml_cmd(output='stdout_setupxml', error='stderr_setupxml')
-    
+    setup_xml_cmd = Executable(Path.absolute(Path.joinpath(_top,'workflow','setup_xml.py')))
+    expdir = Path.absolute(Path.joinpath(Path(setup_expt_args.arguments.expdir),Path(setup_expt_args.arguments.pslot)))
+    setup_xml_cmd.add_default_arg(str(expdir))
 
+    logger.info(f'run command: {setup_xml_cmd.command}')
+    setup_xml_cmd(output='stdout_setupxml', error='stderr_setupxml')
