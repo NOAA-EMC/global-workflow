88b5b29d334055b175d07bb888e1885936030b07
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-13 12:29:33 -0600

    Debugging task-level scripts.

---
+++ jobs/rocoto/gfs_init.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+source "${HOMEgfs}/ush/preamble.sh"
+
+###############################################################
+# Source FV3GFS workflow modules
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ ${status} -ne 0 ]] && exit ${status}
+
+# TODO: clean this up
+source "${HOMEgfs}/ush/detect_machine.sh"
+set +x
+module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
+module load modules.ufs_model.lua
+module load prod_util
+if [[ "${MACHINE_ID}" = "wcoss2" ]]; then
+  module load cray-pals
+fi
+module list
+unset MACHINE_ID
+set_trace
+
+export job="gfsinit"
+#export jobid="${job}.$$"
+export jobid=
+
+
+###############################################################
+# Execute the JJOB
+${HOMEgfs}/jobs/JGLOBAL_GFS_INITIALIZE
+status=$?
+
+
+exit ${status}
