7c9c0e617362433d94e71d91e3225b85d64fdda6
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-13 12:37:08 -0600

    Debugging module load issue.

--- jobs/rocoto/gfs_init.sh
+++ jobs/rocoto/gfs_init.sh
@@ -11,6 +11,9 @@ source "${HOMEgfs}/ush/preamble.sh"
 # TODO: clean this up
 source "${HOMEgfs}/ush/detect_machine.sh"
 set +x
+
+. /apps/lmod/lmod/init/sh
+
 module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
 module load modules.ufs_model.lua
 module load prod_util
