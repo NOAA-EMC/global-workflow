1bb775909f4f2143d6d5396795243512998dcb69
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-13 12:21:34 -0600

    Driver script.

---
+++ scripts/exglobal_gfs_initialize.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from pygw.logger import Logger
+from pygw.configuration import cast_strdict_as_dtypedict
+
+from pygfs.forecast.gfs import GFS
+
+# Initialize root logger.
+logger = Logger(level='DEBUG', colored_log=True)
+
+# ----
+
+if __name__ == '__main__':
+
+    # Take configuration from environment and cast it as Python
+    # dictionary.
+    script_name = os.path.basename(__file__)
+    start_time = time.time()
+    config = cast_strdict_as_dtypedict(os.environ)
+
+    # Launch the task.
+    task = GFS(config=config)
+    task.initialize()
+
+    stop_time = time.time()
+    msg = f"Completed application {script_name}."
+    Logger().info(msg=msg)
+    total_time = stop_time - start_time
+    msg = f"Total Elapsed Time: {total_time} seconds."
