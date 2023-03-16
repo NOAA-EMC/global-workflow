209262a2aa0313ceb99e00bb1009174b6ffbca07
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-16 08:44:21 -0600

    Turned off for debug purposes.

--- jobs/rocoto/gfs_init.sh
+++ jobs/rocoto/gfs_init.sh
@@ -11,7 +11,7 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 # TODO: clean this up
 source "${HOMEgfs}/ush/detect_machine.sh"
-set +x
+#set +x # TEST --
 source "${HOMEgfs}/ush/module-setup.sh"
 module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
 module load modules.ufs_model.lua
