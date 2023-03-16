2ee652b6e3a80b59f0d6e127cfeb16a4760556c5
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2023-03-15 21:04:42 -0500

    Update sound job for COM refactor
    The sounding job is updated to use the new COM templates.
    
    Refs: #761

--- scripts/exgfs_atmos_postsnd.sh
+++ scripts/exgfs_atmos_postsnd.sh
@@ -42,14 +42,11 @@ export NINT1=${FHOUT_HF_GFS:-1}
 export NEND1=${FHMAX_HF_GFS:-120}
 export NINT3=${FHOUT_GFS:-3}
 
-rm -f -r ${COMOUT}/bufr.${cycle}
-mkdir -p ${COMOUT}/bufr.${cycle}
-if [ -f $HOMEgfs/ush/getncdimlen ]; then
-  GETDIM=$HOMEgfs/ush/getncdimlen
-else
-  GETDIM=$EXECbufrsnd/getncdimlen
-fi
-export LEVS=$($GETDIM $COMIN/${RUN}.${cycle}.atmf000.${atmfm} pfull)
+rm -f -r "${COM_ATMOS_BUFR}"
+mkdir -p "${COM_ATMOS_BUFR}"
+GETDIM="${HOMEgfs}/ush/getncdimlen"
+LEVS=$(${GETDIM} "${COM_ATMOS_HISTORY}/${RUN}.${cycle}.atmf000.${atmfm}" pfull)
+declare -x LEVS
 
 ### Loop for the hour and wait for the sigma and surface flux file:
 export FSTART=$STARTHOUR
@@ -73,10 +70,8 @@ export FINT=$NINT1
    fi
 
    ic=0
-   while [ $ic -lt 1000 ]
-   do
-      if [ ! -f $COMIN/${RUN}.${cycle}.logf$FEND.${logfm} ]
-      then
+   while [ $ic -lt 1000 ]; do
+      if [[ ! -f "${COM_ATMOS_HISTORY}/${RUN}.${cycle}.logf${FEND}.${logfm}" ]]; then
           sleep 10
           ic=$(expr $ic + 1)
       else
@@ -102,17 +97,16 @@ done
 ##############################################################
 # Tar and gzip the individual bufr files and send them to /com
 ##############################################################
-cd ${COMOUT}/bufr.${cycle}
-tar -cf - . | /usr/bin/gzip > ../${RUN}.${cycle}.bufrsnd.tar.gz
-cd $DATA
+cd "${COM_ATMOS_BUFR}" || exit 2
+tar -cf - . | /usr/bin/gzip > "${RUN}.${cycle}.bufrsnd.tar.gz"
+cd "${DATA}" || exit 2
 
 ########################################
 # Send the single tar file to OSO
 ########################################
-if test "$SENDDBN" = 'YES'
-then
-    $DBNROOT/bin/dbn_alert MODEL GFS_BUFRSND_TAR $job \
-  $COMOUT/${RUN}.${cycle}.bufrsnd.tar.gz
+if [[ "${SENDDBN}" == 'YES' ]]; then
+    "${DBNROOT}/bin/dbn_alert" MODEL GFS_BUFRSND_TAR "${job}" \
+        "${COM_ATMOS_BUFR}/${RUN}.${cycle}.bufrsnd.tar.gz"
 fi
 
 ########################################
