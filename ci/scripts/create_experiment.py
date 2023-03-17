b3ca327a6b8dfceb15e8c178398256b5634fc5cd
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-03-17 18:22:14 +0000

    fixing pycodestyls

--- ci/scripts/create_experiment.py
+++ ci/scripts/create_experiment.py
@@ -28,10 +28,12 @@ from workflow.hosts import Host
 logger = Logger(level='DEBUG', colored_log=True)
 
 def input_args():
+
     """Method to collect user arguments for `create_experiment.py`
     """
 
     description =
+
     """Single agument as a yaml file containing the
     key value pairs as arguments to setup_expt.py
     """
@@ -39,7 +41,7 @@ def input_args():
     parser = ArgumentParser(description=description,
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--yaml', help='yaml configuration file per experiment', type=str, required=True )
+    parser.add_argument('--yaml', help='yaml configuration file per experiment', type=str, required=True)
 
     args = parser.parse_args()
     return args
@@ -49,8 +51,8 @@ if __name__ == '__main__':
     user_inputs = input_args()
 
     try:
-       host = Host()
-       logger.info( f'Running on HOST:{host.machine}')
+        host = Host()
+        logger.info( f'Running on HOST:{host.machine}')
     except:
         logger.error(f'HOST:{socket.gethostname()} is not currently supported')
         sys.exit(1)
@@ -70,11 +72,11 @@ if __name__ == '__main__':
          setup_expt_cmd.add_default_arg(str(value))
 
     logger.info(f'Run command: {setup_expt_cmd.command}')
-    setup_expt_cmd(output='stdout_expt', error='stderr_expt') 
+    setup_expt_cmd(output='stdout_expt',error='stderr_expt')
 
     setup_xml_cmd = Executable(Path.absolute(Path.joinpath(Path(HOMEgfs),'workflow','setup_xml.py')))
     expdir = Path.absolute(Path.joinpath(Path(setup_expt_args.arguments.expdir),Path(setup_expt_args.arguments.pslot)))
     setup_xml_cmd.add_default_arg(str(expdir))
 
     logger.info(f'Run command: {setup_xml_cmd.command}')
-    setup_xml_cmd(output='stdout_setupxml', error='stderr_setupxml')
+    setup_xml_cmd(output='stdout_setupxml',error='stderr_setupxml')
