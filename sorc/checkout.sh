1afc7cc29ceb1833ea89aaa5dddd4a9059a5e2ce
Jiarui Dong <Jiarui.Dong@noaa.gov>
2023-03-21 08:53:16 -0400

    Change the GDASApp commit hash version.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -165,7 +165,7 @@ if [[ ${checkout_gsi} == "YES" ]]; then
 fi
 
 if [[ ${checkout_gdas} == "YES" ]]; then
-  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "5ae5687"; errs=$((errs + $?))
+  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "5c750ce"; errs=$((errs + $?))
 fi
 
 if [[ ${checkout_gsi} == "YES" || ${checkout_gdas} == "YES" ]]; then
