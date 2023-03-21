c6b2b1b0880023e5b57c068ae1307892b725720a
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2023-03-21 16:08:16 -0500

    Update AWIPS for COM refactor
    AWIPS jobs are updated to use the new COM templates. The job currently
    completes up until the final program call (`mkgfsawps.x`), which errors
    for a yet-unknown reason.
    
    An additional step was added remove `5WAVH` from the expected parameter
    list when the wave model is not run. This allows the job to run when
    running with waves off in development.
    
    Refs: #761

--- scripts/exgfs_atmos_awips_20km_1p0deg.sh
+++ scripts/exgfs_atmos_awips_20km_1p0deg.sh
@@ -19,47 +19,39 @@
 # echo " "
 ###############################################################################
 
-source "$HOMEgfs/ush/preamble.sh"
+source "${HOMEgfs}/ush/preamble.sh"
 
 fcsthrs="$1"
 num=$#
-job_name=$(echo $job|sed 's/[jpt]gfs/gfs/')
+job_name=${job/[jpt]gfs/gfs}
 
-if test "$num" -ge 1
-then
+if (( num != 1 )); then
    echo ""
-   echo " Appropriate number of arguments were passed"
-   echo ""
-else
-   echo ""
-   echo " Number of arguments were not passed "
+   echo " FATAL ERROR: Incorrect number of argumentS "
    echo ""
    echo ""
-   echo "Usage: $0  \$fcsthrs (3 digits) "
+   echo "Usage: $0  \${fcsthrs} (3 digits) "
    echo ""
    exit 16
 fi
 
-cd $DATA
+cd "${DATA}" || exit 2
 
 ###############################################
 # Wait for the availability of the pgrb file
 ###############################################
 icnt=1
-while [ $icnt -lt 1000 ]
-do
-  if [ -s $COMIN/${RUN}.${cycle}.pgrb2b.0p25.f$fcsthrs.idx ]
-  then
-     break
-  fi
+while (( icnt < 1000 )); do
+   if [[ -s "${COM_ATMOS_GRIB_0p25}/${RUN}.${cycle}.pgrb2b.0p25.f${fcsthrs}.idx" ]]; then
+      break
+   fi
 
-  sleep 10
-  icnt=$((icnt + 1))
-  if [ $icnt -ge 180 ]
-  then
-    msg="ABORTING after 30 min of waiting for the GFS pgrb2 file!"
-    err_exit $msg
-  fi
+   sleep 10
+   icnt=$((icnt + 1))
+   if (( icnt >= 180 )); then
+      msg="FATAL ERROR: No GFS pgrb2 file after 30 min of waiting"
+      err_exit "${msg}"
+   fi
 done
 
 ########################################
@@ -94,157 +86,184 @@ export SCALEDEC=${SCALDEC:-$USHgfs/scale_dec.sh}
 #    Process GFS GRIB AWIP PRODUCTS IN GRIB2                  #
 ###############################################################
 
-cp $COMIN/gfs.t${cyc}z.pgrb2.0p25.f${fcsthrs}    tmpfile2${fcsthrs}
-cp $COMIN/gfs.t${cyc}z.pgrb2b.0p25.f${fcsthrs}   tmpfile2b${fcsthrs}
-cat tmpfile2${fcsthrs}   tmpfile2b${fcsthrs}  >  tmpfile${fcsthrs}
-$WGRIB2 tmpfile${fcsthrs} | grep  -F -f $PARMproduct/gfs_awips_parmlist_g2 | $WGRIB2 -i -grib masterfile  tmpfile${fcsthrs}
+cp "${COM_ATMOS_GRIB_0p25}/gfs.t${cyc}z.pgrb2.0p25.f${fcsthrs}" "tmpfile2${fcsthrs}"
+cp "${COM_ATMOS_GRIB_0p25}/gfs.t${cyc}z.pgrb2b.0p25.f${fcsthrs}" "tmpfile2b${fcsthrs}"
+cat "tmpfile2${fcsthrs}" "tmpfile2b${fcsthrs}" > "tmpfile${fcsthrs}"
+${WGRIB2} "tmpfile${fcsthrs}" | grep -F -f "${PARMproduct}/gfs_awips_parmlist_g2" | \
+   ${WGRIB2} -i -grib masterfile "tmpfile${fcsthrs}"
 export err=$?
-if [[ $err -ne 0 ]] ; then
+if [[ $err -ne 0 ]]; then
    echo " FATAL ERROR: masterfile does not exist."
    exit $err
 fi
 
-$WGRIB2 masterfile -match ":PWAT:entire atmosphere" -grib gfs_pwat.grb
-$WGRIB2 masterfile | grep -v ":PWAT:entire atmosphere" | $WGRIB2 -i -grib temp_gfs  masterfile
+${WGRIB2} masterfile -match ":PWAT:entire atmosphere" -grib gfs_pwat.grb
+${WGRIB2} masterfile | grep -v ":PWAT:entire atmosphere" | ${WGRIB2} -i -grib temp_gfs masterfile
 ##################################################################
 #  Process to change PWAT from level 200 to 10 (Entire Atmosphere)
 #  in production defintion template (PDT) 4.0
 ##################################################################
-$WGRIB2 gfs_pwat.grb -set_byte 4 23 10 -grib gfs_pwat_levels_10.grb
+${WGRIB2} gfs_pwat.grb -set_byte 4 23 10 -grib gfs_pwat_levels_10.grb
 export err=$?; err_chk
 
 cat temp_gfs   gfs_pwat_levels_10.grb > tmp_masterfile
 
-for GRID in conus ak prico pac 003
-do
-   case $GRID in
-     conus)
-        #  Grid 20km_conus - CONUS - 20 km Quadruple Resolution (Lambert Conformal)
-        # export grid_20km_conus="30 6 0 0 0 0 0 0 369 257 12190000 226541000 8 25000000 265000000 20318000 20318000 0 64 25000000 25000000 0 0"
-        # $COPYGB2 -g "$grid_20km_conus" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
+for GRID in conus ak prico pac 003; do
+   # shellcheck disable=SC2086
+   case ${GRID} in
+      conus)
+         #  Grid 20km_conus - CONUS - 20 km Quadruple Resolution (Lambert Conformal)
+         # export grid_20km_conus="30 6 0 0 0 0 0 0 369 257 12190000 226541000 8 25000000 265000000 20318000 20318000 0 64 25000000 25000000 0 0"
+         # $COPYGB2 -g "$grid_20km_conus" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
 
-        export gridconus="lambert:265.0:25.0:25.0 226.541:369:20318.0 12.19:257:20318.0"
-        $WGRIB2  tmp_masterfile $opt1uv $opt21 $opt22 $opt23 $opt24 $opt25 $opt26 $opt27 $opt28 -new_grid $gridconus awps_file_f${fcsthrs}_${GRID}
-        ;;
-     ak)
-        #  Grid 20km_ak - Alaska - Double Resolution (Polar Stereographic)
-        #  Redefined grid 217 for Alaska region
-        # export grid_20km_ak="20 6 0 0 0 0 0 0 277 213 30000000 187000000 8 60000000 225000000 22500000 22500000 0 64"
-        # $COPYGB2 -g "$grid_20km_ak" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
+         export gridconus="lambert:265.0:25.0:25.0 226.541:369:20318.0 12.19:257:20318.0"
+         ${WGRIB2} tmp_masterfile ${opt1uv} ${opt21} ${opt22} ${opt23} ${opt24} ${opt25} ${opt26} \
+            ${opt27} ${opt28} -new_grid ${gridconus} "awps_file_f${fcsthrs}_${GRID}"
+         ;;
+      ak)
+         #  Grid 20km_ak - Alaska - Double Resolution (Polar Stereographic)
+         #  Redefined grid 217 for Alaska region
+         # export grid_20km_ak="20 6 0 0 0 0 0 0 277 213 30000000 187000000 8 60000000 225000000 22500000 22500000 0 64"
+         # $COPYGB2 -g "$grid_20km_ak" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
 
-        export gridak="nps:210.0:60.0 170.0:277:22500 35.0:225:22500"
-        $WGRIB2  tmp_masterfile $opt1uv $opt21 $opt22 $opt23 $opt24 $opt25 $opt26 $opt27 $opt28 -new_grid $gridak awps_file_f${fcsthrs}_${GRID}
-        ;;
-    prico)
-        #  Grid 20km_prico - 0.25 degree Lat/Lon grid for Puerto Rico (20km)
-        # export grid_20km_prico="0 6 0 0 0 0 0 0 275 205 0 0 50750000 271750000 48 -250000 340250000 250000 250000 0"
-        # $COPYGB2 -g "$grid_20km_prico" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
+         export gridak="nps:210.0:60.0 170.0:277:22500 35.0:225:22500"
+         ${WGRIB2} tmp_masterfile ${opt1uv} ${opt21} ${opt22} ${opt23} ${opt24} ${opt25} ${opt26} \
+            ${opt27} ${opt28} -new_grid ${gridak} "awps_file_f${fcsthrs}_${GRID}"
+         ;;
+      prico)
+         #  Grid 20km_prico - 0.25 degree Lat/Lon grid for Puerto Rico (20km)
+         # export grid_20km_prico="0 6 0 0 0 0 0 0 275 205 0 0 50750000 271750000 48 -250000 340250000 250000 250000 0"
+         # $COPYGB2 -g "$grid_20km_prico" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
 
-        export gridprico="latlon 271.75:275:0.25 50.75:205:-0.25"
-        $WGRIB2  tmp_masterfile $opt1 $opt21 $opt22 $opt23 $opt24 $opt25 $opt26 $opt27 $opt28 -new_grid $gridprico awps_file_f${fcsthrs}_${GRID}
-        ;;
-     pac)
-        #  Grid 20km_pac - 20 km Mercator grid for Pacific Region
-        # export grid_20km_pac="10 6 0 0 0 0 0 0 837 692 -45000000 110000000 48 20000000 65720000 270000000 64 0 20000000 20000000"
-        # NEW export grid_20km_pac="10 6 0 0 0 0 0 0 837 725 -45000000 110000000 48 20000000 65734500 270000000 64 0 20000000 20000000"
-        # $COPYGB2 -g "$grid_20km_pac" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
+         export gridprico="latlon 271.75:275:0.25 50.75:205:-0.25"
+         ${WGRIB2} tmp_masterfile ${opt1} ${opt21} ${opt22} ${opt23} ${opt24} ${opt25} ${opt26} \
+            ${opt27} ${opt28} -new_grid ${gridprico} "awps_file_f${fcsthrs}_${GRID}"
+         ;;
+      pac)
+         #  Grid 20km_pac - 20 km Mercator grid for Pacific Region
+         # export grid_20km_pac="10 6 0 0 0 0 0 0 837 692 -45000000 110000000 48 20000000 65720000 270000000 64 0 20000000 20000000"
+         # NEW export grid_20km_pac="10 6 0 0 0 0 0 0 837 725 -45000000 110000000 48 20000000 65734500 270000000 64 0 20000000 20000000"
+         # $COPYGB2 -g "$grid_20km_pac" -i0 -x tmp_masterfile  awps_file_f${fcsthrs}_${GRID}
 
-        export gridpac="mercator:20.0 110.0:837:20000:270.0 -45.0:725:20000:65.7345"
-        $WGRIB2  tmp_masterfile $opt1 $opt21 $opt22 $opt23 $opt24 $opt25 $opt26 $opt27 $opt28 -new_grid $gridpac awps_file_f${fcsthrs}_${GRID}
-        ;;
-     003)
-        ######################################################################
-        #    Process GFS GRIB AWIP 1.0 DEGREE (GRID 003)  PRODUCTS IN GRIB2  #
-        ######################################################################
-        export grid003="latlon 0:360:1.0 90:181:-1.0"
-        $WGRIB2  tmp_masterfile $opt1 $opt21 $opt22 $opt23 $opt24 $opt25 $opt26 $opt27 $opt28 -new_grid $grid003 awps_file_f${fcsthrs}_${GRID}
-        ;;
+         export gridpac="mercator:20.0 110.0:837:20000:270.0 -45.0:725:20000:65.7345"
+         ${WGRIB2} tmp_masterfile ${opt1} ${opt21} ${opt22} ${opt23} ${opt24} ${opt25} ${opt26} \
+            ${opt27} ${opt28} -new_grid ${gridpac} "awps_file_f${fcsthrs}_${GRID}"
+         ;;
+      003)
+         ######################################################################
+         #    Process GFS GRIB AWIP 1.0 DEGREE (GRID 003)  PRODUCTS IN GRIB2  #
+         ######################################################################
+         export grid003="latlon 0:360:1.0 90:181:-1.0"
+         ${WGRIB2} tmp_masterfile ${opt1} ${opt21} ${opt22} ${opt23} ${opt24} ${opt25} ${opt26} \
+            ${opt27} ${opt28} -new_grid ${grid003} "awps_file_f${fcsthrs}_${GRID}"
+         ;;
+      *)
+         echo "FATAL ERROR: Unknown output grid ${GRID}"
+         exit 2
+         ;;
    esac
-   $TRIMRH awps_file_f${fcsthrs}_${GRID}   
-   $SCALEDEC awps_file_f${fcsthrs}_${GRID}
-   $GRB2INDEX awps_file_f${fcsthrs}_${GRID}  awps_file_fi${fcsthrs}_${GRID}
+   # shellcheck disable=
+   ${TRIMRH} "awps_file_f${fcsthrs}_${GRID}" 
+   ${SCALEDEC} "awps_file_f${fcsthrs}_${GRID}"
+   ${GRB2INDEX} "awps_file_f${fcsthrs}_${GRID}" "awps_file_fi${fcsthrs}_${GRID}"
 
-###########################################################################
-# Checking fields in awps_file_f${fcsthrs}_${GRID} file
-# before TOCGRIB2 adding WMO headers for AWIPS products.
-#
-# NOTE: numparm is the total of fields in grib2_awpgfs_20km_conusf000 file
-###########################################################################
-numparm=247
-numrec=$( $WGRIB2 awps_file_f${fcsthrs}_${GRID} | wc -l )
+   ###########################################################################
+   # Checking fields in awps_file_f${fcsthrs}_${GRID} file
+   # before TOCGRIB2 adding WMO headers for AWIPS products.
+   #
+   # NOTE: numparm is the total of fields in grib2_awpgfs_20km_conusf000 file
+   ###########################################################################
+   numparm=247
+   numrec=$( ${WGRIB2} "awps_file_f${fcsthrs}_${GRID}" | wc -l )
 
-if [ $numrec -lt $numparm ]
-then
-    msg="ABORTING : awps_file_f${fcsthrs}_${GRID} file is missing fields for AWIPS !"
-    err_exit $msg
-fi
+   if (( numrec < numparm )); then
+       msg="FATAL ERROR: awps_file_f${fcsthrs}_${GRID} file is missing fields for AWIPS !"
+       err_exit "${msg}" || exit 10
+   fi
 
-# Processing AWIPS GRIB2 grids with WMO headers
+   # Processing AWIPS GRIB2 grids with WMO headers
 
    pgm=tocgrib2
    export pgm; prep_step
    startmsg
 
-   if [ $GRID = "003" -a $(expr ${fcsthrs} % 6) -eq 0 ] ; then
-      export FORT11=awps_file_f${fcsthrs}_${GRID}
-      export FORT31=awps_file_fi${fcsthrs}_${GRID}
-      export FORT51=grib2.awpgfs${fcsthrs}.${GRID}
+   if [[ ${GRID} = "003" && $(( fcsthrs % 6 )) == 0 ]]; then
+      export FORT11="awps_file_f${fcsthrs}_${GRID}"
+      export FORT31="awps_file_fi${fcsthrs}_${GRID}"
+      export FORT51="grib2.awpgfs${fcsthrs}.${GRID}"
 
-      $TOCGRIB2 < $PARMwmo/grib2_awpgfs${fcsthrs}.${GRID} >> $pgmout 2> errfile
+      cp "${PARMwmo}/grib2_awpgfs${fcsthrs}.${GRID}" "parm_list"
+      if [[ ${DO_WAVE} != "YES" ]]; then
+         # Remove wave field it not running wave model
+         grep -vw "5WAVH" "parm_list" > "parm_list_temp"
+         mv "parm_list_temp" "parm_list"
+      fi
+
+      ${TOCGRIB2} < "parm_list" >> "${pgmout}" 2> errfile
       export err=$?; err_chk
       echo " error from tocgrib2=",$err
 
-      if [ $SENDCOM = "YES" ] ; then
+      if [[ ${SENDCOM} == "YES" ]]; then
         ##############################
-        # Post Files to  ${COMOUTwmo}
+        # Post Files to  ${COM_ATMOS_WMO}
         ##############################
 
-        mv grib2.awpgfs${fcsthrs}.${GRID}    ${COMOUTwmo}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc}
+        mv "grib2.awpgfs${fcsthrs}.${GRID}" \
+         "${COM_ATMOS_WMO}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc}"
 
         ##############################
         # Distribute Data
         ##############################
 
-        if [ "$SENDDBN" = 'YES' -o "$SENDAWIP" = 'YES' ] ; then
-           $DBNROOT/bin/dbn_alert NTC_LOW $NET $job  ${COMOUTwmo}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc}
+        if [[ "${SENDDBN}" == 'YES' || "${SENDAWIP}" == 'YES' ]]; then
+           "${DBNROOT}/bin/dbn_alert" NTC_LOW "${NET}" "${job}" \
+            "${COM_ATMOS_WMO}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc}"
         else
-           echo "File ${COMOUTwmo}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc} not posted to db_net."
+           echo "File ${COM_ATMOS_WMO}/grib2.awpgfs${fcsthrs}.${GRID}.gfs_awips_f${fcsthrs}_1p0deg_${cyc} not posted to db_net."
         fi
       fi
-   elif [ $GRID != "003" ] ; then
-      export FORT11=awps_file_f${fcsthrs}_${GRID}
-      export FORT31=awps_file_fi${fcsthrs}_${GRID}
-      export FORT51=grib2.awpgfs_20km_${GRID}_f${fcsthrs}
+   elif [[ ${GRID} != "003" ]]; then
+      export FORT11="awps_file_f${fcsthrs}_${GRID}"
+      export FORT31="awps_file_fi${fcsthrs}_${GRID}"
+      export FORT51="grib2.awpgfs_20km_${GRID}_f${fcsthrs}"
 
-      $TOCGRIB2 < $PARMwmo/grib2_awpgfs_20km_${GRID}f${fcsthrs} >> $pgmout 2> errfile
-      export err=$? ;err_chk
-      echo " error from tocgrib2=",$err
-
-      if [ $SENDCOM = "YES" ] ; then
-
-      ##############################
-      # Post Files to  ${COMOUTwmo} 
-      ##############################
-
-      mv grib2.awpgfs_20km_${GRID}_f${fcsthrs}    ${COMOUTwmo}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.$job_name
-
-      ##############################
-      # Distribute Data
-      ##############################
-
-      if [ "$SENDDBN" = 'YES' -o "$SENDAWIP" = 'YES' ] ; then
-         $DBNROOT/bin/dbn_alert NTC_LOW $NET $job  ${COMOUTwmo}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.$job_name
-      else
-         echo "File  ${COMOUTwmo}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.$job_name not posted to db_net."
+      cp "${PARMwmo}/grib2_awpgfs_20km_${GRID}f${fcsthrs}" "parm_list"
+      if [[ ${DO_WAVE} != "YES" ]]; then
+         # Remove wave field it not running wave model
+         grep -vw "5WAVH" "parm_list" > "parm_list_temp"
+         mv "parm_list_temp" "parm_list"
       fi
-     fi
+
+      ${TOCGRIB2} < "parm_list" >> "${pgmout}" 2> errfile
+      export err=$?; err_chk || exit "${err}"
+
+      if [[ ${SENDCOM} = "YES" ]]; then
+
+         ##############################
+         # Post Files to  ${COM_ATMOS_WMO} 
+         ##############################
+
+         mv "grib2.awpgfs_20km_${GRID}_f${fcsthrs}" \
+            "${COM_ATMOS_WMO}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.${job_name}"
+
+         ##############################
+         # Distribute Data
+         ##############################
+
+         if [[ "${SENDDBN}" = 'YES' || "${SENDAWIP}" = 'YES' ]]; then
+            "${DBNROOT}/bin/dbn_alert" NTC_LOW "${NET}" "${job}" \
+               "${COM_ATMOS_WMO}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.${job_name}"
+         else
+            echo "File ${COM_ATMOS_WMO}/grib2.awpgfs_20km_${GRID}_f${fcsthrs}.${job_name} not posted to db_net."
+         fi
+      fi
    fi
    echo "Awip Processing ${fcsthrs} hour completed normally"
 
 done
 
-if [ -e "$pgmout" ] ; then
-   cat $pgmout
+if [[ -e "${pgmout}" ]]; then
+   cat "${pgmout}"
 fi
 
 
