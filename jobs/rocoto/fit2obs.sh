5653ef278b661d2a4bb022e4d15e5ce5ac79da72
Kate.Friedman <Kate.Friedman@noaa.gov>
2023-03-24 15:46:10 +0000

    Create new rocoto job script for fit2obs job
    - Create fit2obs.sh.
    - Script will run new jobs/JGDAS_FIT2OBS script.
    
    Refs #1405, #1232

---
+++ jobs/rocoto/fit2obs.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+source "${HOMEgfs}/ush/preamble.sh"
+
+###############################################################
+echo
+echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
+. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+status=$?
+[[ ${status} -ne 0 ]] && exit ${status}
+
+export job="fit2obs"
+export jobid="${job}.$$"
+
+###############################################################
+echo
+echo "=============== START TO RUN FIT2OBS ==============="
+# Execute the JJOB
+${HOMEgfs}/jobs/JGDAS_FIT2OBS
+status=$?
+[[ ${status} -ne 0 ]] && exit ${status}
+
+exit 0
