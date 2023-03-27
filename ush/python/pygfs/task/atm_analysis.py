bb3e89d53ea8dd0f5cabc1c820fe119300d7cb70
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-03-27 16:33:52 +0000

    update UFS-DA ATM var crtm and fix stage (#1313)

--- ush/python/pygfs/task/atm_analysis.py
+++ ush/python/pygfs/task/atm_analysis.py
@@ -74,10 +74,16 @@ class AtmAnalysis(Analysis):
         super().initialize()
 
         # stage CRTM fix files
-        super().stage_fix('atm_crtm_coeff.yaml')
+        crtm_fix_list_path = os.path.join(self.task_config['HOMEgfs'], 'parm', 'parm_gdas', 'atm_crtm_coeff.yaml')
+        logger.debug(f"Staging CRTM fix files from {crtm_fix_list_path}")
+        crtm_fix_list = parse_yamltmpl(crtm_fix_list_path, self.task_config)
+        FileHandler(crtm_fix_list).sync()
 
         # stage fix files
-        super().stage_fix('atm_jedi_fix.yaml')
+        jedi_fix_list_path = os.path.join(self.task_config['HOMEgfs'], 'parm', 'parm_gdas', 'atm_jedi_fix.yaml')
+        logger.debug(f"Staging JEDI fix files from {jedi_fix_list_path}")
+        jedi_fix_list = parse_yamltmpl(jedi_fix_list_path, self.task_config)
+        FileHandler(jedi_fix_list).sync()
 
         # stage berror files
         # copy static background error files, otherwise it will assume ID matrix
