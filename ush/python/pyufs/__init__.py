2214ec990d26c7f7db72592ee953f0b0dbc4c221
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-28 16:01:01 -0600

    Added package loader.

---
+++ ush/python/pyufs/__init__.py
@@ -0,0 +1,23 @@
+"""
+Module
+------
+
+    __init__.py
+
+Description
+-----------
+
+    This module loads the pyufs package.
+
+Author(s)
+---------
+
+
+    Henry R. Winterbottom; 28 March 2023
+
+History
+-------
+
+    2023-03-28: Henry Winterbottom -- Initial implementation.
+
+"""
