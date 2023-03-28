fca40c672b0131bbc71419bb8439f6935bd64a7c
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-28 16:09:31 -0600

    Added Rocoto level script.

---
+++ jobs/rocoto/gfs_fcst_init.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash --posix
+
+# TODO: Add documentation block.
+
+source "${HOMEgfs}/ush/preamble.sh"
+
+###############################################################
+# Source FV3GFS workflow modules
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ ${status} -ne 0 ]] && exit ${status}
+
+# TODO: clean this up
+source "${HOMEgfs}/ush/detect_machine.sh"
+set +x
+source "${HOMEgfs}/ush/module-setup.sh"
+module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
+module load modules.ufs_model.lua
+module load prod_util
+if [[ "${MACHINE_ID}" = "wcoss2" ]]; then
+  module load cray-pals
+fi
+module list
+unset MACHINE_ID
+set_trace
+
+export job="fcst"
+export jobid="${job}.$$"
+
+###############################################################
+# Execute the JJOB
+${HOMEgfs}/jobs/JGLOBAL_UFS_FORECAST_INIT
+status=$?
+
+
+exit ${status}
