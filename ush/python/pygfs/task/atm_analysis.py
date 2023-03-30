4c747e14e5777eae5ab396f77b61d1baece8201b
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-03-30 23:13:11 +0000

    sync UFS-DA ATM var jobs and atm_analysis task with g-w python (#1313)

--- ush/python/pygfs/task/atm_analysis.py
+++ ush/python/pygfs/task/atm_analysis.py
@@ -4,18 +4,17 @@ import os
 import glob
 import gzip
 import tarfile
-from netCDF4 import Dataset
 from logging import getLogger
 from typing import Dict, List, Any
 
 from pygw.attrdict import AttrDict
 from pygw.file_utils import FileHandler
-from pygw.timetools import to_isotime, to_fv3time, to_timedelta, datetime_to_YMDH
-from pygw.fsutils import rm_p
-from pygw.template import Template, TemplateConstants
+from pygw.timetools import add_to_datetime, to_fv3time, to_timedelta, to_YMDH
+from pygw.fsutils import rm_p, chdir
 from pygw.yaml_file import YAMLFile, parse_yamltmpl, parse_j2yaml, save_as_yaml
 from pygw.logger import logit
 from pygw.executable import Executable
+from pygw.exceptions import WorkflowException
 from pygfs.task.analysis import Analysis
 
 logger = getLogger(__name__.split('.')[-1])
@@ -30,8 +29,9 @@ class AtmAnalysis(Analysis):
         super().__init__(config)
 
         _res = int(self.config['CASE'][1:])
-        _res_enkf = int(self.config['CASE_ENKF'][1:])
-        _window_begin = self.runtime_config.current_cycle - to_timedelta(f"{self.config['assim_freq']}H") / 2
+        _res_anl = int(self.config['CASE_ANL'][1:])
+        _window_begin = add_to_datetime(self.runtime_config.current_cycle, -to_timedelta(f"{self.config['assim_freq']}H") / 2)
+        _fv3jedi_yaml = os.path.join(self.runtime_config.DATA, f"{self.runtime_config.CDUMP}.t{self.runtime_config['cyc']:02d}z.atmvar.yaml")
 
         # Create a local dictionary that is repeatedly used across this class
         local_dict = AttrDict(
@@ -40,15 +40,16 @@ class AtmAnalysis(Analysis):
                 'npy_ges': _res + 1,
                 'npz_ges': self.config.LEVS - 1,
                 'npz': self.config.LEVS - 1,
-                'npx_anl': _res_enkf + 1,
-                'npy_anl': _res_enkf + 1,
+                'npx_anl': _res_anl + 1,
+                'npy_anl': _res_anl + 1,
                 'npz_anl': self.config['LEVS'] - 1,
-                'ATM_WINDOW_BEGIN': to_isotime(_window_begin),
+                'ATM_WINDOW_BEGIN': _window_begin,
                 'ATM_WINDOW_LENGTH': f"PT{self.config['assim_freq']}H",
-                'BKG_ISOTIME': to_isotime(self.runtime_config.current_cycle),
-                'BKG_YYYYmmddHHMMSS': to_fv3time(self.runtime_config.current_cycle),
-                'cdate_fv3': to_fv3time(self.runtime_config.current_cycle),
                 'comin_ges_atm': self.config.COMIN_GES,
+                'OPREFIX': f"{self.runtime_config.CDUMP}.t{self.runtime_config.cyc:02d}z.",  # TODO: CDUMP is being replaced by RUN
+                'APREFIX': f"{self.runtime_config.CDUMP}.t{self.runtime_config.cyc:02d}z.",  # TODO: CDUMP is being replaced by RUN
+                'GPREFIX': f"gdas.t{self.runtime_config.previous_cycle.hour:02d}z.",
+                'fv3jedi_yaml': _fv3jedi_yaml,
             }
         )
 
@@ -69,8 +70,6 @@ class AtmAnalysis(Analysis):
         - linking the JEDI executable (TODO make it copyable, requires JEDI fix)
         - creating output directories
         """
-
-        # stage observations and bias corrections
         super().initialize()
 
         # stage CRTM fix files
@@ -94,21 +93,21 @@ class AtmAnalysis(Analysis):
         FileHandler(self.get_bkg_dict(AttrDict(self.task_config, **self.task_config))).sync()
 
         # generate variational YAML file
-        yaml_out = os.path.join(self.task_config['DATA'], f"{self.task_config['CDUMP']}.t{self.runtime_config['cyc']:02d}z.atmvar.yaml")
-        varda_yaml = YAMLFile(path=self.task_config['ATMVARYAML'])
-        varda_yaml = Template.substitute_structure(varda_yaml, TemplateConstants.DOUBLE_CURLY_BRACES, self.task_config.get)
-        varda_yaml = Template.substitute_structure(varda_yaml, TemplateConstants.DOLLAR_PARENTHESES, self.task_config.get)
-        varda_yaml.save(yaml_out)
-        logger.info(f"Wrote YAML to {yaml_out}")
+        logger.debug(f"Generate variational YAML file: {self.task_config.fv3jedi_yaml}")
+        varda_yaml = parse_j2yaml(self.task_config['ATMVARYAML'], self.task_config)
+        save_as_yaml(varda_yaml, self.task_config.fv3jedi_yaml)
+        logger.info(f"Wrote variational YAML to: {self.task_config.fv3jedi_yaml}")
 
-        # link var executable
+        # link executable to DATA/ directory
         exe_src = self.task_config['JEDIVAREXE']
+        logger.debug(f"Link executable {exe_src} to DATA/")  # TODO: linking is not permitted per EE2.  Needs work in JEDI to be able to copy the exec.
         exe_dest = os.path.join(self.task_config['DATA'], os.path.basename(exe_src))
         if os.path.exists(exe_dest):
             rm_p(exe_dest)
         os.symlink(exe_src, exe_dest)
 
         # need output dir for diags and anl
+        logger.debug("Create empty output [anl, diags] directories to receive output from executable")
         newdirs = [
             os.path.join(self.task_config['DATA'], 'anl'),
             os.path.join(self.task_config['DATA'], 'diags'),
@@ -116,14 +115,34 @@ class AtmAnalysis(Analysis):
         FileHandler({'mkdir': newdirs}).sync()
 
     @logit(logger)
+    def execute(self: Analysis) -> None:
+
+        chdir(self.task_config.DATA)
+
+        exec_cmd = Executable(self.task_config.APRUN_ATMANL)
+        exec_name = os.path.join(self.task_config.DATA, 'fv3jedi_var.x')
+        exec_cmd.add_default_arg(exec_name)
+        exec_cmd.add_default_arg(self.task_config.fv3jedi_yaml)
+
+        try:
+            logger.debug(f"Executing {exec_cmd}")
+            exec_cmd()
+        except OSError:
+            raise OSError(f"Failed to execute {exec_cmd}")
+        except Exception:
+            raise WorkflowException(f"An error occured during execution of {exec_cmd}")
+
+        pass
+
+    @logit(logger)
     def finalize(self: Analysis) -> None:
         """Finalize a global atm analysis
 
         This method will finalize a global atm analysis using JEDI.
         This includes:
-        - tarring up output diag files and place in ROTDIR
-        - copying the generated YAML file from initialize to the ROTDIR
-        - copying the updated bias correction files to ROTDIR
+        - tar output diag files and place in ROTDIR
+        - copy the generated YAML file from initialize to the ROTDIR
+        - copy the updated bias correction files to ROTDIR
         - rewrite UFS-DA atm increment to UFS model readable format with delp and hydrostatic delz calculation
 
         Please note that some of these steps are temporary and will be modified
@@ -134,7 +153,7 @@ class AtmAnalysis(Analysis):
         atmstat = os.path.join(self.task_config['COMOUTatmos'], f"{self.task_config['APREFIX']}atmstat")
 
         # get list of diag files to put in tarball
-        diags = glob.glob('diags/diag*nc4')
+        diags = glob.glob(os.path.join(self.task_config['DATA'], 'diags', 'diag*nc4'))
 
         # gzip the files first
         for diagfile in diags:
@@ -144,7 +163,8 @@ class AtmAnalysis(Analysis):
         # open tar file for writing
         with tarfile.open(atmstat, "w") as archive:
             for diagfile in diags:
-                archive.add(f"{diagfile}.gz")
+                diaggzip = f"{diagfile}.gz"
+                archive.add(diaggzip, arcname=os.path.basename(diaggzip))
 
         # copy full YAML from executable to ROTDIR
         src = os.path.join(self.task_config['DATA'], f"{self.task_config['CDUMP']}.t{self.runtime_config['cyc']:02d}z.atmvar.yaml")
@@ -165,10 +185,10 @@ class AtmAnalysis(Analysis):
             biaslist.append([src, dest])
 
         gprefix = f"{self.task_config['GPREFIX']}"
-        gdate = datetime_to_YMDH(self.task_config['GDATE'])
+        gdate = to_YMDH(self.task_config.previous_cycle)
         gsuffix = f"{gdate}" + ".txt"
         aprefix = f"{self.task_config['APREFIX']}"
-        adate = datetime_to_YMDH(self.task_config['CDATE'])
+        adate = to_YMDH(self.task_config.current_cycle)
         asuffix = f"{adate}" + ".txt"
 
         obsdir = os.path.join(self.task_config['DATA'], 'obs')
@@ -190,17 +210,17 @@ class AtmAnalysis(Analysis):
         # rewrite UFS-DA atm increment to UFS model readable format with delp and hydrostatic delz calculation
         case_berror = int(self.config['CASE_ANL'][1:])
         case = int(self.config['CASE'][1:])
-        gprefix = self.task_config['GPREFIX']
         if case_berror == case:
             atmges_fv3 = os.path.join(self.task_config.comin_ges_atm, f"{self.task_config['GPREFIX']}atmf006.nc")
         else:
             atmges_fv3 = os.path.join(self.task_config.comin_ges_atm, f"{self.task_config['GPREFIX']}atmf006.ensres.nc")
 
-        cdate_inc = self.task_config.cdate_fv3.replace('.', '_')
+        cdate = to_fv3time(self.task_config.current_cycle)
+        cdate_inc = cdate.replace('.', '_')
         atminc_jedi = os.path.join(self.task_config['DATA'], 'anl', f'atminc.{cdate_inc}z.nc4')
         atminc_fv3 = os.path.join(self.task_config['COMOUTatmos'], f"{self.task_config['CDUMP']}.t{self.runtime_config['cyc']:02d}z.atminc.nc")
-
         incpy = os.path.join(self.task_config['HOMEgfs'], 'sorc/gdas.cd/ush/jediinc2fv3.py')
+
         cmd = Executable(incpy)
         cmd.add_default_arg(atmges_fv3)
         cmd.add_default_arg(atminc_jedi)
@@ -227,38 +247,33 @@ class AtmAnalysis(Analysis):
         bkg_dict: Dict
             a dictionary containing the list of model background files to copy for FileHandler
         """
-        super().get_bkg_dict(task_config)
         # NOTE for now this is FV3 RESTART files and just assumed to be fh006
 
         # get FV3 RESTART files, this will be a lot simpler when using history files
         rst_dir = os.path.join(task_config.comin_ges_atm, 'RESTART')  # for now, option later?
+        run_dir = os.path.join(task_config['DATA'], 'bkg')
 
+        # Start accumulating list of background files to copy
         bkglist = []
-        basename = f'{task_config.cdate_fv3}.coupler.res'
-        bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-        basename = f'{task_config.cdate_fv3}.fv_core.res.nc'
-        bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-        basename_cadat = f'{task_config.cdate_fv3}.ca_data.tileX.nc'
-        basename_core = f'{task_config.cdate_fv3}.fv_core.res.tileX.nc'
-        basename_srfwnd = f'{task_config.cdate_fv3}.fv_srf_wnd.res.tileX.nc'
-        basename_tracer = f'{task_config.cdate_fv3}.fv_tracer.res.tileX.nc'
-        basename_phydat = f'{task_config.cdate_fv3}.phy_data.tileX.nc'
-        basename_sfcdat = f'{task_config.cdate_fv3}.sfc_data.tileX.nc'
-        for itile in range(1, task_config.ntiles + 1):
-            basename = basename_cadat.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-            basename = basename_core.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-            basename = basename_srfwnd.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-            basename = basename_tracer.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-            basename = basename_phydat.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
-            basename = basename_sfcdat.replace('tileX', f'tile{itile}')
-            bkglist.append([os.path.join(rst_dir, basename), os.path.join(task_config['DATA'], 'bkg', basename)])
+
+        # atm DA needs coupler
+        basename = f'{to_fv3time(task_config.current_cycle)}.coupler.res'
+        bkglist.append([os.path.join(rst_dir, basename), os.path.join(run_dir, basename)])
+
+        # atm DA needs core, srf_wnd, tracer, phy_data, sfc_data
+        for ftype in ['core', 'srf_wnd', 'tracer']:
+            template = f'{to_fv3time(self.task_config.current_cycle)}.fv_{ftype}.res.tile{{tilenum}}.nc'
+            for itile in range(1, task_config.ntiles + 1):
+                basename = template.format(tilenum=itile)
+                bkglist.append([os.path.join(rst_dir, basename), os.path.join(run_dir, basename)])
+        for ftype in ['phy_data', 'sfc_data']:
+            template = f'{to_fv3time(self.task_config.current_cycle)}.{ftype}.tile{{tilenum}}.nc'
+            for itile in range(1, task_config.ntiles + 1):
+                basename = template.format(tilenum=itile)
+                bkglist.append([os.path.join(rst_dir, basename), os.path.join(run_dir, basename)])
+
         bkg_dict = {
-            'mkdir': [os.path.join(task_config['DATA'], 'bkg')],
+            'mkdir': [run_dir],
             'copy': bkglist,
         }
         return bkg_dict
@@ -283,41 +298,40 @@ class AtmAnalysis(Analysis):
         if config.STATICB_TYPE in ['bump']:
             # BUMP atm static-B needs nicas, cor_rh, cor_rv and stddev files.
             b_dir = config.BERROR_DATA_DIR
-            b_datestr = config.BERROR_DATE
+            b_datestr = to_fv3time(config.BERROR_DATE)
             berror_list = []
+
             for ftype in ['cor_rh', 'cor_rv', 'stddev']:
-                template_bump_coupler = f'{b_datestr}.{ftype}.coupler.res'
-                template_bump_tracer = f'{b_datestr}.{ftype}.fv_tracer.res.tileX.nc'
+                coupler = f'{b_datestr}.{ftype}.coupler.res'
                 berror_list.append([
-                    os.path.join(b_dir, template_bump_coupler),
-                    os.path.join(config.DATA, 'berror', template_bump_coupler)
+                    os.path.join(b_dir, coupler), os.path.join(config.DATA, 'berror', coupler)
                 ])
+                template = '{b_datestr}.{ftype}.fv_tracer.res.tile{{tilenum}}.nc'
                 for itile in range(1, config.ntiles + 1):
-                    bump_tracer = template_bump_tracer.replace('tileX', f'tile{itile}')
+                    tracer = template.format(tilenum=itile)
                     berror_list.append([
-                        os.path.join(b_dir, bump_tracer),
-                        os.path.join(config.DATA, 'berror', bump_tracer)
+                        os.path.join(b_dir, tracer), os.path.join(config.DATA, 'berror', tracer)
                     ])
+
             nproc = config.ntiles * config.layout_x * config.layout_y
             for nn in range(1, nproc + 1):
                 berror_list.append([
-                    os.path.join(b_dir, f'nicas_atm_nicas_local_{nproc:06}-{nn:06}.nc'),
-                    os.path.join(config.DATA, 'berror', f'nicas_atm_nicas_local_{nproc:06}-{nn:06}.nc')
+                    os.path.join(b_dir, f'nicas_aero_nicas_local_{nproc:06}-{nn:06}.nc'),
+                    os.path.join(config.DATA, 'berror', f'nicas_aero_nicas_local_{nproc:06}-{nn:06}.nc')
                 ])
-            berror_dict = {
-                'mkdir': [os.path.join(config.DATA, 'berror')],
-                'copy': berror_list,
-            }
         else:
-            # gsi atm static-B needs namelist and coefficient files.
+            # GSI atm static-B needs namelist and coefficient files.
+            b_dir = os.path.join(config.FV3JEDI_FIX, 'gsibec', config.CASE_ANL)
             berror_list = []
             for ftype in ['gfs_gsi_global.nml', 'gsi-coeffs-gfs-global.nc4']:
                 berror_list.append([
-                    os.path.join(config.FV3JEDI_FIX, 'gsibec', config.CASE_ANL, ftype),
+                    os.path.join(b_dir, ftype),
                     os.path.join(config.DATA, 'berror', ftype)
                 ])
-            berror_dict = {
-                'mkdir': [os.path.join(config.DATA, 'berror')],
-                'copy': berror_list,
-            }
+
+        # stage background error files
+        berror_dict = {
+            'mkdir': [os.path.join(config.DATA, 'berror')],
+            'copy': berror_list,
+        }
         return berror_dict
