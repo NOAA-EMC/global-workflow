7cca6182695644c29b755e47b721e92d2fc732a8
Terry Mcguinness <mterry@Orion-login-1.HPC.MsState.Edu>
2023-03-30 13:09:19 -0500

    added environment for orion

--- ci/environments/orion.sh
+++ ci/environments/orion.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-export GFS_CI_ROOT="TDB" #TODO
+export GFS_CI_ROOT=/work2/noaa/global/mterry/GFS_CI_ROOT
+export ICSDIR_ROOT=/work/noaa/stmp/kfriedma/ICs
 export SLURM_ACCOUNT=fv3-cpu
 export SALLOC_ACCOUNT=${SLURM_ACCOUNT}
 export SBATCH_ACCOUNT=${SLURM_ACCOUNT}
