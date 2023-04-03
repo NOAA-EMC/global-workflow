0c584569871a00a3b7d1cdb55ba01133646ab26b
TerrenceMcGuinness-NOAA <terrence.mcguinness@cox.net>
2023-04-03 10:36:50 -0400

    Update checkout.sh
    Updated HASH as suggested by Cory

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -165,7 +165,7 @@ if [[ ${checkout_gsi} == "YES" ]]; then
 fi
 
 if [[ ${checkout_gdas} == "YES" ]]; then
-  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "fb56a55"; errs=$((errs + $?))
+  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "db2f998"; errs=$((errs + $?))
 fi
 
 if [[ ${checkout_gsi} == "YES" || ${checkout_gdas} == "YES" ]]; then
