2eee6047e4d5755256e48f190ea285a397eedb0f
Terry.McGuinness <Terry.McGuinness@noaa.gov>
2023-04-07 22:56:27 +0000

    added wgrib2 to ci module file for Hera

--- modulefiles/module_ci.hera.lua
+++ modulefiles/module_ci.hera.lua
@@ -10,5 +10,11 @@ prepend_path("MODULEPATH", "/scratch2/NCEPDEV/ensemble/save/Walter.Kolczynski/hp
 load(pathJoin("hpc", "1.2.0"))
 load(pathJoin("hpc-miniconda3", "4.6.14"))
 load(pathJoin("gfs_workflow", "1.0.0"))
+load(pathJoin("nccmp","1.9.0.1"))
+
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+
+load(pathJoin("netcdf", "4.7.4"))
+load(pathJoin("wgrib2", "2.0.8"))
 
 whatis("Description: GFS run setup environment")
