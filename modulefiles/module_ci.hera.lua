a3324a602e0aad8976deb29a61b367fbc339b6a0
Terry.McGuinness <Terry.McGuinness@noaa.gov>
2023-04-07 23:21:05 +0000

    fixed module load order for virtutal python environment

--- modulefiles/module_ci.hera.lua
+++ modulefiles/module_ci.hera.lua
@@ -6,12 +6,6 @@ Load environment to run GFS workflow setup scripts on Hera
 
 load(pathJoin("rocoto"))
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/ensemble/save/Walter.Kolczynski/hpc-stack/modulefiles/stack")
-load(pathJoin("hpc", "1.2.0"))
-load(pathJoin("hpc-miniconda3", "4.6.14"))
-load(pathJoin("gfs_workflow", "1.0.0"))
-load(pathJoin("nccmp","1.9.0.1"))
-
 prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
 
 load(pathJoin("hpc", "1.1.0"))
@@ -20,5 +14,11 @@ load(pathJoin("hpc-impi", "2018.0.4"))
 
 load(pathJoin("netcdf", "4.7.4"))
 load(pathJoin("wgrib2", "2.0.8"))
+
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/ensemble/save/Walter.Kolczynski/hpc-stack/modulefiles/stack")
+load(pathJoin("hpc", "1.2.0"))
+load(pathJoin("hpc-miniconda3", "4.6.14"))
+load(pathJoin("nccmp","1.9.0.1"))
+load(pathJoin("gfs_workflow", "1.0.0"))
 
 whatis("Description: GFS run setup environment")
