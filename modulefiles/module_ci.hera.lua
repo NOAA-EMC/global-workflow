72a4bfdc33380916d392c95d6f1f3d846aaaf56a
Terry.McGuinness <Terry.McGuinness@noaa.gov>
2023-04-07 23:10:59 +0000

    had to add hpc 1.1.0 to module file to get wgrib2

--- modulefiles/module_ci.hera.lua
+++ modulefiles/module_ci.hera.lua
@@ -14,6 +14,10 @@ load(pathJoin("nccmp","1.9.0.1"))
 
 prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
 
+load(pathJoin("hpc", "1.1.0"))
+load(pathJoin("hpc-intel", "18.0.5.274"))
+load(pathJoin("hpc-impi", "2018.0.4"))
+
 load(pathJoin("netcdf", "4.7.4"))
 load(pathJoin("wgrib2", "2.0.8"))
 
