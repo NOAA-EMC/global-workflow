8e5881676ffc5abb341195457b72460714cf57b8
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-04-19 08:18:17 -0600

    Manually updating git.

--- ush/python/pygfs/ufswm/gfs.py
+++ ush/python/pygfs/ufswm/gfs.py
@@ -1,6 +1,8 @@
-import copy
+import os
 import logging
 
+from pygw.attrdict import AttrDict
+from pygw.yaml_file import parse_yamltmpl
 from pygw.logger import logit
 from pygfs.ufswm.ufs import UFS
 
@@ -17,4 +19,46 @@ class GFS(UFS):
         # Start putting fixed properties of the GFS
         self.ntiles = 6
 
+        # Get and read the information regarding configuration of the UFS for this instance
+        self.ufs_config_yaml = self._config.UFS_CONFIG_FILE
+        self.ufs_config_dict = parse_yamltmpl(self.ufs_config_yaml)
+
+        # Set the paths to the various fix directories
+        self.ufs_fix = self.set_ufs_fix(
+            os.path.join(self._config.HOMEgfs, "fix"))
+
         # Determine coupled/uncoupled from config and define as appropriate
+        self.ufs_config = self.set_ufs_config()
+
+    @logit(logger)
+    def prepare_DATA(self) -> None:
+        """
+        Prepare the DATA directory for the GFS
+        Reads the `stage` section of UFS_CONFIG_FILE and creates the necessary directories
+        """
+
+        localconf = AttrDict()
+        localconf.DATA = self._config.DATA
+
+        data = parse_yamltmpl(self.ufs_config_yaml, localconf)
+        self.stage(data.stage)
+
+        self.mdl_config()
+
+    @logit(logger)
+    def stage_fix(self) -> None:
+
+        localconf = AttrDict()
+        localconf.HOMEgfs = self._config.HOMEgfs
+        localconf.DATA = self._config.DATA
+
+        # All the various fix directories
+        for key, value in self.ufs_fix.items():
+            localconf[key] = value
+
+        # various resolution stuff that is needed
+        localconf.atm_res = self.ufs_config.atm_res
+        localconf.ocn_res = self.ufs_config.ocn_res
+
+        data = parse_yamltmpl(self.ufs_config_yaml, localconf)
+        # self.stage(data.fix) HRW: for now
--- ush/python/pygfs/ufswm/ufs.py
+++ ush/python/pygfs/ufswm/ufs.py
@@ -1,10 +1,14 @@
+import os
 import re
 import copy
 import logging
-from typing import Dict, Any
+from typing import Dict, Any, Union, List
 
+from pygw.attrdict import AttrDict
 from pygw.template import Template, TemplateConstants
+from pygw.file_utils import FileHandler
 from pygw.logger import logit
+from pygw.timetools import datetime_to_YMDH
 
 logger = logging.getLogger(__name__.split('.')[-1])
 
@@ -33,6 +37,7 @@ class UFS:
         # Make a deep copy of incoming config for caching purposes. _config should not be updated
         self._config = copy.deepcopy(config)
 
+    @staticmethod
     @logit(logger)
     def parse_ufs_templates(input_template, output_file, ctx: Dict) -> None:
         """
@@ -56,3 +61,120 @@ class UFS:
 
         with open(output_file, 'w') as fho:
             fho.write(file_out)
+
+    @staticmethod
+    @logit(logger)
+    def set_ufs_fix(FIX_dir: str) -> Dict[str, str]:
+        """
+        This method sets the paths to the UFS-weather-model fixed files based on the FIX_dir
+        TODO:  extract this out to a YAML when we have a better idea of what the structure will be
+        """
+
+        fix = AttrDict()
+
+        fix.FIX_aer = os.path.join(FIX_dir, 'aer')
+        fix.FIX_am = os.path.join(FIX_dir, 'am')
+        fix.FIX_lut = os.path.join(FIX_dir, 'lut')
+        fix.FIX_orog = os.path.join(FIX_dir, 'orog')
+        fix.FIX_ugwd = os.path.join(FIX_dir, 'ugwd')
+
+        return fix
+
+    @logit(logger)
+    def set_ufs_config(self) -> Dict[str, Any]:
+        """
+        This method sets the UFS-weather-model configuration based on the big experiment config
+
+
+        Returns
+        -------
+        cfg : Dict
+            UFS-weather-model resolution and other configuration as necessary
+
+        TODO: This method could be broken up into smaller methods for each component, but maintain this as the entry point
+        """
+
+        cfg = AttrDict()
+
+        # TODO: break this into smaller methods for atmos, ocean, etc.
+        cfg.atm_res = self._config.get('CASE', 'C96')
+        cfg.atm_levs = self._config.get('LEVS', 127)
+
+        cfg.ocn_res = self._config.get('OCNRES', '100')
+
+        return cfg
+
+    def mdl_config_defs(self) -> AttrDict:
+        """
+        Description
+        -----------
+
+        This method assigns the default (i.e., non-resolution)
+        dependent attributes for the UFS `model_configure` file.
+
+        Returns
+        -------
+
+        cfg: AttrDict
+
+            A Python dictionary containing the default
+            `model_configure` attributes values.
+
+        """
+
+        cfg = AttrDict()
+
+        CDATE = datetime_to_YMDH(dt=self._config.CDATE)
+        cfg.SYEAR = CDATE[0:4]
+        cfg.SMONTH = CDATE[5:6]
+        cfg.SDAY = CDATE[7:8]
+        cfg.SHOUR = CDATE[9:10]
+
+        cfg.FHMAX = self._config.FHMAX
+        cfg.RESTART_INTERVAL = self._config.restart_interval
+
+        # HRW: This is NoneType for now and will be constructed in a subsquent PR.
+        cfg.FHOUT = None
+        cfg.FILENAME_BASE = "atm", "sfc"
+        cfg.IDEFLATE = self._config.ideflate
+
+        # HRW: Is this resolution dependent?
+        cfg.NBITS = self._config.deflate
+
+        # HRW: What is this? Setting to NoneType for now and will fix
+        # in a subsequent PR.
+        cfg.NSOUT = None
+        cfg.OUTPUT_FILE = f"{self._config.OUTPUT_FILETYPE_ATM}", f"{self._config.OUTPUT_FILETYPE_SFC}"
+        cfg.OUTPUT_GRID = self._config.OUTPUT_GRID
+        cfg.QUILTING = self._config.QUILTING
+        cfg.WRITE_DOPOST = self._config.WRITE_DOPOST
+
+        return cfg
+
+    @logit(logger)
+    def mdl_config(self) -> None:
+        """
+        Description
+        -----------
+
+        This method sets the UFS-weather-model `model_configure` template
+        attributes.
+
+        """
+
+        # Initialize the Python dictionary with the default
+        # `model_configure` attribute values.
+        cfg = self.mdl_config_defs()
+
+    @staticmethod
+    @logit(logger)
+    def stage(data: Union[List[Dict[str, List]], Dict[str, List]]) -> None:
+        """
+        This method stages the UFS-weather-model fixed files
+        """
+
+        if isinstance(data, list):
+            for item in data:
+                FileHandler(item).sync()
+        else:
+            FileHandler(data).sync()
