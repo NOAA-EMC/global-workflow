3e3165602b9919136fb49fcf486c0bb56cd8b691
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-04-24 14:54:34 -0400

    Revert "update GDASApp hash to d34f616 (#1505) (#1506)"
    This reverts commit 699a759f80352aaade4203425c5df24842fcefa3.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -165,7 +165,7 @@ if [[ ${checkout_gsi} == "YES" ]]; then
 fi
 
 if [[ ${checkout_gdas} == "YES" ]]; then
-  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "d34f616"; errs=$((errs + $?))
+  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "db2f998"; errs=$((errs + $?))
 fi
 
 if [[ ${checkout_gsi} == "YES" || ${checkout_gdas} == "YES" ]]; then
