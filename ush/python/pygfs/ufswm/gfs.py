46b349c986848a157820f22b0ce9c3cc435d21cb
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-04-25 10:40:37 -0600

    Fixed linter stuff.

--- ush/python/pygfs/ufswm/gfs.py
+++ ush/python/pygfs/ufswm/gfs.py
@@ -7,15 +7,14 @@ from pygw.yaml_file import parse_yamltmpl
 from pygw.logger import logit
 from pygfs.ufswm.ufs import UFS
 
-logger = logging.getLogger(__name__.split('.')[-1])
+logger = logging.getLogger(__name__.split(".")[-1])
 
 
 class GFS(UFS):
-
     @logit(logger, name="GFS")
     def __init__(self, config):
 
-        super().__init__("GFS", config, HOMEufs=config['HOMEgfs'])
+        super().__init__("GFS", config, HOMEufs=config["HOMEgfs"])
 
         # Start putting fixed properties of the GFS in the ufs_model container
         self.ufs_model.ntiles = 6
@@ -24,7 +23,7 @@ class GFS(UFS):
         self.build_ufs_model()
 
         # Is this an ensemble member?
-        self.member = self._config.get('MEMBER', None)
+        self.member = self._config.get("MEMBER", None)
 
     @logit(logger)
     def build_ufs_model(self) -> None:
@@ -52,29 +51,23 @@ class GFS(UFS):
         # Get model dates relevant for this run
         self._get_date_info()
 
-        logger.debug(
-            f"UFS-weather-model configuration:\n{pformat(self.ufs_model)}")
+        logger.debug(f"UFS-weather-model configuration:\n{pformat(self.ufs_model)}")
 
     @logit(logger)
     def _get_fix_info(self) -> None:
         self.ufs_model.fix = AttrDict()
-        self.ufs_model.fix.FIX_aer = os.path.join(
-            self.ufs_model.HOMEufs, 'fix', 'aer')
-        self.ufs_model.fix.FIX_am = os.path.join(
-            self.ufs_model.HOMEufs, 'fix', 'am')
-        self.ufs_model.fix.FIX_lut = os.path.join(
-            self.ufs_model.HOMEufs, 'fix', 'lut')
-        self.ufs_model.fix.FIX_orog = os.path.join(
-            self.ufs_model.HOMEufs, 'fix', 'orog')
-        self.ufs_model.fix.FIX_ugwd = os.path.join(
-            self.ufs_model.HOMEufs, 'fix', 'ugwd')
+        self.ufs_model.fix.FIX_aer = os.path.join(self.ufs_model.HOMEufs, "fix", "aer")
+        self.ufs_model.fix.FIX_am = os.path.join(self.ufs_model.HOMEufs, "fix", "am")
+        self.ufs_model.fix.FIX_lut = os.path.join(self.ufs_model.HOMEufs, "fix", "lut")
+        self.ufs_model.fix.FIX_orog = os.path.join(self.ufs_model.HOMEufs, "fix", "orog")
+        self.ufs_model.fix.FIX_ugwd = os.path.join(self.ufs_model.HOMEufs, "fix", "ugwd")
 
     @logit(logger)
     def _get_res_info(self) -> None:
         # TODO: break this into smaller methods for atmos, ocean, etc.
-        self.ufs_model.atm_res = self._config.get('CASE', 'C96')
-        self.ufs_model.atm_levs = self._config.get('LEVS', 127)
-        self.ufs_model.ocn_res = str(self._config.get('OCNRES', 100))
+        self.ufs_model.atm_res = self._config.get("CASE", "C96")
+        self.ufs_model.atm_levs = self._config.get("LEVS", 127)
+        self.ufs_model.ocn_res = str(self._config.get("OCNRES", 100))
 
     @logit(logger)
     def get_start_info(self) -> None:
@@ -145,8 +138,7 @@ class GFS(UFS):
         if self.ufs_model.warm_start:  # warm start
             if self.is_a_rerun():  # This is a rerun restart from an intermediate failure point
                 # TODO: To be implemented
-                raise NotImplementedError(
-                    "Rerun restarts are not yet implemented!")
+                raise NotImplementedError("Rerun restarts are not yet implemented!")
             else:  # This is a warm start from the beginning
                 if self.ufs_model.do_iau:
                     start_date = self.ufs_model.previous_cycle
@@ -169,10 +161,9 @@ class GFS(UFS):
         self.ufs_model.iau_offset = iau_offset
         self.ufs_model.fhrot = fhrot
 
-        logger.debug('Returning from _get_date_info() with:')
-        for key in ['start_date', 'current_date', 'iau_offset', 'fhrot']:
-            logger.debug(
-                f"\tself.ufs_model.{key} = {getattr(self.ufs_model, key)}")
+        logger.debug("Returning from _get_date_info() with:")
+        for key in ["start_date", "current_date", "iau_offset", "fhrot"]:
+            logger.debug(f"\tself.ufs_model.{key} = {getattr(self.ufs_model, key)}")
 
     @logit(logger)
     def prepare_DATA(self) -> None:
@@ -213,17 +204,17 @@ class GFS(UFS):
     @logit(logger)
     def stage_tables(self, table, target) -> None:
 
-        run = self._config.get('RUN', 'gdas')
+        run = self._config.get("RUN", "gdas")
 
         localconf = AttrDict()
         localconf.HOMEgfs = self._config.HOMEgfs
         data = parse_yamltmpl(self.ufs_model.yaml_config, localconf).get(table)
-        tables = data.get(run, data.get('default'))
+        tables = data.get(run, data.get("default"))
 
         # Loop over the tables and concatenate into the target
-        with open(target, 'w') as fh:
+        with open(target, "w") as fh:
             for tt in tables:
-                with open(tt, 'r') as fih:
+                with open(tt, "r") as fih:
                     fh.write(fih.read())
 
     @logit(logger)
@@ -238,8 +229,8 @@ class GFS(UFS):
         localconf.SDAY = self.ufs_model.start_date.day
         localconf.SHOUR = self.ufs_model.start_date.hour
 
-        diag_table = os.path.join(self._config.DATA, 'diag_table')
-        self.parse_ufs_templates(diag_table + '.tmpl', diag_table, localconf)
+        diag_table = os.path.join(self._config.DATA, "diag_table")
+        self.parse_ufs_templates(diag_table + ".tmpl", diag_table, localconf)
 
     @logit(logger)
     def prepare_model_configure(self):
@@ -271,10 +262,11 @@ class GFS(UFS):
         cfg = AttrDict()
 
         # TODO: Populate this dictionary as configurations are added.
-        nems_var_dict = {"atm_model": "fv3",
-                         "atm_omp_num_threads": self._config.ATMTHREADS,
-                         "atm_petlist_bounds": self._config.ATMPETS,
-                         }
+        nems_var_dict = {
+            "atm_model": "fv3",
+            "atm_omp_num_threads": self._config.ATMTHREADS,
+            "atm_petlist_bounds": self._config.ATMPETS,
+        }
 
         for (key, value) in nems_var_dict.items():
             setattr(cfg, key, value)
