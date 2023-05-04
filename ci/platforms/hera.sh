c20a681bea026e04f6153ee0ee0b8979fa0392b0
Terry McGuinness <terry.mcguinness@noaa.gov>
2023-05-03 21:39:08 -0500

    update platform source scritps to nems account

--- ci/platforms/hera.sh
+++ ci/platforms/hera.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
 export GFS_CI_ROOT=/scratch1/NCEPDEV/global/Terry.McGuinness/GFS_CI_ROOT
-export SLURM_ACCOUNT=fv3-cpu
-export SALLOC_ACCOUNT="${SLURM_ACCOUNT}"
-export SBATCH_ACCOUNT="${SLURM_ACCOUNT}"
-export SLURM_QOS=debug
+export SLURM_ACCOUNT=nems
 export ICSDIR_ROOT="/scratch1/NCEPDEV/global/glopara/data/ICSDIR"
--- ci/platforms/orion.sh
+++ ci/platforms/orion.sh
@@ -3,9 +3,3 @@
 export GFS_CI_ROOT=/work2/noaa/global/mterry/GFS_CI_ROOT
 export ICSDIR_ROOT=/work/noaa/global/glopara/data/ICSDIR
 export SLURM_ACCOUNT=nems
-export SALLOC_ACCOUNT=${SLURM_ACCOUNT}
-export SBATCH_ACCOUNT=${SLURM_ACCOUNT}
-export SLURM_QOS=debug
-export SLURM_EXCLUSIVE=user
-export OMP_NUM_THREADS=1
-ulimit -s unlimited
