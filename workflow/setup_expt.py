f9216fe1442c12af98e9aa4923913d1b3eb1683e
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2023-05-11 17:59:35 +0000

    Address comments by helpful reviewer

--- workflow/setup_expt.py
+++ workflow/setup_expt.py
@@ -227,11 +227,12 @@ def fill_EXPDIR(inputs):
 
 def update_configs(host, inputs):
 
-    # First update config.base
-    edit_baseconfig(host, inputs)
-
+    # Read in the YAML file to fill out templates and override host defaults
     yaml_path = inputs.yaml
     yaml_dict = YAMLFile(path=yaml_path)
+
+    # First update config.base
+    edit_baseconfig(host, inputs, yaml_dict)
 
     # loop over other configs and update them
     for cfg in yaml_dict.keys():
@@ -244,7 +245,7 @@ def update_configs(host, inputs):
     return
 
 
-def edit_baseconfig(host, inputs):
+def edit_baseconfig(host, inputs, yaml_dict):
     """
     Parses and populates the templated `config.base.emc.dyn` to `config.base`
     """
@@ -291,14 +292,10 @@ def edit_baseconfig(host, inputs):
     extend_dict = {"@CCPP_SUITE@": "FV3_GFS_v17_p8", "@IMP_PHYSICS@": 8}
     tmpl_dict = dict(tmpl_dict, **extend_dict)
 
-    # Read in the YAML file to fill out templates and override host defaults
-    yaml_path = inputs.yaml
-    yaml_dict = YAMLFile(path=yaml_path)
     try:
-        cfg_dict = get_template_dict(yaml_dict['base'])
+        tmpl_dict = dict(tmpl_dict, **get_template_dict(yaml_dict['base']))
     except KeyError:
-        cfg_dict = {}
-    tmpl_dict = dict(tmpl_dict, **cfg_dict)
+        pass
 
     base_input = f'{inputs.configdir}/config.base.emc.dyn'
     base_output = f'{inputs.expdir}/{inputs.pslot}/config.base'
