139ca4d88cbc371414e94adc7010e599f4d1e32b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-05-16 10:01:05 -0400

    update hash for GDASapp to point to feature/stable-nightly

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -165,7 +165,7 @@ if [[ ${checkout_gsi} == "YES" ]]; then
 fi
 
 if [[ ${checkout_gdas} == "YES" ]]; then
-  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "feature/prepland"; errs=$((errs + $?))
+  checkout "gdas.cd" "https://github.com/NOAA-EMC/GDASApp.git" "81675c9"; errs=$((errs + $?))
 fi
 
 if [[ ${checkout_gsi} == "YES" || ${checkout_gdas} == "YES" ]]; then
