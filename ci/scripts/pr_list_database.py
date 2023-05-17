0c2bc0faf9d5f5c8d0917051337d7d5b50a4850c
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-05-17 14:46:31 +0000

    added NumPy Style Python Docstrings to pr_list script

--- ci/scripts/pr_list_database.py
+++ ci/scripts/pr_list_database.py
@@ -2,44 +2,113 @@
 
 import sys
 from pathlib import Path
-import sqlite3
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import sqlite3
 
 
-def sql_connection(filename):
+def sql_connection(filename: Path) -> sqlite3.Connection:
+    """
+    Returns an Sqlite3 Cursor object from a given path to a sqlite3 database file
+
+    Parameters
+    ----------
+    filename : Path
+        Full path to a sqlite3 database file
+
+    Returns
+    -------
+    sqlite3.Connection
+        Sqlite3 Connection object for updating table
+
+    """
     try:
-        con = sqlite3.connect(Path(filename))
-        return con
+        return sqlite3.connect(Path(filename))
     except sqlite3.Error:
         print(sqlite3.Error)
         sys.exit(-1)
 
 
-def sql_table(obj):
+def sql_table(obj: sqlite3.Cursor) -> None:
+    """
+    Creates the initial sqlite3 table for PR states and status
+
+    Parameters
+    ----------
+    obj : sqlite3.Cursor
+         Cursor object for Sqlite3
+
+    """
+
     obj.execute("CREATE TABLE processing(pr integer PRIMARY KEY, state text, status text)")
 
 
-def sql_insert(obj, entities):
+def sql_insert(obj: sqlite3.Cursor, entities: list) -> None:
+    """
+    Inserts a new row in sqlite3 table with PR, state, and status
+
+    Parameters
+    ----------
+    obj : sqlite3.Cursor
+        Cursor object for Sqlite3
+    entities : list
+        The list three string values that go into sqlite table (pr, state, status)
+
+    """
+
     obj.execute('INSERT INTO processing(pr, state, status) VALUES(?, ?, ?)', entities)
 
 
-def sql_update(obj, pr, state, status):
+def sql_update(obj: sqlite3.Cursor, pr: str, state: str, status: str) -> None:
+    """Updates table for a given pr with new values for state and status
+
+    Parameters
+    ----------
+    obj : sqlite.sql_connection
+        sqlite3 Cursor Object
+    pr : str
+        The given pr number to update in the table
+    state : str
+        The new value for the state (Open, Closed)
+    status: str
+        The new value for the status (Ready, Running, Failed)
+
+    """
+
     obj.execute(f'UPDATE processing SET state = "{state}", status = "{status}" WHERE pr = {pr}')
 
 
-def sql_fetch(obj):
+def sql_fetch(obj: sqlite3.Cursor) -> list:
+    """ Gets list of all rows in table
+
+    Parameters
+    ----------
+    obj : sqlite.sql_connection
+        sqlite3 Cursor Object
+
+    """
+
     obj.execute('SELECT * FROM processing')
-    rows = obj.fetchall()
-    return rows
+    return obj.fetchall()
 
 
-def sql_remove(obj, pr):
+def sql_remove(obj: sqlite3.Cursor, pr: str) -> None:
+    """ Removes the row from table with given pr number
+
+    Parameters
+    ----------
+    obj : sqlite.sql_connection
+        sqlite3 Connection Object
+    pr : str
+        pr number acting as key for removing the row with in it
+
+    """
+
     obj.execute(f'DELETE FROM processing WHERE pr = {pr}').rowcount
 
 
 def input_args():
 
-    description = """Arguemtns for creating and updating db file for pr states
+    description = """Arguments for creating and updating db file for pr states
     """
 
     parser = ArgumentParser(description=description,
