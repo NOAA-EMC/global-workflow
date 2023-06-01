c6964caaf636c089ffb06eee7622ebc77e9ada47
Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>
2023-06-01 15:40:41 -0400

    Add GEFS support to rocoto XML generation (#1601)
    To facilitate this, a few classes were refactored into abstract
    classes with child classes for each of the three `$NET`/`$MODE`
    combinations (gfs/cycled, gfs/forecast-only, and gefs). These child
    classes then implement the details particular to their configuration.
    
    GEFS is implemented as only forecast jobs right now: a fcst job
    intended for the control and a metatask of efcs jobs intended for the
    perturbed members. A couple of additional config files were needed in
    order to parse the configs; these were copied from the gfs versions.
    Beyond that, changes have not yet been made to update any configs for
    GEFS. For instance, in contrast to GFS, even the control member of
    GEFS will want `MEMDIR` defined (to 'mem000').
    
    Fixes #1577

--- workflow/applications.py
+++ workflow/applications.py
@@ -1,88 +1,42 @@
 #!/usr/bin/env python3
 
-from typing import Dict, Any
+from typing import Dict, List, Any
 from datetime import timedelta
 from hosts import Host
 from pygw.configuration import Configuration
+from pygw.factory import Factory
+from abc import ABC, ABCMeta, abstractmethod
 
 __all__ = ['AppConfig']
 
 
-def get_gfs_interval(gfs_cyc: int) -> str:
-    """
-    return interval in hours based on gfs_cyc
-    """
-
-    gfs_internal_map = {'0': None, '1': '24:00:00', '2': '12:00:00', '4': '06:00:00'}
-
-    try:
-        return gfs_internal_map[str(gfs_cyc)]
-    except KeyError:
-        raise KeyError(f'Invalid gfs_cyc = {gfs_cyc}')
-
-
-def get_gfs_cyc_dates(base: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Generate GFS dates from experiment dates and gfs_cyc choice
-    """
-
-    base_out = base.copy()
-
-    gfs_cyc = base['gfs_cyc']
-    sdate = base['SDATE']
-    edate = base['EDATE']
-    base_out['INTERVAL'] = '06:00:00'  # Cycled interval is 6 hours
-
-    interval_gfs = get_gfs_interval(gfs_cyc)
-
-    # Set GFS cycling dates
-    hrinc = 0
-    hrdet = 0
-    if gfs_cyc == 0:
-        return base_out
-    elif gfs_cyc == 1:
-        hrinc = 24 - sdate.hour
-        hrdet = edate.hour
-    elif gfs_cyc == 2:
-        if sdate.hour in [0, 12]:
-            hrinc = 12
-        elif sdate.hour in [6, 18]:
-            hrinc = 6
-        if edate.hour in [6, 18]:
-            hrdet = 6
-    elif gfs_cyc == 4:
-        hrinc = 6
-    sdate_gfs = sdate + timedelta(hours=hrinc)
-    edate_gfs = edate - timedelta(hours=hrdet)
-    if sdate_gfs > edate:
-        print('W A R N I N G!')
-        print('Starting date for GFS cycles is after Ending date of experiment')
-        print(f'SDATE = {sdate.strftime("%Y%m%d%H")},     EDATE = {edate.strftime("%Y%m%d%H")}')
-        print(f'SDATE_GFS = {sdate_gfs.strftime("%Y%m%d%H")}, EDATE_GFS = {edate_gfs.strftime("%Y%m%d%H")}')
-        gfs_cyc = 0
-
-    base_out['gfs_cyc'] = gfs_cyc
-    base_out['SDATE_GFS'] = sdate_gfs
-    base_out['EDATE_GFS'] = edate_gfs
-    base_out['INTERVAL_GFS'] = interval_gfs
-
-    fhmax_gfs = {}
-    for hh in ['00', '06', '12', '18']:
-        fhmax_gfs[hh] = base.get(f'FHMAX_GFS_{hh}', base.get('FHMAX_GFS_00', 120))
-    base_out['FHMAX_GFS'] = fhmax_gfs
-
-    return base_out
+class AppConfigInit(ABCMeta):
+    def __call__(cls, *args, **kwargs):
+        '''
+        We want the child classes to be able to define additional settings
+          before we source the configs and complete the rest of the process,
+          so break init up into two methods, one to run first (both in the
+          base class and the child class) and one to finalize the initiali-
+          zation after both have completed.
+        '''
+        obj = type.__call__(cls, *args, **kwargs)
+        obj._init_finalize(*args, **kwargs)
+        return obj
 
 
-class AppConfig:
+class AppConfig(ABC, metaclass=AppConfigInit):
 
     VALID_MODES = ['cycled', 'forecast-only']
+    app_config_factory = Factory(__qualname__)
 
     def __init__(self, conf: Configuration) -> None:
 
         self.scheduler = Host().scheduler
 
         _base = conf.parse_config('config.base')
+        # Define here so the child __init__ functions can use it; will
+        # be overwritten later during _init_finalize().
+        self._base = _base
 
         self.mode = _base['MODE']
 
@@ -91,8 +45,8 @@ class AppConfig:
                                       'Valid application modes are:\n' +
                                       f'{", ".join(self.VALID_MODES)}')
 
+        self.net = _base['NET']
         self.model_app = _base.get('APP', 'ATM')
-        self.do_hybvar = _base.get('DOHYBVAR', False)
         self.do_atm = _base.get('DO_ATM', True)
         self.do_wave = _base.get('DO_WAVE', False)
         self.do_wave_bnd = _base.get('DOBNDPNT_WAVE', False)
@@ -103,15 +57,11 @@ class AppConfig:
         self.do_gempak = _base.get('DO_GEMPAK', False)
         self.do_awips = _base.get('DO_AWIPS', False)
         self.do_vrfy = _base.get('DO_VRFY', True)
-        self.do_fit2obs = _base.get('DO_FIT2OBS', True)
         self.do_metp = _base.get('DO_METP', False)
-        self.do_jediatmvar = _base.get('DO_JEDIATMVAR', False)
-        self.do_jediatmens = _base.get('DO_JEDIATMENS', False)
-        self.do_jediocnvar = _base.get('DO_JEDIOCNVAR', False)
-        self.do_jedilandda = _base.get('DO_JEDILANDDA', False)
-        self.do_mergensst = _base.get('DO_MERGENSST', False)
 
         self.do_hpssarch = _base.get('HPSSARCH', False)
+
+        self.nens = _base.get('NMEM_ENS', 0)
 
         self.wave_cdumps = None
         if self.do_wave:
@@ -121,15 +71,8 @@ class AppConfig:
             elif wave_cdump in ['gfs', 'gdas']:
                 self.wave_cdumps = [wave_cdump]
 
-        self.lobsdiag_forenkf = False
-        self.eupd_cdumps = None
-        if self.do_hybvar:
-            self.lobsdiag_forenkf = _base.get('lobsdiag_forenkf', False)
-            eupd_cdump = _base.get('EUPD_CYC', 'gdas').lower()
-            if eupd_cdump in ['both']:
-                self.eupd_cdumps = ['gfs', 'gdas']
-            elif eupd_cdump in ['gfs', 'gdas']:
-                self.eupd_cdumps = [eupd_cdump]
+    def _init_finalize(self, conf: Configuration):
+        print("Finalizing initialize")
 
         # Get a list of all possible config_files that would be part of the application
         self.configs_names = self._get_app_configs()
@@ -137,15 +80,8 @@ class AppConfig:
         # Source the config_files for the jobs in the application
         self.configs = self._source_configs(conf)
 
-        # Update the base config dictionary based on application
-        upd_base_map = {'cycled': self._cycled_upd_base,
-                        'forecast-only': self._forecast_only_upd_base}
-        try:
-            self.configs['base'] = upd_base_map[self.mode](self.configs['base'])
-        except KeyError:
-            raise NotImplementedError(f'{self.mode} is not a valid application mode.\n' +
-                                      'Valid application modes are:\n' +
-                                      f'{", ".join(self.VALID_MODES)}')
+        # Update the base config dictionary base on application
+        self.configs['base'] = self._update_base(self.configs['base'])
 
         # Save base in the internal state since it is often needed
         self._base = self.configs['base']
@@ -156,21 +92,119 @@ class AppConfig:
         # Finally get task names for the application
         self.task_names = self.get_task_names()
 
+    @abstractmethod
     def _get_app_configs(self):
+        pass
 
-        configs_map = {'cycled': self._cycled_configs,
-                       'forecast-only': self._forecast_only_configs}
+    @staticmethod
+    @abstractmethod
+    def _update_base(base_in: Dict[str, Any]) -> Dict[str, Any]:
+        '''
+        Make final updates to base and return an updated copy
+
+        Parameters
+        ----------
+        base_in: Dict
+                 Beginning base settings
+
+        Returns
+        -------
+        Dict: A copy of base_in with possible modifications based on the
+              net and mode.
+
+        '''
+        pass
+
+    def _source_configs(self, conf: Configuration) -> Dict[str, Any]:
+        """
+        Given the configuration object and jobs,
+        source the configurations for each config and return a dictionary
+        Every config depends on "config.base"
+        """
+
+        configs = dict()
+
+        # Return config.base as well
+        configs['base'] = conf.parse_config('config.base')
+
+        # Source the list of all config_files involved in the application
+        for config in self.configs_names:
+
+            # All must source config.base first
+            files = ['config.base']
+
+            if config in ['eobs', 'eomg']:
+                files += ['config.anal', 'config.eobs']
+            elif config in ['eupd']:
+                files += ['config.anal', 'config.eupd']
+            elif config in ['efcs']:
+                files += ['config.fcst', 'config.efcs']
+            elif 'wave' in config:
+                files += ['config.wave', f'config.{config}']
+            else:
+                files += [f'config.{config}']
+
+            print(f'sourcing config.{config}')
+            configs[config] = conf.parse_config(files)
+
+        return configs
+
+    @abstractmethod
+    def get_task_names(self) -> Dict[str, List[str]]:
+        '''
+        Create a list of task names for each CDUMP valid for the configuation.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        Dict[str, List[str]]: Lists of tasks for each CDUMP.
+
+        '''
+        pass
+
+    @staticmethod
+    def get_gfs_interval(gfs_cyc: int) -> str:
+        """
+        return interval in hours based on gfs_cyc
+        """
+
+        gfs_internal_map = {'0': None, '1': '24:00:00', '2': '12:00:00', '4': '06:00:00'}
+
         try:
-            configs_names = configs_map[self.mode]
+            return gfs_internal_map[str(gfs_cyc)]
         except KeyError:
-            raise NotImplementedError(f'{self.mode} is not a valid application mode.\n' +
-                                      'Valid application modes are:\n' +
-                                      f'{", ".join(self.VALID_MODES)}')
+            raise KeyError(f'Invalid gfs_cyc = {gfs_cyc}')
 
-        return configs_names
 
-    @property
-    def _cycled_configs(self):
+class GFSCycledAppConfig(AppConfig):
+    '''
+    Class to define GFS cycled configurations
+    '''
+
+    def __init__(self, conf: Configuration):
+        super().__init__(conf)
+        self.do_hybvar = self._base.get('DOHYBVAR', False)
+        self.do_fit2obs = self._base.get('DO_FIT2OBS', True)
+        self.do_jediatmvar = self._base.get('DO_JEDIATMVAR', False)
+        self.do_jediatmens = self._base.get('DO_JEDIATMENS', False)
+        self.do_jediocnvar = self._base.get('DO_JEDIOCNVAR', False)
+        self.do_jedilandda = self._base.get('DO_JEDILANDDA', False)
+        self.do_mergensst = self._base.get('DO_MERGENSST', False)
+
+        self.lobsdiag_forenkf = False
+        self.eupd_cdumps = None
+        if self.do_hybvar:
+            self.lobsdiag_forenkf = self._base.get('lobsdiag_forenkf', False)
+            eupd_cdump = self._base.get('EUPD_CYC', 'gdas').lower()
+            if eupd_cdump in ['both']:
+                self.eupd_cdumps = ['gfs', 'gdas']
+            elif eupd_cdump in ['gfs', 'gdas']:
+                self.eupd_cdumps = [eupd_cdump]
+
+    def _get_app_configs(self):
         """
         Returns the config_files that are involved in the cycled app
         """
@@ -226,106 +260,12 @@ class AppConfig:
 
         return configs
 
-    @property
-    def _forecast_only_configs(self):
-        """
-        Returns the config_files that are involved in the forecast-only app
-        """
-
-        configs = ['coupled_ic', 'fcst', 'arch']
-
-        if self.do_atm:
-            configs += ['post', 'vrfy']
-
-        if self.do_aero:
-            configs += ['aerosol_init']
-
-        if self.do_ocean or self.do_ice:
-            configs += ['ocnpost']
-
-        if self.do_atm and self.do_metp:
-            configs += ['metp']
-
-        if self.do_gempak:
-            configs += ['gempak']
-
-        if self.do_awips:
-            configs += ['awips']
-
-        if self.do_wave:
-            configs += ['waveinit', 'waveprep', 'wavepostsbs', 'wavepostpnt']
-            if self.do_wave_bnd:
-                configs += ['wavepostbndpnt', 'wavepostbndpntbll']
-            if self.do_gempak:
-                configs += ['wavegempak']
-            if self.do_awips:
-                configs += ['waveawipsbulls', 'waveawipsgridded']
-
-        return configs
-
     @staticmethod
-    def _cycled_upd_base(base_in):
+    def _update_base(base_in):
 
-        return get_gfs_cyc_dates(base_in)
-
-    @staticmethod
-    def _forecast_only_upd_base(base_in):
-
-        base_out = base_in.copy()
-        base_out['INTERVAL_GFS'] = get_gfs_interval(base_in['gfs_cyc'])
-        base_out['CDUMP'] = 'gfs'
-
-        return base_out
-
-    def _source_configs(self, conf: Configuration) -> Dict[str, Any]:
-        """
-        Given the configuration object and jobs,
-        source the configurations for each config and return a dictionary
-        Every config depends on "config.base"
-        """
-
-        configs = dict()
-
-        # Return config.base as well
-        configs['base'] = conf.parse_config('config.base')
-
-        # Source the list of all config_files involved in the application
-        for config in self.configs_names:
-
-            # All must source config.base first
-            files = ['config.base']
-
-            if config in ['eobs', 'eomg']:
-                files += ['config.anal', 'config.eobs']
-            elif config in ['eupd']:
-                files += ['config.anal', 'config.eupd']
-            elif config in ['efcs']:
-                files += ['config.fcst', 'config.efcs']
-            elif 'wave' in config:
-                files += ['config.wave', f'config.{config}']
-            else:
-                files += [f'config.{config}']
-
-            print(f'sourcing config.{config}')
-            configs[config] = conf.parse_config(files)
-
-        return configs
+        return GFSCycledAppConfig.get_gfs_cyc_dates(base_in)
 
     def get_task_names(self):
-
-        # Get a list of all possible tasks that would be part of the application
-        tasks_map = {'cycled': self._get_cycled_task_names,
-                     'forecast-only': self._get_forecast_only_task_names}
-        try:
-            task_names = tasks_map[self.mode]()
-        except KeyError:
-            raise NotImplementedError(f'{self.mode} is not a valid application mode.\n' +
-                                      'Valid application modes are:\n' +
-                                      f'{", ".join(self.VALID_MODES)}')
-
-        return task_names
-
-    def _get_cycled_task_names(self):
         """
         Get the task names for all the tasks in the cycled application.
         Note that the order of the task names matters in the XML.
@@ -444,7 +384,114 @@ class AppConfig:
 
         return tasks
 
-    def _get_forecast_only_task_names(self):
+    @staticmethod
+    def get_gfs_cyc_dates(base: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Generate GFS dates from experiment dates and gfs_cyc choice
+        """
+
+        base_out = base.copy()
+
+        gfs_cyc = base['gfs_cyc']
+        sdate = base['SDATE']
+        edate = base['EDATE']
+        base_out['INTERVAL'] = '06:00:00'  # Cycled interval is 6 hours
+
+        interval_gfs = AppConfig.get_gfs_interval(gfs_cyc)
+
+        # Set GFS cycling dates
+        hrinc = 0
+        hrdet = 0
+        if gfs_cyc == 0:
+            return base_out
+        elif gfs_cyc == 1:
+            hrinc = 24 - sdate.hour
+            hrdet = edate.hour
+        elif gfs_cyc == 2:
+            if sdate.hour in [0, 12]:
+                hrinc = 12
+            elif sdate.hour in [6, 18]:
+                hrinc = 6
+            if edate.hour in [6, 18]:
+                hrdet = 6
+        elif gfs_cyc == 4:
+            hrinc = 6
+        sdate_gfs = sdate + timedelta(hours=hrinc)
+        edate_gfs = edate - timedelta(hours=hrdet)
+        if sdate_gfs > edate:
+            print('W A R N I N G!')
+            print('Starting date for GFS cycles is after Ending date of experiment')
+            print(f'SDATE = {sdate.strftime("%Y%m%d%H")},     EDATE = {edate.strftime("%Y%m%d%H")}')
+            print(f'SDATE_GFS = {sdate_gfs.strftime("%Y%m%d%H")}, EDATE_GFS = {edate_gfs.strftime("%Y%m%d%H")}')
+            gfs_cyc = 0
+
+        base_out['gfs_cyc'] = gfs_cyc
+        base_out['SDATE_GFS'] = sdate_gfs
+        base_out['EDATE_GFS'] = edate_gfs
+        base_out['INTERVAL_GFS'] = interval_gfs
+
+        fhmax_gfs = {}
+        for hh in ['00', '06', '12', '18']:
+            fhmax_gfs[hh] = base.get(f'FHMAX_GFS_{hh}', base.get('FHMAX_GFS_00', 120))
+        base_out['FHMAX_GFS'] = fhmax_gfs
+
+        return base_out
+
+
+class GFSForecastOnlyAppConfig(AppConfig):
+    '''
+    Class to define GFS forecast-only configurations
+    '''
+
+    def __init__(self, conf: Configuration):
+        super().__init__(conf)
+
+    def _get_app_configs(self):
+        """
+        Returns the config_files that are involved in the forecast-only app
+        """
+
+        configs = ['coupled_ic', 'fcst', 'arch']
+
+        if self.do_atm:
+            configs += ['post', 'vrfy']
+
+        if self.do_aero:
+            configs += ['aerosol_init']
+
+        if self.do_ocean or self.do_ice:
+            configs += ['ocnpost']
+
+        if self.do_atm and self.do_metp:
+            configs += ['metp']
+
+        if self.do_gempak:
+            configs += ['gempak']
+
+        if self.do_awips:
+            configs += ['awips']
+
+        if self.do_wave:
+            configs += ['waveinit', 'waveprep', 'wavepostsbs', 'wavepostpnt']
+            if self.do_wave_bnd:
+                configs += ['wavepostbndpnt', 'wavepostbndpntbll']
+            if self.do_gempak:
+                configs += ['wavegempak']
+            if self.do_awips:
+                configs += ['waveawipsbulls', 'waveawipsgridded']
+
+        return configs
+
+    @staticmethod
+    def _update_base(base_in):
+
+        base_out = base_in.copy()
+        base_out['INTERVAL_GFS'] = AppConfig.get_gfs_interval(base_in['gfs_cyc'])
+        base_out['CDUMP'] = 'gfs'
+
+        return base_out
+
+    def get_task_names(self):
         """
         Get the task names for all the tasks in the forecast-only application.
         Note that the order of the task names matters in the XML.
@@ -495,3 +542,46 @@ class AppConfig:
         tasks += ['arch']  # arch **must** be the last task
 
         return {f"{self._base['CDUMP']}": tasks}
+
+
+class GEFSAppConfig(AppConfig):
+    '''
+    Class to define GEFS configurations
+    '''
+
+    def __init__(self, conf: Configuration):
+        super().__init__(conf)
+
+    def _get_app_configs(self):
+        """
+        Returns the config_files that are involved in gefs
+        """
+        configs = ['fcst']
+
+        if self.nens > 0:
+            configs += ['efcs']
+
+        return configs
+
+    @staticmethod
+    def _update_base(base_in):
+
+        base_out = base_in.copy()
+        base_out['INTERVAL_GFS'] = AppConfig.get_gfs_interval(base_in['gfs_cyc'])
+        base_out['CDUMP'] = 'gefs'
+
+        return base_out
+
+    def get_task_names(self):
+
+        tasks = ['fcst']
+
+        if self.nens > 0:
+            tasks += ['efcs']
+
+        return {f"{self._base['CDUMP']}": tasks}
+
+
+AppConfig.app_config_factory.register('gfs_cycled', GFSCycledAppConfig)
+AppConfig.app_config_factory.register('gfs_forecast-only', GFSForecastOnlyAppConfig)
+AppConfig.app_config_factory.register('gefs_forecast-only', GEFSAppConfig)
