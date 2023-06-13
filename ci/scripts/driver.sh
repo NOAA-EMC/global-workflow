d5218adb770d7567a8c9c7be83b665b80548c97c
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-06-13 13:52:55 +0000

    f left beheind

--- ci/scripts/driver.sh
+++ ci/scripts/driver.sh
@@ -87,7 +87,6 @@ for pr in ${pr_list}; do
       sacct --format=jobid,jobname%35,WorkDir%100,stat | grep "${pslot}" | grep "PR\/${pr}\/RUNTESTS" |  awk '{print $1}' | xargs scancel || true
     done
     rm -Rf "${pr_dir}"
-  f
 done
 
 pr_list=""
