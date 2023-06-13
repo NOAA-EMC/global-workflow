fac8082ecc0e7c5fee41e0fee03213e79c2192bc
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-06-13 11:07:58 -0400

    update ufs-weather-model hash and update compile signature

--- sorc/build_ufs.sh
+++ sorc/build_ufs.sh
@@ -5,11 +5,7 @@ cwd=$(pwd)
 
 # Default settings
 APP="S2SWA"
-CCPP_SUITES="FV3_GFS_v16,FV3_GFS_v16_no_nsst,FV3_GFS_v16_ugwpv1,FV3_GFS_v17_p8,FV3_GFS_v16_coupled_nsstNoahmpUGWPv1,FV3_GFS_v17_coupled_p8"
-
-export RT_COMPILER="intel"
-source "${cwd}/ufs_model.fd/tests/detect_machine.sh"
-source "${cwd}/ufs_model.fd/tests/module-setup.sh"
+CCPP_SUITES="FV3_GFS_v16,FV3_GFS_v16_no_nsst,FV3_GFS_v16_ugwpv1,FV3_GFS_v17_p8,FV3_GFS_v16_coupled_nsstNoahmpUGWPv1,FV3_GFS_v17_coupled_p8"  # TODO: does the g-w need to build with all these CCPP_SUITES?
 
 while getopts ":da:v" option; do
   case "${option}" in
@@ -27,15 +23,19 @@ done
 
 cd "${cwd}/ufs_model.fd"
 
+source "./tests/detect_machine.sh"
+source "./tests/module-setup.sh"
+
 MAKE_OPT="-DAPP=${APP} -D32BIT=ON -DCCPP_SUITES=${CCPP_SUITES}"
 [[ ${BUILD_TYPE:-"Release"} = "DEBUG" ]] && MAKE_OPT+=" -DDEBUG=ON"
 COMPILE_NR=0
 CLEAN_BEFORE=YES
 CLEAN_AFTER=NO
 
-./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_NR}" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
+./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "intel" "${COMPILE_NR}" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
 mv "./tests/fv3_${COMPILE_NR}.exe" ./tests/ufs_model.x
 mv "./tests/modules.fv3_${COMPILE_NR}.lua" ./tests/modules.ufs_model.lua
 cp "./modulefiles/ufs_common.lua" ./tests/ufs_common.lua
+cp "./modulefiles/ufs_common_spack.lua" ./tests/ufs_common_spack.lua
 
 exit 0
