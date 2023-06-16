235463f91a55088660023b9550f1a8ad3d75384a
Henry R. Winterbottom <49202169+HenryWinterbottom-NOAA@users.noreply.github.com>
2023-06-16 12:22:47 -0600

    Update sorc/build_ufs.sh
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- sorc/build_ufs.sh
+++ sorc/build_ufs.sh
@@ -33,7 +33,7 @@ CLEAN_BEFORE=YES
 CLEAN_AFTER=NO
 
 if [[ "${MACHINE_ID}" != "noaacloud" ]]; then
-  ./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_NR}" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
+  ./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_NR}" "intel" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
   mv "./tests/fv3_${COMPILE_NR}.exe" ./tests/ufs_model.x
   mv "./tests/modules.fv3_${COMPILE_NR}.lua" ./tests/modules.ufs_model.lua
   cp "./modulefiles/ufs_common.lua" ./tests/ufs_common.lua
