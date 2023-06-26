4ef9d9a65639c0a132172b55edee4de723245e16
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-06-26 14:15:32 -0400

    take suggestion

--- modulefiles/module_gwsetup.s4.lua
+++ modulefiles/module_gwsetup.s4.lua
@@ -3,5 +3,7 @@ Load environment to run GFS workflow setup scripts on S4
 ]])
 
 load(pathJoin("miniconda", "3.8-s4"))
+load(pathJoin("rocoto","1.3.5"))
+load(pathJoin("git","2.30.0"))
 
 whatis("Description: GFS run setup environment")
