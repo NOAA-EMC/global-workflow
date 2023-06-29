4a6b0c19b1c175305bdd5a5d1f88dc9735c91b45
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-06-28 21:59:14 -0400

    Update ush/forecast_postdet.sh
    Co-authored-by: Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -1024,7 +1024,7 @@ GOCART_postdet() {
       rm -f "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
     fi
 
-    ${NLN} "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:4}${vdate:4:2}${vdate:6:2}_${vdate:8:2}00z.nc4" \
-           "${DATA}/gocart.inst_aod.${vdate:0:4}${vdate:4:2}${vdate:6:2}_${vdate:8:2}00z.nc4"
+    ${NLN} "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4" \
+           "${DATA}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
   done
 }
