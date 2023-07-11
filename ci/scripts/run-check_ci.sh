72fa5394dc5805a8eab0ac2ff45d7b000f1b6dc0
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-07-11 16:58:47 -0400

    tighten the runner dirs

--- ci/scripts/run-check_ci.sh
+++ ci/scripts/run-check_ci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -eux
 
 #####################################################################################
 #
@@ -8,63 +7,21 @@ set -eux
 #                     to run from within a cron job in the CI Managers account
 #####################################################################################
 
-scriptname=$(basename "${BASH_SOURCE[0]}")
-echo "Begin ${scriptname} at $(date -u)" || true
-export PS4='+ $(basename ${BASH_SOURCE})[${LINENO}]'
-
-#########################################################################
-#  Set up runtime environment varibles for accounts on supproted machines
-#########################################################################
-
-source "${HOMEgfs}/ush/detect_machine.sh"
-case ${MACHINE_ID} in
-  hera | orion)
-   echo "Running Automated Testing on ${MACHINE_ID}"
-   source "${HOMEgfs}/ci/platforms/${MACHINE_ID}.sh"
-   ;;
- *)
-   echo "Unsupported platform. Exiting with error."
-   exit 1
-   ;;
-esac
-set +x
-source "${HOMEgfs}/ush/module-setup.sh"
-module use "${HOMEgfs}/modulefiles"
-module load "module_gwsetup.${MACHINE_ID}"
-module list
-set -x
-rocotorun=$(command -v rocotorun)
-if [[ -z ${rocotorun} ]]; then
-  echo "rocotorun not found on system"
-  exit 1
-else
-  echo "rocotorun being used from ${rocotorun}"
-fi
-rocotostat=$(command -v rocotostat)
-if [[ -z ${rocotostat+x} ]]; then
-  echo "rocotostat not found on system"
-  exit 1
-else
-  echo "rocotostat being used from ${rocotostat}"
-fi
-rocotocheck=$(command -v rocotocheck)
-if [[ -z ${rocotocheck+x} ]]; then
-  echo "rocotocheck not found on system"
-  exit 1
-else
-  echo "rocotocheck being used from ${rocotocheck}"
-fi
+source "${HOMEgfs}/ush/preamble.sh"
+source "${HOMEgfs}/workflow/gw_setup.sh"
 
 xml="${EXPDIR}/${pslot}/${pslot}.xml"
 db="${EXPDIR}/${pslot}/${pslot}.db"
 
-RUN_COMPLETE="FALSE"
-while [[ ${RUN_COMPLETE} == "FALSE" ]]; do
+# Ensure the XML is present for the experiment
+if [[ ! -f "${xml}" ]]; then
+  echo "XML file ${xml} not found, experiment ${pslot} failed"
+  exit 1
+fi
 
-  if [[ ! -f "${xml}" ]]; then
-    echo "XML file ${xml} not found, experiment ${pslot} failed"
-    exit 1
-  fi
+rc=99
+while true; do
+
   echo "Running: ${rocotorun} -v 10 -w ${xml} -d ${db}"
   "${rocotorun}" -v 10 -w "${xml}" -d "${db}"
 
@@ -72,14 +29,16 @@ while [[ ${RUN_COMPLETE} == "FALSE" ]]; do
   sleep 30
   if [[ ! -f "${db}" ]]; then
     echo "Database file ${db} not found, experiment ${pslot} failed"
-    exit 1
+    exit 2
   fi
 
+  # Get job statistics
   rocoto_stat_output=$("${rocotostat}" -w "${xml}" -d "${db}" -s | grep -v CYCLE) || true
   num_cycles=$(echo "${rocoto_stat_output}" | wc -l) || true
   num_done=$(echo "${rocoto_stat_output}" | grep -c Done) || true
   num_succeeded=$("${rocotostat}" -w "${xml}" -d "${db}" -a | grep -c SUCCEEDED) || true
   num_failed=$("${rocotostat}" -w "${xml}" -d "${db}" -a | grep -c -E 'FAIL|DEAD') || true
+
   echo "${pslot} Total Cycles: ${num_cycles} number done: ${num_done}"
 
   if [[ ${num_failed} -ne 0 ]]; then
@@ -87,6 +46,7 @@ while [[ ${RUN_COMPLETE} == "FALSE" ]]; do
       echo "Experiment ${pslot} Terminated: *FAILED*"
       echo "Experiment ${pslot} Terminated with ${num_failed} tasks failed at $(date)" || true
     } >> "${HOMEgfs}/global-workflow.log"
+
     error_logs=$("${rocotostat}" -d "${db}" -w "${xml}" | grep -E 'FAIL|DEAD' | awk '{print "-c", $1, "-t", $2}' | xargs "${rocotocheck}" -d "${db}" -w "${xml}" | grep join | awk '{print $2}') || true
     {
      echo "Error logs:"
@@ -96,18 +56,20 @@ while [[ ${RUN_COMPLETE} == "FALSE" ]]; do
     sacct --format=jobid,jobname%35,WorkDir%100,stat | grep "${pslot}" | grep "${pr}\/RUNTESTS" |  awk '{print $1}' | xargs scancel || true
   fi
 
-  if [[ "${num_done}" -eq  "${num_cycles}" ]]; then
+  if [[ "${num_done}" -eq "${num_cycles}" ]]; then
     {
       echo "Experiment ${pslot} completed: *SUCCESS*"
       echo "Experiment ${pslot} Completed at $(date)" || true
       echo "with ${num_succeeded} successfully completed jobs" || true
     } >> "${HOMEgfs}/global-workflow.log"
     sed -i "s/\`\`\`//2g" "${HOMEgfs}/global-workflow.log"
-    RUN_COMPLETE="TRUE"
-    exit 0
+    rc=0
+    break
   fi
 
   # Wait before running rocotorun again
-  sleep 210
+  sleep 300
 
 done
+
+exit "${rc}"
