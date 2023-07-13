3656a48ea145287035643f6cc136e09957160757
Terry McGuinness <terry.mcguinness@noaa.gov>
2023-07-12 19:03:32 -0500

    removed Rocoto utilities as vars as this scripts is not run in cron and does not need full paths

--- ci/scripts/run-check_ci.sh
+++ ci/scripts/run-check_ci.sh
@@ -26,8 +26,8 @@ fi
 rc=99
 while true; do
 
-  echo "Running: ${rocotorun} -v 10 -w ${xml} -d ${db}"
-  "${rocotorun}" -v 10 -w "${xml}" -d "${db}"
+  echo "Running: rocotorun -v 10 -w ${xml} -d ${db}"
+  rocotorun -v 10 -w "${xml}" -d "${db}"
 
   # Wait before running rocotostat
   sleep 30
@@ -37,11 +37,11 @@ while true; do
   fi
 
   # Get job statistics
-  rocoto_stat_output=$("${rocotostat}" -w "${xml}" -d "${db}" -s | grep -v CYCLE) || true
-  num_cycles=$(echo "${rocoto_stat_output}" | wc -l) || true
-  num_done=$(echo "${rocoto_stat_output}" | grep -c Done) || true
-  num_succeeded=$("${rocotostat}" -w "${xml}" -d "${db}" -a | grep -c SUCCEEDED) || true
-  num_failed=$("${rocotostat}" -w "${xml}" -d "${db}" -a | grep -c -E 'FAIL|DEAD') || true
+  rocoto_stat_output=$(rocotostat -w "${xml}" -d "${db}" -s | grep -v CYCLE) || true
+  num_cycles=$(echo rocoto_stat_output | wc -l) || true
+  num_done=$(echo rocoto_stat_output | grep -c Done) || true
+  num_succeeded=$(rocotostat -w "${xml}" -d "${db}" -a | grep -c SUCCEEDED) || true
+  num_failed=$(rocotostat -w "${xml}" -d "${db}" -a | grep -c -E 'FAIL|DEAD') || true
 
   echo "${pslot} Total Cycles: ${num_cycles} number done: ${num_done}"
 
@@ -51,7 +51,7 @@ while true; do
       echo "Experiment ${pslot} Terminated with ${num_failed} tasks failed at $(date)" || true
     } >> "${HOMEgfs}/global-workflow.log"
 
-    error_logs=$("${rocotostat}" -d "${db}" -w "${xml}" | grep -E 'FAIL|DEAD' | awk '{print "-c", $1, "-t", $2}' | xargs "${rocotocheck}" -d "${db}" -w "${xml}" | grep join | awk '{print $2}') || true
+    error_logs=$(rocotostat -d "${db}" -w "${xml}" | grep -E 'FAIL|DEAD' | awk '{print "-c", $1, "-t", $2}' | xargs rocotocheck -d "${db}" -w "${xml}" | grep join | awk '{print $2}') || true
     {
      echo "Error logs:"
      echo "${error_logs}"
