c7bfc8bdca15bf68394fadd68e1f2dbf3bac431a
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-07-14 12:20:57 -0400

    minor updates to tighten the output and better structure for each PR id

--- ci/scripts/run-check_ci.sh
+++ ci/scripts/run-check_ci.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+set -eu
+
 #####################################################################################
-#
-# Script description: BASH script for checking for cases in a given PR and
-#                     simply running rocotorun on each.  This script is intended
-#                     to run from within a cron job in the CI Managers account
+# Script description: script to check the status of an experiment as reported
+#                     by Rocoto
 #####################################################################################
 
-HOMEgfs=${1:-${HOMEgfs:-?}}
-EXPDIR=${2:-${EXPDIR:-?}}
-pslot=${3:-${pslot:-?}}
+TEST_DIR=${1:-${TEST_DIR:-?}}  # Location of the root of the testing directory
+pslot=${2:-${pslot:-?}}        # Name of the experiment being tested by this script
 
-source "${HOMEgfs}/ush/preamble.sh"
+# TEST_DIR contains 2 directories;
+# 1. HOMEgfs: clone of the global-workflow
+# 2. RUNTESTS: A directory containing EXPDIR and COMROT for experiments
+# # e.g. $> tree ./TEST_DIR
+# ./TEST_DIR
+# ├── HOMEgfs
+# └── RUNTESTS
+#     ├── COMROT
+#     │   └── ${pslot}
+#     └── EXPDIR
+#         └── ${pslot}
+HOMEgfs="${TEST_DIR}/HOMEgfs"
+RUNTESTS="${TEST_DIR}/RUNTESTS"
+
+# Source modules and setup logging
 source "${HOMEgfs}/workflow/gw_setup.sh"
 
-xml="${EXPDIR}/${pslot}/${pslot}.xml"
-db="${EXPDIR}/${pslot}/${pslot}.db"
+# cd into the experiment directory
+cd "${RUNTESTS}/EXPDIR/${pslot}" || (echo "FATAL ERROR: Unable to cd into '${RUNTESTS}/EXPDIR/${pslot}', ABORT!"; exit 1)
+
+# Name of the Rocoto XML and database files
+xml="${pslot}.xml"
+db="${pslot}.db"
 
 # Ensure the XML is present for the experiment
 if [[ ! -f "${xml}" ]]; then
-  echo "XML file ${xml} not found, experiment ${pslot} failed"
+  echo "FATAL ERROR: XML file ${xml} not found in '${pslot}', experiment ${pslot} failed, ABORT!"
   exit 1
 fi
 
@@ -30,13 +47,14 @@ while true; do
   rocotorun -v 10 -w "${xml}" -d "${db}"
 
   # Wait before running rocotostat
-  sleep 30
+  echo "wait 30s"; sleep 30
   if [[ ! -f "${db}" ]]; then
     echo "Database file ${db} not found, experiment ${pslot} failed"
     exit 2
   fi
 
   # Get job statistics
+  echo "Gather Rocoto statistics"
   rocotostat_output=$(rocotostat -w "${xml}" -d "${db}" -s | grep -v CYCLE) || true
   num_cycles=$(echo "${rocotostat_output}" | wc -l) || true
   num_done=$(echo "${rocotostat_output}" | grep -c Done) || true
@@ -47,16 +65,16 @@ while true; do
 
   if [[ ${num_failed} -ne 0 ]]; then
     {
-      echo "Experiment ${pslot} Terminated: *FAILED*"
       echo "Experiment ${pslot} Terminated with ${num_failed} tasks failed at $(date)" || true
-    } >> "${HOMEgfs}/global-workflow.log"
+      echo "Experiment ${pslot} Terminated: *FAILED*"
+    } >> "${RUNTESTS}/ci.log"
 
     error_logs=$(rocotostat -d "${db}" -w "${xml}" | grep -E 'FAIL|DEAD' | awk '{print "-c", $1, "-t", $2}' | xargs rocotocheck -d "${db}" -w "${xml}" | grep join | awk '{print $2}') || true
     {
      echo "Error logs:"
      echo "${error_logs}"
-    } >> "${HOMEgfs}/global-workflow.log"
-    sed -i "s/\`\`\`//2g" "${HOMEgfs}/global-workflow.log"
+    } >> "${RUNTESTS}/ci.log"
+    sed -i "s/\`\`\`//2g" "${RUNTESTS}/ci.log"
     sacct --format=jobid,jobname%35,WorkDir%100,stat | grep "${pslot}" | grep "${pr}\/RUNTESTS" |  awk '{print $1}' | xargs scancel || true
     rc=1
     break
@@ -64,11 +82,11 @@ while true; do
 
   if [[ "${num_done}" -eq "${num_cycles}" ]]; then
     {
-      echo "Experiment ${pslot} completed: *SUCCESS*"
       echo "Experiment ${pslot} Completed at $(date)" || true
       echo "with ${num_succeeded} successfully completed jobs" || true
-    } >> "${HOMEgfs}/global-workflow.log"
-    sed -i "s/\`\`\`//2g" "${HOMEgfs}/global-workflow.log"
+      echo "Experiment ${pslot} Completed: *SUCCESS*"
+    } >> "${RUNTESTS}/ci.log"
+    sed -i "s/\`\`\`//2g" "${RUNTESTS}/ci.log"
     rc=0
     break
   fi
