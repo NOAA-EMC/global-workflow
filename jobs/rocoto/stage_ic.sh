d05f73bfa419070b955c08607abc84549889e16f
AnilKumar-NOAA <anil.kumar@noaa.gov>
2023-08-28 16:53:03 +0000

    shell check warning remove from stage_ic

--- jobs/rocoto/stage_ic.sh
+++ jobs/rocoto/stage_ic.sh
@@ -3,7 +3,7 @@
 source "${HOMEgfs}/ush/preamble.sh"
 
 # Source FV3GFS workflow modules
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+. "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
 [[ ${status} -ne 0 ]] && exit ${status}
 
