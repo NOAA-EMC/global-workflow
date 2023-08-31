3d614c2365b397d68db537b78a7fa204da0adbe4
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-08-31 10:07:35 +0000

    clean up prepiodaobs scripting (#1820)

--- jobs/rocoto/prepiodaobs.sh
+++ jobs/rocoto/prepiodaobs.sh
@@ -12,13 +12,6 @@ export job="prepatmobs"
 export jobid="${job}.$$"
 
 ###############################################################
-# setup python path for workflow utilities and tasks
-##wxflowPATH="${HOMEgfs}/ush/python:${HOMEgfs}/ush/python/wxflow/src"
-##gdasappPATH="${HOMEgfs}/sorc/gdas.cd/iodaconv/src:${HOMEgfs}/sorc/gdas.cd/build/lib/python3.7"
-##PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${wxflowPATH}:${gdasappPATH}"
-##export PYTHONPATH
-
-###############################################################
 # Execute the JJOB
 "${HOMEgfs}/jobs/JGLOBAL_PREP_IODA_OBS"
 status=$?
