13c1f478729c3d2cfe6fcd3b0531a82d89db7f64
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-09-06 01:19:40 +0000

    replace CDUMP with RUN in JGLOBAL_ATM_PREP_IODA_OBS, update PYTHONPATH in prepatmiodaobs.sh (#1820)

--- jobs/rocoto/prepatmiodaobs.sh
+++ jobs/rocoto/prepatmiodaobs.sh
@@ -12,6 +12,13 @@ export job="prepatmobs"
 export jobid="${job}.$$"
 
 ###############################################################
+# setup python path for workflow and ioda utilities
+wxflowPATH="${HOMEgfs}/ush/python:${HOMEgfs}/ush/python/wxflow/src"
+PYIODALIB="${HOMEgfs}/sorc/gdas.cd/build/lib/python3.7"
+PYTHONPATH="${PYIODALIB}:${wxflowPATH}:${PYTHONPATH}"
+export PYTHONPATH
+
+###############################################################
 # Execute the JJOB
 "${HOMEgfs}/jobs/JGLOBAL_ATM_PREP_IODA_OBS"
 status=$?
