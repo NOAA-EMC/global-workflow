811fa459c4dc9823fe7ac5a68b1485eaae90b866
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-09-11 13:26:58 -0400

    Update ush/load_fv3gfs_modules.sh

--- ush/load_fv3gfs_modules.sh
+++ ush/load_fv3gfs_modules.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 ###############################################################
-if [[ "${DEBUG_WORKFLOW:-NO}" == "YES" ]]; then
+if [[ "${DEBUG_WORKFLOW:-NO}" == "NO" ]]; then
     echo "Loading modules quietly..."
     set +x
 fi
