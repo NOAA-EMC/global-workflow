c4e1b45785ecf693d6f3c310211f3807d288f306
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2023-09-14 13:14:29 -0500

    Remove unused lines in gfs_transfer

--- ush/gfs_transfer.sh
+++ ush/gfs_transfer.sh
@@ -17,56 +17,7 @@
 
 source "${HOMEgfs}/ush/preamble.sh"
 
-# export CNVGRIB=/nwprod/util/exec/cnvgrib
-# export GRB2INDX=/nwprod/util/exec/grb2index
-# export WGRIB2=/nwprod/util/exec/wgrib2
-
- 
-#if test "$SENDCOM" = "YES"
-#then
-   #
-   # Save Pressure and SFLUX GRIB/GRIB Index files
-   #
-#  cp flxifile $COMOUT/${RUN}.${cycle}.sfluxgrbif$fhr
-   
-# Chuang: keeping gfs surface files around because post and dng
-# use them now   
-   #if [[ $fhr -gt 84 ]]
-   #then
-   #  if [[ $fhr -ne 120 && $fhr -ne 168 ]]
-   #    then
-   #      rm $COMOUT/${RUN}.${cycle}.bf$fhr
-   #  fi
-   #fi
-#fi
-
-############################################
-# Convert the sflux file to grib2 format:
-############################################
-#cp $COMIN/${RUN}.${cycle}.sfluxgrbf$fhr sfluxgrbf$fhr
-#if [ $(expr $fhr % 3) -eq 0 ]; then
-#$CNVGRIB -g12 -p40 $COMIN/${RUN}.${cycle}.sfluxgrbf$fhr sfluxgrbf${fhr}.grib2
-#$WGRIB2 sfluxgrbf${fhr}.grib2 -s> sfluxgrbf${fhr}.grib2.idx
-
-#if [ $SENDCOM = YES ]
-#then
-#  cp sfluxgrbf${fhr}.grib2 $COMOUT/${RUN}.${cycle}.sfluxgrbf${fhr}.grib2
-#  cp sfluxgrbf${fhr}.grib2.idx $COMOUT/${RUN}.${cycle}.sfluxgrbf${fhr}.grib2.idx
-#fi
-
-#fi
-#
-# DBNet Alerts for gfs suite
-#
-
 if [[ "${SENDDBN}" = 'YES' && "${RUN}" = 'gfs' ]]; then
-  #if [ $(expr $fhr % 3) -eq 0 ]; then
-  #echo $DBNROOT/bin/dbn_alert MODEL GFS_SGB $job $COMOUT/${RUN}.${cycle}.sfluxgrbf$fhr
-  #echo $DBNROOT/bin/dbn_alert MODEL GFS_SGBI $job $COMOUT/${RUN}.${cycle}.sfluxgrbif$fhr
-  #echo $DBNROOT/bin/dbn_alert MODEL GFS_SGB_GB2 $job $COMOUT/${RUN}.${cycle}.sfluxgrbf${fhr}.grib2
-  #echo $DBNROOT/bin/dbn_alert MODEL GFS_SGB_GB2_WIDX $job $COMOUT/${RUN}.${cycle}.sfluxgrbf${fhr}.grib2.idx
-  #fi
-
   fhr3=$(printf "%03d" "${fhr}")
   "${DBNROOT}/bin/dbn_alert" MODEL GFS_SF "${job}" "${COMOUT}/${RUN}.t${cyc}z.atmf${fhr3}.nc"
  
@@ -78,5 +29,5 @@ if [[ "${SENDDBN}" = 'YES' && "${RUN}" = 'gfs' ]]; then
   fi
 fi
 
-exit 0 
+exit 0
 
