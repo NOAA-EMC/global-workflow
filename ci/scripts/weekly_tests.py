cabb84e1b1240e884036bbbc6e80ad837c399bbe
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2023-09-18 20:18:08 +0000

    update token read and a list of labels and printed a done message

--- ci/scripts/weekly_tests.py
+++ ci/scripts/weekly_tests.py
@@ -1,27 +1,26 @@
 #! /usr/bin/env python3
 
 import time
+from pathlib import Path
 from github import Github
 from datetime import datetime
-from pathlib import Path
 
 now = datetime.now()
 date = now.strftime("%m %d %Y")
 
 # Provide your personal access token or use a GitHub token if available
-
 with open(Path.home().joinpath('.gittokens','access_token.txt'), 'r') as file:
     access_token = file.read().strip()
 
 # Create a PyGitHub instance using the access token
-githue = Github(access_token)
+github = Github(access_token)
 
 # Specify the repository where you want to open the Pull Request and modify the label
 repo_owner = 'TerrenceMcGuinness-NOAA'
 repo_name = 'global-workflow'
 pr_title = 'Weekly Tests '+date
 pr_body = 'Weekly High Resolution Forecast Tests'
-label_name = 'CI-Hera-Ready'
+label_names = ['CI-Hera-Ready','CI-Orion-Ready']
 
 # Get the repository object
 repo = github.get_repo(f'{repo_owner}/{repo_name}')
@@ -33,8 +32,6 @@ repo.create_git_ref(ref=f'refs/heads/{branch_name}', sha=sb.commit.sha)
 time.sleep(5)
 branch = repo.get_branch(branch_name)
 
-#pr_cases = repo.get_dir_contents(_top+'ci/cases/pr')
-print('/ci/cases/pr')
 pr_cases = repo.get_contents('/ci/cases/pr', ref=branch.name)
 for file in pr_cases:
     if file.name.endswith('.yaml'):
@@ -52,5 +49,8 @@ new_commit = repo.get_commits()[0]
 # Create a new Pull Request
 pr = repo.create_pull(title=pr_title, body=pr_body, head='weekly_tests', base='develop')
 
-# Add label to the Pull Request
-pr.add_to_labels(label_name)
+# Add labels to the Pull Request
+for label_name in label_names:
+    pr.add_to_labels(label_name)
+
+print(f'Pull Request {pr_title} successfully created')
