2a1603ba15801f953b6db652f346c9f30340c04e
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-09-19 18:10:10 +0000

    add scripting to load gsi_enkf modules in anal.sh, eobs.sh, and eupd.sh (#1863)

--- jobs/rocoto/eupd.sh
+++ jobs/rocoto/eupd.sh
@@ -4,9 +4,26 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
-status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ ${status} -ne 0 ]] && exit ${status}
+
+# TODO: clean this up
+source "${HOMEgfs}/ush/detect_machine.sh"
+
+# Append compiler (only on machines that have multiple compilers)
+COMPILER=${COMPILER:-"intel"}
+if [[ "${MACHINE_ID}" = "hera" ]] || [[ "${MACHINE_ID}" = "cheyenne" ]]; then
+    MACHINE_ID=${MACHINE_ID}.${COMPILER}
+fi
+
+set +x
+source "${HOMEgfs}/ush/module-setup.sh"
+module use "${HOMEgfs}/sorc/gsi_enkf.fd/modulefiles"
+module load gsi_"${MACHINE_ID}"
+module list
+unset MACHINE_ID
+set_trace
 
 export job="eupd"
 export jobid="${job}.$$"
