bb20a411d36d2bfca065bda04e5f3a599341d002
TerrenceMcGuinness-NOAA <terrence.mcguinness@cox.net>
2023-09-20 20:27:56 +0000

    Update ci/scripts/driver_weekly.sh
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- ci/scripts/driver_weekly.sh
+++ ci/scripts/driver_weekly.sh
@@ -58,7 +58,7 @@ set -x
 #########################################################
 # Create a new branch from develop and move yaml files
 #########################################################
-branch="weekly_ci_$(date +%A_%b_%Y)"
+branch="weekly_ci_$(date +%Y%m%d)"
 develop_dir="${GFS_CI_ROOT}/develop_temp"
 HOMEgfs_PR="${develop_dir}/global-workflow"
 echo "Creating new branch ${branch} from develop on ${MACHINE_ID} in ${develop_dir}"
