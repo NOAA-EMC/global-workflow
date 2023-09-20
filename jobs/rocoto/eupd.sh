4f25f861c32961ca9092ae3e0f9fbdfab3db6d9a
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-09-20 17:22:49 +0000

    refactor module loads in anal.sh, eobs.sh, and eupd.sh to account for wcoss2 (#1863)

--- jobs/rocoto/eupd.sh
+++ jobs/rocoto/eupd.sh
@@ -9,21 +9,31 @@ source "${HOMEgfs}/ush/preamble.sh"
 #[[ ${status} -ne 0 ]] && exit ${status}
 
 # TODO: clean this up
+#Source appropriate modulefiles based on machine
+
 source "${HOMEgfs}/ush/detect_machine.sh"
+if [[ "${MACHINE_ID}" = "wcoss2" ]]; then
+    # Source FV3GFS workflow modules
+    . "${HOMEgfs}"/ush/load_fv3gfs_modules.sh
+    status=$?
+    [[ ${status} -ne 0 ]] && exit "${status}"
+else
+    # Append compiler (only on machines that have multiple compilers)
+    COMPILER=${COMPILER:-"intel"}
+    if [[ "${MACHINE_ID}" = "hera" ]] || [[ "${MACHINE_ID}" = "cheyenne" ]]; then
+	MACHINE_ID=${MACHINE_ID}.${COMPILER}
+    fi
 
-# Append compiler (only on machines that have multiple compilers)
-COMPILER=${COMPILER:-"intel"}
-if [[ "${MACHINE_ID}" = "hera" ]] || [[ "${MACHINE_ID}" = "cheyenne" ]]; then
-    MACHINE_ID=${MACHINE_ID}.${COMPILER}
+    # Source machine specific GSI-EnKF modules
+    set +x
+    source "${HOMEgfs}/ush/module-setup.sh"
+    module use "${HOMEgfs}/sorc/gsi_enkf.fd/modulefiles"
+    module load gsi_"${MACHINE_ID}"
+
+    module list
+    unset MACHINE_ID
+    set_trace
 fi
-
-set +x
-source "${HOMEgfs}/ush/module-setup.sh"
-module use "${HOMEgfs}/sorc/gsi_enkf.fd/modulefiles"
-module load gsi_"${MACHINE_ID}"
-module list
-unset MACHINE_ID
-set_trace
 
 export job="eupd"
 export jobid="${job}.$$"
