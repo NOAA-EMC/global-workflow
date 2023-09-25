9eec36c7a3a6055c62c124abb8ba59475e669092
Terry McGuinness <terry.mcguinness@noaa.gov>
2023-09-25 10:47:57 -0500

    added line in main CI driver to remove previous pslot_{sha} in case of restart

--- ci/platforms/hera.sh
+++ ci/platforms/hera.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
+
 export GFS_CI_ROOT=/scratch1/NCEPDEV/global/Terry.McGuinness/GFS_CI_ROOT
+export ICSDIR_ROOT=/scratch1/NCEPDEV/global/glopara/data/ICSDIR
+export STMP="/scratch1/NCEPDEV/stmp2/${USER}"
 export SLURM_ACCOUNT=nems
-export ICSDIR_ROOT="/scratch1/NCEPDEV/global/glopara/data/ICSDIR"
 export max_concurrent_cases=2
 export max_concurrent_pr=2
