f7e37fb9bbba8c3a2a3d0789ceefa97f487721a9
AnilKumar-NOAA <anil.kumar@noaa.gov>
2023-09-29 18:04:21 +0000

    memdir loop over all the components of exglobal_stage_ic

--- scripts/exglobal_stage_ic.sh
+++ scripts/exglobal_stage_ic.sh
@@ -9,15 +9,16 @@ gPDY="${GDATE:0:8}"
 gcyc="${GDATE:8:2}"
 
 
-member_dirs=()
+MEMDIR=()
 
 if [[ "${RUN}" == "gefs" ]]; then
 # Populate the member_dirs array based on the value of NMEM_ENS
   for ((i = 0; i < "${NMEM_ENS}"; i++)); do
-    member_dirs+=("mem$(printf "%03d" "${i}")")
+    MEMDIR+=("mem$(printf "%03d" "${i}")")
   done
 else
-member_dirs+=("")
+MEMDIR+=("")
+NMEM_ENS=0
 fi
 
 # Initialize return code
@@ -28,100 +29,89 @@ error_message(){
 }
 
 ###############################################################
-# Start staging gefs and gfs here
 # Stage the FV3 initial conditions to ROTDIR (cold start)
 
 for MEMDIR in ${memdir[@]}; do
 
-member_dir=""
-NMEM_ENS=0
 YMD=${PDY} HH=${cyc} generate_com -r COM_ATMOS_INPUT
 [[ ! -d "${COM_ATMOS_INPUT}" ]] && mkdir -p "${COM_ATMOS_INPUT}"
-  for member_dir in $(seq -w 0 $((NMEM_ENS - 1))); do
-    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${member_dir}/atmos/gfs_ctrl.nc"
-    target="${COM_ATMOS_INPUT}/gfs_ctrl.nc"
-    ${NCP} "${source}" "${target}"
+source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${MEMDIR}/atmos/gfs_ctrl.nc"
+target="${COM_ATMOS_INPUT}/gfs_ctrl.nc"
+${NCP} "${source}" "${target}"
+rc=$?
+(( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
+err=$((err + rc))
+for ftype in gfs_data sfc_data; do
+  for tt in $(seq 1 6); do
+    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${MEMDIR}/atmos/${ftype}.tile${tt}.nc"
+    target="${COM_ATMOS_INPUT}/${ftype}.tile${tt}.nc"
     rc=$?
     (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
     err=$((err + rc))
-    for ftype in gfs_data sfc_data; do
-      for tt in $(seq 1 6); do
-        source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${member_dir}/atmos/${ftype}.tile${tt}.nc"
-        target="${COM_ATMOS_INPUT}/${ftype}.tile${tt}.nc"
-        rc=$?
-        (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
-        err=$((err + rc))
-      done
-    done
   done
+done
 
 # Stage ocean initial conditions to ROTDIR (warm start)
 if [[ "${DO_OCN:-}" = "YES" ]]; then
-member_dir=""
-NMEM_ENS=0
   YMD=${gPDY} HH=${gcyc} generate_com -r COM_OCEAN_RESTART
   [[ ! -d "${COM_OCEAN_RESTART}" ]] && mkdir -p "${COM_OCEAN_RESTART}"
-    for member_dir in $(seq -w 0 $((NMEM_ENS - 1))); do
-    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${member_dir}/ocean/${PDY}.${cyc}0000.MOM.res.nc"
-    target="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.MOM.res.nc"
-    ${NCP} "${source}" "${target}"
-    rc=$?
-    (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
-    err=$((err + rc))
-    done
+  source="${BASE_CPLIC}/${CPL_OCNIC}/${PDY}${cyc}/${MEMDIR}/ocean/${OCNRES}/MOM.res.nc"
+  target="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.MOM.res.nc"
+  ${NCP} "${source}" "${target}"
+  rc=$?
+  [[ ${rc} -ne 0 ]] && error_message "${source}" "${target}" "${rc}"
+  err=$((err + rc))
+  case "${OCNRES}" in
+    "500" | "100")  # Only 5 degree or 1 degree ocean does not have MOM.res_[1-4].nc files
+    ;;
+    "025")  # Only 1/4 degree ocean has MOM.res_[1-4].nc files
+      for nn in $(seq 1 4); do
+        source="${BASE_CPLIC}/${CPL_OCNIC}/${PDY}${cyc}/${MEMDIR}/ocean/${OCNRES}/MOM.res_${nn}.nc"
+        if [[ -f "${source}" ]]; then
+          target="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.MOM.res_${nn}.nc"
+          ${NCP} "${source}" "${target}"
+          rc=$?
+          [[ ${rc} -ne 0 ]] && error_message "${source}" "${target}" "${rc}"
+          err=$((err + rc))
+        fi
+      done
+    ;;
+    *)
       echo "FATAL ERROR: Unsupported ocean resolution ${OCNRES}"
       rc=1
       err=$((err + rc))
+    ;;
+  esac
 fi
 
 # Stage ice initial conditions to ROTDIR (warm start)
 if [[ "${DO_ICE:-}" = "YES" ]]; then
-  member_dir=""
-  NMEM_ENS=0
   YMD=${gPDY} HH=${gcyc} generate_com -r COM_ICE_RESTART
   [[ ! -d "${COM_ICE_RESTART}" ]] && mkdir -p "${COM_ICE_RESTART}"
-    for member_dir in $(seq -w 0 $((NMEM_ENS - 1))); do
-    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${member_dir}/ice/${PDY}.${cyc}0000.cice_model.res.nc"
+#  ICERESdec=$(echo "${ICERES}" | awk '{printf "%0.2f", $1/100}')
+#  source="${BASE_CPLIC}/${CPL_ICEIC}/${PDY}${cyc}/ice/${ICERES}/cice5_model_${ICERESdec}.res_${PDY}${cyc}.nc"
+    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${MEMDIR}/ice/${PDY}.${cyc}0000.cice_model.res.nc"
     target="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.cice_model.res.nc"
     ${NCP} "${source}" "${target}"
     rc=$?
     (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
     err=$((err + rc))
-    done
-      echo "FATAL ERROR: Unsupported ocean resolution ${OCNRES}"
-      rc=1
-      err=$((err + rc))
 fi
 
 # Stage the WW3 initial conditions to ROTDIR (warm start; TODO: these should be placed in $RUN.$gPDY/$gcyc)
 if [[ "${DO_WAVE:-}" = "YES" ]]; then
-  member_dir=""
-  NMEM_ENS=0
   YMD=${PDY} HH=${cyc} generate_com -r COM_WAVE_RESTART
   [[ ! -d "${COM_WAVE_RESTART}" ]] && mkdir -p "${COM_WAVE_RESTART}"
   for grdID in ${waveGRD}; do  # TODO: check if this is a bash array; if so adjust
-    source="${BASE_CPLIC}/${CPL_WAVIC}/${PDY}${cyc}/wav/${grdID}/${PDY}.${cyc}0000.restart.${grdID}"
+    source="${BASE_CPLIC}/${CPL_WAVIC}/${PDY}${cyc}/${MEMDIR}/wave/${grdID}/${PDY}.${cyc}0000.restart.${grdID}"
     target="${COM_WAVE_RESTART}/${PDY}.${cyc}0000.restart.${grdID}"
     ${NCP} "${source}" "${target}"
     rc=$?
     (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
     err=$((err + rc))
   done
-elif [[ "${DO_WAVE:-}" = "YES" && "${RUN}" = "gefs" ]]; then
-  YMD=${gPDY} HH=${gcyc} generate_com -r COM_WAVE_RESTART
-  [[ ! -d "${COM_WAVE_RESTART}" ]] && mkdir -p "${COM_WAVE_RESTART}"
-    for member_dir in $(seq -w 0 $((NMEM_ENS - 1))); do
-    source="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${member_dir}/ice/${PDY}.${cyc}0000.restart.${waveGRD}"
-    target="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.restart.${waveGRD}"
-    ${NCP} "${source}" "${target}"
-    rc=$?
-    (( rc != 0 )) && error_message "${source}" "${target}" "${rc}"
-    err=$((err + rc))
-    done
-      echo "FATAL ERROR: Unsupported ocean resolution ${OCNRES}"
-      rc=1
-      err=$((err + rc))
 fi
+
 done
 ###############################################################
 # Check for errors and exit if any of the above failed
