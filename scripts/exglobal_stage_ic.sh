ebc0ebae82c18c2c2a3acf88e5be444f7f08dd98
AnilKumar-NOAA <anil.kumar@noaa.gov>
2023-10-04 15:43:26 +0000

    removed -r switch for all the generate_com

--- scripts/exglobal_stage_ic.sh
+++ scripts/exglobal_stage_ic.sh
@@ -28,7 +28,7 @@ error_message() {
 ###############################################################
 for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
   # Stage the FV3 initial conditions to ROTDIR (cold start)
-  YMD=${PDY} HH=${cyc} generate_com -r COM_ATMOS_INPUT
+  YMD=${PDY} HH=${cyc} generate_com COM_ATMOS_INPUT
   [[ ! -d "${COM_ATMOS_INPUT}" ]] && mkdir -p "${COM_ATMOS_INPUT}"
   src="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${MEMDIR}/atmos/gfs_ctrl.nc"
   tgt="${COM_ATMOS_INPUT}/gfs_ctrl.nc"
@@ -52,7 +52,7 @@ for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
 
   # Stage ocean initial conditions to ROTDIR (warm start)
   if [[ "${DO_OCN:-}" = "YES" ]]; then
-    YMD=${gPDY} HH=${gcyc} generate_com -r COM_OCEAN_RESTART
+    YMD=${gPDY} HH=${gcyc} generate_com COM_OCEAN_RESTART
     [[ ! -d "${COM_OCEAN_RESTART}" ]] && mkdir -p "${COM_OCEAN_RESTART}"
     src="${BASE_CPLIC}/${CPL_OCNIC}/${PDY}${cyc}/${MEMDIR}/ocean/${OCNRES}/MOM.res.nc"
     tgt="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.MOM.res.nc"
@@ -84,7 +84,7 @@ for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
   fi
   # Stage ice initial conditions to ROTDIR (warm start)
   if [[ "${DO_ICE:-}" = "YES" ]]; then
-    YMD=${gPDY} HH=${gcyc} generate_com -r COM_ICE_RESTART
+    YMD=${gPDY} HH=${gcyc} generate_com COM_ICE_RESTART
     [[ ! -d "${COM_ICE_RESTART}" ]] && mkdir -p "${COM_ICE_RESTART}"
     src="${BASE_CPLIC}/${CPL_ATMIC}/${YMD}${HH}/${MEMDIR}/ice/${PDY}.${cyc}0000.cice_model.res.nc"
     tgt="${COM_OCEAN_RESTART}/${PDY}.${cyc}0000.cice_model.res.nc"
@@ -96,7 +96,7 @@ for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
 
   # Stage the WW3 initial conditions to ROTDIR (warm start; TODO: these should be placed in $RUN.$gPDY/$gcyc)
   if [[ "${DO_WAVE:-}" = "YES" ]]; then
-    YMD=${PDY} HH=${cyc} generate_com -r COM_WAVE_RESTART
+    YMD=${PDY} HH=${cyc} generate_com COM_WAVE_RESTART
     [[ ! -d "${COM_WAVE_RESTART}" ]] && mkdir -p "${COM_WAVE_RESTART}"
     for grdID in ${waveGRD}; do # TODO: check if this is a bash array; if so adjust
       src="${BASE_CPLIC}/${CPL_WAVIC}/${PDY}${cyc}/${MEMDIR}/wave/${grdID}/${PDY}.${cyc}0000.restart.${grdID}"
