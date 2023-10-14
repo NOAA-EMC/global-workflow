6a61d8a213ff07176358381b9c6e3a910a72b25f
Jessica Meixner <Jessica.Meixner@noaa.gov>
2023-10-14 21:18:43 +0000

    move linked gocart files to a copy statement at the end of run due to crashes in gocart

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -1038,7 +1038,21 @@ GOCART_postdet() {
       rm -f "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
     fi
 
+    #To Do: Temporarily removing this as this will crash gocart, adding copy statement at the end 
     #${NLN} "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4" \
     #       "${DATA}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
   done
 }
+
+GOCART_out() {
+  echo "SUB ${FUNCNAME[0]}: Copying output data for GOCART"
+
+  # Copy gocart.inst_aod after the forecast is run (and successfull)
+  # TO DO: this should be linked but there were issues where gocart was crashing if it was linked
+  for fhr in ${FV3_OUTPUT_FH}; do
+    local vdate=$(date --utc -d "${current_cycle:0:8} ${current_cycle:8:2} + ${fhr} hours" +%Y%m%d%H)
+    ${NCP} "${DATA}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"  \
+           "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
+  done 
+}
+
