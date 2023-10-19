fc9b4b3ffd8ce604718c1bf8363707179ee796e9
Jessica Meixner <jessica.meixner@noaa.gov>
2023-10-19 10:24:29 -0400

    Update ush/forecast_postdet.sh
    Suggested update from Walter
    
    Co-authored-by: Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -1050,11 +1050,12 @@ GOCART_out() {
   # Copy gocart.inst_aod after the forecast is run (and successfull)
   # TO DO: this should be linked but there were issues where gocart was crashing if it was linked
   #${NCP} "${DATA}/gocart.inst_aod.*" "${COM_CHEM_HISTORY}/"
-  if (( $( ls -1 "${DATA}/gocart.inst_aod."* 2> /dev/null | wc -l) > 0 )); then
-    for file in $(ls "${DATA}/gocart.inst_aod."*) ; do
-      ${NCP} "${file}"  "${COM_CHEM_HISTORY}/"
-    done
-  fi
+  for fhr in ${FV3_OUTPUT_FH}; do
+    if (( fhr == 0 )); then continue; fi
+    local vdate=$(date --utc -d "${current_cycle:0:8} ${current_cycle:8:2} + ${fhr} hours" +%Y%m%d%H)
+    ${NCP} "${DATA}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4" \
+      "${COM_CHEM_HISTORY}/gocart.inst_aod.${vdate:0:8}_${vdate:8:2}00z.nc4"
+  done
 
 
 }
