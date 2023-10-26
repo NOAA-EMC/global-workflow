a673c02af763360196417929c63fb01640a0d823
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2023-10-26 15:36:06 +0000

    add ocean analysis to arch task

--- scripts/exglobal_archive.sh
+++ scripts/exglobal_archive.sh
@@ -239,6 +239,7 @@ if [[ ${HPSSARCH} = "YES" || ${LOCALARCH} = "YES" ]]; then
         #gdasocean
         if [ "${DO_OCN}" = "YES" ]; then
             targrp_list="${targrp_list} gdasocean"
+            targrp_list="${targrp_list} gdasocean_analysis"
         fi
 
         #gdasice
