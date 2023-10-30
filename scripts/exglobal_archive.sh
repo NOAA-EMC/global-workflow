5c5e8b301547d1858e1a2db567c639c800fb29f2
AndrewEichmann-NOAA <58948505+AndrewEichmann-NOAA@users.noreply.github.com>
2023-10-30 09:16:21 -0400

    combine tar group list entry
    Co-authored-by: Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>

--- scripts/exglobal_archive.sh
+++ scripts/exglobal_archive.sh
@@ -238,8 +238,7 @@ if [[ ${HPSSARCH} = "YES" || ${LOCALARCH} = "YES" ]]; then
 
         #gdasocean
         if [ "${DO_OCN}" = "YES" ]; then
-            targrp_list="${targrp_list} gdasocean"
-            targrp_list="${targrp_list} gdasocean_analysis"
+            targrp_list="${targrp_list} gdasocean gdasocean_analysis"
         fi
 
         #gdasice
