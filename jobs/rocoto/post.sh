c48cd43a50542db1c88dc013dc9f3e3fb74a5aeb
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-10-30 14:22:47 -0600

    Docstring update.

--- jobs/rocoto/post.sh
+++ jobs/rocoto/post.sh
@@ -4,7 +4,6 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 ## NCEP post driver script
-## FHRGRP : forecast hour group to post-process (e.g. 0, 1, 2 ...)
 ## FHRLST : forecast hourlist to be post-process (e.g. anl, f000, f000_f001_f002, ...)
 ###############################################################
 
