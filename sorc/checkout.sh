797b514b243b11db8c5b9cfad74ccdea8337b4c3
Eric.Sinsky <eric.sinsky@noaa.gov>
2023-10-30 15:57:40 +0000

    Update GSL-prep-chem to gefs_v12.3.8
    checkout.sh has been modified to use new gefs tag from GSL-prep-chem, which was created to include a bugfix.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -87,7 +87,7 @@ if [[ ! -d gsd_prep_chem.fd ]] ; then
     rc=$?
     ((err+=$rc))
     cd gsd_prep_chem.fd
-    git checkout gefs_v12.3.6
+    git checkout gefs_v12.3.8
     cd ${topdir}
 else
     echo 'Skip.  Directory gsd_prep_chem.fd already exists.'
