4b5abfacba684053bcec9316891288081bc4dbb2
Jessica Meixner <Jessica.Meixner@noaa.gov>
2023-11-14 18:23:25 +0000

    update for prod_util diffs

--- ush/load_ufswm_modules.sh
+++ ush/load_ufswm_modules.sh
@@ -14,6 +14,7 @@ source "${HOMEgfs}/ush/module-setup.sh"
 if [[ "${MACHINE_ID}" != "noaacloud" ]]; then
   module use "${HOMEgfs}/sorc/ufs_model.fd/tests"
   module load modules.ufs_model.lua
+  module load prod-util
   if [[ "${MACHINE_ID}" = "wcoss2" ]]; then
     module load prod_util
     module load cray-pals
@@ -22,16 +23,10 @@ if [[ "${MACHINE_ID}" != "noaacloud" ]]; then
     module load craype-network-ucx
     module load cray-mpich-ucx
   else
-    module load prod-util
     export UTILROOT=${prod_util_ROOT}
   fi
   module load wgrib2
   export WGRIB2=wgrib2
-fi
-if [[ "${MACHINE_ID}" = "hera" ]]; then
-  module use "/scratch2/NCEPDEV/ensemble/save/Walter.Kolczynski/modulefiles/core"
-  module load "miniconda3/4.6.14"
-  module load "gfs_workflow/1.0.0"
 fi
 if [[ "${MACHINE_ID}" == "noaacloud" ]]; then
    if [[ "${PW_CSP:-}" = "aws" ]]; then
