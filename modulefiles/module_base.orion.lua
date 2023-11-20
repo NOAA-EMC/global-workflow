123c8ffc4fd67cbfc10c6c8da807d91b52526313
David Huber <david.huber@noaa.gov>
2023-11-20 13:46:53 -0600

    Fix Orion modules. #1868

--- modulefiles/module_base.orion.lua
+++ modulefiles/module_base.orion.lua
@@ -2,30 +2,36 @@ help([[
 Load environment to run GFS on Orion
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+spack_stack_ver=(os.getenv("spack_stack_ver") or "None")
+spack_env=(os.getenv("spack_env") or "None")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-" .. spack_stack_ver .. "/envs/" .. spack_env .. "/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2022.0.2"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
+load(pathJoin("stack-intel", os.getenv("stack_intel_ver")))
+load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver")))
+load(pathJoin("python", os.getenv("python_ver")))
 
 load(pathJoin("gempak", os.getenv("gempak_ver")))
 load(pathJoin("ncl", os.getenv("ncl_ver")))
 load(pathJoin("jasper", os.getenv("jasper_ver")))
-load(pathJoin("png", os.getenv("libpng_ver")))
+load(pathJoin("libpng", os.getenv("libpng_ver")))
 load(pathJoin("cdo", os.getenv("cdo_ver")))
 
 load(pathJoin("hdf5", os.getenv("hdf5_ver")))
-load(pathJoin("netcdf", os.getenv("netcdf_ver")))
+load(pathJoin("netcdf-c", os.getenv("netcdf_c_ver")))
+load(pathJoin("netcdf-fortran", os.getenv("netcdf_fortran_ver")))
 
 load(pathJoin("nco", os.getenv("nco_ver")))
 load(pathJoin("prod_util", os.getenv("prod_util_ver")))
-load(pathJoin("grib_util", os.getenv("grib_util_ver")))
+load(pathJoin("grib-util", os.getenv("grib_util_ver")))
 load(pathJoin("g2tmpl", os.getenv("g2tmpl_ver")))
-load(pathJoin("ncdiag", os.getenv("ncdiag_ver")))
+load(pathJoin("gsi-ncdiag", os.getenv("gsi_ncdiag_ver")))
 load(pathJoin("crtm", os.getenv("crtm_ver")))
+load(pathJoin("bufr", os.getenv("bufr_ver")))
 load(pathJoin("wgrib2", os.getenv("wgrib2_ver")))
+load(pathJoin("py-netcdf4", os.getenv("py_netcdf4_ver")))
+load(pathJoin("py-pyyaml", os.getenv("py_pyyaml_ver")))
+load(pathJoin("py-jinja2", os.getenv("py_jinja2_ver")))
+
 setenv("WGRIB2","wgrib2")
 
 --prepend_path("MODULEPATH", pathJoin("/work/noaa/global/glopara/git/prepobs/v" .. os.getenv("prepobs_run_ver"), "modulefiles"))
