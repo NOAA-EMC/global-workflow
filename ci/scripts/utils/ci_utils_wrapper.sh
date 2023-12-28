6156b342eb11465bc82970ebb4e08e66664c6ce2
TerrenceMcGuinness-NOAA <terrence.mcguinness@cox.net>
2023-12-27 21:24:11 -0500

    Update ci_utils_wrapper.sh
    added HOMEgfs defined inside wrapper

--- ci/scripts/utils/ci_utils_wrapper.sh
+++ ci/scripts/utils/ci_utils_wrapper.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+HOMEgfs="$(cd "$(dirname  "${BASH_SOURCE[0]}")/../../.." >/dev/null 2>&1 && pwd )"
+
 utitilty_function="${1}"
 
 source "${HOMEgfs}/ci/scripts/utils/ci_utils.sh"
