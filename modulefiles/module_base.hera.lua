94c9937f0bff380b0ebd6acdcfd2509e1ae99389
DavidHuber <david.huber@noaa.gov>
2024-01-11 17:42:53 +0000

    Comment met/metplus out from Hera modulefile. #2123

--- modulefiles/module_base.hera.lua
+++ modulefiles/module_base.hera.lua
@@ -34,8 +34,9 @@ load(pathJoin("py-netcdf4", (os.getenv("py_netcdf4_ver") or "None")))
 load(pathJoin("py-pyyaml", (os.getenv("py_pyyaml_ver") or "None")))
 load(pathJoin("py-jinja2", (os.getenv("py_jinja2_ver") or "None")))
 
-load(pathJoin("met", (os.getenv("met_ver") or "None")))
-load(pathJoin("metplus", (os.getenv("metplus_ver") or "None")))
+-- MET/METplus are not available for use with spack-stack, yet
+--load(pathJoin("met", (os.getenv("met_ver") or "None")))
+--load(pathJoin("metplus", (os.getenv("metplus_ver") or "None")))
 
 setenv("WGRIB2","wgrib2")
 setenv("UTILROOT",(os.getenv("prod_util_ROOT") or "None"))
