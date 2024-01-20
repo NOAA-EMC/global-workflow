bf18bc80b6bc91e317b8b759b361fc7dc81d6ac3
tmcguinness <terry.mcguinness@noaa.gov>
2024-01-20 00:11:50 +0000

    added my spak python 3

--- modulefiles/module_gwsetup.orion.lua
+++ modulefiles/module_gwsetup.orion.lua
@@ -7,6 +7,7 @@ prepend_path("MODULEPATH", "/apps/modulefiles/core")
 -- prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/shared/spack/share/spack/modules/linux-amzn2-skylake")
 
+load ("python-3.9.12-gcc-7.3.1-cfrtozo")
 load ("py-numpy-1.22.4-gcc-7.3.1-ylp7efj")
 load ("py-pyyaml-6.0-gcc-7.3.1-z36arc4")
 load ("py-jinja2-3.0.3-gcc-7.3.1-oegomip")
