b972f66fc924790c48f38d395e7141fa78ef9d90
David Huber <david.huber@noaa.gov>
2024-02-09 10:47:00 -0600

    Fix SS versions for CI modules.

--- modulefiles/module_gwci.hera.lua
+++ modulefiles/module_gwci.hera.lua
@@ -8,7 +8,7 @@ load(pathJoin("stack-intel", os.getenv("2021.5.0")))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("2021.5.1")))
 
 load(pathJoin("netcdf-c", os.getenv("4.9.2")))
-load(pathJoin("netcdf-fortran", os.getenv("4.6.0")))
+load(pathJoin("netcdf-fortran", os.getenv("4.6.1")))
 load(pathJoin("nccmp","1.9.0.1"))
 load(pathJoin("wgrib2", "2.0.8"))
 
