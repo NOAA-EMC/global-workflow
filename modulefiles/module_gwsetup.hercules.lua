4568653a67aa37c902379db628fb10f69fe7190f
David Huber <david.huber@noaa.gov>
2024-02-14 14:22:13 -0600

    Reupgrade Hercules to SS/1.6.0

--- modulefiles/module_gwsetup.hercules.lua
+++ modulefiles/module_gwsetup.hercules.lua
@@ -5,10 +5,10 @@ Load environment to run GFS workflow ci scripts on Hercules
 load(pathJoin("contrib","0.1"))
 load(pathJoin("rocoto","1.3.5"))
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-local python_ver=os.getenv("python_ver") or "3.10.8"
+local python_ver=os.getenv("python_ver") or "3.11.6"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("python", python_ver))
