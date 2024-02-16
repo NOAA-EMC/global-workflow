646002e22fd854c411b603117ea6d64208276048
tmcguinness <terry.mcguinness@noaa.gov>
2024-02-16 18:02:35 +0000

    added check case test in python for return false if skip_ci_on_hosts is set for the host it is called from meaning false to the answer shall I run

--- ci/scripts/utils/check_case_skip.py
+++ ci/scripts/utils/check_case_skip.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import os
 import sys
+sys.path.insert(0,'...')
 from wxflow import AttrDict, parse_j2yaml
-from hosts import Host
+from workflow import hosts
 
 _here = os.path.dirname(__file__)
 _top = os.path.abspath(os.path.join(os.path.abspath(_here), '../../..'))
@@ -10,14 +11,17 @@ _top = os.path.abspath(os.path.join(os.path.abspath(_here), '../../..'))
 if __name__ == '__main__':
 
     HOMEgfs = _top
-    data = AttrDict(HOMEgfs)
-    host = Host()
+    data = AttrDict(HOMEgfs=_top)
+    host = hosts.Host()
 
     case_name = sys.argv[1]
-    case_yaml = HOMEgfs + '/ci/pr/'+case_name+'.yaml'
+    case_yaml = HOMEgfs + '/ci/cases/pr/'+case_name+'.yaml'
     data.update(os.environ)
     case_conf = parse_j2yaml(path=case_yaml, data=data)
 
+    if 'skip_ci_on_hosts' not in case_conf:
+        sys.exit(0)
+
     if host.machine.lower() in [machine.lower() for machine in case_conf.skip_ci_on_hosts]:
-        print(f'Skipping creation of case: {case_conf.arguments.pslot} on {host.machine.capitalize()}')
+        print(f'Skipping creation of case: {case_name} on {host.machine.capitalize()}')
         sys.exit(1)
