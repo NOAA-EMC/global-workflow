c231f9b875898ed045e976bb94b4f91ad233b114
AnilKumar-NOAA <Anil.Kumar@gaea53.ncrc.gov>
2024-02-23 10:24:46 -0500

    updated gw setup for gaea

--- modulefiles/module_gwsetup.gaea.lua
+++ modulefiles/module_gwsetup.gaea.lua
@@ -4,9 +4,14 @@ Load environment to run GFS workflow setup scripts on Gaea C5
 
 whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea C5 ]===])
 
+prepend_path("MODULEPATH", "/sw/gaea-c5/spack-envs/base/modules/spack/linux-sles15-x86_64/Core")
+load(pathJoin("git","2.35.2"))
+
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/modulefiles")
-load(pathJoin("rocoto","1.3.5"))
+prepend_path("MODULEPATH", "/ncrc/proj/epic/rocoto/modulefiles")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/modulefiles")
+load(pathJoin("rocoto","1.3.6"))
 
 local intel_classic_ver=os.getenv("intel_classic_ver") or "2023.1.0"
 load(pathJoin("intel-classic", intel_classic_ver))
@@ -23,7 +28,7 @@ load("py-numpy")
 local ecflow_ver=os.getenv("ecflow_ver") or "5.8.4"
 load(pathJoin("ecflow", ecflow_ver))
 
-local mysql_ver=os.getenv("mysql_ver") or "8.0.31"
+local mysql_ver=os.getenv("mysql_ver") or "8.0.36"
 load(pathJoin("mysql", mysql_ver))
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
@@ -38,7 +43,6 @@ load(pathJoin("stack-python", stack_python_ver))
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
-load("ufs_common")
 load("nccmp/1.9.0.1")
 
 unload("darshan-runtime")
