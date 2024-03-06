25e2483b8c7649bab8b3e3599a7487362cbd0d62
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2024-03-06 19:06:01 +0000

    Rocky-8 updates.

--- modulefiles/module_gwsetup.hera.lua
+++ modulefiles/module_gwsetup.hera.lua
@@ -4,7 +4,6 @@ Load environment to run GFS workflow setup scripts on Hera
 
 load(pathJoin("rocoto"))
 
---prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev-rocky8/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
