193be93af3e5cfcf0d2369df8189ce5e706b55e1
RussTreadon-NOAA <russ.treadon@noaa.gov>
2024-03-11 15:00:11 +0000

    streamline sourcing of JEDI ATM configs in applications.py (#2294)

--- workflow/applications/applications.py
+++ workflow/applications/applications.py
@@ -145,14 +145,10 @@ class AppConfig(ABC, metaclass=AppConfigInit):
                 files += ['config.anal', 'config.eupd']
             elif config in ['efcs']:
                 files += ['config.fcst', 'config.efcs']
-            elif config in ['atmanlinit']:
-                files += ['config.atmanl', 'config.atmanlinit']
-            elif config in ['atmanlrun']:
-                files += ['config.atmanl', 'config.atmanlrun']
-            elif config in ['atmensanlinit']:
-                files += ['config.atmensanl', 'config.atmensanlinit']
-            elif config in ['atmensanlrun']:
-                files += ['config.atmensanl', 'config.atmensanlrun']
+            elif config in ['atmanlinit', 'atmanlrun']:
+                files += ['config.atmanl', f'config.{config}']
+            elif config in ['atmensanlinit', 'atmensanlrun']:
+                files += ['config.atmensanl', f'config.{config}']
             elif 'wave' in config:
                 files += ['config.wave', f'config.{config}']
             else:
