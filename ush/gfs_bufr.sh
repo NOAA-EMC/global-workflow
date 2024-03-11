59b2649b16d29a00812f04e3a24f1c592986cbbb
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2024-03-11 17:33:45 +0000

    Allow non-matching bufr table
    PR #2383 added a new bufr table, along with a fatal error if there was
    not one that matched the resolution. However, workflow usually uses a
    low resolution to test, and it works fine even if the bufr parm file is
    a mismatch, so turned the fatal error into a warning and links the bufr
    parm file as before.

--- ush/gfs_bufr.sh
+++ ush/gfs_bufr.sh
@@ -88,8 +88,8 @@ case "${CASE}" in
         ln -sf "${PARMbufrsnd}/bufr_ij9km.txt"  fort.7
         ;;
     *)
-        echo "FATAL ERROR: Unrecognized bufr_ij*km.txt For CASE ${CASE}, ABORT!"
-        exit 1
+        echo "WARNING: No bufr table for this resolution, using the one for C768"
+        ln -sf "${PARMbufrsnd}/bufr_ij13km.txt" fort.7
         ;;
 esac
 
