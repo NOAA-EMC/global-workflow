3e2f5415d4d983bc33710bcfa241ad9c9808dd6b
tmcguinness <terry.mcguinness@noaa.gov>
2024-03-18 22:51:05 +0000

    pystyle spaces

--- ci/scripts/utils/publish_logs.py
+++ ci/scripts/utils/publish_logs.py
@@ -14,5 +14,5 @@ with open(file_content_path, 'r') as file:
 
 file_path=f"ci/error_logs/{file_subdir}/"+file_content_path.split("/")[-1]
 print(f"Uploading {file_path} to {emcbot_url}")
-emcbot_gh.repo.create_file(file_path,"adding log file" , file_content, branch="error_logs")
+emcbot_gh.repo.create_file(file_path, "adding log file", file_content, branch="error_logs")
 
