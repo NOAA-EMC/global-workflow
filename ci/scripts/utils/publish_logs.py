c82b70394b50532823b9aaae24ffa5aae5417c30
Terry McGuinness <terry.mcguinness@noaa.gov>
2024-03-18 15:40:02 -0500

    tested and added string from subdir to publish_logs

--- ci/scripts/utils/publish_logs.py
+++ ci/scripts/utils/publish_logs.py
@@ -9,15 +9,14 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, REMAINDER
 emcbot_url = "https://github.com/emcbot/global-workflow.git"
 emcbot_gh = GitHubPR(emcbot_url)
 
-file_content_path = sys.argv[1]
+file_subdir = sys.argv[1]
+file_content_path = sys.argv[2]
 
 with open(file_content_path, 'r') as file:
     file_content = file.read()
 
 file_commit_message = "Add new file"
-file_path="/ci/error_logs/"+file_content_path.split("/")[-1]
-
-file_path = "test.txt"
+file_path=f"ci/error_logs/{file_subdir}/"+file_content_path.split("/")[-1]
 
 print(f"Uploading {file_path} to {emcbot_url}")
 
