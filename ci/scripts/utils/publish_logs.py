205b3c48fec691a34a57f122425eb1b59897ca7e
Terry McGuinness <terry.mcguinness@noaa.gov>
2024-03-22 14:40:49 -0500

    fixed and tested path_header update when exisits in error_log repo

--- ci/scripts/utils/publish_logs.py
+++ ci/scripts/utils/publish_logs.py
@@ -49,7 +49,7 @@ def input_args():
 
     parser.add_argument('--file', help='path to file for uploading to GitHub', required=False, type=FileType('r'), nargs='+')
     parser.add_argument('--gist', help='create a gist of the file', nargs=1, metavar='identifier_string', required=False)
-    parser.add_argument('--repo', help='create a file in a repo', nargs=1, metavar='path_in_repo', required=False)
+    parser.add_argument('--repo', help='create a file in a repo', nargs=1, metavar='path_header', required=False)
     parser.add_argument('--delete_gists', help='deletes all the gits from authenticated user', action='store_true', default=False, required=False)
     args = parser.parse_args()
     if not args.delete_gists and not args.file:
@@ -76,22 +76,21 @@ if __name__ == '__main__':
         gist = emcbot_gh.user.create_gist(public=True, files=gist_files, description=f"error log file from CI run {args.gist[0]}")
         print(gist.html_url)
 
-    if args.repo: # Add error logs to emcbot/ci-global-workflows in the error_logs branch under a folder with the name {path_in_repo} 
+    if args.repo: # Upload error logs to emcbot's ci-global-workflows error_logs branch
+        path_header = args.repo[0]
+        file_path_in_repo = f"ci/error_logs/{path_header}/" + str(os.path.basename(args.file[0].name))
+        try:
+            content = emcbot_gh.repo.get_contents(file_path_in_repo,ref='error_logs')
+            path_header = f'{path_header}_{random_string()}'
+        except GitHubDBError.GithubException as e:
+            pass
 
         for file in args.file:
             file_content = file.read()
-            file_path = f"ci/error_logs/{args.repo[0]}/"
-            file_path_in_repo = f"{file_path}/" + str(os.path.basename(file.name))
-            try:
-               emcbot_gh.repo.create_file(file_path_in_repo, "Adding error log file", file_content, branch="error_logs")
-            # If the branch error_logs file with {path_in_repo} all ready exists, add a random string to {path_in_repo} and try again
-            except GitHubDBError.GithubException as e:
-               if e.status == 422:
-                    file_path = f"ci/error_logs/{args.repo[0]}_{random_string()}/"
-                    file_path_in_repo = f"{file_path}" + str(os.path.basename(file.name))
-                    emcbot_gh.repo.create_file(file_path_in_repo, "Adding error log file", file_content, branch="error_logs")
+            file_path_in_repo = f"{path_header}/" + str(os.path.basename(file.name))
+            emcbot_gh.repo.create_file(file_path_in_repo, "Adding error log file", file_content, branch="error_logs")
 
-        file_url = f"{emcbot_ci_url.rsplit('.',1)[0]}/tree/error_logs/{file_path}"
+        file_url = f"{emcbot_ci_url.rsplit('.',1)[0]}/tree/error_logs/{path_header}"
         print(file_url)
 
     if args.delete_gists:  # Helper feature to delete all gists from authenticated user account
