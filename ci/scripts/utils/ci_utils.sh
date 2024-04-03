808b3598e8b3341a67f50c2be4b289d3f103c2ae
Terry McGuinness <terry.mcguinness@noaa.gov>
2024-04-03 16:12:01 -0500

    SC2027 and 2086 disable

--- ci/scripts/utils/ci_utils.sh
+++ ci/scripts/utils/ci_utils.sh
@@ -147,7 +147,8 @@ function publish_logs() {
     done < "${file}"
 
     if [[ -n "${full_paths}" ]]; then
-        "${HOMEgfs}/ci/scripts/utils/publish_logs.py --file "${full_paths}" --repo "${PR_header}"" > /dev/null
+        # shellcheck disable=SC2027,SC2086
+        ${HOMEgfs}/ci/scripts/utils/publish_logs.py --file ${full_paths} --repo ${PR_header} > /dev/null
         URL="$(${HOMEgfs}/ci/scripts/utils/publish_logs.py --file "${full_paths}" --gist "${PR_header}")"
     fi
     echo "${URL}"
