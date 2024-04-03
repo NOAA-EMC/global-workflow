32bd4ffc09d722aac4a11412d4b5ba26e3d4e88c
danholdaway <danholdaway@users.noreply.github.com>
2024-04-03 20:06:30 +0000

    add some jcb things

--- sorc/link_workflow.sh
+++ sorc/link_workflow.sh
@@ -211,7 +211,7 @@ fi
 #------------------------------
 if [[ -d "${HOMEgfs}/sorc/gdas.cd" ]]; then
   cd "${HOMEgfs}/parm/gdas" || exit 1
-  declare -a gdasapp_comps=("aero" "atm" "io" "ioda" "snow" "soca")
+  declare -a gdasapp_comps=("aero" "atm" "io" "ioda" "snow" "soca" "jcb-gdas" "atm.yaml.j2")
   for comp in "${gdasapp_comps[@]}"; do
     [[ -d "${comp}" ]] && rm -rf "${comp}"
     ${LINK_OR_COPY} "${HOMEgfs}/sorc/gdas.cd/parm/${comp}" .
