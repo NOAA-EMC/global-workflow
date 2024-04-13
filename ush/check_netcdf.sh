e3473c565d8cddba53981a2a40b0ace6688f68b5
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-04-13 00:48:33 -0400

    fix shellnorms

--- ush/check_netcdf.sh
+++ ush/check_netcdf.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-readonly HOMEgfs=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
+HOMEgfs=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 declare -rx HOMEgfs
 
 source "${HOMEgfs}/ush/load_fv3gfs_modules.sh" 1>/dev/null 2>&1
