43a6b33b84f1ecb974683bb3c61f77b1f378d367
Wei Huang <Wei.Huang@noaa.gov>
2024-04-16 19:30:01 +0000

    update gdas modulefile

--- sorc/mods4aws/update-modulefiles.sh
+++ sorc/mods4aws/update-modulefiles.sh
@@ -5,18 +5,19 @@ set -x
 #machine=azure
 machine=aws
 
+srcdir=/contrib/Wei.Huang/debug/global-workflow-cloud
+
 for item in gdas gfs_utils gsi_enkf gsi_monitor gsi_utils \
             ufs_model ufs_utils upp verif-global
 do
   tf=${item}.${machine}.tar
-  if [ -f ${tf} ]
+  if [ -f ${srcdir}/sorc/mods4${machine}/${tf} ]
   then
-    dir=${item}.fd
+    dir=${srcdir}/sorc/${item}.fd
     if [ -d ${dir} ]
     then
       cd ${dir}
       tar xvf ../mods4${machine}/${tf}
-      cd ..
     fi
   fi
 done
