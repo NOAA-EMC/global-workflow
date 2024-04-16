b7e5b6ef76d13270c56ae5b919c4334084d3d569
TerrenceMcGuinness-NOAA <terrence.mcguinness@cox.net>
2024-04-16 17:59:23 +0000

    Upadte and tested CI Bash for WCOSS2 (#2481)
    Updates to the driver scripts to the CI framework to work on WCOSS with
    Cron Bash.
    
    Resolves #2268

--- ci/scripts/driver_weekly.sh
+++ ci/scripts/driver_weekly.sh
@@ -38,7 +38,7 @@ export PS4='+ $(basename ${BASH_SOURCE[0]})[${LINENO}]'
 
 source "${ROOT_DIR}/ush/detect_machine.sh"
 case ${MACHINE_ID} in
-  hera | orion | hercules)
+  hera | orion | hercules | wcoss2)
     echo "Running Automated Testing on ${MACHINE_ID}"
     source "${ROOT_DIR}/ci/platforms/config.${MACHINE_ID}"
     ;;
