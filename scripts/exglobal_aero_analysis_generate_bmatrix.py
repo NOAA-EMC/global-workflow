da333e9e04751b11cbc267514a79d940f715c199
Cory Martin <cory.r.martin@noaa.gov>
2024-04-17 16:16:44 -0400

    More changes needed; end of day commit

---
+++ scripts/exglobal_aero_analysis_generate_bmatrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# exglobal_aero_analysis_generate_bmatrix.py
+# This script creates an AerosolVariance object
+# and runs the methods needed
+# to stage files, compute the variance, and write to com
+# files needed for the variational solver
+import os
+
+from wxflow import Logger, cast_strdict_as_dtypedict
+from pygfs.task.aero_variance import AerosolVariance
+
+# Initialize root logger
+logger = Logger(level='DEBUG', colored_log=True)
+
+
+if __name__ == '__main__':
+
+    # Take configuration from environment and cast it as python dictionary
+    config = cast_strdict_as_dtypedict(os.environ)
+
+    # Instantiate the aerosol variance tasks
+    AeroB = AerosolVariance(config)
+    AeroB.initialize()
+    AeroB.computeVariance()
+    AeroB.finalize()
