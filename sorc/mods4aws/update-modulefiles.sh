956a3e594e09b36c5295a231ded838bed76d1d4b
Wei Huang <Wei.Huang@noaa.gov>
2024-04-21 14:36:44 +0000

    compiled OK for ca-sfc-emc account

--- sorc/mods4aws/update-modulefiles.sh
+++ sorc/mods4aws/update-modulefiles.sh
@@ -5,7 +5,8 @@ set -x
 #machine=azure
 machine=aws
 
-srcdir=/contrib/Wei.Huang/debug/global-workflow-cloud
+#srcdir=/contrib/Wei.Huang/debug/global-workflow-cloud
+srcdir=/contrib/Wei.Huang/src/global-workflow-cloud
 
 for item in gdas gfs_utils gsi_enkf gsi_monitor gsi_utils \
             ufs_model ufs_utils upp verif-global
