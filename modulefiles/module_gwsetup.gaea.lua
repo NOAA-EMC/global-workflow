86084769f87279efab521ae15a66df9261c314e4
AnilKumar-NOAA <Anil.Kumar@gaea53.ncrc.gov>
2024-05-01 13:03:01 -0400

    gaea module updates in modulefiles, env, and versions

--- modulefiles/module_gwsetup.gaea.lua
+++ modulefiles/module_gwsetup.gaea.lua
@@ -1,20 +1,21 @@
 help([[
-Load environment to run GFS workflow setup scripts on Gaea C5 
+Load environment to run GFS workflow setup scripts on Gaea
 ]])
 
+prepend_path("MODULEPATH", "/ncrc/proj/epic/rocoto/modulefiles")
 load(pathJoin("rocoto"))
 
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/modulefiles")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/rocoto/modulefiles")
-load(pathJoin("rocoto","1.3.6"))
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
-local python_ver=os.getenv("python_ver") or "3.9.12"
+local python_ver=os.getenv("python_ver") or "3.10.13"
 
 load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("python", python_ver))
 load("py-jinja2")
 load("py-pyyaml")
 load("py-numpy")
+local git_ver=os.getenv("git_ver") or "2.35.2"
+load(pathJoin("git", git_ver))
 
 whatis("Description: GFS run setup environment")
