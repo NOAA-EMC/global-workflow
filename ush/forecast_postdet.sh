a005244bbfb2f1c525bca60d966f8095aa1acb8e
Guoqing Ge <guoqing.ge@noaa.gov>
2024-05-02 20:32:53 -0600

    Add nest capability (#2545)
    This PR adds the global nesting capability to the global workflow so
    that one can run a regional domain nested in a global run.
    
    Resolves #2544 
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -180,6 +180,10 @@ EOF
       if [[ "${WRITE_DOPOST}" == ".true." ]]; then
         ${NLN} "${COM_ATMOS_MASTER}/${RUN}.t${cyc}z.master.grb2f${FH3}" "GFSPRS.GrbF${FH2}"
         ${NLN} "${COM_ATMOS_MASTER}/${RUN}.t${cyc}z.sfluxgrbf${FH3}.grib2" "GFSFLX.GrbF${FH2}"
+        if [[ "${DO_NEST:-NO}" == "YES" ]] ; then
+          ${NLN} "${COM_ATMOS_MASTER}/${RUN}.t${cyc}z.nest.grb2f${FH3}" "GFSPRS.GrbF${FH2}.nest02"
+          ${NLN} "${COM_ATMOS_MASTER}/${RUN}.t${cyc}z.nest.sfluxgrbf${FH3}.grib2" "GFSFLX.GrbF${FH2}.nest02"
+        fi
       fi
     done
   else  # TODO: Is this even valid anymore?
@@ -201,7 +205,14 @@ FV3_nml() {
   source "${USHgfs}/parsing_namelists_FV3.sh"
   source "${USHgfs}/parsing_model_configure_FV3.sh"
 
-  FV3_namelists
+  # Call the appropriate namelist functions
+  if [[ "${DO_NEST:-NO}" == "YES" ]] ; then
+    source "${USHgfs}/parsing_namelists_FV3_nest.sh"
+    FV3_namelists_nest global
+    FV3_namelists_nest nest
+  else
+    FV3_namelists
+  fi
   FV3_model_configure
 
   echo "SUB ${FUNCNAME[0]}: FV3 name lists and model configure file created"
