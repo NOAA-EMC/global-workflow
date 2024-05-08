eb3589c549186845f1d119fdc6042a53651adbde
tmcguinness <terry.mcguinness@noaa.gov>
2024-05-08 15:31:44 +0000

    updated third arg to moc

--- ci/scripts/utils/test_rocotostat.py
+++ ci/scripts/utils/test_rocotostat.py
@@ -6,10 +6,10 @@ class MockCommand:
     def __init__(self):
         self.add_default_arg = MagicMock()
 
-@patch('os.path.abspath', return_value='path/to/workflow.xml')
+@patch('os.path.abspath', return_value='workflow.xml')
 @patch('rocotostat.which', return_value=MockCommand())
 @patch('rocotostat.input_args')
-def test_rocoto_statcount(mock_input_args, mock_which):
+def test_rocoto_statcount(mock_input_args, mock_which, mock_abspath):
     # Arrange
     mock_input_args.return_value = MagicMock(w=MagicMock(name='workflow.xml'), d=MagicMock(name='database.db'))
 
@@ -20,6 +20,7 @@ def test_rocoto_statcount(mock_input_args, mock_which):
     mock_which.assert_called_with('rocotostat')
     mock_input_args.assert_called_once()
 
+@patch('os.path.abspath', return_value='workflow.xml')
 @patch('rocotostat.which', side_effect=CommandNotFoundError('rocotostat not found in PATH'))
 @patch('rocotostat.input_args')
 def test_rocoto_statcount_raises_error_when_rocotostat_not_found(mock_input_args, mock_which, mock_abspath):
