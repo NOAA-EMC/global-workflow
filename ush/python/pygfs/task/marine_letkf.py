00a14d9a0e4464b2f9bb1c48f5d2a6473d046524
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-05-13 21:36:21 +0000

    copy bkg

--- ush/python/pygfs/task/marine_letkf.py
+++ ush/python/pygfs/task/marine_letkf.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from datetime import timedelta
 from logging import getLogger
+from os import path
 from pygfs.task.analysis import Analysis
+from soca import bkg_utils
 from typing import Dict
 from wxflow import (chdir,
+                    FileHandler,
                     logit,
                     Task)
 
@@ -30,6 +34,18 @@ class MarineLETKF(Analysis):
         logger.info("init")
         super().__init__(config)
 
+        PDY = self.runtime_config['PDY']
+        cyc = self.runtime_config['cyc']
+        DATA = self.runtime_config.DATA
+        cdate = PDY + timedelta(hours=cyc)
+
+        half_assim_freq = timedelta(hours=int(config['assim_freq'])/2)
+        window_begin = cdate - half_assim_freq
+
+        self.config['window_begin'] = window_begin
+        self.config['BKG_LIST'] = 'bkg_list.yaml'
+        self.config['bkg_dir'] = path.join(DATA, 'bkg')
+
     @logit(logger)
     def initialize(self):
         """Method initialize for ocean and sea ice LETKF task
@@ -42,6 +58,13 @@ class MarineLETKF(Analysis):
         """
 
         logger.info("initialize")
+
+
+        FileHandler({'mkdir': [self.config.bkg_dir]}).sync()
+        bkg_utils.gen_bkg_list(bkg_path=self.config.COM_OCEAN_HISTORY_PREV,
+                               out_path=self.config.bkg_dir,
+                               window_begin=self.config.window_begin,
+                               yaml_name=self.config.BKG_LIST)
 
     @logit(logger)
     def run(self):
