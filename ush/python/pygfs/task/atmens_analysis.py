16c5de41347cad95797a8078e6b67d06d95e8932
DavidNew-NOAA <david.new@noaa.gov>
2024-05-13 21:27:04 +0000

    Update atmens_analysis.py

--- ush/python/pygfs/task/atmens_analysis.py
+++ ush/python/pygfs/task/atmens_analysis.py
@@ -244,7 +244,16 @@ class AtmEnsAnalysis(Analysis):
         }
         FileHandler(yaml_copy).sync()
 
-        # Copy FV3 atm increment to comrot directory
+        # create template dictionaries
+        template_inc = self.task_config.COM_ATMOS_ANALYSIS_TMPL
+        tmpl_inc_dict = {
+            'ROTDIR': self.task_config.ROTDIR,
+            'RUN': self.task_config.RUN,
+            'YMD': to_YMD(self.task_config.current_cycle),
+            'HH': self.task_config.current_cycle.strftime('%H')
+        }
+        
+        # copy FV3 atm increment to comrot directory
         logger.info("Copy UFS model readable atm increment file")
         cdate = to_fv3time(self.task_config.current_cycle)
         cdate_inc = cdate.replace('.', '_')
@@ -252,8 +261,13 @@ class AtmEnsAnalysis(Analysis):
         for imem in range(1, self.task_config.NMEM_ENS + 1):
             memchar = f"mem{imem:03d}"
 
+            # create output path for member analysis increment
+            tmpl_inc_dict['MEMDIR'] = memchar
+            incdir = Template.substitute_structure(template_inc, TemplateConstants.DOLLAR_CURLY_BRACE, tmpl_inc_dict.get)
             src = os.path.join(self.task_config.DATA, 'anl', memchar, f"atminc.{cdate_inc}z.nc4")
-            dest = os.path.join(self.task_config.COM_ATMOS_ANALYSIS_ENS, f'{self.task_config.CDUMP}.t{self.task_config.cyc:02d}z.atminc.nc')
+            dest = os.path.join(incdir, f"{self.task_config.CDUMP}.t{self.task_config.cyc:02d}z.atminc.nc")
+
+            # copy increment
             logger.debug(f"Copying {src} to {dest}")
             inc_copy = {
                 'copy': [[src, dest]]
