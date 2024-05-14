04d75e375932ecb9e6641d88961e6a97dd49535f
Kate.Friedman <Kate.Friedman@noaa.gov>
2024-05-14 15:04:57 +0000

    Add initial changes to pythonize and yamlize staging
    - Add initial new yaml files for staging information
    - Add new stage.py to python tasks.
    - Add first draft pythonization of stage ex-script.
    
    Much more work is still to be done.
    
    Refs #2475

--- scripts/exglobal_stage_ic.py
+++ scripts/exglobal_stage_ic.py
@@ -1,182 +1,52 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-source "${USHgfs}/preamble.sh"
+import os
 
-# Locally scoped variables and functions
-# shellcheck disable=SC2153
-GDATE=$(date --utc -d "${PDY} ${cyc} - ${assim_freq} hours" +%Y%m%d%H)
-gPDY="${GDATE:0:8}"
-gcyc="${GDATE:8:2}"
+from pygfs.task.stage import Stage
+from wxflow import AttrDict, Logger, cast_strdict_as_dtypedict, chdir, logit
 
-MEMDIR_ARRAY=()
-if [[ "${RUN:-}" = "gefs" ]]; then
-  # Populate the member_dirs array based on the value of NMEM_ENS
-  for ((ii = 0; ii <= "${NMEM_ENS:-0}"; ii++)); do
-    MEMDIR_ARRAY+=("mem$(printf "%03d" "${ii}")")
-  done
-else
-  MEMDIR_ARRAY+=("")
-fi
+# Initialize root logger
+logger = Logger(level=os.environ.get("LOGGING_LEVEL", "DEBUG"), colored_log=True)
 
-# Initialize return code
-err=0
+@logit(logger)
+def main():
 
-error_message() {
-  echo "FATAL ERROR: Unable to copy ${1} to ${2} (Error code ${3})"
-}
+    config = cast_strdict_as_dtypedict(os.environ)
 
-###############################################################
-for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
+    # Instantiate the Stage object
+    stage = Stage(config)
 
-  # Stage atmosphere initial conditions to ROTDIR
-  if [[ ${EXP_WARM_START:-".false."} = ".true." ]]; then
-    # Stage the FV3 restarts to ROTDIR (warm start)
-    RUN=${rCDUMP} YMD=${gPDY} HH=${gcyc} declare_from_tmpl COM_ATMOS_RESTART_PREV:COM_ATMOS_RESTART_TMPL
-    [[ ! -d "${COM_ATMOS_RESTART_PREV}" ]] && mkdir -p "${COM_ATMOS_RESTART_PREV}"
-    for ftype in coupler.res fv_core.res.nc; do
-      src="${BASE_CPLIC}/${CPL_ATMIC:-}/${PDY}${cyc}/${MEMDIR}/atmos/${PDY}.${cyc}0000.${ftype}"
-      tgt="${COM_ATMOS_RESTART_PREV}/${PDY}.${cyc}0000.${ftype}"
-      ${NCP} "${src}" "${tgt}"
-      rc=$?
-      ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-      err=$((err + rc))
-    done
-    for ftype in ca_data fv_core.res fv_srf_wnd.res fv_tracer.res phy_data sfc_data; do
-      for ((tt = 1; tt <= ntiles; tt++)); do
-        src="${BASE_CPLIC}/${CPL_ATMIC:-}/${PDY}${cyc}/${MEMDIR}/atmos/${PDY}.${cyc}0000.${ftype}.tile${tt}.nc"
-        if (( tt > 6 )) ; then
-            tgt="${COM_ATMOS_RESTART_PREV}/${PDY}.${cyc}0000.${ftype}.nest0$((tt-5)).tile${tt}.nc"
-        else
-            tgt="${COM_ATMOS_RESTART_PREV}/${PDY}.${cyc}0000.${ftype}.tile${tt}.nc"
-        fi
-        ${NCP} "${src}" "${tgt}"
-        rc=$?
-        ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-        err=$((err + rc))
-      done
-    done
-  else
-    # Stage the FV3 cold-start initial conditions to ROTDIR
-    YMD=${PDY} HH=${cyc} declare_from_tmpl COM_ATMOS_INPUT
-    [[ ! -d "${COM_ATMOS_INPUT}" ]] && mkdir -p "${COM_ATMOS_INPUT}"
-    src="${BASE_CPLIC}/${CPL_ATMIC:-}/${PDY}${cyc}/${MEMDIR}/atmos/gfs_ctrl.nc"
-    tgt="${COM_ATMOS_INPUT}/gfs_ctrl.nc"
-    ${NCP} "${src}" "${tgt}"
-    rc=$?
-    ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-    err=$((err + rc))
-    for ftype in gfs_data sfc_data; do
-      for ((tt = 1; tt <= ntiles; tt++)); do
-        src="${BASE_CPLIC}/${CPL_ATMIC:-}/${PDY}${cyc}/${MEMDIR}/atmos/${ftype}.tile${tt}.nc"
-        tgt="${COM_ATMOS_INPUT}/${ftype}.tile${tt}.nc"
-        ${NCP} "${src}" "${tgt}"
-        rc=$?
-        ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-        err=$((err + rc))
-      done
-      if (( ntiles > 6 )); then
-        ${NLN} "${COM_ATMOS_INPUT}/${ftype}.tile7.nc" "${COM_ATMOS_INPUT}/${ftype}.nest02.tile7.nc"
-      fi
-    done
-  fi
+    #Pull out all the configuration keys needed to run stage job
+    keys = ['RUN','MODE','EXP_WARM_START','CDUMP','rCDUMP',
+            'ntiles',
+            'BASE_CPLIC','waveGRD','OCNRES','USE_OCN_PERTURB_FILES',
+            'CPL_ATMIC','CPL_ICEIC','CPL_MEDIC','CPL_OCNIC','CPL_WAVIC']
 
-  # Stage ocean initial conditions to ROTDIR (warm start)
-  if [[ "${DO_OCN:-}" = "YES" ]]; then
-    RUN=${rCDUMP} YMD=${gPDY} HH=${gcyc} declare_from_tmpl COM_OCEAN_RESTART_PREV:COM_OCEAN_RESTART_TMPL
-    [[ ! -d "${COM_OCEAN_RESTART_PREV}" ]] && mkdir -p "${COM_OCEAN_RESTART_PREV}"
-    src="${BASE_CPLIC}/${CPL_OCNIC:-}/${PDY}${cyc}/${MEMDIR}/ocean/${PDY}.${cyc}0000.MOM.res.nc"
-    tgt="${COM_OCEAN_RESTART_PREV}/${PDY}.${cyc}0000.MOM.res.nc"
-    ${NCP} "${src}" "${tgt}"
-    rc=$?
-    ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-    err=$((err + rc))
-    case "${OCNRES}" in
-      "500" | "100")
-        # Nothing more to do for these resolutions
-        ;;
-      "025" )
-        for nn in $(seq 1 3); do
-          src="${BASE_CPLIC}/${CPL_OCNIC:-}/${PDY}${cyc}/${MEMDIR}/ocean/${PDY}.${cyc}0000.MOM.res_${nn}.nc"
-          tgt="${COM_OCEAN_RESTART_PREV}/${PDY}.${cyc}0000.MOM.res_${nn}.nc"
-          ${NCP} "${src}" "${tgt}"
-          rc=$?
-          ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-          err=$((err + rc))
-        done
-        ;;
-      *)
-        echo "FATAL ERROR: Unsupported ocean resolution ${OCNRES}"
-        rc=1
-        err=$((err + rc))
-        ;;
-    esac
+    stage_dict = AttrDict()
+    for key in keys:
+        stage_dict[key] = stage.task_config[key]
 
-    # Ocean Perturbation Files
-    # Extra zero on MEMDIR ensure we have a number even if the string is empty
-    if (( 0${MEMDIR:3} > 0 )) && [[ "${USE_OCN_PERTURB_FILES:-false}" == "true" ]]; then
-        src="${BASE_CPLIC}/${CPL_OCNIC:-}/${PDY}${cyc}/${MEMDIR}/ocean/${PDY}.${cyc}0000.mom6_increment.nc"
-        tgt="${COM_OCEAN_RESTART_PREV}/${PDY}.${cyc}0000.mom6_increment.nc"
-        ${NCP} "${src}" "${tgt}"
-        rc=${?}
-        ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-        err=$((err + rc))
-    fi
+    # Also import all COM* directory and template variables
+    for key in stage.task_config.keys():
+        if key.startswith("COM"):
+            stage_dict[key] = stage.task_config[key]
 
-    # TODO: Do mediator restarts exists in a ATMW configuration?
-    # TODO: No mediator is presumably involved in an ATMA configuration
-    if [[ ${EXP_WARM_START:-".false."} = ".true." ]]; then
-      # Stage the mediator restarts to ROTDIR (warm start/restart the coupled model)
-      RUN=${rCDUMP} YMD=${gPDY} HH=${gcyc} declare_from_tmpl COM_MED_RESTART_PREV:COM_MED_RESTART_TMPL
-      [[ ! -d "${COM_MED_RESTART_PREV}" ]] && mkdir -p "${COM_MED_RESTART_PREV}"
-      src="${BASE_CPLIC}/${CPL_MEDIC:-}/${PDY}${cyc}/${MEMDIR}/med/${PDY}.${cyc}0000.ufs.cpld.cpl.r.nc"
-      tgt="${COM_MED_RESTART_PREV}/${PDY}.${cyc}0000.ufs.cpld.cpl.r.nc"
-      if [[ -f "${src}" ]]; then
-        ${NCP} "${src}" "${tgt}"
-        rc=$?
-        ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-        err=$((err + rc))
-      else
-        echo "WARNING: No mediator restarts available with warm_start=${EXP_WARM_START}"
-      fi
-    fi
+    #TEST PRINT
+    #for key in stage_dict:
+    #    print(f'{key} = {stage_dict[key]}')
 
-  fi
+    cwd = os.getcwd()
 
-  # Stage ice initial conditions to ROTDIR (warm start)
-  if [[ "${DO_ICE:-}" = "YES" ]]; then
-    RUN=${rCDUMP} YMD=${gPDY} HH=${gcyc} declare_from_tmpl COM_ICE_RESTART_PREV:COM_ICE_RESTART_TMPL
-    [[ ! -d "${COM_ICE_RESTART_PREV}" ]] && mkdir -p "${COM_ICE_RESTART_PREV}"
-    src="${BASE_CPLIC}/${CPL_ICEIC:-}/${PDY}${cyc}/${MEMDIR}/ice/${PDY}.${cyc}0000.cice_model.res.nc"
-    tgt="${COM_ICE_RESTART_PREV}/${PDY}.${cyc}0000.cice_model.res.nc"
-    ${NCP} "${src}" "${tgt}"
-    rc=$?
-    ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-    err=$((err + rc))
-  fi
+    os.chdir(config.ROTDIR)
 
-  # Stage the WW3 initial conditions to ROTDIR (warm start; TODO: these should be placed in $RUN.$gPDY/$gcyc)
-  if [[ "${DO_WAVE:-}" = "YES" ]]; then
-    YMD=${PDY} HH=${cyc} declare_from_tmpl COM_WAVE_RESTART
-    [[ ! -d "${COM_WAVE_RESTART}" ]] && mkdir -p "${COM_WAVE_RESTART}"
-    for grdID in ${waveGRD}; do # TODO: check if this is a bash array; if so adjust
-      src="${BASE_CPLIC}/${CPL_WAVIC:-}/${PDY}${cyc}/${MEMDIR}/wave/${PDY}.${cyc}0000.restart.${grdID}"
-      tgt="${COM_WAVE_RESTART}/${PDY}.${cyc}0000.restart.${grdID}"
-      ${NCP} "${src}" "${tgt}"
-      rc=$?
-      ((rc != 0)) && error_message "${src}" "${tgt}" "${rc}"
-      err=$((err + rc))
-    done
-  fi
+    # Determine which ICs to stage
+    stage_set = stage.determine(stage_dict)
 
-done # for MEMDIR in "${MEMDIR_ARRAY[@]}"; do
+    # Stage ICs
+    # TODO - create and invoke copies
+    stage.execute(stage_set)
 
-###############################################################
-# Check for errors and exit if any of the above failed
-if [[ "${err}" -ne 0 ]]; then
-  echo "FATAL ERROR: Unable to copy ICs from ${BASE_CPLIC} to ${ROTDIR}; ABORT!"
-  exit "${err}"
-fi
+    os.chdir(cwd)
 
-##############################################################
-# Exit cleanly
-exit "${err}"
+if __name__ == '__main__':
+    main()
---
+++ ush/python/pygfs/task/stage.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import shutil
+from datetime import timedelta
+from logging import getLogger
+from typing import Any, Dict, List
+
+from wxflow import (AttrDict,
+                    FileHandler,
+                    Hsi,
+                    Htar,
+                    Task,
+                    cast_strdict_as_dtypedict,
+                    chgrp,
+                    get_gid,
+                    logit,
+                    mkdir_p,
+                    parse_j2yaml,
+                    rm_p,
+                    strftime,
+                    to_YMD,
+                    to_YMDH,
+                    Template,
+                    TemplateConstants)
+
+logger = getLogger(__name__.split('.')[-1])
+
+class Stage(Task):
+    """Task to stage initial conditions
+    """
+
+    @logit(logger, name="Stage")
+    def __init__(self, config: Dict[str, Any]) -> None:
+        """Constructor for the Stage task
+        The constructor is responsible for collecting necessary yamls based on
+        the runtime options and RUN.
+
+        Parameters
+        ----------
+        config : Dict[str, Any]
+            Incoming configuration for the task from the environment
+
+        Returns
+        -------
+        None
+        """
+        super().__init__(config)
+
+        rotdir = self.config.ROTDIR + os.sep
+
+        # Find all absolute paths in the environment and get their relative paths from ${ROTDIR}
+        path_dict = self._gen_relative_paths(rotdir)
+
+        self.task_config = AttrDict(**self.config, **self.runtime_config, **path_dict)
+
+    @logit(logger)
+    def _gen_relative_paths(self, root_path: str) -> Dict:
+        """Generate a dict of paths in self.config relative to root_path
+
+        Parameters
+        ----------
+        root_path : str
+            Path to base all relative paths off of
+
+        Return
+        ------
+        rel_path_dict : Dict
+            Dictionary of paths relative to root_path.  Members will be named
+            based on the dict names in self.config.  For COM paths, the names will
+            follow COM_<NAME> --> <name>_dir.  For all other directories, the
+            names will follow <NAME> --> <name>_dir.
+        """
+
+        rel_path_dict = {}
+        for key, value in self.config.items():
+            if isinstance(value, str):
+                if root_path in value:
+                    rel_path = value.replace(root_path, "")
+                    rel_key = (key[4:] if key.startswith("COM_") else key).lower() + "_dir"
+                    rel_path_dict[rel_key] = rel_path
+
+        return rel_path_dict
+
+    @logit(logger)
+    def determine(self, stage_dict: Dict[str, Any]) -> (Dict[str, Any], List[Dict[str, Any]]):
+        """Determine which initial condition files need to be placed in ROTDIR.
+
+        Parameters
+        ----------
+        stage_dict : Dict[str, Any]
+            Task specific keys, e.g. runtime options (DO_AERO, DO_ICE, etc)
+
+        Return
+        ------
+        stage_set : Dict[str, Any]
+            Set of FileHandler instructions to copy files to the ROTDIR
+        """
+
+        stage_parm = os.path.join(stage_dict.PARMgfs, "stage")
+
+        # Add the glob.glob function for capturing log filenames
+        # TODO remove this kludge once log filenames are explicit
+        stage_dict['glob'] = glob.glob
+
+        # Add the os.path.exists function to the dict for yaml parsing
+        stage_dict['path_exists'] = os.path.exists
+
+        if not os.path.isdir(stage_dict.ROTDIR):
+            raise FileNotFoundError(f"FATAL ERROR: The ROTDIR ({stage_dict.ROTDIR}) does not exist!")
+
+        if stage_dict.RUN == "gfs" or stage_dict.RUN == "gdas":
+            if stage_dict.MODE == "cycled":
+                master_yaml = "master_cycled.yaml.j2"
+            elif stage_dict.MODE == "forecast-only":
+                master_yaml = "master_forecast_only.yaml.j2"
+        elif stage_dict.RUN == "gefs":
+            raise NotImplementedError("FATAL ERROR: Staging is not yet set up for GEFS runs")
+        elif stage_dict.RUN == "sfs":
+            raise NotImplementedError("FATAL ERROR: Staging is not yet set up for SFS runs")
+        else:
+            raise ValueError(f"FATAL ERROR: Staging is not enabled for {stage_dict.RUN} runs")
+
+        parsed_sets = parse_j2yaml(os.path.join(stage_parm, master_yaml), stage_dict)
+
+        stage_sets = []
+
+        for dataset in parsed_sets.datasets.values():
+
+            dataset["fileset"] = Stage._create_fileset(dataset)
+
+            stage_sets.append(dataset)
+
+        return stage_sets
+
+#TODO - create def for staging
