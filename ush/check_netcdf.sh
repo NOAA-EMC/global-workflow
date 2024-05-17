478a5ba14987bc1c2b4d5b6430efdbc8ea2b432f
DavidHuber <david.huber@noaa.gov>
2024-05-17 10:30:02 +0000

    Ignore shellcheck warning

--- ush/check_netcdf.sh
+++ ush/check_netcdf.sh
@@ -8,6 +8,7 @@ source "${HOMEgfs}/ush/load_fv3gfs_modules.sh" 1>/dev/null 2>&1
 
 ncfile=${1?}
 
+# shellcheck disable=SC2086
 (( $(ncdump ${file} 2> /dev/null | grep -Po '(?<=time = UNLIMITED ; // \()\d+(?= currently)' || echo 0) > 0 )) # redirect stdout and stderr to /dev/null to suppress output in cron
 rc=$?
 # If there is no error, rc=0, else rc!=0
