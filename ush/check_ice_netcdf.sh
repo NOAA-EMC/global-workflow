697769116893665b2a5aa3f04fdf286e287bceb5
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-05-21 17:44:24 +0000

    Improve formatting
    The formatting has been improved in check_ice_netcdf.sh.

--- ush/check_ice_netcdf.sh
+++ ush/check_ice_netcdf.sh
@@ -15,22 +15,22 @@ fhr=${5?}
 ROTDIR=${6?}
 member=${7?}
 
-fhri=$(echo $fhr | sed 's/^0*//')
+fhri=$(echo "${fhr}" | sed 's/^0*//')
 
 if [[ "${cyc}" != "00" ]] && (( FHOUT_ICE_GFS % 24 == 0 ));then
-  (( fhri = ${fhri} - cyc ))
+  (( fhri = fhri - cyc ))
   fhr3=$(printf %03i "${fhri}")
   if (( fhri <= 24  )); then
     (( interval = 24 - cyc )) 
-    ncfile=$ROTDIR/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${interval}hr_avg.f${fhr3}.nc
+    ncfile=${ROTDIR}/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${interval}hr_avg.f${fhr3}.nc
   else
-    ncfile=$ROTDIR/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${FHOUT_ICE_GFS}hr_avg.f${fhr3}.nc
+    ncfile=${ROTDIR}/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${FHOUT_ICE_GFS}hr_avg.f${fhr3}.nc
   fi
 else
-  ncfile=$ROTDIR/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${FHOUT_ICE_GFS}hr_avg.f${fhr}.nc
+  ncfile=${ROTDIR}/gefs.${yyyy}${mm}${dd}/${cyc}/mem${member}/model_data/ice/history/gefs.ice.t${cyc}z.${FHOUT_ICE_GFS}hr_avg.f${fhr}.nc
 fi
 
-ls -l $ncfile 2>&1  # redirect stdout and stderr to /dev/null to suppress output in cron
+ls -l "${ncfile}" 2>&1  # redirect stdout and stderr to /dev/null to suppress output in cron
 
 rc=$?
 # If there is no error, rc=0, else rc!=0
