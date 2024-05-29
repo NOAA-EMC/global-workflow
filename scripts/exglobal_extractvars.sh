c17397911b13bde6f927a1a3470d4bb933b5695d
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-05-29 19:24:18 +0000

    Add corrections
    Corrections were made to some new scripts.

--- scripts/exglobal_extractvars.sh
+++ scripts/exglobal_extractvars.sh
@@ -13,13 +13,13 @@ EXTRCTVARO=${EXTRCTVARO:-"${USHgfs}/gefs_ocn_extractvars.sh"}
 EXTRCTVARW=${EXTRCTVARW:-"${USHgfs}/gefs_wav_extractvars.sh"}
 
 #Paramater Tables used
-export varlist_2d=${varlist_2d:-"${PARMgfs}/product/gefs_shortparmlist_2d.parm"} #Parameter table for surface atmos variables
+export varlist_2d=${varlist_2d:-"${PARMgfs}/product/gefs_shortparmlist_2d.parm"} #Parameter table for surface variables
 export varlist_3d=${varlist_3d:-"${PARMgfs}/product/gefs_shortparmlist_3d_h.parm"} #Parameter table for upper air instantaneous variables
 export varlist_3d_d=${varlist_3d_d:-"${PARMgfs}/product/gefs_shortparmlist_3d_d.parm"} #Parameter table for upper air daily-averaged variables
 export varlist_wav=${varlist_wav:-"${PARMgfs}/product/gefs_wav_shortparmlist.parm"} #Parameter table for wave variables
 
 # Variables used in this job
-export FHMIN=${FHMIN:-0} #The total number of lead hours
+export FHMIN=${FHMIN:-0} #The first lead hour
 export FHMAX=${FHMAX:-120} #The total number of lead hours
 export FHMAXHF=${FHMAXHF:-60} #The maximum lead hour that will use a high output frequency  
 export FHOUTHF=${FHOUTHF:-6} #The high output frequency (in hours)
