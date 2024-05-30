ecac7485dd1f52ec1bc661a0c8970235ab94fbc5
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2024-05-29 20:38:16 -0500

    Disable tracker jobs on Hercules
    The external tracker package is not yet available on Rocky 9, so a
    hack is added to skip those jobs on Hercules until the package is
    available. This will need to be extended to Orion if that machine
    transitions to Rocky 9 before a package is available.
    
    Refs #2639

--- jobs/rocoto/genesis_fsu.sh
+++ jobs/rocoto/genesis_fsu.sh
@@ -2,6 +2,11 @@
 
 source "${HOMEgfs}/ush/preamble.sh"
 
+# Hack to temporary skip this as the tracker has not been build
+#   on Hercules Rocky 9 yet
+# TODO: Remove this after tracker has been built for Rocky 9 #2639
+if [[ "${machine}" == 'hercules' ]]; then exit 0; fi
+
 ###############################################################
 # Source FV3GFS workflow modules
 source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
