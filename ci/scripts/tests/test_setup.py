65343f9f95f3984ed24c559f2dcf14be5b7bf762
tmcguinness <terry.mcguinness@noaa.gov>
2024-06-04 21:51:33 +0000

    added addtional check to make sure the Configure object is getting popululated correctly after config parms are in place

--- ci/scripts/tests/test_setup.py
+++ ci/scripts/tests/test_setup.py
@@ -1,4 +1,4 @@
-from wxflow import Executable
+from wxflow import Executable, Configuration
 from shutil import rmtree
 import os
 
@@ -12,7 +12,7 @@ def test_setup_expt():
     arguments = [
         "gfs", "forecast-only",
         "--pslot", "C48_ATM", "--app", "ATM", "--resdetatmos", "48",
-        "--comroot", f"{RUNDIR}/COMROT", "--expdir", f"{RUNDIR}/EXPDIR",
+        "--comroot", f"{RUNDIR}", "--expdir", f"{RUNDIR}",
         "--idate", "2021032312", "--edate", "2021032312", "--overwrite"
     ]
     setup_expt_script = Executable(os.path.join(HOMEgfs, "workflow", "setup_expt.py"))
@@ -25,10 +25,14 @@ def test_setup_xml():
 
     env = os.environ.copy()
     env['ACCOUNT'] = "foo"
-    env['HOMEgfs'] = "bar"
 
     setup_xml_script = Executable(os.path.join(HOMEgfs, "workflow", "setup_xml.py"))
-    setup_xml_script.add_default_arg(f"{RUNDIR}/EXPDIR/C48_ATM")
+    setup_xml_script.add_default_arg(f"{RUNDIR}/C48_ATM")
     setup_xml_script(env=env)
     assert (setup_xml_script.returncode == 0)
+
+    cfg = Configuration(f"{RUNDIR}/C48_ATM")
+    base = cfg.parse_config('config.base')
+    assert base.ACCOUNT == 'fv3-cpu' 
+
     rmtree(RUNDIR)
