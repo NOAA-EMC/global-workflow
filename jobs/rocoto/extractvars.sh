b1076f386bb1a97c8a3816d0b2d1b43986744df8
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-06-04 14:22:56 +0000

    Clean up more scripts
    More scripts have been cleaned up.

--- jobs/rocoto/extractvars.sh
+++ jobs/rocoto/extractvars.sh
@@ -7,7 +7,7 @@ echo
 echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
 . "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+[[ "${status}" -ne 0 ]] && exit "${status}"
 
 export job="extractvars"
 export jobid="${job}.$$"
@@ -16,8 +16,8 @@ export jobid="${job}.$$"
 echo
 echo "=============== START TO RUN EXTRACTVARS ==============="
 # Execute the JJOB
-${HOMEgfs}/jobs/JGLOBAL_EXTRACTVARS
+"${HOMEgfs}/jobs/JGLOBAL_EXTRACTVARS"
 status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+[[ "${status}" -ne 0 ]] && exit "${status}"
 
 exit 0
