a1a8747d2ea3f902f50016eedfe7298f02735a29
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2024-06-07 20:08:01 +0000

    Small correction.

--- scripts/exglobal_archive.py
+++ scripts/exglobal_archive.py
@@ -37,9 +37,9 @@ def main():
     for key in keys:
         archive_dict[key] = archive.task_config[key]
 
-    # Also import all COMIN* directory and template variables
+    # Also import all COMIN* and COMOUT* directory and template variables
     for key in archive.task_config.keys():
-        if key.startswith("COMIN"):
+        if key.startswith("COMIN") or key.startswith("COMOUT"):
             archive_dict[key] = archive.task_config[key]
 
     cwd = os.getcwd()
