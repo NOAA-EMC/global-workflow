8e2a26a16ddac4d8c7086bb8b6380e15363adf2e
Jessica Meixner <Jessica.Meixner@noaa.gov>
2024-06-07 13:39:08 +0000

    revert changes to allow for asynchronous development updates

--- jobs/rocoto/waveinit.sh
+++ jobs/rocoto/waveinit.sh
@@ -5,7 +5,8 @@ source "${HOMEgfs}/ush/preamble.sh"
 ###############################################################
 echo
 echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+. ${HOMEgfs}/ush/load_ufswm_modules.sh
 status=$?
 [[ ${status} -ne 0 ]] && exit ${status}
 
--- jobs/rocoto/wavepostsbs.sh
+++ jobs/rocoto/wavepostsbs.sh
@@ -4,7 +4,8 @@ source "${HOMEgfs}/ush/preamble.sh"
 
 ###############################################################
 # Source FV3GFS workflow modules
-. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+#. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
+. ${HOMEgfs}/ush/load_ufswm_modules.sh
 status=$?
 [[ ${status} -ne 0 ]] && exit ${status}
 
