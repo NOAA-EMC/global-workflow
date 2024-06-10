5a52e5795d13505213a114194f712033cb5ef330
L. Gwen Chen <lichuan.chen@noaa.gov>
2024-06-10 15:33:28 +0000

    Update gempak job to run one fcst hour per task

--- jobs/rocoto/gempak.sh
+++ jobs/rocoto/gempak.sh
@@ -1,17 +1,29 @@
 #! /usr/bin/env bash
 
 source "${HOMEgfs}/ush/preamble.sh"
-
-###############################################################
-. "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-if (( status != 0 )); then exit "${status}"; fi
+(( status != 0 )) && exit "${status}"
 
 export job="gempak"
 export jobid="${job}.$$"
 
+source "${HOMEgfs}/ush/jjob_header.sh" -e "gempak" -c "base gempak"
+
+fhrlst=$(echo "${FHRLST}" | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
+
+for fhr3 in ${fhrlst}; do
+    fhr=$(( 10#${fhr3} ))
+    if (( fhr > FHMAX_GFS )); then
+	echo "Nothing to process for FHR = ${fhr3}, cycle"
+	continue
+    fi
+
+    export fcsthrs="${fhr3}"
+
 # Execute the JJOB
-"${HOMEgfs}/jobs/J${RUN^^}_ATMOS_GEMPAK"
+    "${HOMEgfs}/jobs/J${RUN^^}_ATMOS_GEMPAK"
+done
 
 status=$?
 exit "${status}"
