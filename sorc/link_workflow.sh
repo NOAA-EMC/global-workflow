c98474a7076a3419a93e63cf8609901e02979ccc
Jessica Meixner <jessica.meixner@noaa.gov>
2024-06-12 11:03:13 -0400

    Update ufs-weather-model  (#2663)
    Updates ufs-weather-model, this updates RDHPCS to the newer spack-stack
    allowing some temporary fixes to be reverted.
    * removes upp submodule
    * uses upp from the ufs-weather-model
    * restores the build and link that were hacked during the Hera Rocky 8
    transition to allow for UPP submodule
    * Removes forecast directories in clean-up
    
    Resolves #2617 
    Resolves #2437
    
    ---------
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>

--- sorc/link_workflow.sh
+++ sorc/link_workflow.sh
@@ -370,11 +370,10 @@ fi
 #--link source code directories
 #------------------------------
 cd "${HOMEgfs}/sorc" || exit 8
-# TODO: Commenting out until UPP is up-to-date with Rocky-8.
-#if [[ -d ufs_model.fd ]]; then
-#  [[ -d upp.fd ]] && rm -rf upp.fd
-#  ${LINK} ufs_model.fd/FV3/upp upp.fd
-#fi
+if [[ -d ufs_model.fd ]]; then
+  [[ -d upp.fd ]] && rm -rf upp.fd
+  ${LINK} ufs_model.fd/FV3/upp upp.fd
+fi
 
 if [[ -d gsi_enkf.fd ]]; then
   [[ -d gsi.fd ]] && rm -rf gsi.fd
