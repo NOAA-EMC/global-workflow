1cab6d5bf97fddd778779660a76cc614c83dad9e
L. Gwen Chen <lichuan.chen@noaa.gov>
2024-06-13 18:14:49 +0000

    Upload GDAS and GOES job scripts

--- jobs/rocoto/gempakgrb2spec.sh
+++ jobs/rocoto/gempakgrb2spec.sh
@@ -1,17 +1,30 @@
 #! /usr/bin/env bash
 
 source "${HOMEgfs}/ush/preamble.sh"
-
-###############################################################
-. "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
 if (( status != 0 )); then exit "${status}"; fi
 
 export job="gempakpgrb2spec"
 export jobid="${job}.$$"
 
+source "${HOMEgfs}/ush/jjob_header.sh" -e "gempak_spec" -c "base"
+
+# shellcheck disable=SC2153
+fhrlst=$(echo "${FHRLST}" | sed -e 's/_/ /g; s/f/ /g; s/,/ /g')
+
+for fhr3 in ${fhrlst}; do
+  fhr=$(( 10#${fhr3} ))
+  if (( fhr > FHMAX_GFS )); then
+    echo "Nothing to process for FHR = ${fhr3}, cycle"
+    continue
+  fi
+
+  export fcsthrs="${fhr3}"
+
 # Execute the JJOB
-"${HOMEgfs}/jobs/JGFS_ATMOS_GEMPAK_PGRB2_SPEC"
+  "${HOMEgfs}/jobs/JGFS_ATMOS_GEMPAK_PGRB2_SPEC"
+done
 
 status=$?
 exit "${status}"
