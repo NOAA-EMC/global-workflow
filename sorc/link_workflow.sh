a72720f8f8ad6149ec1e6a5c0c8ddf25646d4326
Dan Holdaway <27729500+danholdaway@users.noreply.github.com>
2024-06-14 18:05:23 -0400

    Update GDASapp hash to move JCB into GDASapp (#2665)
    This PR moves JCB into GDASapp. The PR
    also bumps up the hash of GDASapp to what is in `feature/move_jcb`,
    which at time of writing is develop plus the absorption of JCB into
    GDASapp.
    Note that I also took the changes from
    https://github.com/NOAA-EMC/global-workflow/pull/2641 to follow the
    testing @RussTreadon-NOAA has done.

--- sorc/link_workflow.sh
+++ sorc/link_workflow.sh
@@ -87,11 +87,19 @@ source "${HOMEgfs}/versions/fix.ver"
 
 # Link python pacakges in ush/python
 # TODO: This will be unnecessary when these are part of the virtualenv
-packages=("wxflow" "jcb")
+packages=("wxflow")
 for package in "${packages[@]}"; do
     cd "${HOMEgfs}/ush/python" || exit 1
     [[ -s "${package}" ]] && rm -f "${package}"
     ${LINK} "${HOMEgfs}/sorc/${package}/src/${package}" .
+done
+
+# Link GDASapp python packages in ush/python
+packages=("jcb")
+for package in "${packages[@]}"; do
+    cd "${HOMEgfs}/ush/python" || exit 1
+    [[ -s "${package}" ]] && rm -f "${package}"
+    ${LINK} "${HOMEgfs}/sorc/gdas.cd/sorc/${package}/src/${package}" .
 done
 
 # Link wxflow in workflow and ci/scripts
