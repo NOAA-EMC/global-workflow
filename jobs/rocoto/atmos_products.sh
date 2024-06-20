e2966360e4a5eab4f7db80d8843803015b6ec1e5
Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>
2024-06-19 21:51:22 -0400

    Eliminate post groups (#2667)
    Eliminates the post groups used for upp and products jobs so that each
    task only processes one forecast hour. This is more efficient and
    greatly simplifies downstream dependencies that depend on a specific
    forecast hour.
    
    Resolves #2666
    Refs #2642

--- jobs/rocoto/atmos_products.sh
+++ jobs/rocoto/atmos_products.sh
@@ -15,21 +15,13 @@ if (( status != 0 )); then exit "${status}"; fi
 export job="atmos_products"
 export jobid="${job}.$$"
 
+# Negatation needs to be before the base
+fhr3_base="10#${FHR3}"
+export FORECAST_HOUR=$(( ${fhr3_base/10#-/-10#} ))
+
 ###############################################################
-# shellcheck disable=SC2153,SC2001
-IFS='_' read -ra fhrs <<< "${FHRLST//f}" # strip off the 'f's and convert to array
-
-#---------------------------------------------------------------
 # Execute the JJOB
-for fhr in "${fhrs[@]}"; do
-    # The analysis fhr is -001.  Performing math on negative, leading 0 integers is tricky.
-    # The negative needs to be in front of "10#", so do some regex magic to make it happen.
-    fhr="10#${fhr}"
-    fhr=${fhr//10\#-/-10\#}
-    export FORECAST_HOUR=$(( fhr ))
-    "${HOMEgfs}/jobs/JGLOBAL_ATMOS_PRODUCTS"
-    status=$?
-    if (( status != 0 )); then exit "${status}"; fi
-done
+###############################################################
+"${HOMEgfs}/jobs/JGLOBAL_ATMOS_PRODUCTS"
 
-exit 0
+exit $?
