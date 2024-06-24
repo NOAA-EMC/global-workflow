8fc02e29e75aad7d8ce274176a35cc2d03e31900
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2024-06-24 18:05:11 +0000

    add g-w python to PYTHONPATH on WCOSS2 in upp.sh (#2700)

--- jobs/rocoto/upp.sh
+++ jobs/rocoto/upp.sh
@@ -29,6 +29,12 @@ if [[ "${MACHINE_ID}" = "wcoss2" ]]; then
   module load python/3.8.6
   module load crtm/2.4.0  # TODO: This is only needed when UPP_RUN=goes.  Is there a better way to handle this?
   set_trace
+
+  # Add wxflow to PYTHONPATH
+  wxflowPATH="${HOMEgfs}/ush/python"
+  PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/ush:${wxflowPATH}"
+  export PYTHONPATH
+
 else
   . "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
   status=$?
