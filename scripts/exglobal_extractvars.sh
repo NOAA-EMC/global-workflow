2be9a15aeb03c7e2e8c137e5b9f0f404d8dcdf23
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-07-02 18:35:50 +0000

    Move extractvars variable defs to config
    The extractvars variables have been moved from exscript to config.extractvars.

--- scripts/exglobal_extractvars.sh
+++ scripts/exglobal_extractvars.sh
@@ -22,23 +22,6 @@ export varlist_ice_netcdf=${varlist_ice_netcdf:-"${PARMgfs}/product/gefs_ice_sho
 export varlist_ocn_grib2=${varlist_ocn_grib2:-"${PARMgfs}/product/gefs_ocn_shortparmlist_grib2.parm"} #Parameter table for wave variables
 export varlist_ice_grib2=${varlist_ice_grib2:-"${PARMgfs}/product/gefs_ice_shortparmlist_grib2.parm"} #Parameter table for wave variables  
 
-# Variables used in this job
-export FHMIN=${FHMIN:-0} #The first lead hour
-export FHMAX=${FHMAX:-120} #The total number of lead hours
-export FHMAXHF=${FHMAXHF:-60} #The maximum lead hour that will use a high output frequency  
-export FHOUTHF=${FHOUTHF:-6} #The high output frequency (in hours)
-export FHOUTLF=${FHOUTLF:-12} #The low output frequency (in hours)
-export cycle=${cycle:-"t00z"} #cycle
-export ocn_dataformat=${ocn_dataformat:-"grib2"} #the data format of the ocean products to be read
-export ice_dataformat=${ice_dataformat:-"grib2"} #the data format of the ice products to be read
-export ocnres=${ocnres:-"5p00"} #Resolution of ocean products
-export iceres=${iceres:-"5p00"} #Resolution of ice products
-export ocnres=${ocnres:-"5p00"} #Resolution of ocean products
-export wavres=${wavinres:-"5p00"} #Resolution of wave products
-export compress_ocn=${compress_ocn:-1} #1: compress extracted ocean product, 0: do not compress extracted ocean product
-export compress_ice=${compress_ice:-1} #1: compress extracted ice product, 0: do not compress extracted ice product 
-export FHOUT_WAV_NOSCRUB=${FHOUT_WAV_NOSCRUB:-6} #Frequency of wave output to be saved on disk
-
 #Extract variables for atmosphere
 if [[ "${DO_ATM}" == "YES" ]];then
   if [[ ! -d "${DATA}/mem${ENSMEM}_atmos" ]]; then 
