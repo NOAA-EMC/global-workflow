ac855af0ad34a778c7e0bf4464bf43cb57399d98
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-07-09 15:05:02 +0000

    Rename oufile1 in gefs_wav_extractvars
    The oufile1 variable in gefs_wav_extractvars has been renamed to outfile.

--- ush/gefs_wav_extractvars.sh
+++ ush/gefs_wav_extractvars.sh
@@ -12,12 +12,12 @@ while [[ ${nh} -le ${FHMAX_WAV} ]];do
   fnh=$(printf "%3.3d" "${nh}")
 
   infile=${COMIN_WAVE_GRID}/gefswave.${cycle}.global.${wavres}.f${fnh}.grib2
-  oufile1=${DATA}/gefswave.${cycle}.global.${wavres}.f${fnh}.grib2
-  rm -f "${oufile1}" #remove outfile if it already exists before extraction
-            
+  outfile=${DATA}/gefswave.${cycle}.global.${wavres}.f${fnh}.grib2
+  rm -f "${outfile}" #remove outfile if it already exists before extraction
+
   if [[ -f "${infile}" ]]; then #check if input file exists before extraction
     # shellcheck disable=SC2312 
-    ${WGRIB2} "${infile}" | grep -F -f "${varlist_wav}" | ${WGRIB2} -i "${infile}" -append -grib "${oufile1}">/dev/null
+    ${WGRIB2} "${infile}" | grep -F -f "${varlist_wav}" | ${WGRIB2} -i "${infile}" -append -grib "${outfile}">/dev/null
   else
     echo "WARNING: ${infile} does not exist."
   fi 
