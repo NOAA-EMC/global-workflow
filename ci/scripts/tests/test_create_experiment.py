54c931c95ae3609e4618b8fe9e71cde67c376f84
terrance.mcguinness <terrance.mcguinness@noaa.gov>
2024-07-10 19:46:46 -0400

    fixed indent in create test and catch sytnax in pipeline

--- ci/scripts/tests/test_create_experiment.py
+++ ci/scripts/tests/test_create_experiment.py
@@ -17,13 +17,13 @@ def test_create_experiment():
 
     for case in os.listdir(yaml_dir):
         if case.endswith('.yaml'):
-                with open(os.path.join(yaml_dir, case), 'r') as file:
-                    file_contents = file.read()
-                    if 'ICSDIR_ROOT' not in file_contents:
-                        create_experiment = copy.deepcopy(create_experiment_script)
-                        create_experiment.add_default_arg(['-y', f'../../cases/pr/{case}', '--overwrite'])
-                        env['pslot'] = os.path.splitext(case)[0]
-                        create_experiment(env=env)
-                        assert (create_experiment.returncode == 0)
+            with open(os.path.join(yaml_dir, case), 'r') as file:
+                file_contents = file.read()
+                if 'ICSDIR_ROOT' not in file_contents:
+                    create_experiment = copy.deepcopy(create_experiment_script)
+                    create_experiment.add_default_arg(['-y', f'../../cases/pr/{case}', '--overwrite'])
+                    env['pslot'] = os.path.splitext(case)[0]
+                    create_experiment(env=env)
+                    assert (create_experiment.returncode == 0)
 
     rmtree(RUNDIR)
