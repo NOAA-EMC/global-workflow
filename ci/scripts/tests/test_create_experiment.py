0bcbab07461afb7d6934420c48181406a1c4a522
terrance.mcguinness <terrance.mcguinness@noaa.gov>
2024-07-10 14:57:27 -0400

    test creating of all ci pr experments

--- ci/scripts/tests/test_create_experiment.py
+++ ci/scripts/tests/test_create_experiment.py
@@ -1,6 +1,7 @@
 from wxflow import Executable
 from shutil import rmtree
 import os
+import copy
 
 _here = os.path.dirname(__file__)
 HOMEgfs = os.sep.join(_here.split(os.sep)[:-3])
@@ -10,13 +11,18 @@ case = "C48_ATM"
 
 def test_create_experiment():
 
+    create_experiment_script = Executable(f'{HOMEgfs}/workflow/create_experiment.py')
+
     env = os.environ.copy()
     env['pslot'] = case
     env['RUNTESTS'] = RUNDIR
 
-    create_experiment_script = Executable(f'{HOMEgfs}/workflow/create_experiment.py')
-    create_experiment_script.add_default_arg(['-y', f'{HOMEgfs}/ci/cases/pr/{case}.yaml', '--overwrite'])
-    create_experiment_script(env=env)
-    assert (create_experiment_script.returncode == 0)
+    yaml_dir = os.path.join(HOMEgfs, 'ci/cases/pr')
+    for case in os.listdir(yaml_dir):
+        if case.endswith('.yaml'):
+            create_experiment = copy.deepcopy(create_experiment_script)
+            create_experiment.add_default_arg(['-y', case, '--overwrite'])
+            create_experiment(env=env)
+            assert (create_experiment.returncode == 0)
 
     rmtree(RUNDIR)
