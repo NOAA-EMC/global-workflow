5c1734521f9576ce197fec00ad1025817f95929e
AntonMFernando <anton.fernando@noaa.gov>
2024-07-16 17:48:58 -0400

    modified arch_test.sh

--- jobs/rocoto/arch_test.sh
+++ jobs/rocoto/arch_test.sh
@@ -1,24 +1,8 @@
 #! /usr/bin/env bash
 
-source "${HOMEgfs}/ush/preamble.sh"
-
-###############################################################
-# Source FV3GFS workflow modules
-. "${HOMEgfs}"/ush/load_fv3gfs_modules.sh
-status=$?
-[[ ${status} -ne 0 ]] && exit "${status}"
-
-###############################################################
-# setup python path for workflow utilities and tasks
-PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/ush/python"
-export PYTHONPATH
-
-export job="arch"
-export jobid="${job}.$$"
-
 ###############################################################
 # Execute the JJOB
-"${HOMEgfs}"/jobs/JGLOBAL_ARCHIVE
+echo "test is successful!"
 status=$?
 
 exit "${status}"
