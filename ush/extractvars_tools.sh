7bd4b676c86dea09333c52ecb6374d1b5a1466f1
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-07-17 20:08:45 +0000

    Add fatal error if data file does not exist
    A fatal error will occur if an output data file does not exist.

--- ush/extractvars_tools.sh
+++ ush/extractvars_tools.sh
@@ -45,7 +45,8 @@ copy_to_comout() {
   if [[ -f "${rundir_outfile}" ]];then
     cpfs "${rundir_outfile}" "${comout_dir}"
   else
-    echo "WARNING: Output variable (${rundir_outfile}) does not exist."
+    echo "FATAL ERROR: Output file (${rundir_outfile}) does not exist."
+    export err=1; err_chk
   fi
 }
 
