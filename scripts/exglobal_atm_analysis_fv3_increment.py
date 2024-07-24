a1ebc3f02802649c4e47a3f0fb665e3204689853
DavidNew-NOAA <david.new@noaa.gov>
2024-07-24 16:01:24 +0000

    Remove FV3 increment child classes

--- scripts/exglobal_atm_analysis_fv3_increment.py
+++ scripts/exglobal_atm_analysis_fv3_increment.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # exglobal_atm_analysis_fv3_increment.py
-# This script creates an AtmAnalysisFV3Inc object
+# This script creates an AtmAnalysis object
 # and runs the initialize, execute, and finalize methods
-# which convert the JEDI increment into an FV3-readable
-# increment
+# through its parent class, JEDI, which convert the JEDI
+# increment into an FV3-readable increment
 import os
 
 from wxflow import Logger, cast_strdict_as_dtypedict
-from pygfs.task.atm_analysis_fv3inc import AtmAnalysisFV3Inc
+
+from pygfs.task.jedi import JEDI
+from pygfs.task.atm_analysis import AtmAnalysis
 
 # Initialize root logger
 logger = Logger(level='DEBUG', colored_log=True)
@@ -18,8 +20,10 @@ if __name__ == '__main__':
     # Take configuration from environment and cast it as python dictionary
     config = cast_strdict_as_dtypedict(os.environ)
 
-    # Instantiate the atm analysis fv3inc task
-    AtmAnlFV3Inc = AtmAnalysisFV3Inc(config)
-    AtmAnlFV3Inc.initialize()
-    AtmAnlFV3Inc.execute(config.APRUN_ATMANLFV3INC)
-    AtmAnlFV3Inc.finalize()
+    # Instantiate the atm analysis object
+    AtmAnlFV3Inc = AtmAnalysis(config)
+
+    # Run all methods through the parent class, JEDI
+    JEDI.initialize(AtmAnlFV3Inc)
+    JEDI.execute(AtmAnlFV3Inc, config.APRUN_ATMANLFV3INC)
+    JEDI.finalize(AtmAnlFV3Inc)
--- scripts/exglobal_atmens_analysis_fv3_increment.py
+++ scripts/exglobal_atmens_analysis_fv3_increment.py
@@ -2,11 +2,14 @@
 # exglobal_atmens_analysis_fv3_increment.py
 # This script creates an AtmEnsAnalysis object
 # and runs the initialize, execute, and finalize methods
-# which convert the JEDI increment into an FV3 increment
+# through its parent class, JEDI, which convert the JEDI
+# increment into an FV3 increment
 import os
 
 from wxflow import Logger, cast_strdict_as_dtypedict
-from pygfs.task.atmens_analysis_fv3inc import AtmEnsAnalysisFV3Inc
+
+from pygfs.task.jedi import JEDI
+from pygfs.task.atmens_analysis import AtmEnsAnalysis
 
 # Initialize root logger
 logger = Logger(level='DEBUG', colored_log=True)
@@ -17,8 +20,10 @@ if __name__ == '__main__':
     # Take configuration from environment and cast it as python dictionary
     config = cast_strdict_as_dtypedict(os.environ)
 
-    # Instantiate the atmens analysis task
-    AtmEnsAnlFV3Inc = AtmEnsAnalysisFV3Inc(config)
-    AtmEnsAnlFV3Inc.initialize()
-    AtmEnsAnlFV3Inc.execute(config.APRUN_ATMENSANLFV3INC)
-    AtmEnsAnlFV3Inc.finalize()
+    # Instantiate the atmens analysis object
+    AtmEnsAnlFV3Inc = AtmEnsAnalysis(config)
+
+    # Call all methods through the parent class, JEDI
+    JEDI.initialize(AtmEnsAnlFV3Inc)
+    JEDI.execute(AtmEnsAnlFV3Inc, config.APRUN_ATMENSANLFV3INC)
+    JEDI.finalize(AtmEnsAnlFV3Inc)
