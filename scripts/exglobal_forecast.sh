50d1be4af8c127e06b6f4b6170a8878efbd23af1
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-07-31 20:04:21 +0000

    Execute cice_avg function in forecast exscript

--- scripts/exglobal_forecast.sh
+++ scripts/exglobal_forecast.sh
@@ -159,6 +159,7 @@ FV3_out
 [[ ${cplflx} = .true. ]] && MOM6_out
 [[ ${cplflx} = .true. ]] && CMEPS_out
 [[ ${cplwav} = .true. ]] && WW3_out
+[[ ${cplice} = .true. ]] && CICE_avg
 [[ ${cplice} = .true. ]] && CICE_out
 [[ ${cplchm} = .true. ]] && GOCART_out
 [[ ${esmf_profile:-} = .true. ]] && CPL_out
