a260f22d6ec54889083a13793f103eb40d85713e
AntonMFernando <anton.fernando@noaa.gov>
2024-08-01 12:38:24 -0400

    updated gefs_tasks.py

--- workflow/rocoto/gefs_tasks.py
+++ workflow/rocoto/gefs_tasks.py
@@ -567,7 +567,7 @@ class GEFSTasks(Tasks):
                 deps.append(rocoto.add_dependency(dep_dict))
                 dep_dict = {'type': 'metatask', 'name': 'wave_post_bndpnt_bull'}
                 deps.append(rocoto.add_dependency(dep_dict))
-        dependencies = rocoto.create_dependency(dep=deps,  dep_condition='and')
+        dependencies = rocoto.create_dependency(dep=deps, dep_condition='and')
 
         resources = self.get_resource('arch')
         task_name = 'arch'
