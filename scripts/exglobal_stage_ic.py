5f755aeed72b115eb2bf6aa32af86ab1695b7698
kate.friedman <kate.friedman@noaa.gov>
2024-08-05 20:28:10 +0000

    Add previous_cycle to staging yaml for sfcanl_data
    - Add previous_cycle as p_prefix
    - Add sfcanl_data tile files to warm start file pickup
    - Change some m_prefix to o_prefix
    
    Refs #2475

--- scripts/exglobal_stage_ic.py
+++ scripts/exglobal_stage_ic.py
@@ -18,7 +18,8 @@ def main():
     stage = Stage(config)
 
     # Pull out all the configuration keys needed to run stage job
-    keys = ['RUN', 'MODE', 'EXP_WARM_START', 'current_cycle',
+    keys = ['RUN', 'MODE', 'EXP_WARM_START',
+            'previous_cycle', 'current_cycle',
             'current_cycle_offset', 'model_start_date_current_cycle',
             'ROTDIR', 'ICSDIR', 'STAGE_IC_YAML_TMPL',
             'OCNRES', 'waveGRD', 'ntiles',
