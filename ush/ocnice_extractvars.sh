a005e5fe4cac42e3c6a7af2fa1d74366bab6da08
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-08-15 15:58:40 +0000

    Extract ice variables from history file instead
    Ice variables are extracted from the history files instead of the product files. This is necessary because there are certain ice variables that are needed that are only available in the history files and not the product files.

--- ush/ocnice_extractvars.sh
+++ ush/ocnice_extractvars.sh
@@ -25,11 +25,19 @@ for (( nh = FHMIN_GFS; nh <= FHMAX_GFS; nh = nh + fhout_ocnice )); do
   fnh=$(printf "%3.3d" "${nh}")
 
   if [[ ${component_name} == "ocn" ]]; then
-    infile=${COMIN_OCEAN_NETCDF}/${datares}/${RUN}.ocean.t${cyc}z.${datares}.f${fnh}.nc
+    if [[ "${datares}" == "native" ]]; then
+      infile=${COMIN_OCN_HISTORY}/${RUN}.ocean.t${cyc}z.${fhout_ocnice}hr_avg.f${fnh}.nc
+    else
+      infile=${COMIN_OCEAN_NETCDF}/${datares}/${RUN}.ocean.t${cyc}z.${datares}.f${fnh}.nc
+    fi
     # For ocean products, add an argument to extract a subset of levels
     otherargs=(-d "${depthvar_name},""${zmin},""${zmax}")
   elif [[ ${component_name} == "ice" ]]; then
-    infile=${COMIN_ICE_NETCDF}/${datares}/${RUN}.ice.t${cyc}z.${datares}.f${fnh}.nc
+    if [[ "${datares}" == "native" ]]; then
+      infile=${COMIN_ICE_HISTORY}/${RUN}.ice.t${cyc}z.${fhout_ocnice}hr_avg.f${fnh}.nc
+    else
+      infile=${COMIN_ICE_NETCDF}/${datares}/${RUN}.ice.t${cyc}z.${datares}.f${fnh}.nc
+    fi
     otherargs=()
   fi
   outfile=${subdata}/${RUN}.${component_name}.t${cyc}z.${datares}.f${fnh}.nc
