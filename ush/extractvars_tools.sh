af31d2fcc0431fe991ef339c33faab106fb99b9a
Eric.Sinsky <eric.sinsky@noaa.gov>
2024-08-15 13:11:55 +0000

    Fix bug in variable check
    Fix a bug in a variable check in the extractvars task where parenthesis in the variable name will prompt a false warning message.

--- ush/extractvars_tools.sh
+++ ush/extractvars_tools.sh
@@ -17,7 +17,7 @@ check_atmos() {
   done
   mapfile -t requestedvar_in_allgrb2file_arr < "${requestedvar_in_allgrb2file}"
   while read -r vari; do
-    if [[ ! ${requestedvar_in_allgrb2file_arr[*]} =~ ${vari} ]] ;then
+    if [[ ! ${requestedvar_in_allgrb2file_arr[*]} =~ "${vari}" ]] ;then
       echo "WARNING: PARM VARIABLE (${vari}) is not available in pgrb and pgrb2b for f${fnhl}."
     fi
   done <"${varlistl}"
