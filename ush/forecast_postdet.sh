77f44e1056b1ecc52d6fedf4714bf9d6cfe8a89a
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-10-10 14:32:33 +0000

    ocn inc corrected for gefs

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -463,7 +463,7 @@ MOM6_postdet() {
     if [[ "${RUN}" == "gefs" ]]; then 
     #     to ensure it does not interfere with the GFS when ensemble is updated in the GFS
       if (( MEMBER > 0 )) && [[ "${ODA_INCUPD:-False}" == "True" ]]; then
-        ${NCP} "${COMIN_OCEAN_ANALYSIS}/mom6_increment.nc" "${DATA}/INPUT/mom6_increment.nc" \
+        ${NCP} "${COMIN_OCEAN_ANALYSIS}/${RUN}.t${cyc}z.ocninc.nc" "${DATA}/INPUT/mom6_increment.nc" \
         || ( echo "FATAL ERROR: Unable to copy ensemble MOM6 increment, ABORT!"; exit 1 )
       fi
     fi # if [[ "${RUN}" == "gefs" ]]; then 
