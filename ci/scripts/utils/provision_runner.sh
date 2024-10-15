b01d0b0012eea72bd1c180bd1e114f0fa75afd62
tmcguinness <terry.mcguinness@noaa.gov>
2024-10-15 19:28:13 +0000

    couple of more shell check updates to provision runner

--- ci/scripts/utils/provision_runner.sh
+++ ci/scripts/utils/provision_runner.sh
@@ -13,7 +13,7 @@ else
   exit 1
 fi
 
-running=$(pgrep -u $USER run-helper -c) || true
+running=$(pgrep -u ${USER} run-helper -c) || true
 if [[ "${running}" -gt 0 ]]; then
    echo "actions-runner is already running"
    exit
@@ -24,4 +24,4 @@ cd $HOME || exit
 tar -xf "actions-runner_${OS_NAME}.tar.gz"
 cd actions-runner || exit
 d=$(date +%Y-%m-%d-%H:%M)
-nohup ./run.sh >& run_noup${d}.log &
+nohup ./run.sh >& "run_noup${d}.log" &
