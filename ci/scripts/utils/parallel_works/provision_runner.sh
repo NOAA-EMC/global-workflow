2d77414fbcf5aea1d5084fc3b0e4a6c343bc2215
tmcguinness <terry.mcguinness@noaa.gov>
2024-10-16 18:51:14 +0000

    added comments to provision script

--- ci/scripts/utils/parallel_works/provision_runner.sh
+++ ci/scripts/utils/parallel_works/provision_runner.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# This script provisions a GitHub Actions runner on a Rocky or CentOS system.
+# It performs the following steps:
+# 1. Checks the operating system from /etc/os-release.
+# 2. Verifies if the operating system is either Rocky or CentOS.
+# 3. Checks if an actions-runner process is already running for the current user.
+# 4. Copies the actions-runner tar file from a specified directory to the home directory.
+# 5. Extracts the tar file and starts the actions-runner in the background.
+#
+# The actions-runner tar file contains the necessary binaries and scripts to run
+# a GitHub Actions runner. It is specific to the operating system and is expected
+# to be located in the /contrib/${CI_USER}/SETUP/ directory.
+
 CI_USER="Terry.McGuinness"
 
 # Get the Operating System name from /etc/os-release
