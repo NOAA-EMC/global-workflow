9ca366d3b0a02afb00a3e3960934484bdb8f041c
tmcguinness <terry.mcguinness@noaa.gov>
2024-10-16 18:15:17 +0000

    shell norms quotes added

--- ci/scripts/utils/parallel_works/provision_runner.sh
+++ ci/scripts/utils/parallel_works/provision_runner.sh
@@ -13,13 +13,13 @@ else
   exit 1
 fi
 
-running=$(pgrep -u ${USER} run-helper -c) || true
+running=$(pgrep -u "${USER}" run-helper -c) || true
 if [[ "${running}" -gt 0 ]]; then
    echo "actions-runner is already running"
    exit
 fi
 
-cp /contrib/${CI_USER}/SETUP/actions-runner_${OS_NAME}.tar.gz ${HOME}
+cp "/contrib/${CI_USER}/SETUP/actions-runner_${OS_NAME}.tar.gz ${HOME}"
 cd $HOME || exit
 tar -xf "actions-runner_${OS_NAME}.tar.gz"
 cd actions-runner || exit
