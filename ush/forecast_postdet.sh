6797c4f335fbad12d453282b2dc62fc32b3ac1df
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2024-10-23 19:16:04 -0500

    Fix stage_ic and postdet branching for single wave restart

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -340,15 +340,17 @@ WW3_postdet() {
     echo "Copying WW3 restarts for 'RUN=${RUN}' at '${restart_date}' from '${restart_dir}'"
     ww3_restart_file="${restart_dir}/${restart_date:0:8}.${restart_date:8:2}0000.restart.ww3"
     if [[ ! -f "${ww3_restart_file}" ]]; then
-      echo "WARNING: WW3 restart file '${ww3_restart_file}' not found for warm_start='${warm_start}', will start from rest!"
+      ${NCP} "${ww3_restart_file}" "${DATA}/restart.ww3" \
+      || ( echo "FATAL ERROR: Unable to copy WW3 IC, ABORT!"; exit 1 )
+    else
       if [[ "${RERUN}" == "YES" ]]; then
         # In the case of a RERUN, the WW3 restart file is required
         echo "FATAL ERROR: WW3 restart file '${ww3_restart_file}' not found for RERUN='${RERUN}', ABORT!"
         exit 1
+      else
+        echo "WARNING: WW3 restart file '${ww3_restart_file}' not found for warm_start='${warm_start}', will start from rest!"
       fi
     fi
-    ${NCP} "${ww3_restart_file}" "${DATA}/restart.ww3" \
-      || ( echo "FATAL ERROR: Unable to copy WW3 IC, ABORT!"; exit 1 )
 
     first_ww3_restart_out=$(date --utc -d "${restart_date:0:8} ${restart_date:8:2} + ${restart_interval} hours" +%Y%m%d%H)
   else  # cold start
