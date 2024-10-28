3d8c9d33c2e89b6d2f80b2b0002e7e700f81b5c7
Jiarui Dong <Jiarui.Dong@noaa.gov>
2024-10-28 14:33:26 -0400

    Update the esnowrecen job dependency.

--- workflow/rocoto/gfs_tasks.py
+++ workflow/rocoto/gfs_tasks.py
@@ -621,17 +621,11 @@ class GFSTasks(Tasks):
     def esnowrecen(self):
 
         deps = []
-        dep_dict = {'type': 'task', 'name': f'{self.run.replace("enkf","")}_prepsnowcover'}
+        dep_dict = {'type': 'task', 'name': f'{self.run.replace("enkf","")}_snowanl'}
         deps.append(rocoto.add_dependency(dep_dict))
-
-        deps2 = []
-        dep_dict = {'type': 'taskvalid', 'name': f'{self.run.replace("enkf","")}_prepsnowcover', 'condition': 'not'}
-        deps2.append(rocoto.add_dependency(dep_dict))
         dep_dict = {'type': 'metatask', 'name': f'{self.run}_epmn', 'offset': f"-{timedelta_to_HMS(self._base['interval_gdas'])}"}
-        deps2.append(rocoto.add_dependency(dep_dict))
-        deps.append(rocoto.create_dependency(dep_condition='and', dep=deps2))
-
-        dependencies = rocoto.create_dependency(dep_condition='or', dep=deps)
+        deps.append(rocoto.add_dependency(dep_dict))
+        dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
 
         resources = self.get_resource('esnowrecen')
         task_name = f'{self.run}_esnowrecen'
