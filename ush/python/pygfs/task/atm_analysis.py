7ac6ccb2bbf88b25fb533185c5d481cd328415ee
DavidNew-NOAA <david.new@noaa.gov>
2024-11-06 11:45:46 +0000

    Forgot a line

--- ush/python/pygfs/task/atm_analysis.py
+++ ush/python/pygfs/task/atm_analysis.py
@@ -122,8 +122,8 @@ class AtmAnalysis(Task):
             FileHandler(bias_dict).sync()
             logger.debug(f"Bias correction files:\n{pformat(bias_dict)}")
 
-        # extract bias corrections
-        Jedi.extract_tar_from_filehandler_dict(bias_dict)
+            # extract bias corrections
+            Jedi.extract_tar_from_filehandler_dict(bias_dict)
 
         # stage CRTM fix files
         logger.info(f"Staging CRTM fix files from {self.task_config.CRTM_FIX_YAML}")
