6d12fb30351a2190260e7c7b130192438b384f20
Barry Baker <bbakernoaa@users.noreply.github.com>
2024-11-12 14:19:44 -0500

    Update ush/python/pygfs/task/aero_emissions.py
    Co-authored-by: Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>

--- ush/python/pygfs/task/aero_emissions.py
+++ ush/python/pygfs/task/aero_emissions.py
@@ -46,7 +46,7 @@ class AerosolEmissions(Task):
         super().__init__(config)
 
         nforecast_hours = self.task_config["FHMAX_GFS"]
-        blend_start_date = self.task_config.PDY
+        blend_start_date = self.task_config["PDY"]
         blend_end_date = blend_start_date + to_timedelta(f'{nforecast_hours + 24}H')
         forecast_dates = list(rrule(freq=DAILY, dtstart=blend_start_date, until=blend_end_date))
 
