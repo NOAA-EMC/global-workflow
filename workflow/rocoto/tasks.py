da1aae220c16485c4cd8a7fc40d88e4a24452851
AntonMFernando <anton.fernando@noaa.gov>
2024-11-18 02:21:35 -0500

    modified tasks.py

--- workflow/rocoto/tasks.py
+++ workflow/rocoto/tasks.py
@@ -151,7 +151,7 @@ class Tasks:
 
         if component in ['wave']:
             local_config['FHOUT_HF_GFS'] = config['FHOUT_HF_WAV']
-            local_config['FHOUT_GFS'] = config['FHOUT_WAV']
+            local_config['FHOUT_GFS'] = config['FHOUT_HF_WAV']
             local_config['FHOUT'] = config['FHOUT_WAVE']
 
         fhmin = local_config['FHMIN']
