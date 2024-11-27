4af258c8b4c2c64b1f12b8a050391fd14051287a
DavidNew-NOAA <david.new@noaa.gov>
2024-11-27 19:30:48 +0000

    pynorms

--- ush/python/pygfs/task/marine_analysis.py
+++ ush/python/pygfs/task/marine_analysis.py
@@ -214,7 +214,7 @@ class MarineAnalysis(Task):
         envconfig_jcb['HOMEgfs'] = self.task_config.HOMEgfs
         envconfig_jcb['DO_TEST_MODE'] = self.task_config.DO_TEST_MODE
         envconfig_jcb['RUN'] = self.task_config.RUN
-        envconfig_jcb['current_cycle'] = self.task_config.current_cycle        
+        envconfig_jcb['current_cycle'] = self.task_config.current_cycle
 
         # Write obs_list_short
         save_as_yaml(parse_obs_list_file(self.task_config.MARINE_OBS_LIST_YAML), 'obs_list_short.yaml')
