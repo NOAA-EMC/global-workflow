38838a67407acde3f8e9d4589868432bea5e9ebe
Terry McGUinness <terrene.mcguinness@gmail.com>
2024-12-09 15:13:09 -0500

    refining get_job_names

--- ci/scripts/utils/get_job_names.py
+++ ci/scripts/utils/get_job_names.py
@@ -22,40 +22,39 @@ def valid_file_path(path):
         raise ArgumentTypeError(f"Invalid file path: {path}")
 
 def input_args():
-
-    description = "Extracts the string between <cyclestr> tags within <jobname> tags."
+    description = "Extracts the string between <task name> and <metatask name> tags."
     parser = ArgumentParser(description=description,
                             formatter_class=ArgumentDefaultsHelpFormatter)
     
     parser.add_argument('--xmlfile', type=valid_file_path, help='The path to the XML file.')
     return parser.parse_args()
 
-def get_cyclestr_from_jobname(xml_file):
+def get_names_from_tags(xml_file):
     """
-    Extracts the string between <cyclestr> tags within <jobname> tags.
+    Extracts the string between <task name> and <metatask name> tags.
 
     Args:
         xml_file (str): The path to the XML file.
 
     Returns:
-        list: A list of strings found between <cyclestr> tags within <jobname> tags.
+        list: A list of strings found between <task name> and <metatask name> tags.
     """
     tree = etree.parse(xml_file)
     root = tree.getroot()
-    cyclestr_list = []
+    name_list = []
 
-    for jobname in root.findall('.//jobname'):
-        cyclestr = jobname.find('cyclestr')
-        if cyclestr is not None:
-            cyclestr_list.append(cyclestr.text)
+    for tag in ['task name', 'metatask name']:
+        for element in root.findall(f'.//{tag.replace(" ", "")}'):
+            if element is not None:
+                name_list.append(element.text)
 
-    return cyclestr_list
+    return name_list
 
 # Example usage
 if __name__ == "__main__":
     args = input_args()
     xml_file = args.xmlfile
-    job_names = get_cyclestr_from_jobname(xml_file)
+    job_names = get_names_from_tags(xml_file)
     for job_name in job_names:
         if "#" not in job_name:
             if job_name.endswith("_@H"):
