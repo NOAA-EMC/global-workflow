899b20baf29d0caba45f605acdfce56f92ef5950
Kevin Dougherty <kevin.dougherty@noaa.gov>
2024-12-12 19:59:48 +0000

    updated scripts to that run successfully

--- scripts/exglobal_analysis_stats.py
+++ scripts/exglobal_analysis_stats.py
@@ -2,8 +2,8 @@
 
 # exglobal_stat_analysis.py
 # This script creates an StatAnalysis class
-# and runs the initialize method
-# which create and stage the runtime directory
+# and runs the initialize, execute, and finalize
+# methods which create and stage the runtime directory
 # and create the YAML configuration
 # for a global stat analysis
 import os
@@ -23,7 +23,15 @@ if __name__ == '__main__':
     # Instantiate the atm analysis task
     StatAnl = StatAnalysis(config)
 
+    # Create list based on DA components
+    StatAnl.task_config['STAT_OBS'] = []
+    if StatAnl.task_config.DO_AERO:
+        StatAnl.task_config['STAT_OBS'].append('aero')
+    if StatAnl.task_config.DO_JEDISNOWDA:
+        StatAnl.task_config['STAT_OBS'].append('snow')
+
     # Initialize JEDI variational analysis
     StatAnl.initialize()
-    StatAnl.execute('anlstat')
-    StatAnl.finalize()
+    for ob in StatAnl.task_config.STAT_OBS:
+        StatAnl.execute(ob)
+        StatAnl.finalize(ob)
