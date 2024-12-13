670150e64fd8be10f30aa008a2f8cb054d4a0737
AntonMFernando <anton.fernando@noaa.gov>
2024-12-13 18:02:22 -0500

    modifed scripts\exgfs_wave_post_gridded_sbs.sh

--- scripts/exgfs_wave_post_gridded_sbs.sh
+++ scripts/exgfs_wave_post_gridded_sbs.sh
@@ -387,7 +387,7 @@ source "${USHgfs}/preamble.sh"
     res=${wavepostGRD:-${waveinterpGRD}}
     if [ ${waveMEMB} ]; then ENSTAG=".${membTAG}${waveMEMB}" ; fi
     gribchk="${RUN}wave.${cycle}${ENSTAG}.${GRDNAME}.${GRDRES}.f${FH3}.grib2"
-    if [[ ! -s "${COMOUT_WAVE_GRID}/${res}/${gribchk}" ]]; then
+    if [[ ! -s "${COMOUT_WAVE_GRID}/${GRDNAME}.${GRDRES}/${gribchk}" ]]; then
       set +x
       echo ' '
       echo '********************************************'
