2446f7de6c7ec2719b208415fb3e89852354abf5
AntonMFernando <anton.fernando@noaa.gov>
2024-12-16 16:25:29 -0500

    modified ush\wave_extractvars.sh

--- ush/wave_extractvars.sh
+++ ush/wave_extractvars.sh
@@ -13,13 +13,41 @@ source "${USHgfs}/preamble.sh"
 
 subdata=${1}
 
+for grdID in ${waveGRD}; do
+    case ${grdID} in
+        glo_10m) GRDNAME='global' ; GRDRES=0p16;;
+        glo_15mxt) GRDNAME='global' ; GRDRES=0p25;;       
+        glo_30mxt) GRDNAME='global' ; GRDRES=0p50 ;;
+        glo_30m) GRDNAME='global' ; GRDRES=0p50 ;;
+        glo_025) GRDNAME='global' ; GRDRES=0p25 ;;
+        glo_100) GRDNAME='global' ; GRDRES=1p00;;
+        glo_200) GRDNAME='global' ; GRDRES=2p00;;
+        glo_500) GRDNAME='global' ; GRDRES=5p00;;
+        at_10m) GRDNAME='atlocn' ; GRDRES=0p16 ;;
+        ep_10m) GRDNAME='epacif' ; GRDRES=0p16;;
+        wc_10m) GRDNAME='wcoast' ; GRDRES=0p16 ;;
+        ak_10m) GRDNAME='alaska' ; GRDRES=0p16;;
+        aoc_9km) GRDNAME='arctic' ; GRDRES=9km;;
+        ant_9km) GRDNAME='antarc' ; GRDRES=9km ;;
+        gnh_10m) GRDNAME='global' ; GRDRES=0p16;;
+        gsh_15m) GRDNAME='gsouth' ; GRDRES=0p25;;
+        ao_20m) GRDNAME='arctic' ; GRDRES=0p33;;
+        so_20m) GRDNAME='antarc' ; GRDRES=0p33;;
+        reg025) GRDNAME='global' ; GRDRES=0p25;;
+        gwes_30m) GRDNAME='global' ; GRDRES=0p50;;
+        *)
+        echo "FATAL ERROR: No grid specific wave config values exist for ${grdID}. Aborting."
+        exit 1;;
+    esac
+done
+
 [[ -d "${subdata}" ]] || mkdir -p "${subdata}"
 
 for (( nh = FHOUT_WAV_EXTRACT; nh <= FHMAX_WAV; nh = nh + FHOUT_WAV_EXTRACT )); do
   fnh=$(printf "%3.3d" "${nh}")
 
-  infile=${COMIN_WAVE_GRID}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2
-  outfile=${subdata}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2
+  infile="${COMIN_WAVE_GRID}/${GRDNAME}.${GRDRES}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2"
+  outfile="${subdata}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2"
   rm -f "${outfile}" # Remove outfile if it already exists before extraction
 
   if [[ -f "${infile}" ]]; then # Check if input file exists before extraction
