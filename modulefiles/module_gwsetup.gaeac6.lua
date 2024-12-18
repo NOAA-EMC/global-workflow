6d0c7c9ad299d4522ba5cdd7d42f39cd7287444d
DavidBurrows-NCO <David.Burrows@noaa.gov>
2024-12-18 08:50:26 -0500

    syndat path updates

--- modulefiles/module_gwsetup.gaeac6.lua
+++ modulefiles/module_gwsetup.gaeac6.lua
@@ -5,7 +5,7 @@ Load environment to run GFS workflow setup scripts on Gaea C6
 prepend_path("MODULEPATH", "/ncrc/proj/epic/rocoto/modulefiles")
 load(pathJoin("rocoto"))
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
 local python_ver=os.getenv("python_ver") or "3.10.13"
