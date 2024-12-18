c96c16a433be9eb2744c143dee503b08ce0b80e2
DavidBurrows-NCO <David.Burrows@noaa.gov>
2024-12-18 08:38:10 -0500

    add space in hosts.py

--- workflow/hosts.py
+++ workflow/hosts.py
@@ -17,7 +17,7 @@ class Host:
 
     SUPPORTED_HOSTS = ['HERA', 'ORION', 'JET', 'HERCULES',
                        'WCOSS2', 'S4', 'CONTAINER', 'GAEAC5',
-                       'GAEAC6','AWSPW', 'AZUREPW', 'GOOGLEPW']
+                       'GAEAC6', 'AWSPW', 'AZUREPW', 'GOOGLEPW']
 
     def __init__(self, host=None):
 
