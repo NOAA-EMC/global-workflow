3088bffcb925c2e204af864ee14b0ef03ed03854
TerrenceMcGuinness-NOAA <terrance.mcguinness@noaa.gov>
2024-12-19 10:40:01 -0500

    added a few comments for each section

--- ci/ctests/create_functional_ctests.py
+++ ci/ctests/create_functional_ctests.py
@@ -23,7 +23,6 @@ def parse_args():
     argparse.Namespace
         The parsed command line arguments.
     """
-
     description = """Arguments for creating and updating error log files
     """
     parser = ArgumentParser(description=description)
@@ -32,28 +31,27 @@ def parse_args():
     return parser.parse_args()
 
 if __name__ == '__main__':
-
+    # Locate cmake executable
     try:
         cmake = which("cmake")
     except CommandNotFoundError:
         logger.exception("cmake not found in PATH")
         raise CommandNotFoundError("cmake not found in PATH")
 
-    # Put HOMEgfs into the test configuration
+    # Parse command line arguments
     args = parse_args()
     data = AttrDict(HOMEgfs=_top)
     data.update(os.environ)
 
-    # Used for getting platform specific information
+    # Initialize host and configuration
     host = Host()
     cfg = Configuration(f'{data.HOMEgfs}/ci/platforms')
 
-    # Create cmake directory and move job list file
+    # Create directory for tests
     test_dir = os.path.join(_here, 'TESTS')
     mkdir_p(test_dir)
 
-    # Get the case name from the yaml file basename
-    # and get the configuration for the case
+    # Process each YAML file
     case_names = ""
     for yaml in args.yaml:
         platform_config = cfg.parse_config(f'config.{host.machine.lower()}')
@@ -63,7 +61,7 @@ if __name__ == '__main__':
         case_cfg = parse_j2yaml(path=yaml, data=data)
         case_cfg.update(platform_config)
 
-        # Get top-level entries and create a job list file for the case
+        # Create job list file for the case
         top_level_entries = [key for key in case_cfg.keys() if isinstance(case_cfg[key], dict)]
         job_list_file = f"TESTS/{case_name}_jobs.txt"
         with open(job_list_file, 'w') as f:
