aeaa85a586977e7c6da92968bc37677201d5b274
Jessica Meixner <Jessica.Meixner@noaa.gov>
2024-12-20 19:59:46 +0000

    more cleaup on namelist WW3 --- note we are writing out extra output in iau window

--- ush/parsing_namelists_WW3.sh
+++ ush/parsing_namelists_WW3.sh
@@ -29,39 +29,6 @@ WW3_namelists(){
   ymdh_beg_out=$YMDH
   time_beg_out="$(echo $ymdh_beg_out | cut -c1-8) $(echo $ymdh_beg_out | cut -c9-10)0000"
 
-  # Restart file times (already has IAU_FHROT in WAVHINDH) 
-  RSTOFFSET=$(( ${WAVHCYC} - ${WAVHINDH} ))
-  # Update restart time is added offset relative to model start
-  RSTOFFSET=$(( ${RSTOFFSET} + ${RSTIOFF_WAV} ))
-  ymdh_rst_ini=$($NDATE ${RSTOFFSET} $YMDH)
-  RST2OFFSET=$(( DT_2_RST_WAV / 3600 ))
-  ymdh_rst2_ini=$($NDATE ${RST2OFFSET} $YMDH) # DT2 relative to first-first-cycle restart file
-  # First restart file for cycling
-  time_rst_ini="$(echo $ymdh_rst_ini | cut -c1-8) $(echo $ymdh_rst_ini | cut -c9-10)0000"
-  if [ ${DT_1_RST_WAV} = 1 ]; then
-    time_rst1_end=${time_rst_ini}
-  else
-    RST1OFFSET=$(( DT_1_RST_WAV / 3600 ))
-    ymdh_rst1_end=$($NDATE $RST1OFFSET $ymdh_rst_ini)
-    time_rst1_end="$(echo $ymdh_rst1_end | cut -c1-8) $(echo $ymdh_rst1_end | cut -c9-10)0000"
-  fi
-  # Second restart file for checkpointing
-  if [ "${RSTTYPE_WAV}" = "T" ]; then
-    time_rst2_ini="$(echo $ymdh_rst2_ini | cut -c1-8) $(echo $ymdh_rst2_ini | cut -c9-10)0000"
-    time_rst2_end=$time_end
-  # Condition for gdas run or any other run when checkpoint stamp is > ymdh_end
-    if [ $ymdh_rst2_ini -ge $ymdh_end ]; then
-      ymdh_rst2_ini=$($NDATE 3 $ymdh_end)
-      time_rst2_ini="$(echo $ymdh_rst2_ini | cut -c1-8) $(echo $ymdh_rst2_ini | cut -c9-10)0000"
-      time_rst2_end=$time_rst2_ini
-    fi
-  else
-    time_rst2_ini="$"
-    time_rst2_end=
-    DT_2_RST_WAV=
-  fi
-
-
   set +x
   echo ' '
   echo 'Times in wave model format :'
