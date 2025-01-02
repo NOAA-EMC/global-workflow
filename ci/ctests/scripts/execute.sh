2602166a72777a5bb6739606ce0aa0c679f6a592
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-01-02 15:25:10 -0600

    execute needed shabang and exit 0

--- ci/ctests/scripts/execute.sh
+++ ci/ctests/scripts/execute.sh
@@ -1,7 +1,10 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 CASE=$1
 JOB=$1
 
 rocotoboot_dryrun=/work2/noaa/global/mterry/rocoto_dryrun/bin/rocotoboot
 job_card=$(${rocotoboot_dryrun} -d ${CASE}_${JOB}.db -w ${CASE}/${JOB}.xml -v 10 -c ${idate}00 -t ${JOB})
+
+echo -e "scheduler job-card:\n${job_card}"
+exit 0
