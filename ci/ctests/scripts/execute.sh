1950af4233b355019e6ca080605d0c3f7ad94ae8
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-01-02 15:42:57 -0600

    almost ready to launch execute.sh

--- ci/ctests/scripts/execute.sh
+++ ci/ctests/scripts/execute.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 CASE=$1
-JOB=$1
+JOB=$2
+idate=$3
 
 rocotoboot_dryrun=/work2/noaa/global/mterry/rocoto_dryrun/bin/rocotoboot
 job_card=$(${rocotoboot_dryrun} -d ${CASE}_${JOB}.db -w ${CASE}/${JOB}.xml -v 10 -c ${idate}00 -t ${JOB})
