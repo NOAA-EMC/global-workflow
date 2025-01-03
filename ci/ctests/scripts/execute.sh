276a9bd7cca8e428b7951c1e36999e0e714a6026
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-01-03 12:05:04 -0600

    added working skeleton bash script for execute using rocotoboot with dryrun

--- ci/ctests/scripts/execute.sh
+++ ci/ctests/scripts/execute.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+set -xe
+
 CASE=$1
 JOB=$2
 idate=$3
 
 rocotoboot_dryrun=/work2/noaa/global/mterry/rocoto_dryrun/bin/rocotoboot
-job_card=$(${rocotoboot_dryrun} -d ${CASE}_${JOB}.db -w ${CASE}/${JOB}.xml -v 10 -c ${idate}00 -t ${JOB})
+CASEDIR="RUNTESTS/EXPDIR/${CASE}_${JOB}"
+cd "${CASEDIR}"
+rm -f *.db
+rm -f jobcard                                                                             
 
-echo -e "scheduler job-card:\n${job_card}"
-exit 0
+job_card=$(yes | ${rocotoboot_dryrun} -d ${CASE}_${JOB}.db -w ${CASE}_${JOB}.xml -v 10 -c ${idate}00 -t ${JOB} 2> jobcard)
+cat jobcard | sed '/^{{\|^}}/d' | sed '1d'  > "${CASE}_${JOB}.sub"
+
+#TODO Generalize for batch system (hard coded to slurm)
+
+sbatch < "${CASE}_${JOB}.sub"
