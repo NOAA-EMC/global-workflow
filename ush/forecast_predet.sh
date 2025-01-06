6e7dfef6acfd99462c1267e1ef1bfc2941e20e2d
Jessica Meixner <Jessica.Meixner@noaa.gov>
2025-01-06 15:23:10 +0000

    shell check

--- ush/forecast_predet.sh
+++ ush/forecast_predet.sh
@@ -756,8 +756,9 @@ CMEPS_predet(){
       local restart_interval_start=${cmeps_restart_interval}
       local restart_interval_end=${FHMAX}
     fi
-    export CMEPS_RESTART_FH="$(seq -s ' ' "${restart_interval_start}" "${cmeps_restart_interval}" "${restart_interval_end}")"
+    CMEPS_RESTART_FH="$(seq -s ' ' "${restart_interval_start}" "${cmeps_restart_interval}" "${restart_interval_end}")"
   fi
+  export CMEPS_RESTART_FH
   # TODO: For GEFS, once cycling waves "self-cycles" and therefore needs to have a restart at 6 hour
 }
 
