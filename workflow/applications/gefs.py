26e42eaaee98eb53151f6570fea5e4e125b38cae
AntonMFernando <anton.fernando@noaa.gov>
2025-01-07 17:11:05 -0500

    modified gefs.py

--- workflow/applications/gefs.py
+++ workflow/applications/gefs.py
@@ -88,10 +88,10 @@ class GEFSAppConfig(AppConfig):
             tasks += ['wavepostsbs']
             if options['do_wave_bnd']:
                 tasks += ['wavepostbndpnt', 'wavepostbndpntbll']
-            tasks += ['wavepostpnt']
+            tasks += ['wavepostpnt', 'arch']
 
         if options['do_extractvars']:
-            tasks += ['extractvars', 'arch']
+            tasks += ['extractvars']
 
         tasks += ['cleanup']
 
