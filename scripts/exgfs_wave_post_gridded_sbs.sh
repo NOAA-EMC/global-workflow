115029947e7dc64943a1fdcdc072c054d387d180
AntonMFernando <anton.fernando@noaa.gov>
2025-01-09 02:58:02 -0500

    modified scripts\exgfs_wave_post_gridded_sbs.sh

--- scripts/exgfs_wave_post_gridded_sbs.sh
+++ scripts/exgfs_wave_post_gridded_sbs.sh
@@ -272,7 +272,7 @@ source "${USHgfs}/preamble.sh"
       gribFL=\'$(echo ${OUTPARS_WAV})\'
       source "${USHgfs}/wave_domain_grid.sh"
       process_grdID "${grdID}"
-      echo "${USHgfs}/wave_grib2_sbs.sh $grdID $GRIDNR $MODNR $ymdh $fhr $GRDNAME $GRDRES $gribFL > grib_$grdID.out 2>&1" >> ${fcmdnow}\
+      echo "${USHgfs}/wave_grib2_sbs.sh $grdID $GRIDNR $MODNR $ymdh $fhr $GRDNAME $GRDRES $gribFL > grib_$grdID.out 2>&1" >> ${fcmdnow}
     done
   fi
 
