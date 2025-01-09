59e32f905b4a6d907564bf5c9a29afb95e2706fc
AntonMFernando <anton.fernando@noaa.gov>
2025-01-08 23:10:57 -0500

    modified scripts\exgfs_wave_prdgen_gridded.sh

--- scripts/exgfs_wave_prdgen_gridded.sh
+++ scripts/exgfs_wave_prdgen_gridded.sh
@@ -100,7 +100,7 @@ grids=${grids:-ak_10m at_10m ep_10m wc_10m glo_30m}
      esac
      #
 
-     GRIBIN="${COM_WAVE_GRID}/${grdID}/${RUNwave}.${cycle}.${grdID}.f${fhr}.grib2"
+     GRIBIN="${COMIN_WAVE_GRID}/${grdID}/${RUNwave}.${cycle}.${grdID}.f${fhr}.grib2"
      GRIBIN_chk="${GRIBIN}.idx"
      sleep_interval=5
      max_tries=1000
