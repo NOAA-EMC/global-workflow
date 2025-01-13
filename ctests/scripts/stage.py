e979f75195168bfb02eead8f5ae3104ca47dc878
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-01-13 10:30:01 -0600

    updated arg to stagy.py for test_date to a str

--- ctests/scripts/stage.py
+++ ctests/scripts/stage.py
@@ -10,7 +10,7 @@ sys.path.insert(0, _top)
 
 from argparse import ArgumentParser
 from pathlib import Path
-from wxflow import parse_j2yaml,  FileHandler, AttrDict, Logger
+from wxflow import parse_j2yaml, FileHandler, Logger
 
 logger = Logger(level=os.environ.get("LOGGING_LEVEL", "DEBUG"), colored_log=False)
 
@@ -28,13 +28,16 @@ def parse_args():
     parser = ArgumentParser(description=description)
 
     parser.add_argument('-y', '--yaml', help='full path to yaml file describing the job test configuration', type=Path, required=True)
-    parser.add_argument('-d', '--test_date', help='full path to yaml file describing the job test configuration', type=datetime, required=False)
+    parser.add_argument('-d', '--test_date', help='test date in YYYYMMDDHH format', type=str, required=False)
     return parser.parse_args()
 
 if __name__ == '__main__':
 
     # Parse command line arguments
     args = parse_args()
+
     data = {'TEST_DATE': args.test_date}
+    if args.test_date:
+        data['TEST_DATE'] = datetime.datetime.strptime(args.test_date, '%Y%m%d%H')
     case_cfg = parse_j2yaml(path=args.yaml, data=data)
     FileHandler(case_cfg.input_files).sync()
