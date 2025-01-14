57bba27df0834bf408af9e95cf9a1ddbf3eddd26
AntonMFernando <anton.fernando@noaa.gov>
2025-01-14 14:14:35 -0500

    modified scripts\exgfs_wave_prdgen_gridded.sh

--- scripts/exgfs_wave_prdgen_gridded.sh
+++ scripts/exgfs_wave_prdgen_gridded.sh
@@ -48,7 +48,7 @@ source "${USHgfs}/preamble.sh"
 # Input grid
 grid_in="${waveinterpGRD:-glo_15mxt}"
 # Output grids
-grids=${GEMPAK_GRIDS}
+grids=${GEMPAK_GRIDS:-ak_10m at_10m ep_10m wc_10m glo_30m}
 # export grids=${wavepostGRD}
  maxtries=${maxtries:-720}
 # 0.b Date and time stuff
