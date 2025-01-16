b52379f44fd981382225d8de8de0e14c626aee08
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2025-01-16 19:21:31 +0000

    old work

--- ush/python/pygfs/task/marine_letkf.py
+++ ush/python/pygfs/task/marine_letkf.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
+import copy
 import f90nml
 import pygfs.utils.marine_da_utils as mdau
 from logging import getLogger
 import os
 from pygfs.task.analysis import Analysis
 from typing import Dict
-from wxflow import (AttrDict,
+from wxflow import (add_to_datetime,
+                    AttrDict,
                     Executable,
                     FileHandler,
                     logit,
                     parse_j2yaml,
+                    save_as_yaml,
+                    Template,
+                    TemplateConstants,
                     to_timedelta,
-                    to_YMDH)
+                    to_YMDH,
+                    YAMLFile)
 
 logger = getLogger(__name__.split('.')[-1])
 
@@ -37,6 +43,7 @@ class MarineLETKF(Analysis):
         logger.info("init")
         super().__init__(config)
 
+        _enspert_relpath = os.path.relpath(self.task_config.DATAens, self.task_config.DATA)
         _half_assim_freq = to_timedelta(f"{self.task_config.assim_freq}H") / 2
         _letkf_yaml_file = 'letkf.yaml'
         _letkf_exec_args = [self.task_config.MARINE_LETKF_EXEC,
@@ -44,16 +51,23 @@ class MarineLETKF(Analysis):
                             'localensembleda',
                             _letkf_yaml_file]
         # compute the relative path from self.task_config.DATA to self.task_config.DATAenspert
-        _enspert_relpath = os.path.relpath(self.task_config.DATAens, self.task_config.DATA)
+        _window_begin = add_to_datetime(self.task_config.current_cycle, -to_timedelta(f"{self.task_config.assim_freq}H") / 2)
+        _window_end = add_to_datetime(self.task_config.current_cycle, to_timedelta(f"{self.task_config.assim_freq}H") / 2)
+
 
         self.task_config.WINDOW_MIDDLE = self.task_config.current_cycle
-        self.task_config.WINDOW_BEGIN = self.task_config.current_cycle - _half_assim_freq
+        self.task_config.MARINE_WINDOW_MIDDLE = self.task_config.current_cycle
+        self.task_config.WINDOW_BEGIN = _window_begin
+        self.task_config.WINDOW_END = _window_end
+        self.task_config.MARINE_WINDOW_BEGIN = _window_begin
+        self.task_config.MARINE_WINDOW_END = _window_end
         self.task_config.letkf_exec_args = _letkf_exec_args
         self.task_config.letkf_yaml_file = _letkf_yaml_file
         self.task_config.mom_input_nml_tmpl = os.path.join(self.task_config.DATA, 'mom_input.nml.tmpl')
         self.task_config.mom_input_nml = os.path.join(self.task_config.DATA, 'mom_input.nml')
         self.task_config.obs_dir = os.path.join(self.task_config.DATA, 'obs')
         self.task_config.ENSPERT_RELPATH = _enspert_relpath
+        self.task_config.PARMsoca = os.path.join(self.task_config.PARMgfs, 'gdas', 'soca')
 
     @logit(logger)
     def initialize(self):
@@ -131,9 +145,53 @@ class MarineLETKF(Analysis):
         FileHandler({'copy': obs_files_to_copy}).sync()
 
         # make the letkf.yaml
-        letkf_yaml = parse_j2yaml(self.task_config.MARINE_LETKF_YAML_TMPL, stageconf)
-        letkf_yaml.observations.observers = obs_to_use
-        letkf_yaml.save(self.task_config.letkf_yaml_file)
+        # letkf_yaml = parse_j2yaml(self.task_config.MARINE_LETKF_YAML_TMPL, stageconf)
+        # letkf_yaml.observations.observers = obs_to_use
+        # letkf_yaml.save(self.task_config.letkf_yaml_file)
+
+####################################################################################################
+        # Write obs_list_short
+#        save_as_yaml(parse_obs_list_file(self.task_config.MARINE_OBS_LIST_YAML), 'obs_list_short.yaml')
+        save_as_yaml(os.path.join(self.task_config.PARMsoca, 'letkf','letkf_obs_list.yaml.j2'), 'obs_list_short.yaml')
+        os.environ['OBS_LIST_SHORT'] = 'obs_list_short.yaml'
+
+        print("self.task_config: ", self.task_config)
+        envconfig_jcb = copy.deepcopy(self.task_config)
+#        envconfig_jcb['cyc'] = int(self.task_config.current_cycle.strftime('%H'))        
+        envconfig_jcb['cyc'] = int(os.getenv('cyc'))
+        envconfig_jcb['PDY'] = self.task_config.current_cycle.strftime('%Y%m%d')
+        print("PDY: ", envconfig_jcb['PDY'])       
+        print("type of cyc: ", type(envconfig_jcb['cyc']))
+        #print("envconfig_jcb: ", envconfig_jcb)
+        # Render the JCB configuration files
+        jcb_base_yaml = os.path.join(self.task_config.PARMsoca, 'marine-jcb-base.yaml')
+        jcb_algo_yaml = os.path.join(self.task_config.PARMsoca, 'letkf/marine-jcb-lektf.yaml.j2')
+
+        jcb_base_config = YAMLFile(path=jcb_base_yaml)
+        jcb_base_config = Template.substitute_structure(jcb_base_config, TemplateConstants.DOUBLE_CURLY_BRACES, envconfig_jcb.get)
+        jcb_base_config = Template.substitute_structure(jcb_base_config, TemplateConstants.DOLLAR_PARENTHESES, envconfig_jcb.get)
+        jcb_algo_config = YAMLFile(path=jcb_algo_yaml)
+        jcb_algo_config = Template.substitute_structure(jcb_algo_config, TemplateConstants.DOUBLE_CURLY_BRACES, envconfig_jcb.get)
+        jcb_algo_config = Template.substitute_structure(jcb_algo_config, TemplateConstants.DOLLAR_PARENTHESES, envconfig_jcb.get)
+
+        # Override base with the application specific config
+        jcb_config = {**jcb_base_config, **jcb_algo_config}
+
+        # convert datetime to string
+        jcb_config['window_begin'] = self.task_config.MARINE_WINDOW_BEGIN.strftime('%Y-%m-%dT%H:%M:%SZ')
+        jcb_config['window_middle'] = self.task_config.MARINE_WINDOW_MIDDLE.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        # Current hack so that this is not done directly in the JCB base yaml
+#        jcb_config['marine_pseudo_model_states'] = parse_yaml('bkg_list.yaml')
+
+        # Render the full JEDI configuration file using JCB
+        jedi_config = render(jcb_config)
+
+        # Save the JEDI configuration file
+        letkf_yaml_jcb = 'letkf.yaml'
+        mdau.clean_empty_obsspaces(jedi_config, target=letkf_yaml_jcb, app='var')
+
+        ######################################
 
         # swap date and stack size in mom_input.nml
         domain_stack_size = self.task_config.DOMAIN_STACK_SIZE
