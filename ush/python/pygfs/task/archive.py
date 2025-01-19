c0cdcb656288721525d2adfbe00a96922c6d46d3
AntonMFernando <anton.fernando@noaa.gov>
2025-01-18 19:55:43 -0500

    modified ush\python\pygfs\task\archive.py

--- ush/python/pygfs/task/archive.py
+++ ush/python/pygfs/task/archive.py
@@ -89,7 +89,7 @@ class Archive(Task):
 
         # Collect datasets that need to be archived
         self.tar_cmd = ""
-        arcdir_set = (arcdir_set, [])
+        arcdir_set = arcdir_set, []
 
         return arcdir_set
 
