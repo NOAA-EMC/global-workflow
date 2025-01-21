65571aa573213528532c12200e0e039804e35326
AntonMFernando <anton.fernando@noaa.gov>
2025-01-21 11:55:05 -0500

    modified workflow\rocoto\gfs_tasks.py

--- workflow/rocoto/gfs_tasks.py
+++ workflow/rocoto/gfs_tasks.py
@@ -2442,9 +2442,9 @@ class GFSTasks(Tasks):
         if 'enkf' in self.run:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_eamn'}
             deps.append(rocoto.add_dependency(dep_dict))
-        else:
             dep_dict = {'type': 'task', 'name': f'{self.run}_arch_vrfy'}
             deps.append(rocoto.add_dependency(dep_dict))
+        else:
             if self.options['do_archtar']:
                 dep_dict = {'type': 'task', 'name': f'{self.run}_arch_tars'}
                 deps.append(rocoto.add_dependency(dep_dict))
