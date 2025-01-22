7e16fcc403a299c5433e26d274dc4deb5a2bf7db
AntonMFernando <anton.fernando@noaa.gov>
2025-01-22 13:51:48 -0500

    modified workflow\rocoto\gfs_tasks.py

--- workflow/rocoto/gfs_tasks.py
+++ workflow/rocoto/gfs_tasks.py
@@ -2440,7 +2440,7 @@ class GFSTasks(Tasks):
     def cleanup(self):
         deps = []
         if 'enkf' in self.run:
-            dep_dict = {'type': 'metatask', 'name': f'{self.run}_earc_vrfy'}
+            dep_dict = {'type': 'task', 'name': f'{self.run}_earc_vrfy'}
             deps.append(rocoto.add_dependency(dep_dict))
             if self.options['do_archtar']:
                 dep_dict = {'type': 'metatask', 'name': f'{self.run}_earc_tars'}
