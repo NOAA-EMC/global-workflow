9ac3313c1facdb0c52d80c5672fb0e6c7afc4b37
Jessica Meixner <Jessica.Meixner@noaa.gov>
2025-01-24 18:09:54 +0000

    change name of restart variables

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -367,16 +367,10 @@ WW3_postdet() {
   elif [[ -s "${ww3_binary_restart_file}" ]]; then 
     # found binary ww3 restart file 
     export WW3_restart_from_binary=true
-    if [[ -f "${DATA}/ufs.cpld.cpl.r.nc" ]]; then
-      #if this is a cmeps continue then the wave restart name is different 
-      seconds=$(to_seconds "${restart_date:8:2}0000")  # convert HHMMSS to seconds
-      local ww3_restart_dest_file="ufs.cpld.ww3.r.${restart_date:0:4}-${restart_date:4:2}-${restart_date:6:2}-${seconds}"
-      ${NCP} "${ww3_binary_restart_file}" "${DATA}/${ww3_restart_dest_file}" \
-          || ( echo "FATAL ERROR: Unable to copy binary WW3 IC, ABORT!"; exit 1 )
-    else 
-      ${NCP} "${ww3_binary_restart_file}" "${DATA}/restart.ww3" \
-          || ( echo "FATAL ERROR: Unable to copy binary WW3 IC, ABORT!"; exit 1 )
-    fi 
+    seconds=$(to_seconds "${restart_date:8:2}0000")  # convert HHMMSS to seconds
+    local ww3_restart_dest_file="ufs.cpld.ww3.r.${restart_date:0:4}-${restart_date:4:2}-${restart_date:6:2}-${seconds}"
+    ${NCP} "${ww3_binary_restart_file}" "${DATA}/${ww3_restart_dest_file}" \
+             || ( echo "FATAL ERROR: Unable to copy binary WW3 IC, ABORT!"; exit 1 )
   else
     if [[ "${RERUN}" == "YES" ]]; then
       # In the case of a RERUN, the WW3 restart file is required
