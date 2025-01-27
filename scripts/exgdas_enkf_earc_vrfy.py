5cd87304bb220f6fa246a72620f07e4293381c9d
AntonMFernando <anton.fernando@noaa.gov>
2025-01-27 14:59:10 -0500

    modified scripts\exgdas_enkf_earc_vrfy.py

--- scripts/exgdas_enkf_earc_vrfy.py
+++ scripts/exgdas_enkf_earc_vrfy.py
@@ -18,8 +18,10 @@ def main():
     archive = Archive(config)
 
     # Pull out all the configuration keys needed to run the rest of archive steps
-    keys = ['current_cycle', 'RUN', 'PSLOT', 'ROTDIR', 'PARMgfs',
-            'ARCDIR', 'MODE', 'DO_JEDIATMENS', 'DO_FIT2OBS']
+    keys = ['current_cycle', 'RUN', 'PSLOT', 'ROTDIR', 'PARMgfs', 'VFYARC',
+            'ARCDIR', 'MODE', 'DO_JEDIATMENS', 'DO_FIT2OBS', 'DO_JEDIATMVAR',
+            'DO_JEDISNOWDA', 'DO_AERO_ANL', 'DO_PREP_OBS_AERO', 'NET', 'MODE', 'FHOUT_GFS',
+            'FHMAX_HF_GFS', 'FHOUT_GFS', 'FHMAX_FITS', 'FHMAX', 'FHOUT', 'FHMAX_GFS']
 
     archive_dict = AttrDict()
     for key in keys:
