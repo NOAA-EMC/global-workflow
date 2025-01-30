895af47eac113c2161ffda2de943b30fba107c1f
David Huber <david.huber@noaa.gov>
2025-01-30 14:53:30 -0600

    Add globus to service tasks

--- workflow/rocoto/tasks.py
+++ workflow/rocoto/tasks.py
@@ -11,8 +11,8 @@ __all__ = ['Tasks']
 
 
 class Tasks:
-    SERVICE_TASKS = ['arch', 'earc', 'stage_ic', 'cleanup']
-    VALID_TASKS = ['aerosol_init', 'stage_ic',
+    SERVICE_TASKS = ['arch', 'earc', 'stage_ic', 'cleanup', 'globus']
+    VALID_TASKS = ['aerosol_init', 'stage_ic', 'globus',
                    'prep', 'anal', 'sfcanl', 'analcalc', 'analdiag', 'arch', "cleanup",
                    'prepatmiodaobs', 'atmanlinit', 'atmanlvar', 'atmanlfv3inc', 'atmanlfinal',
                    'prepoceanobs',
