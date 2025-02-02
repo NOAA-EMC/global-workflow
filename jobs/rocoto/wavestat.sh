6ae4e0fcb2917949026f52145293210f86c25a84
Saeideh Banihashemi <saeideh.banihashemi@noaa.gov>
2025-02-02 03:50:58 +0000

    editting wave_stat job and config files

--- jobs/rocoto/wavestat.sh
+++ jobs/rocoto/wavestat.sh
@@ -7,6 +7,7 @@ echo
 echo "=============== START TO SOURCE FV3GFS WORKFLOW MODULES ==============="
 #. ${HOMEgfs}/ush/load_fv3gfs_modules.sh
 . ${HOMEgfs}/ush/load_ufswm_modules.sh
+
 status=$?
 [[ ${status} -ne 0 ]] && exit ${status}
 
@@ -17,8 +18,17 @@ export jobid="${job}.$$"
 echo
 echo "=============== START TO RUN WAVE PREP ==============="
 # Execute the JJOB
-${HOMEgfs}/jobs/JGLOBAL_WAVE_STAT
-status=$?
-[[ ${status} -ne 0 ]] && exit ${status}
+IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
+
+export FHR3 jobid
+for FORECAST_HOUR in "${fhr_list[@]}"; do
+        FHR3=$(printf '%03d' "${FORECAST_HOUR}")
+        jobid="${job}_f${FHR3}.$$"
+        # Execute the JJOB
+        "${HOMEgfs}/jobs/JGLOBAL_WAVE_STAT"
+        status=$?
+        [[ ${status} -ne 0 ]] && exit "${status}"
+done
 
 exit 0
+
