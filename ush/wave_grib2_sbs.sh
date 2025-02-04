b06793bf974a87154ff45e0bf850862af3855e5d
AntonMFernando <anton.fernando@noaa.gov>
2025-02-04 12:58:19 -0500

    modified ush\wave_grib2_sbs.sh

--- ush/wave_grib2_sbs.sh
+++ ush/wave_grib2_sbs.sh
@@ -83,7 +83,7 @@ else
     echo "Directory ${com_dir} already exists."
 fi
 # Only create file if not present in COM
-if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
+if [[ ! -s "${outfile}.idx" ]]; then
 
   set +x
   echo ' '
@@ -169,11 +169,11 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
   fi
 
   if (( fhr > 0 )); then
-    ${WGRIB2} gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" -grib "${com_dir}/${outfile}"
+    ${WGRIB2} gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" -grib "${outfile}"
     err=$?
   else
     ${WGRIB2} gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" \
-      -set table_1.4 1 -set table_1.2 1 -grib "${com_dir}/${outfile}"
+      -set table_1.4 1 -set table_1.2 1 -grib "${outfile}"
     err=$?
   fi
 
@@ -189,7 +189,7 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
   fi
 
   # Create index
-  ${WGRIB2} -s "${com_dir}/${outfile}" > "${com_dir}/${outfile}.idx"
+  ${WGRIB2} -s "${outfile}" > "${outfile}.idx"
 
   # Create grib2 subgrid is this is the source grid
   if [[ "${grdID}" = "${WAV_SUBGRBSRC}" ]]; then
@@ -198,14 +198,14 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
       subgrbnam=$(echo ${!subgrb} | cut -d " " -f 21)
       subgrbres=$(echo ${!subgrb} | cut -d " " -f 22)
       subfnam="${WAV_MOD_TAG}.${cycle}${ENSTAG}.${subgrbnam}.${subgrbres}.f${FH3}.grib2"
-      ${COPYGB2} -g "${subgrbref}" -i0 -x "${com_dir}/${outfile}" "${com_dir}/${subfnam}"
-      ${WGRIB2} -s "${com_dir}/${subfnam}" > "${com_dir}/${subfnam}.idx"
+      ${COPYGB2} -g "${subgrbref}" -i0 -x "${outfile}" "${subfnam}"
+      ${WGRIB2} -s "${subfnam}" > "${subfnam}.idx"
    done
   fi
 
   # 1.e Save in /com
 
-  if [[ ! -s "${com_dir}/${outfile}" ]]; then
+  if [[ ! -s "${outfile}" ]]; then
     set +x
     echo ' '
     echo '********************************************* '
@@ -217,7 +217,7 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
     set_trace
     exit 4
   fi
-  if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
+  if [[ ! -s "${outfile}.idx" ]]; then
     set +x
     echo ' '
     echo '*************************************************** '
@@ -232,16 +232,23 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
 
   if [[ "${SENDDBN}" = 'YES' ]] && [[ ${outfile} != *global.0p50* ]]; then
     set +x
-    echo "   Alerting GRIB file as ${com_dir}/${outfile}"
-    echo "   Alerting GRIB index file as ${com_dir}/${outfile}.idx"
+    echo "   Alerting GRIB file as ${outfile}"
+    echo "   Alerting GRIB index file as ${outfile}.idx"
     set_trace
-    "${DBNROOT}/bin/dbn_alert" MODEL "${alertName}_WAVE_GB2" "${job}" "${com_dir}/${outfile}"
-    "${DBNROOT}/bin/dbn_alert" MODEL "${alertName}_WAVE_GB2_WIDX" "${job}" "${com_dir}/${outfile}.idx"
+    "${DBNROOT}/bin/dbn_alert" MODEL "${alertName}_WAVE_GB2" "${job}" "${outfile}"
+    "${DBNROOT}/bin/dbn_alert" MODEL "${alertName}_WAVE_GB2_WIDX" "${job}" "${outfile}.idx"
   else
     echo "${outfile} is global.0p50 or SENDDBN is NO, no alert sent"
   fi
 
-
+  # 2.1  Move grib files to COM directory
+  if [[ ! -s "${outfile}" ]] && [[ ! -s "${outfile}.idx" ]]; then
+    cp -f "${outfile}" "${com_dir}/${outfile}"
+    cp -f "${outfile}.idx" "${com_dir}/${outfile}.idx"
+    echo "   Copied grib files from "${GRIBDATA}" to COM"
+  else
+    echo "No grib files found in "${GRIBDATA}" to copy to COM"
+  fi
   # --------------------------------------------------------------------------- #
   # 3.  Clean up the directory
 
@@ -257,7 +264,7 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
 else
   set +x
   echo ' '
-  echo " File ${com_dir}/${outfile} found, skipping generation process"
+  echo " File ${outfile} found, skipping generation process"
   echo ' '
   set_trace
 fi
