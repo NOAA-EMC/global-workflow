c22a8c80deefd54bec92bc26ecdd57c0822ebff1
Clara Draper <33430543+ClaraDraper-NOAA@users.noreply.github.com>
2025-02-06 09:56:05 -0700

    Update scripts/exgdas_enkf_sfc.sh
    Co-authored-by: Cory Martin <cory.r.martin@noaa.gov>

--- scripts/exgdas_enkf_sfc.sh
+++ scripts/exgdas_enkf_sfc.sh
@@ -181,8 +181,7 @@ if [ $DOIAU = "YES" ]; then
             ${NCP} "${FIXgfs}/orog/${CASE}/${CASE}.mx${OCNRES}_oro_data.tile${n}.nc" "${DATA}/fnorog.${cmem}"
 
             if [[ ${DO_GSISOILDA} = "YES" ]]; then
-                FHR=${LFHR}
-                 ${NCP} "${COMIN_ATMOS_ANALYSIS_MEM}/sfci00${FHR}.tile${n}.nc" \
+                 ${NCP} "${COMIN_ATMOS_ANALYSIS_MEM}/sfci00${LFHR}.tile${n}.nc" \
                    "${DATA}/soil_xainc.${cmem}" 
             fi
 
