519767e57dce5b5a7c5517a10b1ba31e7b747b0d
AntonMFernando <anton.fernando@noaa.gov>
2025-02-11 13:45:27 -0500

    modified ush\wave_grib2_sbs.sh

--- ush/wave_grib2_sbs.sh
+++ ush/wave_grib2_sbs.sh
@@ -215,6 +215,9 @@ if [[ ! -s "${com_dir}/${outfile}.idx" ]]; then
 
   if [[ "${grdID}" = "${WAV_SUBGRBSRC}" ]]; then
     for subgrb in ${WAV_SUBGRB}; do
+      subgrbref=$(echo ${!subgrb} | cut -d " " -f 1-20)
+      subgrbnam=$(echo ${!subgrb} | cut -d " " -f 21)
+      subgrbres=$(echo ${!subgrb} | cut -d " " -f 22)
       subfnam="${WAV_MOD_TAG}.${cycle}${ENSTAG}.${subgrbnam}.${subgrbres}.f${FH3}.grib2"
       if [[ -s "${subfnam}" ]] && [[ -s "${subfnam}.idx" ]]; then
         cpfs "${subfnam}" "${com_dir}/${subfnam}"
