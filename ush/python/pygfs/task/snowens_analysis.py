d93db353ed2016bcbd13b81bcfa341f64bfa2ddf
travis.j.elless <travis.j.elless@noaa.gov>
2025-02-14 19:28:42 +0000

    update logger statements

--- ush/python/pygfs/task/snowens_analysis.py
+++ ush/python/pygfs/task/snowens_analysis.py
@@ -199,7 +199,7 @@ class SnowEnsAnalysis(Task):
 
         asc_file = os.path.join(localconf.COMIN_OBS, f"{localconf.OPREFIX}imssnow96.asc")
         if not os.path.isfile(asc_file):
-            logger.exception(f"Obs files are missing. Will not execute CALCFIMSEXE")
+            logger.warn(f"WARNING: Obs files are missing. Will not execute CALCFIMSEXE")
             return
 
         # copy the IMS obs files from COMIN_OBS to DATA/obs
