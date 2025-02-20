bddc2c66fc78419164c04936ae395d5dd0f93e21
AntonMFernando <anton.fernando@noaa.gov>
2025-02-20 12:52:10 -0500

    modified ush\wave_extractvars.sh

--- ush/wave_extractvars.sh
+++ ush/wave_extractvars.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash                                                                                                                                                                          
+#! /usr/bin/env bash
 
 ################################################################################
 ## UNIX Script Documentation Block
@@ -24,16 +24,22 @@ for (( nh = FHOUT_WAV_EXTRACT; nh <= FHMAX_WAV; nh = nh + FHOUT_WAV_EXTRACT )); 
   fnh=$(printf "%3.3d" "${nh}")
 
   infile=${com_dir}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2
+  new_infile=${subdata}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}_ext.grib2
   outfile=${subdata}/${RUN}wave.t${cyc}z.global.${wavres}.f${fnh}.grib2
-  rm -f "${outfile}" # Remove outfile if it already exists before extraction
 
   if [[ -f "${infile}" ]]; then # Check if input file exists before extraction
-    # shellcheck disable=SC2312 
-    ${WGRIB2} "${infile}" | grep -F -f "${varlist_wav}" | ${WGRIB2} -i "${infile}" -append -grib "${outfile}"
+    cpfs "${infile}" "${new_infile}" # Copy infile to the subdata directory
+    rm -f "${outfile}" # Remove outfile if it already exists before extraction
+    # shellcheck disable=SC2312
+    ${WGRIB2} "${new_infile}" | grep -F -f "${varlist_wav}" | ${WGRIB2} -i "${new_infile}" -append -grib "${outfile}"
+  elif [[ -f "${infile}" ]];
+    echo "WARNING: ${infile} does not exist in ${com_dir}."
+  elif [[ -f "${new_infile}" ]];
+    echo "WARNING: ${new_infile} does not exist in ${subdata}. Copying skipped."
   else
-    echo "WARNING: ${infile} does not exist."
-  fi 
+    echo "WARNING: ${infile} and ${new_infile} do not exist."
+  fi
   copy_to_comout "${outfile}" "${ARC_RFCST_PROD_WAV}"
 done # nh
 
-exit 0                                                                                                                                                                                        
+exit 0
