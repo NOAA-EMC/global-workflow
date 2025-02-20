5c2530c76a78f3bd2ffce9b6aabdf6ce5a5a1801
Mingshi Chen <Mingshi.Chen@noaa.gov>
2025-02-19 23:51:28 -0600

    changing double quote into single quote

--- workflow/applications/gfs_cycled.py
+++ workflow/applications/gfs_cycled.py
@@ -25,9 +25,9 @@ class GFSCycledAppConfig(AppConfig):
 
         # Now construct self.runs the desired XML order (gfs, enkfgfs, gdas, enkfgdas)
         self.runs = []
-        self.runs.append("gfs") if base['INTERVAL_GFS'] > 0 else 0  
-        self.runs.append('enkfgfs') if 'gfs' in self.ens_runs and "gfs" in self.runs else 0
-        self.runs.append("gdas")  # We always have a 'gdas' run
+        self.runs.append('gfs') if base['INTERVAL_GFS'] > 0 else 0  
+        self.runs.append('enkfgfs') if 'gfs' in self.ens_runs and 'gfs' in self.runs else 0
+        self.runs.append('gdas')  # We always have a 'gdas' run
         self.runs.append('enkfgdas') if 'gdas' in self.ens_runs else 0
 
     def _get_run_options(self, conf: Configuration) -> Dict[str, Any]:
