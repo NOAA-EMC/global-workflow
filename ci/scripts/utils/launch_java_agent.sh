b2fe776a105af23a4688aae76f9193880437a807
TerrenceMcGuinness-NOAA <terrence.mcguinness@cox.net>
2025-02-28 17:05:29 +0000

    Update ci/scripts/utils/launch_java_agent.sh
    Co-authored-by: David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>

--- ci/scripts/utils/launch_java_agent.sh
+++ ci/scripts/utils/launch_java_agent.sh
@@ -84,11 +84,7 @@ esac
 LOG=lanuched_agent-$(date +%Y%m%d%M).log
 rm -f "${LOG}"
 
-#shellcheck disable=SC2034
-# Needed for the module-setup.sh script
-# but did not want to set it globally
-HOMEgfs="${HOMEGFS_}"
-source "${HOMEGFS_}/ush/module-setup.sh"
+HOMEgfs="${HOMEGFS_}" source "${HOMEGFS_}/ush/module-setup.sh"
 module use "${HOMEGFS_}/modulefiles"
 module load "module_gwsetup.${MACHINE_ID}"
 source "${HOMEGFS_}/ci/platforms/config.${MACHINE_ID}"
