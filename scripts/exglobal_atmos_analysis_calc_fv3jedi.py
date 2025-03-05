da0ffcfb8a844330f7fe607cc82eaa18e49b63b2
DavidNew-NOAA <david.new@noaa.gov>
2025-03-05 16:07:55 +0000

    Address reviewer comments (part 1.5)

--- scripts/exglobal_atmos_analysis_calc_fv3jedi.py
+++ scripts/exglobal_atmos_analysis_calc_fv3jedi.py
@@ -4,6 +4,7 @@
 # and runs the initialize, execute, and finalize
 # methods which executes the analysis calculation
 import os
+from datetime import datetime
 
 from wxflow import Logger, cast_strdict_as_dtypedict
 from pygfs.task.analysis_calc import AnalysisCalc
@@ -24,3 +25,10 @@ if __name__ == '__main__':
     analysis_calc.initialize()
     analysis_calc.execute()
     analysis_calc.finalize()
+
+    # Write analysis log file
+    formatted_date = datetime.now().strftime("%a %b %d %H:%M:%S %Z%Y")
+    log_file = os.path.join(config.COMOUT_ATMOS_ANALYSIS, f"{config.RUN}.t{config.cyc}z.loganl.txt")
+    message = f"{config.rCDUMP} {config.PDY}{config.cyc} atmanl and sfcanl done at {formatted_date}"
+    with open(log_file, "w") as file:
+        file.write(f"{message}\n")
