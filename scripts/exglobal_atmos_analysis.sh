1074007ee5b769e8dd5c203ec28a2422e7091fae
Andrew Collard <andrew.collard@noaa.gov>
2025-03-05 16:14:48 +0000

    Turn on CADS

--- scripts/exglobal_atmos_analysis.sh
+++ scripts/exglobal_atmos_analysis.sh
@@ -803,9 +803,10 @@ cat > gsiparm.anl << EOF
 /
 &OBSQC
   dfact=0.75,dfact1=3.0,noiqc=.true.,oberrflg=.false.,c_varqc=0.02,
-  use_poq7=.true.,qc_noirjaco3_pole=.true.,vqc=.false.,nvqc=.true.,
+  use_poq7=.true.,qc_noirjaco3_pole=.false.,vqc=.false.,nvqc=.true.,
   aircraft_t_bc=.true.,biaspredt=1.0e5,upd_aircraft=.true.,cleanup_tail=.true.,
-  tcp_width=70.0,tcp_ermax=7.35,
+  tcp_width=70.0,tcp_ermax=7.35,airs_cads=.true.,cris_cads=.true.,
+  iasi_cads=.true.,
   $OBSQC
 /
 &OBS_INPUT
