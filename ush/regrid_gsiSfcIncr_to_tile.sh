1d375c4623d473dbe3c9a970d91f244beed725eb
catherine.thomas <catherine.thomas@noaa.gov>
2025-03-14 16:29:51 +0000

    Update regrid script for new soil DA fix file path

--- ush/regrid_gsiSfcIncr_to_tile.sh
+++ ush/regrid_gsiSfcIncr_to_tile.sh
@@ -8,10 +8,6 @@ source "${USHgfs}/preamble.sh"
 # Clara Draper, Dec 2024
 #-------------------------------------------------------------------------------------------------
 
-# temporary files on hera, until g-w issue 3392 is resolved.
-TMP_FIX_FILES=/lfs/h2/emc/da/noscrub/catherine.thomas/gsi/fix/soil/gw_new_fix_files/
-#TMP_FIX_FILES=/scratch2/BMC/gsienkf/Clara.Draper/gw_new_fix_files/
-
 
 export PGMOUT=${PGMOUT:-${pgmout:-'&1'}}
 export PGMERR=${PGMERR:-${pgmerr:-'&2'}}
@@ -74,7 +70,7 @@ cat << EOF > regrid.nml
 EOF
 
 # input, fixed files
-${NCP} "${TMP_FIX_FILES}/gaussian.${LONB_CASE_IN}.${LATB_CASE_IN}.nc" \
+${NCP} "${FIXorog}/${CASE_OUT}/gaussian.${LONB_CASE_IN}.${LATB_CASE_IN}.nc" \
         "${DATA}/gaussian_scrip.nc"
 
 # output, fixed files
