827a48679875794081adf7f38e71c80a114e81f8
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-03-19 16:57:21 -0400

    added all that is needed to download and register gitlab runner and made token a file or input

--- ci/gitlab_runner.sh
+++ ci/gitlab_runner.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-TOKEN=glrt-t3_ZTdxY19NwELUjMTg8Kdn
+# Get token from 2nd arg, env, or file
+GITLAB_RUNNER_TOKEN=${2:-${GITLAB_RUNNER_TOKEN}}
+if [[ -z ${GITLAB_RUNNER_TOKEN} ]]; then
+  if [[ -f gitlab_token ]]; then	
+   source gitlab_token
+  fi 
+fi
+if [[ -z ${GITLAB_RUNNER_TOKEN} ]]; then
+  echo "ERROR: GITLAB_RUNNER_TOKEN not set"
+  exit 1
+fi  
+
+URL=https://vlab.noaa.gov/gitlab-licensed
+BUILDS_DIR=/gpfs/f6/drsa-precip3/scratch/role.glopara/GFS_CI_ROOT/GITLAB/CI
 
 if [[ ! -f gitlab-runner ]]; then
   curl -L --output $PWD/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
-  sudo chmod +x ./gitlab-runner
-fi
-
-# Check if the GitLab Runner user already exists
-if id "gitlab-runner" &>/dev/null; then
-  echo "User gitlab-runner already exists."
-else
-  sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
-fi
-
-if [[ $1 == "install_service" ]]; then
-  sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
-  gitlab-runner start
+  chmod +x ./gitlab-runner
 fi
 
 if [[ $1 == "register" ]]; then
-  gitlab-runner register -n -t ${TOKEN} --url http://localhost:8929 --executor shell
+  ./gitlab-runner register -n -t ${GITLAB_RUNNER_TOKEN} --url ${URL} --executor shell --builds-dir ${BUILDS_DIR} --shell bash --custom_build_dir-enabled true
 fi
