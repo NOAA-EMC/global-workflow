d1cd8eb7686106d25e880f2aa7639872533c23ae
David Huber <david.huber@noaa.gov>
2025-03-19 12:14:58 -0500

    Check for empty transfer sets

--- scripts/exglobal_globus_earc.py
+++ scripts/exglobal_globus_earc.py
@@ -20,7 +20,7 @@ def main():
     keys = ['STAGE_DIR', 'current_cycle', 'RUN', 'PDY', 'HOMEgfs', 'sven_dropbox',
             'doorman_gendel', 'DATASETS_YAML', 'PARMgfs', 'COMIN_CONF', 'KEEPDATA',
             'jobid', 'hpss_target_dir', 'server_home', 'SERVER_NAME', 'DOORMAN_ROOT',
-            'CLIENT_GLOBUS_UUID', 'SERVER_GLOBUS_UUID', 'PSLOT']
+            'CLIENT_GLOBUS_UUID', 'SERVER_GLOBUS_UUID', 'PSLOT', 'ENSGRP']
 
     globus_dict = AttrDict()
     for key in keys:
@@ -33,9 +33,17 @@ def main():
     transfer_sets = globus.configure(globus_dict)
 
     # Send the tarballs to HPSS via Niagara.  Start with non-rstprod (standard) data
+    count_sets = 0
     for transfer_set in ["standard", "rstprod"]:
-        has_rstprod = transfer_set == "rstprod"
-        globus.execute_transfer_data(transfer_sets[transfer_set], has_rstprod)
+        if len(transfer_sets[transfer_set]['locations']) > 0:
+            has_rstprod = transfer_set == "rstprod"
+            print("on reentry")
+            print(transfer_sets[transfer_set]["todo"])
+            globus.execute_transfer_data(transfer_sets[transfer_set], has_rstprod)
+            count_sets += 1
+
+    if count_sets == 0:
+        raise RuntimeError("FATAL ERROR Transfer sets were all empty!")
 
     # Clean up any temporary files
     globus.clean()
