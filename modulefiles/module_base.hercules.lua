26920cd86c70ac4230d8047ea89c00abbef249d9
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2025-03-20 20:25:13 +0000

    Add new spack-stack to hercules, gaea[56], and added explicit versions for the new modules on other machines. Skipping soilDA test on orion, while waiting for spack-stack issue to be resolved.

--- modulefiles/module_base.hercules.lua
+++ modulefiles/module_base.hercules.lua
@@ -17,6 +17,7 @@ load(pathJoin("cdo", (os.getenv("cdo_ver") or "None")))
 load(pathJoin("hdf5", (os.getenv("hdf5_ver") or "None")))
 load(pathJoin("netcdf-c", (os.getenv("netcdf_c_ver") or "None")))
 load(pathJoin("netcdf-fortran", (os.getenv("netcdf_fortran_ver") or "None")))
+load(pathJoin("esmf", (os.getenv("esmf_ver") or "None")))
 
 load(pathJoin("nco", (os.getenv("nco_ver") or "None")))
 load(pathJoin("prod_util", (os.getenv("prod_util_ver") or "None")))
