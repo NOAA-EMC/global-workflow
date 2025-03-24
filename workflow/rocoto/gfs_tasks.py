98a88404549e60f6b469620f62981c0833482ccc
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-03-24 10:21:23 -0400

    fix 3244 garbled change

--- workflow/rocoto/gfs_tasks.py
+++ workflow/rocoto/gfs_tasks.py
@@ -3217,6 +3217,8 @@ class GFSTasks(Tasks):
         if 'enkfgdas' in self.run:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_epmn'}
             deps.append(rocoto.add_dependency(dep_dict))
+            dep_dict = {'type': 'task', 'name': f'{self.run}_echgres'}
+            deps.append(rocoto.add_dependency(dep_dict))
             if not self.options['do_jediatmvar']:
                 dep_dict = {'type': 'task', 'name': f'{self.run}_echgres'}
                 deps.append(rocoto.add_dependency(dep_dict))
