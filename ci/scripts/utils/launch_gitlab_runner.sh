cf8188fcc1bda3d10693074bfdc6b4ff339066b4
Terry McGUinness <terrene.mcguinness@gmail.com>
2025-03-24 12:06:47 -0400

    added double quote shell norms 2

--- ci/scripts/utils/launch_gitlab_runner.sh
+++ ci/scripts/utils/launch_gitlab_runner.sh
@@ -14,7 +14,7 @@
 # Set the HOMEGFS_ variable to the root directory of the global workflow
 HOMEGFS_="$(cd "$(dirname  "${BASH_SOURCE[0]}")/../../.." >/dev/null 2>&1 && pwd )"
 # Get the hostname of the current machine
-host=$(hostname)
+host="$(hostname)"
 
 #########################################################################
 #  Set up runtime environment variables for accounts on supported machines
@@ -23,7 +23,7 @@ host=$(hostname)
 # Source the detect_machine.sh script to determine the MACHINE_ID
 source "${HOMEGFS_}/ush/detect_machine.sh"
 # Check the MACHINE_ID and set up the environment accordingly
-case ${MACHINE_ID} in
+case "${MACHINE_ID}" in
   hera | orion | hercules | wcoss2 | gaeac5 | gaeac6 )
     echo "Launching GitLab Runner on ${MACHINE_ID}";;
   noaacloud )
@@ -37,13 +37,13 @@ esac
 # This file contains platform-specific variables such as GITLAB_URL, GITLAB_BUILDS_DIR, 
 # and GITLAB_RUNNER_DIR which are required for runner registration and execution
 # See config.gaeac6 for details on these variables
-source ${HOMEGFS_}/ci/platforms/config.${MACHINE_ID}
+source "${HOMEGFS_}/ci/platforms/config.${MACHINE_ID}"
 
 # Change to the GitLab runner directory defined in the platform config
-cd ${GITLAB_RUNNER_DIR}
+cd "${GITLAB_RUNNER_DIR}"
 
 # Set the log file name with the current date and time
-GITLAB_LOG=launched_gitlab_runner-$(date +%Y%m%d%M).log
+GITLAB_LOG="launched_gitlab_runner-$(date +%Y%m%d%M).log"
 # Set the GitLab runner name - this name will appear in the GitLab UI
 GITLAB_RUNNER_NAME="RDHPCS Gaea C6"
 # Remove any existing log file
@@ -61,20 +61,20 @@ echo "with runner name: ${GITLAB_RUNNER_NAME}" >> "${GITLAB_LOG}"
 # 1. The second command-line argument
 # 2. The GITLAB_RUNNER_TOKEN environment variable
 # 3. A gitlab_token file in the current directory
-GITLAB_RUNNER_TOKEN=${2:-${GITLAB_RUNNER_TOKEN}}
-if [[ -z ${GITLAB_RUNNER_TOKEN} ]]; then
+GITLAB_RUNNER_TOKEN="${2:-${GITLAB_RUNNER_TOKEN}}"
+if [[ -z "${GITLAB_RUNNER_TOKEN}" ]]; then
   if [[ -f gitlab_token ]]; then	
    source gitlab_token
   fi 
 fi
-if [[ -z ${GITLAB_RUNNER_TOKEN} ]]; then
+if [[ -z "${GITLAB_RUNNER_TOKEN}" ]]; then
   echo "ERROR: GITLAB_RUNNER_TOKEN not set"
   exit 1
 fi  
 
 # Download the GitLab runner binary if it does not exist
 if [[ ! -f gitlab-runner ]]; then
-  curl -L --output $PWD/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+  curl -L --output "$PWD/gitlab-runner" https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
   chmod +x ./gitlab-runner
 fi
 
@@ -83,7 +83,7 @@ fi
 # Registers a new GitLab runner with the GitLab server
 #########################################################################
 
-if [[ $1 == "register" ]]; then
+if [[ "${1}" == "register" ]]; then
   # Register the GitLab runner with the following parameters:
   # -n: Run in non-interactive mode
   # -t: Registration token from GitLab
@@ -93,7 +93,7 @@ if [[ $1 == "register" ]]; then
   # --builds-dir: Directory where builds will be stored (from config.gaeac6)
   # --custom_build_dir-enabled: Enable custom build directories
   # --request-concurrency: Number of concurrent requests that can be handled
-  ./gitlab-runner register -n -t ${GITLAB_RUNNER_TOKEN} --url ${GITLAB_URL} --executor shell --shell bash --builds-dir ${GITLAB_BUILDS_DIR} --custom_build_dir-enabled true --request-concurrency 24
+  ./gitlab-runner register -n -t "${GITLAB_RUNNER_TOKEN}" --url "${GITLAB_URL}" --executor shell --shell bash --builds-dir "${GITLAB_BUILDS_DIR}" --custom_build_dir-enabled true --request-concurrency 24
   
   # Set the concurrent job limit in the GitLab runner config file
   sed -i 's/concurrent.*/concurrent = 24/' ~/.gitlab-runner/config.toml
@@ -104,7 +104,7 @@ fi
 # Starts a GitLab runner in the background
 #########################################################################
 
-if [[ $1 == "run" ]]; then
+if [[ "${1}" == "run" ]]; then
   # Construct the command to run the GitLab runner
   # nohup: Run the command immune to hangups
   # --working-directory: Directory where the runner will store its working files (from config.gaeac6)
@@ -115,10 +115,10 @@ if [[ $1 == "run" ]]; then
   echo -e "Running gitlab-runner with the command:\n${COMMAND}\nsee log ${PWD}/${GITLAB_LOG}"
   
   # Run the command in the background and redirect output to the log file
-  nohup $COMMAND >> ${GITLAB_LOG} 2>&1 &
+  nohup $COMMAND >> "${GITLAB_LOG}" 2>&1 &
   
   # Display the current contents of the log file
-  cat ${GITLAB_LOG}
+  cat "${GITLAB_LOG}"
 fi
 
 #########################################################################
@@ -126,7 +126,7 @@ fi
 # Removes a GitLab runner from the GitLab server
 #########################################################################
 
-if [[ $1 == "unregister" ]]; then
+if [[ "${1}" == "unregister" ]]; then
   # Unregister the GitLab runner by name
-  ./gitlab-runner unregister --name ${GITLAB_RUNNER_NAME}
+  ./gitlab-runner unregister --name "${GITLAB_RUNNER_NAME}"
 fi
