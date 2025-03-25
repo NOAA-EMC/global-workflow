0e87789e3d93f4d17a289dbac6e1b86bbf5d253d
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2025-03-25 18:31:44 +0000

    address shell linting errors

--- ush/create_ozinfo.sh
+++ ush/create_ozinfo.sh
@@ -6,21 +6,22 @@ echo '! For mls data, pressure and obs errors are pulled from bufr, so not liste
 echo '! sens/instr/sat lev  use pressure gross   obs    b_oz  pg_oz'
 echo '!                                  error  error variational qc'
 # loop over satellites
-cd ${FIXgfs}/gsi/build_gsinfo/ozinfo
-for sat in $(cat satellites); do
+cd "${FIXgfs}/gsi/build_gsinfo/ozinfo" || exit 1
+grep -v '^ *#' satellites | while IFS= read -r sat
+do
     # find matching date
     usedate=""
-    for f in $sat/*; do
-        if [ $f != "$sat/readme" ]; then # skip readme file
-           datex=`basename $f`
-	   if [ $date -ge $datex ]; then
+    for f in "${sat}"/*; do
+        if [[ $f != "$sat/readme" ]]; then # skip readme file
+           datex=$(basename $f)
+	   if [[ $date -ge $datex ]]; then
               usedate=$datex
            fi
         fi
     done
     # cat matching date file, or quit if date not found
-    if [ $usedate != "" ]; then
-        cat $sat/$usedate
+    if [[ ${usedate} != "" ]]; then
+        cat "${sat}/$usedate" || exit 1
     else
         echo "date not found for $sat"
 	exit 1
