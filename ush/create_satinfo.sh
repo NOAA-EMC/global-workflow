11ad3f0fae44a6dae689045cde82a0714ba36b12
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2025-03-25 15:33:05 -0400

    add check for existence of satellites file

--- ush/create_satinfo.sh
+++ ush/create_satinfo.sh
@@ -5,7 +5,8 @@ date=$1
 echo '!sensor/instr/sat      chan iuse  error  error_cld  ermax   var_b    var_pg  icld_det icloud iaerosol'
 # loop over satellites
 cd "${FIXgfs}/gsi/build_gsinfo/satinfo" || exit 1
-grep -v '^ *#' satellites | while IFS= read -r sat || true
+[ ! -f satellites ] && echo "satellites file not found!" || exit 1
+grep -v '^ *#' satellites | while IFS= read -r sat 
 do
     # find matching date
     usedate=""
