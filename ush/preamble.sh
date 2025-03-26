5d4ea02296af931502466496546e581282803b0d
David Huber <david.huber@noaa.gov>
2025-03-26 15:11:25 -0400

    Add err_exit function

--- ush/preamble.sh
+++ ush/preamble.sh
@@ -101,6 +101,77 @@ postamble() {
     exit "${rc}"
 }
 
+function err_exit() {
+    # Taken from prod_util v2.1.0
+    # SCRIPT NAME:  err_exit
+    #
+    # ABSTRACT:  This script is to be used when a fatal error or condition 
+    # has been reached and you want to terminate the job.
+    #
+    # USAGE:  To use this script one must export the following variables to the
+    # script: jobid, SENDECF, pgm, pgmout, DATA. One can provide
+    # a message for the logfile by passing it to the script as an argument.
+
+    msg1=${@:-Job ${jobid} failed}
+    if [ -n "${pgm}" ]; then
+      msg1+=", ERROR IN ${pgm}"
+    fi
+    if [ -n "${err}" ]; then
+      msg1+=" RETURN CODE ${err}"
+    fi
+
+    msg2="
+    -------------------------------------------------------------
+    -- FATAL ERROR: ${msg1}
+    -- ABNORMAL EXIT at $(date) on ${HOSTNAME}
+    -------------------------------------------------------------
+    "
+
+    >&2 echo "$msg2"
+
+    # list loaded modules
+    module list
+    >&2 echo ""
+
+    >&2 echo "${msg1}"
+
+    # list files in temporary working directory
+    if [ -n "${DATA}" ]; then
+      >&2 echo "${DATA}"
+      >&2 ls -ltr "${DATA}"
+    else
+      >&2 echo "WARNING: DATA variable not defined"
+    fi
+
+    # save standard output
+    if [ -n "${pgmout}" ]; then
+      if [ -s errfile ]; then
+        echo "----- contents of errfile -----" >> ${pgmout}
+        cat errfile >> ${pgmout}
+      fi
+      >&2 cat "${pgmout}"
+    elif [ -s errfile ]; then
+      >&2 cat errfile
+    fi
+
+    # Write to ecflow log:
+    if [ "${SENDECF}" = "YES" ]; then
+      timeout 30 ecflow_client --msg "$ECF_NAME: ${msg1}"
+      timeout 30 ssh "${ECF_HOST}" "echo \"$msg2\" >> ${ECF_JOBOUT:?}"
+    fi
+
+    # KILL THE JOB:
+    if [ "${SENDECF}" = "YES" ]; then
+      ecflow_client --kill=${ECF_NAME:?}
+    fi
+
+    if [ ! -z "${PBS_JOBID}" ]; then
+      qdel "${PBS_JOBID}"
+    elif [ ! -z "${SLURM_JOB_ID}" ]; then
+      scancel "${SLURM_JOB_ID}"
+    fi
+}
+
 # Place the postamble in a trap so it is always called no matter how the script exits
 # Shellcheck: Turn off warning about substitions at runtime instead of signal time
 # shellcheck disable=SC2064
