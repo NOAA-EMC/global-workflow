136982ebbd586de3aa95b212c9ed0ac083f6f518
AntonMFernando <anton.fernando@noaa.gov>
2025-03-27 10:46:41 -0400

    update jobs\rocoto\gempak_test.sh

--- jobs/rocoto/gempak_test.sh
+++ jobs/rocoto/gempak_test.sh
@@ -1,5 +1,25 @@
 #! /usr/bin/env bash
 ###############################################################
-echo "gempak job passed."
-###############################################################
+source "${HOMEgfs}/ush/preamble.sh"
+source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+status=$?
+if (( status != 0 )); then exit "${status}"; fi
+
+export job="gempak"
+
+# shellcheck disable=SC2153
+IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
+
+export FHR3 jobid
+for fhr in "${fhr_list[@]}"; do
+	FHR3=$(printf '%03d' "${fhr}")
+	jobid="${job}_f${FHR3}.$$"
+	###############################################################
+	# Execute the JJOB
+	###############################################################
+	echo "add J-script here."
+	status=$?
+	[[ ${status} -ne 0 ]] && exit "${status}"
+done
+
 exit 0
