c835bdca6aa5789c648f716a9a98542684b9d92b
AntonMFernando <anton.fernando@noaa.gov>
2025-03-31 16:28:18 -0400

    added fhrs to awips

--- jobs/rocoto/awips.sh
+++ jobs/rocoto/awips.sh
@@ -1,5 +1,26 @@
 #! /usr/bin/env bash
-###############################################################
-echo "awips job passed."
-###############################################################
+
+source "${HOMEgfs}/ush/preamble.sh"
+source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+status=$?
+if (( status != 0 )); then exit "${status}"; fi
+
+export job="awips"
+
+# shellcheck disable=SC2153
+IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
+
+export FHR3 jobid
+for fhr in "${fhr_list[@]}"; do
+    FHR3=$(printf '%03d' "${fhr}")
+    jobid="${job}_f${FHR3}.$$"
+    ###############################################################
+    # Execute the JJOB
+    ###############################################################
+    #"${HOMEgfs}/jobs/J${RUN^^}_ATMOS_GEMPAK"
+    echo "add J-script here."
+    #status=$?
+    #[[ ${status} -ne 0 ]] && exit "${status}"
+done
+
 exit 0
