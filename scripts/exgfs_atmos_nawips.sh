3fb9ab702769c1424c3460af20c2d1e97d0c29fa
kate.friedman <kate.friedman@noaa.gov>
2025-03-31 14:10:03 +0000

    Change ATMOS_GRIB from COMOUT to COMIN
    Refs #2451

--- scripts/exgfs_atmos_nawips.sh
+++ scripts/exgfs_atmos_nawips.sh
@@ -68,7 +68,7 @@ case ${grid} in
   *) grid_in="1p00";;
 esac
 
-source_var="COMOUT_ATMOS_GRIB_${grid_in}"
+source_var="COMIN_ATMOS_GRIB_${grid_in}"
 export GRIBIN="${!source_var}/${model}.${cycle}.pgrb2.${grid_in}.f${fhr3}"
 GRIBIN_chk="${!source_var}/${model}.${cycle}.pgrb2.${grid_in}.f${fhr3}.idx"
 
