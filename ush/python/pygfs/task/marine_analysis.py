793617644a7c69673661e23f017a027b6dd8a4be
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2025-04-01 21:09:21 +0000

    style

--- ush/python/pygfs/task/marine_analysis.py
+++ ush/python/pygfs/task/marine_analysis.py
@@ -132,8 +132,6 @@ class MarineAnalysis(Task):
         obs_files = []
 
         for observer in self.task_config['observations']['observers']:
-            #logger.info(f"******** {self.task_config.OPREFIX}{observer['obs space']['name'].lower()}.{to_YMD(self.task_config.PDY)}{self.task_config.cyc:02d}.nc4")
-            #obs_files.append(f"{self.task_config.OPREFIX}{observer['obs space']['name'].lower()}.{to_YMD(self.task_config.PDY)}{self.task_config.cyc:02d}.nc4")
             filename = f"{self.task_config.OPREFIX}{observer['obs space']['name'].lower()}.{to_YMD(self.task_config.PDY)}{self.task_config.cyc:02d}.nc4"
             logger.info(f"******** {filename}")
             obs_files.append(filename)
