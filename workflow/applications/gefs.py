c3e6b0972c983fb2302f96a30c3bfcd82d20726f
AntonMFernando <anton.fernando@noaa.gov>
2025-04-01 11:30:36 -0400

    remove awips2

--- workflow/applications/gefs.py
+++ workflow/applications/gefs.py
@@ -39,9 +39,6 @@ class GEFSAppConfig(AppConfig):
         if options['nens'] > 0:
             configs += ['efcs', 'atmos_ensstat']
 
-        if options['do_awips']:
-            configs += ['awips']
-
         if options['do_wave']:
             configs += ['waveinit', 'wavepostsbs', 'wavepostpnt']
             if options['do_wave_bnd']:
@@ -99,9 +96,6 @@ class GEFSAppConfig(AppConfig):
 
         if options['nens'] > 0:
             tasks += ['atmos_ensstat']
-
-        if options['do_awips']:
-            tasks += ['awips']
 
         if options['do_ocean']:
             tasks += ['ocean_prod']
