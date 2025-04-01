9f49b2179d31a43dec1bd2973825412bb7e10110
Eric.Sinsky <eric.sinsky@noaa.gov>
2025-04-01 13:16:28 +0000

    Add support for 24hr-averaged ocn/ice products

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -733,7 +733,7 @@ CICE_postdet() {
         dest_file="${RUN}.ice.t${cyc}z.inst.f${fhr3}.nc"
         ;;
       gfs|enkfgfs|gefs|sfs)
-        source_file="iceh_$(printf "%0.2d" "${FHOUT_ICE}")h.${vdatestr}.nc"
+        source_file="iceh.${vdatestr}.nc"
         dest_file="${RUN}.ice.t${cyc}z.${interval}hr_avg.f${fhr3}.nc"
         ;;
       *)
