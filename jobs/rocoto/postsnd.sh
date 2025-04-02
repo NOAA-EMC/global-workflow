b34152e30db0f5289360d78f91f3fd6254c1e060
AntonMFernando <anton.fernando@noaa.gov>
2025-04-02 16:46:55 -0400

    changed indentation postsnd.sh

--- jobs/rocoto/postsnd.sh
+++ jobs/rocoto/postsnd.sh
@@ -4,25 +4,25 @@ source "${HOMEgfs}/ush/preamble.sh"
 source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
 if [[ ${status} -ne 0 ]]; then
-    exit "${status}"
+  exit "${status}"
 fi
 
 export job="postsnd"
 
 # shellcheck disable=SC2153
 if [[ "${RUN}" == "gefs" ]]; then
-    IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
-    export FHR3 jobid
-    for fhr in "${fhr_list[@]}"; do
-        FHR3=$(printf '%03d' "${fhr}")
-        jobid="${job}_f${FHR3}.$$"
-        echo "add J-script here for GEFS."
-    done
+  IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
+  export FHR3 jobid
+  for fhr in "${fhr_list[@]}"; do
+    FHR3=$(printf '%03d' "${fhr}")
+    jobid="${job}_f${FHR3}.$$"
+    echo "add J-script here for GEFS."
+  done
 else
-    export jobid="${job}.$$"
-    ################################################################
-    # Execute the JJOB
-    ${HOMEgfs}/jobs/JGFS_ATMOS_POSTSND
-    err=$?
-    exit "${err}"
+  export jobid="${job}.$$"
+  ################################################################
+  # Execute the JJOB
+  ${HOMEgfs}/jobs/JGFS_ATMOS_POSTSND
+  err=$?
+  exit "${err}"
 fi
