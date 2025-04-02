6eeb0590ec5a065483e597e9f243129eaa808d4e
AntonMFernando-NOAA <167725623+AntonMFernando-NOAA@users.noreply.github.com>
2025-04-02 11:44:58 -0400

    Update jobs/rocoto/postsnd.sh
    Co-authored-by: David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>

--- jobs/rocoto/postsnd.sh
+++ jobs/rocoto/postsnd.sh
@@ -3,7 +3,9 @@
 source "${HOMEgfs}/ush/preamble.sh"
 source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
 status=$?
-if (( status != 0 )); then exit "${status}"; fi
+if [[ ${status} -ne 0 ]]; then
+    exit "${status}"
+fi
 
 export job="postsnd"
 
