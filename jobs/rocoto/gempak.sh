f46fb7abae8448cdc9829ecc7f56d6610a5b0efc
AntonMFernando <anton.fernando@noaa.gov>
2025-04-02 16:39:38 -0400

    comments aerorahul

--- jobs/rocoto/gempak.sh
+++ jobs/rocoto/gempak.sh
@@ -12,20 +12,20 @@ IFS=', ' read -r -a fhr_list <<< "${FHR_LIST}"
 
 export FHR3 jobid
 for fhr in "${fhr_list[@]}"; do
-    FHR3=$(printf '%03d' "${fhr}")
-    jobid="${job}_f${FHR3}.$$"
-    ###############################################################
-    # Execute the JJOB
-    ###############################################################
-    if [[ "${RUN}" == "gefs" ]]; then
-        echo "add J-script here for GEFS."
-    else
-        "${HOMEgfs}/jobs/JGFS_ATMOS_GEMPAK"
-        err=$?
-        if [[ ${err} -ne 0 ]]; then
-          exit "${err}"
-        fi
+  FHR3=$(printf '%03d' "${fhr}")
+  jobid="${job}_f${FHR3}.$$"
+  ###############################################################
+  # Execute the JJOB
+  ###############################################################
+  if [[ "${RUN}" == "gefs" ]]; then
+    echo "add J-script here for GEFS."
+  else
+    "${HOMEgfs}/jobs/JGFS_ATMOS_GEMPAK"
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+      exit "${err}"
     fi
+  fi
 done
 
 exit 0
