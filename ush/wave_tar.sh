6caf9d04b77d2e81df860953e65f3570e9477d96
AliS-Noaa <ali.salimi@noaa.gov>
2025-04-03 14:26:11 +0000

    file counting is changed to be compatible with the new $ID

--- ush/wave_tar.sh
+++ ush/wave_tar.sh
@@ -103,12 +103,12 @@ source "${USHgfs}/preamble.sh"
   while [[ "${tardone}" = "no" ]]
   do
 
-    nf=$(ls | awk '/'$ID.*.$filext'/ {a++} END {print a}')
+    nf=$(find . -maxdepth 1 -type f -name "*.$filext" | wc -l)
     nbm2=$(( $nb - 2 ))
     if [[ "${nf}" -ge "${nbm2}" ]]
     then
 
-      tar -cf "${ID}.${type}.tar" ./${ID}.*.${filext}
+      tar -cf "${ID}.${type}.tar" ./*.${filext}
       exit=$?
       filename="${ID}.${type}.tar"
       if ! wait_for_file "${filename}" "${sleep_interval}" "${countMAX}" ; then
