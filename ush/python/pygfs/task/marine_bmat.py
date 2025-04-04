26089aa3f54f9bd1ab2af04aa29e0b26b577cd15
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2025-04-04 14:29:40 +0000

    cleanup, addition of bmat diags

--- ush/python/pygfs/task/marine_bmat.py
+++ ush/python/pygfs/task/marine_bmat.py
@@ -56,9 +56,11 @@ class MarineBMat(Task):
         # Create a local dictionary that is repeatedly used across this class
         local_dict = AttrDict(
             {
+   #             'cyc': os.getenv('cyc'),
                 'PARMsoca': os.path.join(self.task_config.PARMgfs, 'gdas', 'soca'),
                 'CALC_SCALE_EXEC': _calc_scale_exec,
                 'MARINE_WINDOW_BEGIN': _window_begin,
+                'MARINE_WINDOW_BEGIN_ISO': _window_begin.strftime('%Y-%m-%dT%H:%M:%SZ'),
                 'MARINE_WINDOW_MIDDLE': self.task_config.current_cycle,
                 'MARINE_WINDOW_END': _window_end,
                 'MARINE_WINDOW_LENGTH': f"PT{self.task_config['assim_freq']}H",
@@ -214,6 +216,11 @@ class MarineBMat(Task):
         ----------
         None
         """
+
+        RUN = self.task_config['RUN']
+        cyc = self.task_config['cyc']
+        APREFIX = self.task_config.APREFIX
+
         # Copy the soca grid if it was created
         grid_file = os.path.join(self.task_config.DATA, 'soca_gridspec.nc')
         if os.path.exists(grid_file):
@@ -227,7 +234,7 @@ class MarineBMat(Task):
         for diff_type in ['hz', 'vt']:
             src = os.path.join(self.task_config.DATAstaticb, f"{diff_type}_ocean.nc")
             dest = os.path.join(self.task_config.COMOUT_OCEAN_BMATRIX,
-                                f"{self.task_config.APREFIX}{diff_type}_ocean.nc")
+                                f"{APREFIX}{diff_type}_ocean.nc")
             diffusion_coeff_list.append([src, dest])
 
         FileHandler({'copy': diffusion_coeff_list}).sync()
@@ -242,7 +249,7 @@ class MarineBMat(Task):
                   os.path.join(self.task_config.DATAstaticb, f"ocn.bkgerr_stddev.nc"))
         src = os.path.join(self.task_config.DATAstaticb, f"ocn.bkgerr_stddev.nc")
         dst = os.path.join(self.task_config.COMOUT_OCEAN_BMATRIX,
-                           f"{self.task_config.APREFIX}ocean.bkgerr_stddev.nc")
+                           f"{APREFIX}ocean.bkgerr_stddev.nc")
         diagb_list.append([src, dst])
 
         # ice diag B
@@ -250,7 +257,7 @@ class MarineBMat(Task):
                   os.path.join(self.task_config.DATAstaticb, f"ice.bkgerr_stddev.nc"))
         src = os.path.join(self.task_config.DATAstaticb, f"ice.bkgerr_stddev.nc")
         dst = os.path.join(self.task_config.COMOUT_ICE_BMATRIX,
-                           f"{self.task_config.APREFIX}ice.bkgerr_stddev.nc")
+                           f"{APREFIX}ice.bkgerr_stddev.nc")
         diagb_list.append([src, dst])
 
         FileHandler({'copy': diagb_list}).sync()
@@ -258,18 +265,24 @@ class MarineBMat(Task):
         # Copy the ensemble perturbation diagnostics to the ROTDIR
         if self.task_config.DOHYBVAR_OCN == "YES" or self.task_config.NMEM_ENS >= 2:
             window_middle_iso = self.task_config.MARINE_WINDOW_MIDDLE.strftime('%Y-%m-%dT%H:%M:%SZ')
+            window_begin_iso = self.task_config.MARINE_WINDOW_BEGIN.strftime('%Y-%m-%dT%H:%M:%SZ')
             weight_list = []
             src = os.path.join(self.task_config.DATA, f"ocn.ens_weights.incr.{window_middle_iso}.nc")
             dst = os.path.join(self.task_config.COMOUT_OCEAN_BMATRIX,
-                               f"{self.task_config.APREFIX}ocean.ens_weights.nc")
+                               f"{APREFIX}ocean.ens_weights.nc")
             weight_list.append([src, dst])
 
             src = os.path.join(self.task_config.DATA, f"ice.ens_weights.incr.{window_middle_iso}.nc")
             dst = os.path.join(self.task_config.COMOUT_ICE_BMATRIX,
-                               f"{self.task_config.APREFIX}ice.ens_weights.nc")
+                               f"{APREFIX}ice.ens_weights.nc")
             weight_list.append([src, dst])
 
             # TODO(G): missing ssh_steric_stddev, ssh_unbal_stddev, ssh_total_stddev and steric_explained_variance
+                    # Copy of the ssh diagnostics
+            for string in ['ssh_steric_stddev', 'ssh_unbal_stddev', 'ssh_total_stddev', 'steric_explained_variance']:
+                weight_list.append([os.path.join(self.task_config.DATA, 'staticb', f'ocn.{string}.incr.{window_begin_iso}.nc'),
+                                       os.path.join(self.task_config.COMOUT_OCEAN_BMATRIX, f'{APREFIX}ocean.{string}.nc')])
+
 
             FileHandler({'copy': weight_list}).sync()
 
@@ -278,6 +291,6 @@ class MarineBMat(Task):
         yaml_list = []
         for yaml_file in yamls:
             dest = os.path.join(self.task_config.COMOUT_OCEAN_BMATRIX,
-                                f"{self.task_config.APREFIX}{os.path.basename(yaml_file)}")
+                                f"{APREFIX}{os.path.basename(yaml_file)}")
             yaml_list.append([yaml_file, dest])
         FileHandler({'copy': yaml_list}).sync()
