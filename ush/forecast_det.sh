288d464fd8a805c423b23ca4c4caf2adfc4c48ba
AntonMFernando <anton.fernando@noaa.gov>
2025-04-04 14:45:01 -0400

    modify ush\forecast_det.sh

--- ush/forecast_det.sh
+++ ush/forecast_det.sh
@@ -16,9 +16,14 @@ UFS_det(){
     # Since restarts are not available from the previous cycle, this is likely a cold start
     # Ensure cold start ICs are present when warm start is not set
     # TODO: add checks for other cold start ICs as well
+    if [[ -f "${COMIN_ATMOS_INPUT}/gfs_ctrl.nc" ]]; then
+      echo "File is there and confusing if the job fails ${COMIN_ATMOS_INPUT}/gfs_ctrl.nc"
+      exit 1
+    fi
+
     if [[ ! -f "${COMIN_ATMOS_INPUT}/gfs_ctrl.nc" ]]; then
       echo "FATAL ERROR: Cold start ICs are missing from '${COMIN_ATMOS_INPUT}'"
-      #exit 1
+      exit 1
     fi
 
     # Since warm start is false, we cannot do IAU
