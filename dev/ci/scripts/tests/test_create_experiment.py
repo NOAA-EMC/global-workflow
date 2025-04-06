2077917ac0607848fa35dbd6f30a97b07a5f73ea
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-04-06 11:21:00 -0400

    fix ci_unit_tests

--- dev/ci/scripts/tests/test_create_experiment.py
+++ dev/ci/scripts/tests/test_create_experiment.py
@@ -1,29 +1,31 @@
-from wxflow import Executable
-from shutil import rmtree
 import os
-import copy
+from shutil import rmtree
+
+from wxflow import Executable
 
 _here = os.path.dirname(__file__)
 HOMEgfs = os.sep.join(_here.split(os.sep)[:-3])
-RUNDIR = os.path.join(_here, 'testdata/RUNDIR')
+RUNDIR_FAKE = os.path.join(_here, 'testdata/RUNDIR')
+ICSDIR_FAKE = os.path.join(_here, 'testdata/ICSDIR')
 
 
 def test_create_experiment():
 
-    create_experiment_script = Executable(f'{HOMEgfs}/dev/workflow/create_experiment.py')
+    create_experiment = Executable(f'{HOMEgfs}/dev/workflow/create_experiment.py')
+    create_experiment.add_default_arg(['--overwrite'])
     yaml_dir = yaml_dir = os.path.join(HOMEgfs, 'dev/ci/cases/pr')
     env = os.environ.copy()
-    env['RUNTESTS'] = RUNDIR
+    env['RUNTESTS'] = RUNDIR_FAKE
+    env['ICSDIR_ROOT'] = ICSDIR_FAKE
 
+    err = 0
     for case in os.listdir(yaml_dir):
         if case.endswith('.yaml'):
-            with open(os.path.join(yaml_dir, case), 'r') as file:
-                file_contents = file.read()
-                if 'ICSDIR_ROOT' not in file_contents:
-                    create_experiment = copy.deepcopy(create_experiment_script)
-                    create_experiment.add_default_arg(['-y', f'../../cases/pr/{case}', '--overwrite'])
-                    env['pslot'] = os.path.splitext(case)[0]
-                    create_experiment(env=env)
-                    assert (create_experiment.returncode == 0)
+            env['pslot'] = os.path.splitext(case)[0]
+            create_experiment(['-y', f'{HOMEgfs}/dev/ci/cases/pr/{case}'], env=env)
+            if create_experiment.returncode:
+                print(f"FATAL ERROR: Failed to create experiment for {case}")
+                err = 1
+    assert err == 0, f"create_experiment.py failed!"
 
-    rmtree(RUNDIR)
+    rmtree(RUNDIR_FAKE)
