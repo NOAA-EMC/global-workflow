a8ce8a215aa16ade16b27d1052b8c43ef2015f72
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2025-04-11 18:40:48 +0000

    even more merge issues

--- scripts/exglobal_archive_tars.py
+++ scripts/exglobal_archive_tars.py
@@ -52,17 +52,9 @@ def main():
 
     archive_dict = AttrDict()
     for key in keys:
-<<<<<<< HEAD
-        try:
-            archive_dict[key] = archive.task_config[key]
-        except KeyError:
-            logger.warning(
-                f"WARNING: key ({key}) not found in archive.task_config!")
-=======
         archive_dict[key] = archive.task_config.get(key)
         if archive_dict[key] is None:
             logger.warning(f"WARNING: key ({key}) not found in task_config!")
->>>>>>> upstream/develop
 
     # Also import all COMIN* and COMOUT* directory and template variables
     for key in archive.task_config.keys():
