b20a4c503a801928f190ad1661f471c7ff7cc7b9
David Huber <david.huber@noaa.gov>
2025-04-25 16:28:54 +0000

    Use build.sh instead of compile.sh for wcoss2 UFS builds (avoid parallel restart reads)

--- sorc/build_ufs.sh
+++ sorc/build_ufs.sh
@@ -57,7 +57,38 @@ esac
 CLEAN_BEFORE=YES
 CLEAN_AFTER=NO
 
-BUILD_JOBS=${BUILD_JOBS:-8} ./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_ID}" "intel" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
+# The test/compile.sh script adds " -DENABLE_PARALLELRESTART=ON" when compiling on WCOSS2, which is causing issues
+# TODO: when ufs-weather-model#2716 is fixed, return to using tests/compile.sh
+if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
+   set +x
+   module use modulefiles
+   module load "ufs_wcoss2.intel"
+   module list
+   set -x
+
+   if [[ ${MAKE_OPT} == *-DDEBUG=ON* ]]; then
+      MAKE_OPT+=" -DCMAKE_BUILD_TYPE=Debug"
+   else
+      MAKE_OPT+=" -DCMAKE_BUILD_TYPE=Release"
+   fi
+
+   MAKE_OPT+=" -DMPI=ON"
+
+   BUILD_NAME="fv3_${COMPILE_ID}"
+   BUILD_DIR="$(pwd)/build_${BUILD_NAME}"
+   if [[ "${CLEAN_BEFORE}" == "YES" ]]; then
+      rm -rf "${BUILD_DIR}"
+   fi
+
+   BUILD_DIR=${BUILD_DIR} BUILD_VERBOSE=1 BUILD_JOBS=${BUILD_JOBS:-8} CMAKE_FLAGS="${MAKE_OPT}" ./build.sh
+
+   mv "${BUILD_DIR}/ufs_model" "tests/${BUILD_NAME}.exe"
+   if [[ "${CLEAN_AFTER}" == "YES" ]]; then
+      rm -rf "${BUILD_DIR}"
+   fi
+else
+   BUILD_JOBS=${BUILD_JOBS:-8} ./tests/compile.sh "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_ID}" "intel" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
+fi
 mv "./tests/fv3_${COMPILE_ID}.exe" "./tests/${EXEC_NAME}"
 if [[ ! -f "./tests/modules.ufs_model.lua" ]]; then mv "./tests/modules.fv3_${COMPILE_ID}.lua" "./tests/modules.ufs_model.lua"; fi
 if [[ ! -f "./tests/ufs_common.lua" ]]; then cp "./modulefiles/ufs_common.lua" ./tests/ufs_common.lua; fi
