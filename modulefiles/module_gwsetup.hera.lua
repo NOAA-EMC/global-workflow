8a67325cfcc11c29d067ac5928bc7135e030b9af
Terry McGUinness <terrene.mcguinness@gmail.com>
2025-05-03 19:37:27 -0400

    added review updates: cmake in gw-setup, remove debug cmake exe, renamed CMAKE to CMAKE_NAME, ect

--- modulefiles/module_gwsetup.hera.lua
+++ modulefiles/module_gwsetup.hera.lua
@@ -8,6 +8,7 @@ prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-s
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 local python_ver=os.getenv("python_ver") or "3.11.6"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("python", python_ver))
