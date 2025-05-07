e26f809dc8e4872ad2a71209fa0e642b5992fe27
kate.friedman <kate.friedman@noaa.gov>
2025-05-07 13:19:03 -0500

    Shellcheck updates to ush/getdump.sh

--- ush/getdump.sh
+++ ush/getdump.sh
@@ -4,35 +4,34 @@ COMPONENT=${COMPONENT:-atmos}
 
 CDATE=${1:-""}
 RUN=${2:-""}
-SOURCE_DIR=${3:-$DMPDIR/${RUN}${DUMP_SUFFIX}.${PDY}/${cyc}/${COMPONENT}}
-TARGET_DIR=${4:-$ROTDIR/${RUN}.${PDY}/${cyc}/${COMPONENT}}
+SOURCE_DIR=${3:-${DMPDIR}/${RUN}${DUMP_SUFFIX}.${PDY}/${cyc}/${COMPONENT}}
+TARGET_DIR=${4:-${ROTDIR}/${RUN}.${PDY}/${cyc}/${COMPONENT}}
 
 DUMP_SUFFIX=${DUMP_SUFFIX:-""}
 
 # Exit if SORUCE_DIR does not exist
-if [ ! -s $SOURCE_DIR ]; then 
-   echo "***ERROR*** DUMP SOURCE_DIR=$SOURCE_DIR does not exist"
+if [ ! -s "${SOURCE_DIR}" ]; then
+   echo "***ERROR*** DUMP SOURCE_DIR=${SOURCE_DIR} does not exist"
    exit 99
 fi
    
-
 # Create TARGET_DIR if is does not exist
-if [ ! -s $TARGET_DIR ]; then mkdir -p $TARGET_DIR ;fi
-
+if [ ! -s "${TARGET_DIR}" ]; then
+   mkdir -p "${TARGET_DIR}"
+fi
 
 # Set file prefix
-cyc=$(echo $CDATE |cut -c 9-10)
-prefix="$RUN.t${cyc}z."
-
+cyc=$(echo "${CDATE}" |cut -c 9-10)
+prefix="${RUN}.t${cyc}z."
 
 # Link dump files from SOURCE_DIR to TARGET_DIR
-cd $SOURCE_DIR
-if [ -s ${prefix}updated.status.tm00.bufr_d ]; then
+cd "${SOURCE_DIR}"
+if [ -s "${prefix}updated.status.tm00.bufr_d" ]; then
     for file in $(ls ${prefix}*); do
-	${NLN} $SOURCE_DIR/$file $TARGET_DIR/$file
+	${NLN} "${SOURCE_DIR}/${file}" "${TARGET_DIR}/${file}"
     done
 else
-    echo "***ERROR*** ${prefix}updated.status.tm00.bufr_d NOT FOUND in $SOURCE_DIR"
+    echo "***ERROR*** ${prefix}updated.status.tm00.bufr_d NOT FOUND in ${SOURCE_DIR}"
     exit 99
 fi
 
