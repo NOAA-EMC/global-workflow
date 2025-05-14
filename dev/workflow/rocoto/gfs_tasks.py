5d5abcec6b439f7ddd7bf5a8b8643d020ed27a4c
kate.friedman <kate.friedman@noaa.gov>
2025-05-14 15:33:21 +0000

    Fix pynorms warning
    Refs #3638

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -91,7 +91,7 @@ class GFSTasks(Tasks):
                      'job_name': f'{self.pslot}_{task_name}_@H',
                      'log': f'{self.rotdir}/logs/@Y@m@d@H/{task_name}.log',
                      'maxtries': '&MAXTRIES;'
-                    }
+                     }
 
         task = rocoto.create_task(task_dict)
 
