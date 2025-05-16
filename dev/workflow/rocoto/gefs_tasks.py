e2c45907a73203da510a496f3ca30dcffd50d843
AntonMFernando <anton.fernando@noaa.gov>
2025-05-16 16:10:38 -0400

    modified dev\workflow\rocoto\gefs_tasks.py

--- dev/workflow/rocoto/gefs_tasks.py
+++ dev/workflow/rocoto/gefs_tasks.py
@@ -76,7 +76,7 @@ class GEFSTasks(Tasks):
 
     def fcst(self):
         dependencies = []
-        dep_dict = {'type': 'task', 'name': f'{self.run}_stage_ic'}
+        dep_dict = {'type': 'task', 'name': f'{self.run}_stage_ic_mem#member#'}
         dependencies.append(rocoto.add_dependency(dep_dict))
 
         if self.options['do_wave']:
@@ -122,7 +122,7 @@ class GEFSTasks(Tasks):
 
     def efcs(self):
         dependencies = []
-        dep_dict = {'type': 'task', 'name': f'{self.run}_stage_ic'}
+        dep_dict = {'type': 'task', 'name': f'{self.run}_stage_ic_mem#member#'}
         dependencies.append(rocoto.add_dependency(dep_dict))
 
         if self.options['do_wave']:
