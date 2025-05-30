86c6acb79ce67dc4ddaa47ce5827005dbae43eb1
AntonMFernando <anton.fernando@noaa.gov>
2025-05-30 00:21:03 -0400

    modified dev\workflow\rocoto\gefs_tasks.py

--- dev/workflow/rocoto/gefs_tasks.py
+++ dev/workflow/rocoto/gefs_tasks.py
@@ -11,7 +11,7 @@ class GEFSTasks(Tasks):
     def stage_ic(self):
 
         stage_ic_map = {'general': self._gen_stage_ic,
-                    'real_time': self._RT_stage_ic}
+                        'real_time': self._RT_stage_ic}
         print(f'GEFSTasks: stage_ic: {self.app_config.type}')
 
         try:
@@ -140,13 +140,15 @@ class GEFSTasks(Tasks):
                      'command': f'{self.HOMEgfs}/dev/jobs/fcst.sh',
                      'job_name': f'{self.pslot}_{task_name}_@H',
                      'log': f'{self.rotdir}/logs/@Y@m@d@H/{task_name}.log',
-                     'maxtries': '&MAXTRIES;'}
+                     'maxtries': '&MAXTRIES;'
+                     }
 
         seg_var_dict = {'seg': ' '.join([f"{seg}" for seg in range(0, num_fcst_segments)])}
         metatask_dict = {'task_name': f'{self.run}_fcst_mem000',
                          'is_serial': True,
                          'var_dict': seg_var_dict,
-                         'task_dict': task_dict}
+                         'task_dict': task_dict
+                         }
 
         task = rocoto.create_task(metatask_dict)
 
