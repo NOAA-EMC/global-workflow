1700e8665f199c4fa33e0910fecfc95c60899664
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-06-02 09:43:51 -0400

    Update ush/create_satinfo.sh

--- ush/create_satinfo.sh
+++ ush/create_satinfo.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 # create global_satinfo file for a given date
-date=$1
+date_in=$1
 # header
 echo '!sensor/instr/sat      chan iuse  error  error_cld  ermax   var_b    var_pg  icld_det icloud iaerosol'
 # loop over satellites
