fba7f00e54725d6f1ea8fa1f9c034445abd1efc4
DavidNew-NOAA <david.new@noaa.gov>
2025-06-04 14:57:56 +0000

    Pass HOMEgfs to build_gdas.sh

--- sorc/build_gdas.sh
+++ sorc/build_gdas.sh
@@ -3,11 +3,12 @@ set -eux
 
 OPTIND=1
 _opts="-f "  # forces a clean build
-while getopts ":j:dv" option; do
+while getopts ":j:dvw:" option; do
   case "${option}" in
     d) _opts+="-c -DCMAKE_BUILD_TYPE=Debug " ;;
     j) BUILD_JOBS=${OPTARG};;
     v) _opts+="-v ";;
+    w) HOMEgfs=${OPTARG};;
     :)
       echo "[${BASH_SOURCE[0]}]: ${option} requires an argument"
       usage
@@ -20,11 +21,17 @@ while getopts ":j:dv" option; do
 done
 shift $((OPTIND-1))
 
+if [[ -z "${HOMEgfs:-}" ]]; then
+  echo "Error: HOMEgfs is a required argument" >&2
+  exit 1
+fi
+
 # double quoting opts will not work since it is a string of options
 # shellcheck disable=SC2086
 BUILD_JOBS="${BUILD_JOBS:-8}" \
 WORKFLOW_BUILD="${WORKFLOW_BUILD:-"ON"}" \
 WORKFLOW_TESTS="${WORKFLOW_TESTS:-"OFF"}" \
+HOMEgfs="${HOMEgfs}" \
 ./gdas.cd/build.sh ${_opts} -f
 
 exit
