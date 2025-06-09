794ed1246409dcbd84371a77fc8197478cc14821
AntonMFernando <anton.fernando@noaa.gov>
2025-06-09 10:50:14 -0400

    modified dev\jobs\gen_control_ic.sh

--- dev/jobs/gen_control_ic.sh
+++ dev/jobs/gen_control_ic.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/env bash
-echo "gen_control_ic test passed."
-exit 0
+source "${HOMEgfs}/ush/load_fv3gfs_modules.sh"
+status=$?
+if [[ ${status} -ne 0 ]]; then
+    exit "${status}"
+fi
+
+export job="gen_control"
+export jobid="${job}.$$"
+
+###############################################################
+# Execute the JJOB
+# "${HOMEgfs}/jobs/GEN_CONTROL"
+# status=$?
+
+exit "${status}"
