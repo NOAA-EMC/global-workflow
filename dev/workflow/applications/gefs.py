617efcdcf666e347e2ac419d0c79b918e5525a01
AntonMFernando <anton.fernando@noaa.gov>
2025-06-10 16:11:01 -0400

    add awips job

--- dev/workflow/applications/gefs.py
+++ dev/workflow/applications/gefs.py
@@ -94,6 +94,9 @@ class GEFSAppConfig(AppConfig):
         if options['do_aero_fcst']:
             configs += ['prep_emissions']
 
+        if options['do_awips']:
+            configs += ['awips']
+
         if options['do_extractvars']:
             configs += ['extractvars']
 
@@ -162,6 +165,9 @@ class GEFSAppConfig(AppConfig):
 
         if options['nens'] > 0:
             tasks += ['atmos_ensstat']
+
+        if options['do_awips']:
+            tasks += ['awips']
 
         if options['do_ocean']:
             tasks += ['ocean_prod']
