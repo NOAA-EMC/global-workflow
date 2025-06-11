7601bf086a344b70bf1834ee7281ab6ed2fbb383
DavidNew-NOAA <david.new@noaa.gov>
2025-06-11 17:54:14 +0000

    Initial commit

---
+++ scripts/exgdas_global_ocean_analysis_ecen.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# exgdas_global_marine_analysis_ecen.py
+# This script creates an MarineRecenter class
+# and runs the initialize, execute, and finalize  methods
+import os
+
+from wxflow import Logger, cast_strdict_as_dtypedict
+from pygfs.task.marine_recenter import MarineRecenter
+
+# Initialize root logger
+logger = Logger(level='DEBUG', colored_log=True)
+
+
+if __name__ == '__main__':
+
+    # Take configuration from environment and cast it as python dictionary
+    config = cast_strdict_as_dtypedict(os.environ)
+
+    # Instantiate the aerosol analysis task
+    MarineRecen = MarineRecenter(config)
+    MarineRecen.initialize()
+    MarineRecen.execute('gridgen')
+    MarineRecen.execute('gdas_ens_handler')
+    MarineRecen.run()
+    MarineRecen.finalize()
