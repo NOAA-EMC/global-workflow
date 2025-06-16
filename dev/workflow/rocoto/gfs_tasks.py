5f614fb78daa9f9fad79630707c9e1c74b642e95
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2025-06-16 13:07:43 +0000

    correcting chkpt dep

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -812,15 +812,9 @@ class GFSTasks(Tasks):
 
     def ocnanalecen(self):
 
-        # deps = []
-        # dep_dict = {'type': 'task', 'name': f'{self.run}_marineanlvar'}
-        # deps.append(rocoto.add_dependency(dep_dict))
-        # dependencies = rocoto.create_dependency(dep=deps)
-
-
         ocean_hist_path = self._template_to_rocoto_cycstring(self._base["COM_OCEAN_HISTORY_TMPL"], {'RUN': 'gdas'})
 
-        # can run in parallel with marinebmat 
+        # can run in parallel with marinebmat
         deps = []
         data = f'{ocean_hist_path}/gdas.ocean.t@Hz.inst.f009.nc'
         dep_dict = {'type': 'data', 'data': data, 'offset': f"-{timedelta_to_HMS(self._base['interval_gdas'])}"}
@@ -849,11 +843,11 @@ class GFSTasks(Tasks):
     def marineanlchkpt(self):
 
         deps = []
+        dep_dict = {'type': 'task', 'name': f'{self.run}_marineanlvar'}
+        deps.append(rocoto.add_dependency(dep_dict))
         if self.options['do_hybvar_ocn']:
             dep_dict = {'type': 'task', 'name': f'{self.run}_ocnanalecen'}
-        else:
-            dep_dict = {'type': 'task', 'name': f'{self.run}_marineanlvar'}
-        deps.append(rocoto.add_dependency(dep_dict))
+            deps.append(rocoto.add_dependency(dep_dict))
         if self.options['do_mergensst']:
             data = f'&ROTDIR;/{self.run}.@Y@m@d/@H/atmos/{self.run}.t@Hz.sfcanl.nc'
             dep_dict = {'type': 'data', 'data': data}
@@ -882,7 +876,7 @@ class GFSTasks(Tasks):
         deps = []
         dep_dict = {'type': 'task', 'name': f'{self.run}_marineanlchkpt'}
         deps.append(rocoto.add_dependency(dep_dict))
-        dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
+        dependencies = rocoto.create_dependency(dep=deps)
 
         resources = self.get_resource('marineanlfinal')
         task_name = f'{self.run}_marineanlfinal'
