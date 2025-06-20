bfa52b4c448d961f9944df087a5734b06aa41da8
David Huber <david.huber@noaa.gov>
2025-06-20 10:06:01 -0500

    Convert hercules module to common loading principle; upgrade versions to 1.9.1

--- ush/load_gw_run_modules.sh
+++ ush/load_gw_run_modules.sh
@@ -15,19 +15,48 @@ fi
 # Setup runtime environment by loading modules
 ulimit_s=$( ulimit -S -s )
 
+# Test if HOMEgfs is defined.  If not, then try to determine it with git rev-parse
+_unset_homegfs="NO"
+if [[ -z ${HOMEgfs+x} ]]; then
+    echo "INFO HOMEgfs is not defined.  Attempting to find the global-workflow root directory"
+    # HOMEgfs will be removed from the environment at the end of this script
+    _unset_homegfs="YES"
+
+    script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    export HOMEgfs=$(cd "${script_dir}" && git rev-parse --show-toplevel)
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+        is_git_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-parse --is-inside-work-tree)
+        git_stat=$?
+        if [[ ${git_stat} -ne 0 ]]; then
+            echo "FATAL ERROR unable to determine the root because it is not a git repository."
+        else
+            echo "FATAL ERROR unable to determine the root because git rev-parse --show-toplevel failed for an unknown reason"
+        fi
+        echo "            Unable to load modules.  Exiting"
+        exit 1
+    fi
+fi
+
 # Find module command and purge:
 source "${HOMEgfs}/ush/detect_machine.sh"
 source "${HOMEgfs}/ush/module-setup.sh"
 
 # Source versions file for runtime
-source "${HOMEgfs}/versions/run.ver"
+if [[ -f "${HOMEgfs}/versions/run.ver" ]]; then
+    source "${HOMEgfs}/versions/run.ver"
+else
+    echo "FATAL ERROR ${HOMEgfs}/versions/run.ver does not exist!"
+    echo "HINT: Run link_workflow.sh first."
+    exit 1
+fi
 
 # Load our modules:
 module use "${HOMEgfs}/modulefiles"
 
 case "${MACHINE_ID}" in
   "wcoss2" | "hera" | "orion" | "hercules" | "gaeac5" | "gaeac6" | "noaacloud")
-    module load "module_base.${MACHINE_ID}"
+    module load "gw_run.${MACHINE_ID}"
     ;;
   *)
     echo "WARNING: UNKNOWN PLATFORM"
@@ -52,3 +81,8 @@ export PYTHONPATH
 # Restore stack soft limit:
 ulimit -S -s "${ulimit_s}"
 unset ulimit_s
+
+# Unset HOMEgfs if it was not set at the beginning of this script
+if [[ ${_unset_homegfs} == "YES" ]]; then
+    unset HOMEgfs
+fi
