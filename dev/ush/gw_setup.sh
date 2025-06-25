ba793fde8ad9d6911e520bd3fd78fc98307ebe8f
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-06-25 12:40:05 -0400

    cache apt packages for faster ci. remove wxflow symlinks

--- dev/ush/gw_setup.sh
+++ dev/ush/gw_setup.sh
@@ -8,8 +8,16 @@
 # This script should be SOURCED to properly setup the environment.
 #
 
-HOMEgfs="$(cd "$(dirname  "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd )"
+HOMEgfs="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)"
 source "${HOMEgfs}/ush/detect_machine.sh"
 source "${HOMEgfs}/ush/module-setup.sh"
 module use "${HOMEgfs}/modulefiles"
 module load "module_gwsetup.${MACHINE_ID}"
+
+# Set up the PYTHONPATH for wxflow if it is not already loaded
+if ! python3 -c "import wxflow" 2>/dev/null; then
+  if [[ -d "${HOMEgfs}/sorc/wxflow/src/wxflow" ]]; then
+    PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/sorc/wxflow/src/wxflow"
+    export PYTHONPATH
+  fi
+fi
