ee3a487edff32bcfc71e74f355a1be332080b3b6
NeilBarton-NOAA <neil.barton@noaa.gov>
2025-06-26 10:13:31 -0400

    change to NST to not need to spin up on restart

--- ush/forecast_det.sh
+++ ush/forecast_det.sh
@@ -89,6 +89,9 @@ UFS_det(){
       MOM6_WARMSTART_FILE="none"
       MOM6_INIT_UV="zero"
       ODA_INCUPD="False"
+      #nspinup=0
+      NST_SPINUP=0
+      nstf_name="${NST_MODEL},${NST_SPINUP},${NST_RESV},${ZSEA1},${ZSEA2}"
     fi
 
     # Check for CICE6 restart availability
