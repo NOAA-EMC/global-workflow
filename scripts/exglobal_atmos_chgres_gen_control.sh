c34aadf9a906fbfd9bf3c30c01391fb0b1ead5e4
AntonMFernando <anton.fernando@noaa.gov>
2025-07-08 20:48:49 -0400

    update scripts\exglobal_atmos_chgres_gen_control.sh

--- scripts/exglobal_atmos_chgres_gen_control.sh
+++ scripts/exglobal_atmos_chgres_gen_control.sh
@@ -41,14 +41,14 @@ cp -f "/lfs/h2/emc/global/noscrub/anton.fernando/UFS_UTILS/fix/am/global_hyblev.
 cat << EOF > ./fort.41
 &config
 mosaic_file_target_grid=$DATA
-fix_dir_target_grid="/lfs/h2/emc/nems/noscrub/emc.nems/UFS_UTILS/reg_tests/chgres_cube/fix/C96/sfc"
-orog_dir_target_grid=$DATA/
+fix_dir_target_grid=$DATA
+orog_dir_target_grid=$DATA
 orog_files_target_grid="C96.mx100_oro_data.tile1.nc","C96.mx100_oro_data.tile2.nc","C96.mx100_oro_data.tile3.nc","C96.mx100_oro_data.tile4.nc","C96.mx100_oro_data.tile5.nc","C96.mx100_oro_data.tile6.nc"
-vcoord_file_target_grid=$DATA/
+vcoord_file_target_grid=$DATA
 mosaic_file_input_grid="NULL"
 orog_dir_input_grid="NULL"
 orog_files_input_grid="NULL"
-data_dir_input_grid=$DATA/
+data_dir_input_grid=$DATA
 atm_files_input_grid=$ATMF03
 atm_core_files_input_grid="NULL"
 atm_tracer_files_input_grid="NULL"
