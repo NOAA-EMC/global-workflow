85cf8595a0763fbf141faf77a070a801cd3f104e
AntonMFernando <anton.fernando@noaa.gov>
2025-07-10 12:50:48 -0400

    modify scripts\exglobal_atmos_chgres_gen_control.sh

--- scripts/exglobal_atmos_chgres_gen_control.sh
+++ scripts/exglobal_atmos_chgres_gen_control.sh
@@ -15,35 +15,35 @@ PGMOUT=${PGMOUT:-${pgmout:-'&1'}}
 PGMERR=${PGMERR:-${pgmerr:-'&2'}}
 REDOUT=${REDOUT:-'1>'}
 REDERR=${REDERR:-'2>'}
-DATA="${DATA:-$pwd}"
+DATA=${DATA:-${pwd}}
 
 export OMP_NUM_THREADS=${OMP_NUM_THREADS_CH:-1}
-export APRUN_CHGRES="mpiexec -l -n ${ntasks} -ppn ${tasks_per_node} --cpu-bind depth --depth 1"
+export APRUN_CHGRES=${APRUN_CHGRES:-"mpiexec -l -n ${ntasks} -ppn ${tasks_per_node} --cpu-bind depth --depth 1"}
 ##############################################################
-DESTINATION_DIR="$DATA/gen_control_ic"
-SFC_DESTINATION_DIR="$DESTINATION_DIR/sfc"
-MOSAIC_DESTINATION_FILE="$DESTINATION_DIR/C96_mosaic.nc"
+DESTINATION_DIR="${DATA}/gen_control_ic"
+SFC_DESTINATION_DIR="${DESTINATION_DIR}/sfc"
+MOSAIC_DESTINATION_FILE="${DESTINATION_DIR}/C96_mosaic.nc"
 ATM_FILE="gfs.t00z.atmf000.nc"
 SFC_FILE="gfs.t00z.sfcf000.nc"
-HYBLEV_FILE="$DESTINATION_DIR/global_hyblev.l64.txt"
+HYBLEV_FILE="${DESTINATION_DIR}/global_hyblev.l64.txt"
 SOURCE_DIR="/lfs/h2/emc/nems/noscrub/emc.nems/UFS_UTILS/reg_tests/chgres_cube/fix/C96"
 
 # Remove existing $DATA/gen_control_ic
-rm -rf "$DESTINATION_DIR"
+rm -rf "${DESTINATION_DIR}"
 
 # Create directory $DATA/gen_control_ic
-echo "Creating directory: $DESTINATION_DIR"
-mkdir -p "$DESTINATION_DIR"
+echo "Creating directory: ${DESTINATION_DIR}"
+mkdir -p "${DESTINATION_DIR}"
 
 # Check if the directory creation failed
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to create directory $DESTINATION_DIR"
+if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to create directory ${DESTINATION_DIR}"
     exit 1
 fi
 
 # Ensure the source directory exists
-if [ ! -d "$SOURCE_DIR" ]; then
-    echo "Error: Source directory $SOURCE_DIR does not exist."
+if [[ ! -d "${SOURCE_DIR}" ]]; then
+    echo "Error: Source directory ${SOURCE_DIR} does not exist."
     exit 1
 fi
 
@@ -51,45 +51,45 @@ fi
 copy_file() {
     local src=$1
     local dest=$2
-    echo "Copying $src to $dest"
-    cp -rf "$src" "$dest"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to copy $src to $dest"
+    echo "Copying ${src} to ${dest}"
+    cp -rf "${src}" "${dest}"
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to copy ${src} to ${dest}"
         exit 1
     fi
 }
 
 # Copy all contents (including subdirectories) to $DESTINATION_DIR
-echo "Copying all contents from $SOURCE_DIR to $DESTINATION_DIR..."
-for item in "$SOURCE_DIR"/*; do
-    copy_file "$item" "$DESTINATION_DIR"
-    chmod -R u+w "$DESTINATION_DIR/$(basename "$item")"
+echo "Copying all contents from ${SOURCE_DIR} to ${DESTINATION_DIR}..."
+for item in "${SOURCE_DIR}"/*; do
+    copy_file "${item}" "${DESTINATION_DIR}"
+    chmod -R u+w "${DESTINATION_DIR}/$(basename "${item}")"
 done
 
-echo "All contents from $SOURCE_DIR copied successfully to $DESTINATION_DIR."
+echo "All contents from ${SOURCE_DIR} copied successfully to ${DESTINATION_DIR}."
 
-copy_file "$UTILSufs/reg_tests/chgres_cube/input_data/fv3.netcdf/gfs.t00z.atmf000.nc" "$DESTINATION_DIR"
-copy_file "$UTILSufs/reg_tests/chgres_cube/input_data/fv3.netcdf/gfs.t00z.sfcf000.nc" "$DESTINATION_DIR"
-copy_file "$UTILSufs/UFS_UTILS.git/fix/am/global_hyblev.l64.txt" "$DESTINATION_DIR"
+copy_file "${UTILSufs}/reg_tests/chgres_cube/input_data/fv3.netcdf/gfs.t00z.atmf000.nc" "${DESTINATION_DIR}"
+copy_file "${UTILSufs}/reg_tests/chgres_cube/input_data/fv3.netcdf/gfs.t00z.sfcf000.nc" "${DESTINATION_DIR}"
+copy_file "${UTILSufs}/UFS_UTILS.git/fix/am/global_hyblev.l64.txt" "${DESTINATION_DIR}"
 
 echo "All files copied successfully."
 
 # If analysis increment is written by GSI, regrid forecasts to increment resolution
 cat << EOF > ./fort.41
 &config
-mosaic_file_target_grid="$MOSAIC_DESTINATION_FILE"
-fix_dir_target_grid="$SFC_DESTINATION_DIR"
-orog_dir_target_grid="$DESTINATION_DIR"
+mosaic_file_target_grid="${MOSAIC_DESTINATION_FILE}"
+fix_dir_target_grid="${SFC_DESTINATION_DIR}"
+orog_dir_target_grid="${DESTINATION_DIR}"
 orog_files_target_grid="C96.mx100_oro_data.tile1.nc","C96.mx100_oro_data.tile2.nc","C96.mx100_oro_data.tile3.nc","C96.mx100_oro_data.tile4.nc","C96.mx100_oro_data.tile5.nc","C96.mx100_oro_data.tile6.nc"
-vcoord_file_target_grid="$HYBLEV_FILE"
+vcoord_file_target_grid="${HYBLEV_FILE}"
 mosaic_file_input_grid="NULL"
 orog_dir_input_grid="NULL"
 orog_files_input_grid="NULL"
-data_dir_input_grid="$DESTINATION_DIR"
-atm_files_input_grid=$ATM_FILE
+data_dir_input_grid="${DESTINATION_DIR}"
+atm_files_input_grid="${ATM_FILE}"
 atm_core_files_input_grid="NULL"
 atm_tracer_files_input_grid="NULL"
-sfc_files_input_grid=$SFC_FILE
+sfc_files_input_grid="${SFC_FILE}"
 nst_files_input_grid="NULL"
 grib2_file_input_grid="NULL"
 geogrid_file_input_grid="NULL"
@@ -121,5 +121,5 @@ wam_cold_start=.false.
 /
 EOF
 
-"$APRUN_CHGRES" "$CHGRESEXEC" "$REDOUT$PGMOUT" "$REDERR$PGMERR"
-exit "$err"
+${APRUN_CHGRES} ${CHGRESEXEC} ${REDOUT}${PGMOUT} ${REDERR}${PGMERR}
+exit "${err}"
