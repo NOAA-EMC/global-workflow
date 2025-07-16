1edc43839a7b6e03a986a8899eac9a5146043be9
Jessica Meixner <jessica.meixner@noaa.gov>
2025-07-16 18:48:43 +0000

    Update ush/forecast_postdet.sh
    Co-authored-by: David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -461,7 +461,7 @@ WW3_postdet() {
   # Loop for gridded output (uses FHINC)
   local fhr fhr3 vdate FHINC ww3_grid
   fhr=${FHMIN_WAV}
-  if (( FHMAX_HF_WAV > 0 && FHOUT_HF_WAV > 0 && fhr < FHMAX_HF_WAV )); then 
+  if [[ ${FHMAX_HF_WAV} -gt 0 && ${FHOUT_HF_WAV} -gt 0 && ${fhr} -lt ${FHMAX_HF_WAV} ]]; then
     fhinc=${FHOUT_HF_WAV}
   else
     fhinc=${FHOUT_WAV}
