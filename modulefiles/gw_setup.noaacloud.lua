2cd9ad8c54bfb9562e62d25594a6231d2daf799e
Taylor Roper <taylor.roper@noaa.gov>
2025-07-18 15:08:36 -0600

    Update cloud module and version files for spack-stack 1.9.2 (#29)
    * Update cloud module and version files to use spack-stack 1.9.2 (#3852 #2984)
    
    * Use gcc-13.2.0 for all build environments (#3852 #2984)
    
    * Remove gcc_ver and add comment for setting gnu_ver (#3852 #2984)

--- modulefiles/gw_setup.noaacloud.lua
+++ modulefiles/gw_setup.noaacloud.lua
@@ -2,17 +2,17 @@ help([[
 Load environment to run GFS workflow setup scripts on noaacloud
 ]])
 
-load(pathJoin("rocoto"))
+load("rocoto")
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/ue-intel/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-load("gnu")
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_mpi_ver=os.getenv("stack_mpi_ver") or "2021.10.0"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_mpi_ver=os.getenv("stack_mpi_ver") or "2021.13" 
+local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_mpi_ver))
-unload("gnu")
+load(pathJoin("cmake", cmake_ver))
 
 load("py-jinja2")
 load("py-pyyaml")
