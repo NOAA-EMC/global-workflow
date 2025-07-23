9c978a017b7e7cd18ae0e4db8ea87bf0e43ce664
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-07-23 13:33:47 -0400

    add checking of land orog sfc data consistency

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -74,9 +74,10 @@ FV3_postdet() {
           break
         fi
       done
+
       # If aerosol analysis is to be done, replace fv_tracer with aeroanl_fv_tracer
       # restart files from current cycle (if found)
-      if [[ ${DO_AERO_FCST} == "YES" ]]; then
+      if [[ "${DO_AERO_FCST}" == "YES" ]]; then
         local nn
         local use_anl_aero="YES"
         for (( nn = 1; nn <= ntiles; nn++ )); do
@@ -87,7 +88,7 @@ FV3_postdet() {
             break
           fi
         done
-        if [[ ${use_anl_aero} == "YES" ]]; then
+        if [[ "${use_anl_aero}" == "YES" ]]; then
           for (( nn = 1; nn <= ntiles; nn++ )); do
             rm -f "${DATA}/INPUT/fv_tracer.res.tile${nn}.nc"
             cpreq "${COMIN_TRACER_RESTART}/${restart_date:0:8}.${restart_date:8:2}0000.aeroanl_fv_tracer.res.tile${nn}.nc" \
@@ -100,6 +101,19 @@ FV3_postdet() {
     fi  # if [[ "${RERUN}" == "YES" ]]; then
 
   fi  # if [[ "${warm_start}" == ".true." ]]; then
+
+  # Regardless of warm_start or not, the sfc_data and orography files should be consistent
+  # Check for consistency
+  # TODO: the checker has a --fatal option, which is not used here.  This needs to be decided how to handle.
+  if [[ "${CHECK_LAND_RESTART_OROG:-NO}" == "YES" ]]; then
+    "${USHgfs}/check_land_input_orography.py" \
+      --input_dir "${DATA}/INPUT" --orog_dir "${DATA}/INPUT"
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+      echo "FATAL ERROR: check_land_input_orography.py returned error code ${err}, ABORT!"
+      exit ${err}
+    fi
+  fi
 
   #============================================================================
   # Determine increment files when doing cold start
