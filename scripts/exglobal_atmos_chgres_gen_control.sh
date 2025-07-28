97578a17b295480d3892505b4700acc7c0743161
AntonMFernando <anton.fernando@noaa.gov>
2025-07-28 15:54:11 -0400

    seperate atm and sfc

--- scripts/exglobal_atmos_chgres_gen_control.sh
+++ scripts/exglobal_atmos_chgres_gen_control.sh
@@ -21,6 +21,7 @@ id=$(echo "${CDATE}" | cut -c7-8)
 ih=$(echo "${CDATE}" | cut -c9-10)
 ################################################################################
 # Set up theinput and output directories
+
 DESTINATION_DIR="${DATA}"
 SOURCE_DIR="${HOMEgfs}/fix/orog/${CASE}"
 MOSAIC_DESTINATION_FILE="${DESTINATION_DIR}/${CASE}_mosaic.nc"
@@ -91,54 +92,26 @@ OROG_TARGET_FILES=$(for i in {1..6}; do
     fi
 done)
 ################################################################################
-# add the namelist and run chgres
-cat << EOF > ./fort.41
-&config
-mosaic_file_target_grid="${MOSAIC_DESTINATION_FILE}"
-fix_dir_target_grid="${DESTINATION_DIR}"
-orog_dir_target_grid="${DESTINATION_DIR}"
-orog_files_target_grid=${OROG_TARGET_FILES}
-vcoord_file_target_grid="${HYBLEV_FILE}"
-mosaic_file_input_grid="NULL"
-orog_dir_input_grid="NULL"
-orog_files_input_grid="NULL"
-data_dir_input_grid="${DESTINATION_DIR}"
-atm_files_input_grid="${ATM_FILE}"
-atm_core_files_input_grid="NULL"
-atm_tracer_files_input_grid="NULL"
-sfc_files_input_grid="${SFC_FILE}"
-nst_files_input_grid="NULL"
-grib2_file_input_grid="NULL"
-geogrid_file_input_grid="NULL"
-varmap_file="NULL"
-wam_parm_file="NULL"
-cycle_year=${iy}
-cycle_mon=${im}
-cycle_day=${id}
-cycle_hour=${ih}
-convert_atm=.true.
-convert_sfc=.true.
-convert_nst=.true.
-input_type="gaussian_netcdf"
-tracers="sphum","liq_wat","o3mr","ice_wat","rainwat","snowwat","graupel"
-tracers_input="spfh","clwmr","o3mr","icmr","rwmr","snmr","grle"
-regional=0
-halo_bndy=0
-halo_blend=0
-sotyp_from_climo=.true.
-vgtyp_from_climo=.true.
-vgfrc_from_climo=.true.
-minmax_vgfrc_from_climo=.true.
-tg3_from_soil=.false.
-lai_from_climo=true.
-external_model="GFS"
-nsoill_out=4
-thomp_mp_climo_file="NULL"
-wam_cold_start=.false.
-/
-EOF
-
-eval "${APRUN_CHGRES}" "${CHGRESEXEC}" "${PGMOUT}"
+# run the chgres script to change resolution of sfc file
+convert_atm=".false."
+convert_sfc=".true."
+output_log="./fort.41_1"
+"${HOMEgfs}/ush/gen_control_changres.sh"
+err=$?
+if [[ ${err} -ne 0 ]]; then
+  echo "ERROR: sfc chgres run failed"
+  exit ${err}
+fi
+# run the chgres script to change resolution of atm file
+convert_atm=".true."
+convert_sfc=".false."
+output_log="./fort.41_2"
+"${HOMEgfs}/ush/gen_control_changres.sh"
+err=$?
+if [[ ${err} -ne 0 ]]; then
+  echo "ERROR: atm chgres run failed"
+  exit ${err}
+fi
 ################################################################################
 # Ensure COMIN_ATMOS_INPUT_MEM exists, create if needed, then copy out.atm.tile{1..6}.nc (force overwrite)
 for i in {1..6}; do
---
+++ ush/gen_control_changres.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+################################################################################
+####  UNIX Script Documentation Block
+#                      .                                             .
+# Script name:         gen_chgres_namelist.sh
+# Script description:  Generates chgres_cube namelist and runs the executable
+#
+# Author:              Global Workflow Team
+# Date:                July 2025
+#
+# Usage:               gen_chgres_namelist.sh
+#
+# Script history log:
+# 2025-07-28  Initial script creation
+################################################################################
+
+set -eu
+
+# Generate the namelist for chgres
+cat << EOF > ${output_log:-"./fort.41"}
+&config
+mosaic_file_target_grid="${MOSAIC_DESTINATION_FILE:-"NULL"}"
+fix_dir_target_grid="${DESTINATION_DIR:-"NULL"}"
+orog_dir_target_grid="${DESTINATION_DIR:-"NULL"}"
+orog_files_target_grid=${OROG_TARGET_FILES:-"NULL"}
+vcoord_file_target_grid="${HYBLEV_FILE:-"NULL"}"
+mosaic_file_input_grid="${MOSAIC_FILE_INPUT_GRID:-"NULL"}"
+orog_dir_input_grid="${OROG_DIR_INPUT_GRID:-"NULL"}"
+orog_files_input_grid="${OROG_FILES_INPUT_GRID:-"NULL"}"
+data_dir_input_grid="${DESTINATION_DIR:-"NULL"}"
+atm_files_input_grid="${ATM_FILE:-"NULL"}"
+atm_core_files_input_grid="${ATM_CORE_FILES_INPUT_GRID:-"NULL"}"
+atm_tracer_files_input_grid="${ATM_TRACER_FILES_INPUT_GRID:-"NULL"}"
+sfc_files_input_grid="${SFC_FILE:-"NULL"}"
+nst_files_input_grid="${NST_FILES_INPUT_GRID:-"NULL"}"
+grib2_file_input_grid="${GRIB2_FILE_INPUT_GRID:-"NULL"}"
+geogrid_file_input_grid="${GEOGRID_FILE_INPUT_GRID:-"NULL"}"
+varmap_file="${VARMAP_FILE:-"NULL"}"
+wam_parm_file="${WAM_PARM_FILE:-"NULL"}"
+cycle_year=${iy:-"2020"}
+cycle_mon=${im:-"01"}
+cycle_day=${id:-"01"}
+cycle_hour=${ih:-"00"}
+convert_atm=${CONVERT_ATM:-".false."}
+convert_sfc=${CONVERT_SFC:-".true."}
+convert_nst=${CONVERT_NST:-".true."}
+input_type="${INPUT_TYPE:-"gaussian_netcdf"}"
+tracers="${TRACERS:-"spfh,clwmr,o3mr,icmr,rwmr,snmr,grle"}"
+tracers_input="${TRACERS_INPUT:-"spfh,clwmr,o3mr,icmr,rwmr,snmr,grle"}"
+regional=${REGIONAL:-"0"}
+halo_bndy=${HALO_BNDY:-"0"}
+halo_blend=${HALO_BLEND:-"0"}
+sotyp_from_climo=${SOTYP_FROM_CLIMO:-".true."}
+vgtyp_from_climo=${VGTYPE_FROM_CLIMO:-".true."}
+vgfrc_from_climo=${VGFRC_FROM_CLIMO:-".true."}
+minmax_vgfrc_from_climo=${MINMAX_VGFR_FROM_CLIMO:-".true."}
+tg3_from_soil=${TG3_FROM_SOIL:-".false."}
+lai_from_climo=${LAI_FROM_CLIMO:-".true."}
+external_model=${EXTERNAL_MODEL:-"GFS"}
+nsoill_out=${NSOILL_OUT:-"4"}
+thomp_mp_climo_file=${THOMP_MP_CLIMO_FILE:-"NULL"}
+wam_cold_start=${WAM_COLD_START:-".false."}
+/
+EOF
+
+# Run the chgres executable
+eval "${APRUN_CHGRES}" "${CHGRESEXEC}" "${PGMOUT}"
+err=$?
+
+exit "${err}"
