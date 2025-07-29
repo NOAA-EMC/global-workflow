76d5b8621968435d3e03ff57cc2727342f685147
David Huber <david.huber@noaa.gov>
2025-07-29 16:48:36 -0400

    Post merge cleanup

--- dev/ush/load_gw_run_modules.sh
+++ dev/ush/load_gw_run_modules.sh
@@ -2,14 +2,14 @@
 
 ###############################################################
 if [[ "$-" == *x* ]]; then
-    set_x=YES
+  set_x=YES
 else
-    set_x=NO
+  set_x=NO
 fi
 
 if [[ "${DEBUG_WORKFLOW:-NO}" == "NO" ]]; then
-    echo "Loading modules quietly..."
-    set +x
+  echo "Loading modules quietly..."
+  set +x
 fi
 
 # Setup runtime environment by loading modules
@@ -18,25 +18,25 @@ ulimit_s=$( ulimit -S -s )
 # Test if HOMEgfs is defined.  If not, then try to determine it with git rev-parse
 _unset_homegfs="NO"
 if [[ -z ${HOMEgfs+x} ]]; then
-    echo "INFO HOMEgfs is not defined.  Attempting to find the global-workflow root directory"
-    # HOMEgfs will be removed from the environment at the end of this script
-    _unset_homegfs="YES"
+  echo "INFO HOMEgfs is not defined.  Attempting to find the global-workflow root directory"
+  # HOMEgfs will be removed from the environment at the end of this script
+  _unset_homegfs="YES"
 
-    script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    HOMEgfs=$(cd "${script_dir}" && git rev-parse --show-toplevel)
-    export HOMEgfs
-    err=$?
-    if [[ ${err} -ne 0 ]]; then
-        is_git_dir=$( cd -- "${script_dir}" &> /dev/null && git rev-parse --is-inside-work-tree)
-        git_stat=$?
-        if [[ ${git_stat} -ne 0 || ${is_git_dir} != "true" ]]; then
-            echo "FATAL ERROR unable to determine the root because it is not a git repository."
-        else
-            echo "FATAL ERROR unable to determine the root because git rev-parse --show-toplevel failed for an unknown reason"
-        fi
-        echo "            Unable to load modules.  Exiting"
-        exit 1
+  script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  HOMEgfs=$(cd "${script_dir}" && git rev-parse --show-toplevel)
+  export HOMEgfs
+  err=$?
+  if [[ ${err} -ne 0 ]]; then
+    is_git_dir=$( cd -- "${script_dir}" &> /dev/null && git rev-parse --is-inside-work-tree)
+    git_stat=$?
+    if [[ ${git_stat} -ne 0 || ${is_git_dir} != "true" ]]; then
+      echo "FATAL ERROR unable to determine the root because it is not a git repository."
+    else
+      echo "FATAL ERROR unable to determine the root because git rev-parse --show-toplevel failed for an unknown reason"
     fi
+    echo "            Unable to load modules.  Exiting"
+    exit 1
+  fi
 fi
 
 # Find module command and purge:
@@ -56,17 +56,17 @@ fi
 module use "${HOMEgfs}/modulefiles"
 
 case "${MACHINE_ID}" in
-  "wcoss2" | "ursa" | "hera" | "orion" | "hercules" | "gaeac5" | "gaeac6" | "noaacloud")
-    module load "gw_run.${MACHINE_ID}"
-    export err=$?
-    if [[ ${err} -ne 0 ]]; then
-      echo "FATAL ERROR: Failed to load module_base.${MACHINE_ID}"
-      exit 1
-    fi
-    ;;
-  *)
-    echo "WARNING: UNKNOWN PLATFORM"
-    ;;
+"wcoss2" | "ursa" | "hera" | "orion" | "hercules" | "gaeac5" | "gaeac6" | "noaacloud")
+  module load "gw_run.${MACHINE_ID}"
+  export err=$?
+  if [[ ${err} -ne 0 ]]; then
+    echo "FATAL ERROR: Failed to load module_base.${MACHINE_ID}"
+    exit 1
+  fi
+  ;;
+*)
+  echo "WARNING: UNKNOWN PLATFORM"
+  ;;
 esac
 
 module list
@@ -79,9 +79,13 @@ elif [[ "${set_x}" == "YES" ]]; then
   set -x
 fi
 
-# Add wxflow to PYTHONPATH
-wxflowPATH="${HOMEgfs}/ush/python"
-PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/ush:${wxflowPATH}"
+# Set up the PYTHONPATH to include wxflow from HOMEgfs
+if [[ -d "${HOMEgfs}/sorc/wxflow/src" ]]; then
+  PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/sorc/wxflow/src"
+fi
+
+# Add HOMEgfs/ush/python to PYTHONPATH
+PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEgfs}/ush/python"
 export PYTHONPATH
 
 # Restore stack soft limit:
