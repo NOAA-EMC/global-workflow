d0cef3427bf463052a1a828378748d2f7359c02b
David Huber <david.huber@noaa.gov>
2025-07-30 17:55:36 +0000

    Fixes for Hera

--- dev/jobs/fcst.sh
+++ dev/jobs/fcst.sh
@@ -9,7 +9,11 @@ source "${HOMEgfs}/ush/detect_machine.sh"
 if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
   source "${HOMEgfs}/dev/ush/load_ufswm_modules.sh"
 else
+  #TODO: Replace with gw_run modules when the weather model updates to spack-stack 1.9.2
   source "${HOMEgfs}/dev/ush/load_ufswm_modules.sh"
+  module load py-netcdf4
+  module load py-pyyaml
+  module load py-jinja2
 fi
 status=$?
 if [[ ${status} -ne 0 ]]; then
