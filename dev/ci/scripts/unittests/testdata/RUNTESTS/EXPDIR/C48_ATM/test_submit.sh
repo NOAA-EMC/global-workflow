ae6472863fe4374391976bd658e4ceb1a6af5b07
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-08-01 14:28:17 -0400

    remove redundent EXPDIR test dirs

---
+++ dev/ci/scripts/unittests/testdata/RUNTESTS/EXPDIR/C48_ATM/test_submit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Test job"
+sleep 2
--- dev/ci/scripts/unittests/testdata/RUNTESTS/EXPDIR/C48mx500_3DVarAOWCDA/C48mx500_3DVarAOWCDA.scron.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-source /home/tmcguinness/GITHUB/COPILOT/global-workflow_forked/dev/ush/gw_setup.sh
-/home/tmcguinness/GITHUB/COPILOT/rocoto_forked/bin/rocotorun -d /home/tmcguinness/GITHUB/COPILOT/global-workflow_forked/dev/ci/scripts/unittests/testdata/RUNTESTS/EXPDIR/C48mx500_3DVarAOWCDA/C48mx500_3DVarAOWCDA.db -w /home/tmcguinness/GITHUB/COPILOT/global-workflow_forked/dev/ci/scripts/unittests/testdata/RUNTESTS/EXPDIR/C48mx500_3DVarAOWCDA/C48mx500_3DVarAOWCDA.xml
