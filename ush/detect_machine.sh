bb8bbc6d81ddc6ad6841bd5e802925d2c3d279ab
David Huber <david.huber@noaa.gov>
2025-08-04 14:59:59 -0500

    Use -n instead of ! -z

--- ush/detect_machine.sh
+++ ush/detect_machine.sh
@@ -90,7 +90,7 @@ elif [[ -d /scratch3 ]]; then
 elif [[ -d /work ]]; then
   # We are on MSU Orion or Hercules
   mount=$(findmnt -n -o SOURCE /home)
-  if [[ ! -z "${mount+0}" && ${mount} =~ "hercules" ]]; then
+  if [[ -n "${mount+0}" && ${mount} =~ "hercules" ]]; then
     MACHINE_ID=hercules
   else
     MACHINE_ID=orion
