f4c2482ebfd29a88e65163402f01e223a987bffc
AntonMFernando <anton.fernando@noaa.gov>
2025-08-13 12:35:03 -0400

    update scripts\exglobal_atmos_chgres_gen_control.sh

--- scripts/exglobal_atmos_chgres_gen_control.sh
+++ scripts/exglobal_atmos_chgres_gen_control.sh
@@ -94,7 +94,7 @@ wam_cold_start=.false.
 /
 EOF
 
-"${APRUN_CHGRES}" "${CHGRESEXEC}"
+eval "${APRUN_CHGRES}" "${CHGRESEXEC}"
 ################################################################################
 # Ensure COMIN_ATMOS_INPUT_MEM exists, create if needed, then copy out.atm.tile{1..6}.nc (force overwrite)
 if [[ ! -d "${COMOUT_ATMOS_INPUT_MEM}" ]]; then
