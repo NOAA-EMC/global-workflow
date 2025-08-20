62f9aa0f2306dd018befba6df0aa4e70950a02e0
David Huber <david.huber@noaa.gov>
2025-08-20 17:47:52 +0000

    More informative print statement

--- sorc/build_compute.sh
+++ sorc/build_compute.sh
@@ -110,7 +110,7 @@ runcmd="rocotorun -w ${build_xml} -d ${build_db} ${rocoto_verbose_opt}"
 
 finished=false
 ${runcmd}
-echo "Running builds on compute nodes"
+echo "Monitoring builds on compute nodes"
 while [[ "${finished}" == "false" ]]; do
    sleep 1m
    ${runcmd}
