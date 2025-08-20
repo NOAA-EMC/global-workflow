10094a81698210a38ea3065e4fa9b51d5a1a6ea4
Terry McGuinness <terry.mcguinness@noaa.gov>
2025-08-20 15:17:34 -0400

    fixed reg ex for recovering caseName for tagged pslot

--- dev/ci/scripts/run_check_gitlab_ci.sh
+++ dev/ci/scripts/run_check_gitlab_ci.sh
@@ -154,7 +154,7 @@ while true; do
   echo "Gather Rocoto statistics"
   # shellcheck disable=SC2312 # We want to use the exit code of the command
 
-  caseName="${caseName:-${pslot%_*-*}}"
+  caseName="${pslot%_*}"
   export ROCOTOSTAT_LOG_FILE="${RUNTESTS}/EXPDIR/${pslot}/logs/${caseName}_rocotostat.log"
   full_state=$("${HOMEgfs}/dev/ci/scripts/utils/rocotostat.py" -w "${xml}" -d "${db}" -v --thread-logging)
   error_stat=$?
