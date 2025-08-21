c56e02a5c300882742955c882c8ada379027b4b2
Anton.Fernando <anton.fernando@noaa.gov>
2025-08-21 19:52:33 -0400

    update ush\python\pygfs\task\stage_ic.py

--- ush/python/pygfs/task/stage_ic.py
+++ ush/python/pygfs/task/stage_ic.py
@@ -155,7 +155,7 @@ class Stage(Task):
     @logit(logger)
     def calculate_member_com_paths_gfs(self) -> Dict[str, Any]:
         cycle_vars = self.calculate_cycle_variables()
-        com_dir_vars = AttrDict({
+        com_dir_vars = {
             'COMOUT_ATMOS_INPUT_MEM_list': [],
             'COMOUT_ATMOS_RESTART_PREV_MEM_list': [],
             'COMOUT_ATMOS_RESTART_MEM_list': [],
@@ -167,7 +167,7 @@ class Stage(Task):
             'COMOUT_MED_RESTART_PREV_MEM_list': [],
             'COMOUT_WAVE_RESTART_PREV_MEM_list': [],
             'COMOUT_CHEM_ANALYSIS_MEM_list': [],
-        })
+        }
 
         # Loop over members: for deterministic GFS, first_mem == last_mem == -1 (single pass)
         for mem in range(cycle_vars.first_mem, cycle_vars.last_mem + 1):
@@ -219,7 +219,7 @@ class Stage(Task):
             'COMOUT_MED_RESTART_PREV_MEM_list': [],
             'COMOUT_WAVE_RESTART_PREV_MEM_list': [],
         }
-
+        print(f'cycle_vars_wer2: {com_dir_vars}')
         # Loop over ensemble members
         for mem in range(cycle_vars.first_mem, cycle_vars.last_mem + 1):
             memdir = f"mem{mem:03d}"
