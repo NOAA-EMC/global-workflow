5c469715a83df28136b6617dc4ccdf895fe90e98
Anton.Fernando <anton.fernando@noaa.gov>
2025-08-21 20:28:13 -0400

    update scripts\exglobal_stage_ic.py

--- scripts/exglobal_stage_ic.py
+++ scripts/exglobal_stage_ic.py
@@ -23,7 +23,7 @@ def main():
             'assim_freq', 'current_cycle', 'previous_cycle',
             'ROTDIR', 'ICSDIR', 'STAGE_IC_YAML_TMPL', 'DO_JEDIATMVAR',
             'OCNRES', 'waveGRD', 'ntiles', 'DOIAU',
-            'DO_JEDIOCNVAR', 'DO_STARTMEM_FROM_JEDIICE',
+            'DO_JEDIOCNVAR', 'DO_STARTMEM_FROM_JEDIICE', 'm_prefix','p_prefix',
             'REPLAY_ICS', 'DO_WAVE', 'DO_OCN', 'DO_ICE', 'DO_NEST', 'DO_CA', 'DO_AERO_ANL',
             'USE_ATM_ENS_PERTURB_FILES', 'USE_OCN_ENS_PERTURB_FILES', 'first_mem', 'last_mem']
 
