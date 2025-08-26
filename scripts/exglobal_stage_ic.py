8c66dcc11cf455da7a5ae235820475e857d811b5
Anton.Fernando <anton.fernando@noaa.gov>
2025-08-26 16:14:58 -0400

    upadte exscript

--- scripts/exglobal_stage_ic.py
+++ scripts/exglobal_stage_ic.py
@@ -3,7 +3,7 @@
 import os
 
 from pygfs.task.stage_ic import Stage
-from wxflow import AttrDict, Logger, cast_strdict_as_dtypedict, logit
+from wxflow import Logger, cast_strdict_as_dtypedict, logit
 
 # Initialize root logger
 logger = Logger(level=os.environ.get("LOGGING_LEVEL", "DEBUG"), colored_log=True)
@@ -17,31 +17,8 @@ def main():
     # Instantiate the Stage object
     stage = Stage(config)
 
-    # Pull out all the configuration keys needed to run stage job
-    keys = ['RUN', 'MODE', 'EXP_WARM_START', 'NMEM_ENS',
-            'assim_freq', 'current_cycle', 'previous_cycle',
-            'ROTDIR', 'ICSDIR', 'STAGE_IC_YAML_TMPL', 'DO_JEDIATMVAR',
-            'OCNRES', 'waveGRD', 'ntiles', 'DOIAU',
-            'DO_JEDIOCNVAR', 'DO_STARTMEM_FROM_JEDIICE',
-            'REPLAY_ICS', 'DO_WAVE', 'DO_OCN', 'DO_ICE', 'DO_NEST', 'DO_CA', 'DO_AERO_ANL',
-            'USE_ATM_ENS_PERTURB_FILES', 'USE_OCN_ENS_PERTURB_FILES', 'DO_GSISOILDA', 'DO_LAND_IAU']
-
-    stage_dict = AttrDict()
-    for key in keys:
-        # Make sure OCNRES is three digits
-        if key == "OCNRES":
-            stage.task_config.OCNRES = f"{stage.task_config.OCNRES :03d}"
-        stage_dict[key] = stage.task_config[key]
-
-    # Also import all COM* directory and template variables
-    for key in stage.task_config.keys():
-        if key.startswith("COM"):
-            stage_dict[key] = stage.task_config[key]
-        if "ENSMEM" in stage.task_config:
-            stage_dict["ENSMEM"] = stage.task_config["ENSMEM"]
-
     # Stage ICs
-    stage.execute_stage(stage_dict)
+    stage.calculate_stage_vars()
 
 
 if __name__ == '__main__':
