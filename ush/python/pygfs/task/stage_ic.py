700466802d725ec1b5de2de6cc4cdfd3b9ae0a7f
Anton.Fernando <anton.fernando@noaa.gov>
2025-08-28 15:38:54 -0400

    update ush\python\pygfs\task\stage_ic.py

--- ush/python/pygfs/task/stage_ic.py
+++ ush/python/pygfs/task/stage_ic.py
@@ -374,7 +374,7 @@ class Stage(Task):
         # - current (RUN) for outputs
         current_cycle_in = {**com_vars.current_cycle_dict, "${MEMDIR}": memdir, "${RUN}": com_vars.rRUN}
         # - current (rRUN) for inputs
-        current_cycle = {**com_vars.current_cycle_in, "${RUN}": com_vars.RUN}
+        current_cycle = {**current_cycle_in, "${RUN}": com_vars.RUN}
         # - previous (rRUN) for prev-cycle restarts
         previous_cycle = {**com_vars.previous_cycle_dict, "${MEMDIR}": memdir, "${RUN}": com_vars.rRUN}
 
