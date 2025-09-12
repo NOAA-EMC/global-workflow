32328ac708eca927eb8086d86718cb06e3069d6f
David Huber <david.huber@noaa.gov>
2025-09-12 11:08:16 +0000

    Clarify why perl is needed

--- modulefiles/gw_gsi.wcoss2.lua
+++ modulefiles/gw_gsi.wcoss2.lua
@@ -14,8 +14,6 @@ load(pathJoin("python", (os.getenv("python_ver") or "None")))
 prepend_path("MODULEPATH", "/apps/dev/modulefiles")
 load(pathJoin("ve","gw", (os.getenv("gw_ve_ver") or "None")))
 
-load(pathJoin("perl", (os.getenv("perl_ver") or "None")))
-
 load(pathJoin("hdf5-D", (os.getenv("hdf5_ver") or "None")))
 load(pathJoin("pnetcdf-D", (os.getenv("pnetcdf_ver") or "None")))
 load(pathJoin("netcdf-D", (os.getenv("netcdf_ver") or "None")))
@@ -23,5 +21,8 @@ load(pathJoin("netcdf-D", (os.getenv("netcdf_ver") or "None")))
 load(pathJoin("prod_util", (os.getenv("prod_util_ver") or "None")))
 load(pathJoin("ncdiag-A", (os.getenv("ncdiag_ver") or "None")))
 load(pathJoin("crtm", (os.getenv("crtm_ver") or "None")))
+
+-- Perl is needed by the GSI monitor
+load(pathJoin("perl", (os.getenv("perl_ver") or "None")))
 
 whatis("Description: GFS run environment")
