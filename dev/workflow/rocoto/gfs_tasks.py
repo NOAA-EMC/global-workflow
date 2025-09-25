f3d71130484bc4e72ee4def336e3bd9bf63e232c
AntonMFernando <anton.fernando@noaa.gov>
2025-09-25 15:11:51 -0400

    update dev\workflow\rocoto\gfs_tasks.py

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -1092,7 +1092,7 @@ class GFSTasks(Tasks):
 
         atm_anl_path = self._template_to_rocoto_cycstring(self._base["COM_ATMOS_ANALYSIS_TMPL"])
         deps = []
-        data = f'{atm_anl_path}/{self.run}.t@Hz.atmanl.nc'
+        data = f'{atm_anl_path}/{self.run}.t@Hz.atm.anl.nc'
         dep_dict = {'type': 'data', 'data': data, 'age': 120}
         deps.append(rocoto.add_dependency(dep_dict))
         data = f'{atm_anl_path}/{self.run}.t@Hz.sfc.anl.nc'
