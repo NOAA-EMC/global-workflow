2e8c33261a1ca3e45c9ce3e36b15394b0f286045
Wei Huang <Wei.Huang@noaa.gov>
2025-09-29 17:12:25 +0000

    re-merge on aws

--- modulefiles/gw_run.noaacloud.lua
+++ modulefiles/gw_run.noaacloud.lua
@@ -7,7 +7,7 @@ Load environment to run GFS on NOAA cloud
 local homegfssdir=os.getenv("HOMEgfs") or "None"
 if (homegfssdir == "None") then
     LmodError("FATAL ERROR HOMEgfs variable is unset.\n" ..
-              "Please \"source ush/load_gw_run_modules.sh\" rather than loading this module directly.\n")
+              "Please \"source dev/ush/load_gw_run_modules.sh\" rather than loading this module directly.\n")
 end
 
 load(pathJoin("perl", (os.getenv("perl_ver") or "None")))
@@ -15,12 +15,12 @@ load(pathJoin("mkl", (os.getenv("mkl_ver") or "None")))
 
 load("gw_run.common")
 
-prepend_path("MODULEPATH", pathJoin("/contrib/git/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
-load(pathJoin("prepobs", (os.getenv("prepobs_run_ver") or "None")))
+-- prepend_path("MODULEPATH", pathJoin("/contrib/git/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
+-- load(pathJoin("prepobs", (os.getenv("prepobs_run_ver") or "None")))
 
-prepend_path("MODULEPATH", pathJoin("/contrib/git/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
-load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
+-- prepend_path("MODULEPATH", pathJoin("/contrib/git/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
+-- load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
 
-load(pathJoin("imagemagick", (os.getenv("imagemagick_ver") or "None")))
+-- load(pathJoin("imagemagick", (os.getenv("imagemagick_ver") or "None")))
 
 whatis("Description: GFS run environment")
