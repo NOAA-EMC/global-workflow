757e2b842380bb7beda682f391abf4da79f77e86
Andrew Collard <andrew.collard@noaa.gov>
2025-10-14 16:17:10 +0000

    Add CRTM_FIX path for NOAA Cloud

--- modulefiles/gw_run.noaacloud.lua
+++ modulefiles/gw_run.noaacloud.lua
@@ -3,11 +3,11 @@ Load environment to run GFS on NOAA cloud
 ]])
 
 -- Test that HOMEgfs is set.
--- If not, load_modules.sh was not sourced to load this module.
+-- If not, load_gw_run_modules.sh was not sourced to load this module.
 local homegfssdir=os.getenv("HOMEgfs") or "None"
 if (homegfssdir == "None") then
     LmodError("FATAL ERROR HOMEgfs variable is unset.\n" ..
-              "Please \"source dev/ush/load_modules.sh\" rather than loading this module directly.\n")
+              "Please \"source ush/load_gw_run_modules.sh\" rather than loading this module directly.\n")
 end
 
 load(pathJoin("perl", (os.getenv("perl_ver") or "None")))
@@ -23,5 +23,7 @@ prepend_path("MODULEPATH", pathJoin("/contrib/git/Fit2Obs/v" .. (os.getenv("fit2
 load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
 
 load(pathJoin("imagemagick", (os.getenv("imagemagick_ver") or "None")))
+
+setenv("CRTM_FIX", "/contrib/global-workflow-shared-data/fix/crtm/v2.4.0.2")
 
 whatis("Description: GFS run environment")
