9f157a818e0cfa2b080bd3da70285ee499afc3ae
DavidNew-NOAA <david.new@noaa.gov>
2025-10-28 21:14:30 +0000

    Debug

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -881,7 +881,7 @@ class GFSTasks(Tasks):
             dep_dict = {'type': 'task', 'name': f'enkf{self.run}_marineanlecen'}
             deps.append(rocoto.add_dependency(dep_dict))
         if self.options['do_mergensst']:
-            if do_jediatmvar
+            if self.options['do_jediatmvar']:
                 data = f'&ROTDIR;/{self.run}.@Y@m@d/@H/atmos/{self.run}.t@Hz.jedi_analysis.sfc.a006.nc'
             else:
                 data = f'&ROTDIR;/{self.run}.@Y@m@d/@H/atmos/{self.run}.t@Hz.analysis.sfc.a006.nc'
@@ -1095,13 +1095,13 @@ class GFSTasks(Tasks):
 
         atm_anl_path = self._template_to_rocoto_cycstring(self._base["COM_ATMOS_ANALYSIS_TMPL"])
         deps = []
-        if do_jediatmvar:
+        if self.options['do_jediatmvar']:
             data = f'{atm_anl_path}/{self.run}.t@Hz.jedi_analysis.atm.a006.nc'
         else:
             data = f'{atm_anl_path}/{self.run}.t@Hz.analysis.atm.a006.nc'
         dep_dict = {'type': 'data', 'data': data, 'age': 120}
         deps.append(rocoto.add_dependency(dep_dict))
-        if do_jediatmvar:
+        if self.options['do_jediatmvar']:
             data = f'{atm_anl_path}/{self.run}.t@Hz.jedi_analysis.sfc.a006.nc'
         else:
             data = f'{atm_anl_path}/{self.run}.t@Hz.analysis.sfc.a006.nc'
