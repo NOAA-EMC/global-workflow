3dd5e91703cc7170872ac1809f74419a3747f233
Taylor Roper <taylor.roper@noaa.gov>
2025-10-29 19:52:32 +0000

    Add noaacloud to ufsda case in load_modules

--- dev/ush/load_modules.sh
+++ dev/ush/load_modules.sh
@@ -98,7 +98,7 @@ case "${MODULE_TYPE}" in
     module use "${HOMEgfs}/sorc/gdas.cd/modulefiles"
 
     case "${MACHINE_ID}" in
-      ("hera" | "orion" | "hercules" | "wcoss2" | "gaeac5" | "gaeac6" | "ursa")
+      ("hera" | "orion" | "hercules" | "wcoss2" | "gaeac5" | "gaeac6" | "ursa" | "noaacloud")
         #TODO: Remove LMOD_TMOD_FIND_FIRST line when spack-stack on WCOSS2
         if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
           export LMOD_TMOD_FIND_FIRST=yes
