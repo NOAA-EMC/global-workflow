bbe01ccbd4cd1582d309c64ec8c92c0c8e7ff459
DavidHuber-NOAA <david.huber@noaa.gov>
2025-10-29 20:35:28 +0000

    Fix the input snowinc filenames

--- ush/python/pygfs/task/snowens_analysis.py
+++ ush/python/pygfs/task/snowens_analysis.py
@@ -233,7 +233,7 @@ class SnowEnsAnalysis(Analysis):
 
         if self.task_config.DOIAU:
             logger.info("Copying increments to beginning of window")
-            template_in = f'{to_fv3time(self.task_config.current_cycle)}.snow_increment.sfc_data.tile{{tilenum}}.nc'
+            template_in = f'snowinc.{to_fv3time(self.task_config.current_cycle)}.sfc_data.tile{{tilenum}}.nc'
             template_out = f'{to_fv3time(self.task_config.WINDOW_BEGIN)}.snow_increment.sfc_data.tile{{tilenum}}.nc'
             inclist = []
             for itile in range(1, self.task_config.ntiles + 1):
