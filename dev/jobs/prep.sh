668a93fb9c95a1ea5c12b37e357ab96e710848bb
Russ-Treadon-NOAA <Russ.Treadon@noaa.gov>
2025-11-10 14:05:07 +0000

    place double quotes around script command

--- dev/jobs/prep.sh
+++ dev/jobs/prep.sh
@@ -176,7 +176,7 @@ fi
 # If requested, create radiance bias correction files for JEDI
 if [[ ${CONVERT_BIASCOR:-"NO"} == "YES" ]]; then
     cd "${DATAROOT}" || true
-    ${HOMEgfs}/sorc/gdas.cd/ush/gsi_satbias2ioda_all.sh
+    "${HOMEgfs}/sorc/gdas.cd/ush/gsi_satbias2ioda_all.sh"
     export err=$?
     if [[ ${err} -ne 0 ]]; then
         err_exit "JOBSPROC_GLOBAL_PREP job failed, ABORT!"
