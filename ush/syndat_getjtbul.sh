9bdc6a6dd14e5f0d9b30fb2855a5a8f16b41e56d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-12-17 16:22:16 -0500

    add missing shellcheck

--- ush/syndat_getjtbul.sh
+++ ush/syndat_getjtbul.sh
@@ -107,6 +107,7 @@ if [[ -s prep_step ]]; then
     set_strict
 else
     [[ -f errfile ]] && rm errfile
+    #shellcheck disable=SC2046
     unset FORT00 $(env | grep "^FORT[0-9]\{1,\}=" | awk -F= '{print $1}')
 fi
 
