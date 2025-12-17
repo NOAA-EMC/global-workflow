0f19e92210958048aad254f610ed9eaa7b53ce02
Yangxing Zheng <Yangxing.Zheng@gaea63.ncrc.gov>
2025-12-17 15:09:07 -0500

    handle ocean/ice file format separately

--- ush/python/pygfs/task/oceanice_products.py
+++ ush/python/pygfs/task/oceanice_products.py
@@ -139,8 +139,8 @@ class OceanIceProducts(Task):
         localconf.cosvar = config.oceanice_yaml[config.component].namelist.cosvar
         localconf.angvar = config.oceanice_yaml[config.component].namelist.angvar
         localconf.debug = ".true." if config.oceanice_yaml.ocnicepost.namelist.debug else ".false."
-        localconf.write_grib2 = ".true." if config.oceanice_yaml.ocnicepost.namelist.write_grib2 else ".false."
-        localconf.write_netcdf = ".true." if config.oceanice_yaml.ocnicepost.namelist.write_netcdf else ".false."
+        localconf.write_grib2 = ".true." if config.oceanice_yaml[config.component].namelist.write_grib2 else ".false."
+        localconf.write_netcdf = ".true." if config.oceanice_yaml[config.component].namelist.write_netcdf else ".false."
 
         logger.debug(f"localconf:\n{pformat(localconf)}")
 
@@ -171,10 +171,14 @@ class OceanIceProducts(Task):
         """
 
         # Run the ocnicepost.x executable if interpolated variables are wanted
-        if config.oceanice_yaml.ocnicepost.namelist.write_netcdf or config.oceanice_yaml.ocnicepost.namelist.write_grib2:
+        #localconf.write_grib2 = ".true." if config.oceanice_yaml[config.component].namelist.write_grib2 else ".false."
+        #localconf.write_netcdf = ".true." if config.oceanice_yaml[config.component].namelist.write_netcdf else ".false."
+        print(config.oceanice_yaml[config.component].namelist.write_netcdf)
+        print(config.oceanice_yaml[config.component].namelist.write_grib2)
+        if config.oceanice_yaml[config.component].namelist.write_netcdf or config.oceanice_yaml[config.component].namelist.write_grib2:
             OceanIceProducts.interp(config.DATA, config.APRUN_OCNICEPOST, exec_name="ocnicepost.x")
 
-        if config.oceanice_yaml.ocnicepost.namelist.write_grib2:
+        if config.oceanice_yaml[config.component].namelist.write_grib2:
             # Index the interpolated grib2 file
             OceanIceProducts.index(config, product_grid)
 
