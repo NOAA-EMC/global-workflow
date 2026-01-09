b58e29181878ef9889922ee790e07623bec2f410
YangxingZheng-NOAA <Yangxing.Zheng@noaa.gov>
2026-01-09 21:12:26 +0000

    add wgrib2/3.8.0

--- dev/jobs/atmos_post.sh
+++ dev/jobs/atmos_post.sh
@@ -1,16 +1,12 @@
 #! /usr/bin/env bash
 set -x
+set -e
 
 ###############################################################
 ## atmosphere post (daily and monthly mean) driver script
 ###############################################################
 
-# Source FV3GFS workflow modules
-#source "${HOMEgfs}/dev/ush/load_modules.sh" run
-#status=$?
-#if ((status != 0)); then exit "${status}"; fi
-
-source "${HOMEgfs}/ush/detect_machine.sh"
+source "${HOMEgfs}/ush/detect_machine.sh" || exit 1
 if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
 
     module load craype-x86-rome
@@ -43,15 +39,28 @@ if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
 
     export GMERGE=gmerge
     export WGRIB2=wgrib2
-else
-    source "${HOMEgfs}/dev/ush/load_modules.sh" run
+
+elif [[ "${MACHINE_ID}" == "ursa" ]]; then
+    module use /scratch4/NCEPDEV/nems/Richard.Grubin/spack-stack/envs/ue-oneapi-2025.2.1-wgrib2-3.8.0/modules/Core
+    module load stack-intel-oneapi-compilers/2025.2.1
+    module load stack-intel-oneapi-mpi/2021.13
+    module load wgrib2/3.8.0
     export GMERGE=gmerge
     export WGRIB2=wgrib2
 
-    status=$?
-    if [[ ${status} -ne 0 ]]; then
-        exit "${status}";
-    fi
+elif [[ "${MACHINE_ID}" == "gaeac6" ]]; then
+    source "${HOMEgfs}/dev/ush/load_modules.sh" run || exit 1
+    export GMERGE=/gpfs/f6/sfs-emc/scratch/Karina.Asmar/wgrib2/aux_progs/gmerge
+    export WGRIB2=wgrib2
+else
+    source "${HOMEgfs}/dev/ush/load_modules.sh" run || exit 1
+    export GMERGE=gmerge
+    export WGRIB2=wgrib2
+fi
+
+status=$?
+if [[ ${status} -ne 0 ]]; then
+   exit "${status}";
 fi
 
 export job="atmos_post"
