98ec0dee57c11de5790910752f94b82b32ca696a
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-01-15 14:47:23 +0000

    add for linking GRB files

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -291,7 +291,7 @@ EOF
     #============================================================================
 
     #============================================================================
-    if [[ "${QUILTING}" == ".true." ]] && [[ "${OUTPUT_GRID}" == "gaussian_grid" ]]; then
+    if [[ "${QUILTING}" == ".true." ]] && [[ "${OUTPUT_GRID}" == "gaussian_grid" || "${OUTPUT_GRID}" == "global_latlon" ]]; then
         local FH2 FH3
         for fhr in ${FV3_OUTPUT_FH}; do
             FH3=$(printf %03i "${fhr}")
