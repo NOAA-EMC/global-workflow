c5deb1050f9e761cc8f1163f62bdb7f756d3cf14
YangxingZheng-NOAA <Yangxing.Zheng@noaa.gov>
2026-01-15 16:30:19 +0000

    move job and sh files to dev/jobs/ and dev/job_cards/

---
+++ dev/job_cards/rocoto/atmensanlinit.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+set -x
+
+###############################################################
+# Source UFSDA workflow modules
+source "${HOMEgfs}/dev/ush/load_modules.sh" ufsda
+status=$?
+if [[ ${status} -ne 0 ]]; then
+    exit "${status}"
+fi
+
+export job="atmensanlinit"
+export jobid="${job}.$$"
+
+###############################################################
+# Execute the JJOB
+"${HOMEgfs}/dev/jobs/JGLOBAL_ATMENS_ANALYSIS_INITIALIZE"
+status=$?
+exit "${status}"
---
+++ dev/job_cards/rocoto/ecen_fv3jedi.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+set -x
+
+###############################################################
+# Source UFSDA workflow modules
+source "${HOMEgfs}/dev/ush/load_modules.sh" ufsda
+status=$?
+[[ ${status} -ne 0 ]] && exit "${status}"
+
+export job="ecen_fv3jedi"
+export jobid="${job}.$$"
+
+###############################################################
+# Execute the JJOB
+"${HOMEgfs}/dev/jobs/JGDAS_ENKF_ECEN_FV3JEDI"
+status=$?
+exit "${status}"
