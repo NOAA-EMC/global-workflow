a693179aaf47a45d57b0e78c26aed3f4a3dc0645
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-01-16 13:59:52 -0500

    updates for change in MOM6_INTERP_ICS and bug in build_ufs.sh

--- sorc/build_ufs.sh
+++ sorc/build_ufs.sh
@@ -100,7 +100,6 @@ else
     BUILD_JOBS=${BUILD_JOBS:-8} "${HOMEgfs_}/sorc/ufs_model.fd/tests/compile.sh" "${MACHINE_ID}" "${MAKE_OPT}" "${COMPILE_ID}" "intel" "${CLEAN_BEFORE}" "${CLEAN_AFTER}"
 fi
 mv "./tests/fv3_${COMPILE_ID}.exe" "./tests/${EXEC_NAME}"
-ln -sf "./tests/${EXEC_NAME}" "${HOMEgfs_}/exec/${EXEC_NAME}"
 if [[ ! -f "./tests/modules.ufs_model.lua" ]]; then mv "./tests/modules.fv3_${COMPILE_ID}.lua" "./tests/modules.ufs_model.lua"; fi
 if [[ ! -f "./tests/ufs_common.lua" ]]; then cp "./modulefiles/ufs_common.lua" ./tests/ufs_common.lua; fi
 
