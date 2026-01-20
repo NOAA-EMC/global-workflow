2d1bf18ce3f3a68d15cffb0f6ebffbf90e4736ab
Anton Fernando <anton.fernando@noaa.gov>
2026-01-20 20:00:06 +0000

    update dev/workflow/rocoto/rocoto_xml.py

--- dev/workflow/rocoto/rocoto_xml.py
+++ dev/workflow/rocoto/rocoto_xml.py
@@ -123,7 +123,7 @@ class RocotoXML(WorkflowSuite, ABC):
         if self._base["DO_ARCHCOM"] and self._base["ARCHCOM_TO"] == "globus_hpss":
             self._write_server_crontab()
 
-    def _get_cron_script_content(self, rocotorunstr: str, replyto: str) -> str:
+    def _get_scron_script_content(self, rocotorunstr: str, replyto: str) -> str:
         """
         Load and format the cron script template with experiment-specific values.
 
@@ -212,7 +212,7 @@ class RocotoXML(WorkflowSuite, ABC):
 
             # Now write the script that actually runs rocotorun and monitors for failures
             cron_cmd = f"{self.expdir}/{self.pslot}.scron.sh"
-            script_content = "#!/usr/bin/env bash\nset -x\n" + self._get_cron_script_content(rocotorunstr, replyto)
+            script_content = "#!/usr/bin/env bash\nset -x\n" + self._get_scron_script_content(rocotorunstr, replyto)
 
             with open(cron_cmd, "w") as script_fh:
                 script_fh.write(script_content)
