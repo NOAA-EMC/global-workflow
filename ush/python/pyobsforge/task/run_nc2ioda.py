1dcf75a9c66e8f2fb04b778346d8a357b918b506
Cory Martin <Cory.R.Martin@noaa.gov>
2026-01-27 16:07:41 +0000

    more changes

--- ush/python/pyobsforge/task/run_nc2ioda.py
+++ ush/python/pyobsforge/task/run_nc2ioda.py
@@ -18,13 +18,13 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
     Returns:
         int: Returns 0 upon successful execution. Logs errors and warnings for failures.
     """
-    jinja_template = join(task_config['HOMEobsforge'], "parm", "nc2ioda", "nc2ioda.yaml.j2")
+    jinja_template = join(task_config['HOMEgcafs'], "parm", "chem", "nc2ioda.yaml.j2")
     yaml_config = parse_j2yaml(jinja_template, context)
     nc2ioda_yaml = join(task_config['DATA'], obs_space, f"{obs_space}_nc2ioda.yaml")
     save_as_yaml(yaml_config, nc2ioda_yaml)
 
     # Run the ioda converter
-    nc2ioda_exe = join(task_config['HOMEobsforge'], 'build', 'bin', 'obsforge_obsprovider2ioda.x')
+    nc2ioda_exe = join(task_config['HOMEgcafs'], 'exec', 'gcdas_obsprovider2ioda.x')
     try:
         result = subprocess.run([nc2ioda_exe, nc2ioda_yaml],
                                 cwd=task_config['DATA'],
