e443f6b289749c325ec31e6de97b2ba955f51904
Anton Fernando <anton.fernando@noaa.gov>
2026-02-03 16:15:25 -0500

    update new flags 2

--- ush/python/pygfs/task/stage_ic.py
+++ ush/python/pygfs/task/stage_ic.py
@@ -43,7 +43,7 @@ class Stage(Task):
             'assim_freq', 'current_cycle', 'previous_cycle',
             'ROTDIR', 'ICSDIR', 'STAGE_IC_YAML_TMPL', 'DO_JEDIATMVAR',
             'OCNRES', 'waveGRD', 'ntiles', 'DOIAU', 'ATMINC_GRID',
-            'DO_JEDIOCNVAR', 'DO_STARTMEM_FROM_JEDIICE',
+            'DO_JEDIOCNVAR', 'DO_STARTMEM_FROM_JEDIICE', 'DO_JEDIATMENS',
             'DO_WAVE', 'DO_OCN', 'DO_ICE', 'DO_NEST', 'DO_CA', 'DO_AERO_ANL', 'MOM6_INTERP_ICS',
             'USE_ATM_ENS_PERTURB_FILES', 'USE_OCN_ENS_PERTURB_FILES', 'DO_GSISOILDA', 'DO_LAND_IAU'
         ]
