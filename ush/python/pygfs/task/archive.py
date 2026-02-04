2b06abd6528cf31d0345e7cf83645833d482e13c
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2026-02-04 14:57:09 -0500

    Fix v17 group b warm restart archiving (#4502)
    This fixes warm restart archiving for the v17 branch. For group b, the number of days between SDATE and the archive cycle is now correct.

--- ush/python/pygfs/task/archive.py
+++ ush/python/pygfs/task/archive.py
@@ -9,7 +9,8 @@ from typing import Any, Dict, List
 
 from wxflow import (AttrDict, FileHandler, Hsi, Htar, Task, to_timedelta,
                     chgrp, get_gid, logit, mkdir_p, parse_j2yaml, rm_p, rmdir,
-                    strftime, to_YMDH, which, chdir, ProcessError, save_as_yaml)
+                    strftime, to_YMDH, which, chdir, ProcessError, save_as_yaml,
+                    add_to_datetime)
 
 git_filename = "git_info.log"
 logger = getLogger(__name__.split('.')[-1])
@@ -688,7 +689,8 @@ class Archive(Task):
             # Not the right cycle hour
             return False
 
-        days_since_sdate = (arch_dict.current_cycle - SDATE).days
+        ics_offset_cycle = add_to_datetime(arch_dict.current_cycle, to_timedelta(f"+{assim_freq}H"))
+        days_since_sdate = (ics_offset_cycle - SDATE).days
         if arch_dict.ARCH_WARMICFREQ > 0 and days_since_sdate % arch_dict.ARCH_WARMICFREQ == 0:
             # We are on the right cycle hour and the right day
             return True
