d9397aca0bdcc2f99906cf4e39317542b07e3e56
David Huber <david.huber@noaa.gov>
2026-02-10 08:47:44 -0500

    More disables

--- dev/scripts/exglobal_atmos_analysis.sh
+++ dev/scripts/exglobal_atmos_analysis.sh
@@ -906,7 +906,7 @@ if [[ "${SENDECF}" == "YES" && "${RUN}" != "enkf" ]]; then
     ecflow_client --event release_fcst
 fi
 
-# shellcheck ingore=SC2312
+# shellcheck disable=SC2312
 echo "${rCDUMP} ${PDY}${cyc} atminc done at $(date)" > "${COMOUT_ATMOS_ANALYSIS}/${APREFIX}increment.done.txt"
 
 # Diagnostic files
