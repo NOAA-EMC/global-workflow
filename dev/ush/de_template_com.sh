68673febf006c58a9ac8afebec3a1330209bb7f7
DavidHuber-NOAA <david.huber@noaa.gov>
2026-02-13 14:34:50 +0000

    Make de_template_com.sh work on all template cases

--- dev/ush/de_template_com.sh
+++ dev/ush/de_template_com.sh
@@ -34,9 +34,8 @@
 #
 #
 # Here are two cases of COM variable generation in the current workflow:
-# YMD="${PDY}" HH="${cyc}" declare_from_tmpl -rx \
-#    COMIN_ATMOS_INPUT:COM_ATMOS_INPUT_TMPL \
-#    COMOUT_CONF:COM_CONF_TMPL
+# YMD="${PDY}" HH="${cyc}" declare_from_tmpl -rx COMIN_ATMOS_INPUT:COM_ATMOS_INPUT_TMPL
+# YMD="${PDY}" HH="${cyc}" declare_from_tmpl -rx COMOUT_CONF:COM_CONF_TMPL
 #
 #
 # Here is the config.com for these TMPL variables:
@@ -51,21 +50,23 @@
 # COMIN_ATMOS_INPUT=${ROTDIR}/${RUN}.${YMD}/${HH}/${MEMDIR}/model/atmos/input
 
 if [[ $# -ne 3 ]]; then
-    echo "Usage: $0 <config.com> <jjob>"
+    echo "Usage: $0 <path/to/config.com> <path/to/jjob> <path/to/bash_utils.sh>" >&2
     exit 1
 fi
+
+set -eu -o pipefail
 
 config_com="${1}"
 jjob="${2}"
 bash_utils="${3}"
 
 if [[ ! -f "${config_com}" ]]; then
-    echo "Error: config.com file '${config_com}' not found!"
+    echo "Error: config.com file '${config_com}' not found!" >&2
     exit 1
 fi
 
 if [[ ! -f "${jjob}" ]]; then
-    echo "Error: jjob file '${jjob}' not found!"
+    echo "Error: jjob file '${jjob}' not found!" >&2
     exit 1
 fi
 
@@ -74,14 +75,63 @@ source "${config_com}"
 # Source the bash_utils.sh to get the declare_from_tmpl function
 source "${bash_utils}"
 
+# Set static variable for use in templates
+export ROTDIR='${ROTDIR}'
+
 # Replace the declare_from_tmpl calls in the jjob with the generated COM variable declarations and replace them in the jjob
+line_num=0
 while IFS= read -r line; do
+    line_num=$((line_num + 1))
+    # Intialize RUN. This can be overridden by the prefix assignments.
+    export RUN='${RUN}'
     if [[ "${line}" =~ declare_from_tmpl ]]; then
-        # Extract the arguments from the declare_from_tmpl call
-        args=$(echo "${line}" | sed -E 's/.*declare_from_tmpl[[:space:]]+(-[rx]+)?[[:space:]]+(.*)/\2/')
-        # Generate the COM variable declarations using declare_from_tmpl
-        generated_declarations=$(declare_from_tmpl ${args})
-        # Replace the declare_from_tmpl call with the generated declarations in the jjob
-        jjob=$(echo "${jjob}" | sed "s|${line}|${generated_declarations}|")
+        # Use awk to get the number of leading spaces
+        COUNT=$(awk '{print match($0, /[^ ]|$/)-1}' <<<"$line")
+        spaces=""
+        for i in $(seq 1 $COUNT); do
+            spaces="${spaces} "
+        done
+        # Extract the prefix assignments to declare_from_tmpl
+        prefix=$(echo "${line}" | sed -E 's/^(.*)declare_from_tmpl.*/\1/')
+        # Parse the prefix to get all of the variable assignments (e.g. YMD="${PDY}" HH="${cyc}")
+        IFS=' ' read -ra prefix_args <<< "${prefix}"
+        # Convert these into literal variable assignments (e.g. YMD='${PDY}' HH='${cyc}')
+        for i in "${!prefix_args[@]}"; do
+            prefix_args[$i]=$(echo "${prefix_args[$i]}" | sed -E 's/(.*)="*([a-zA-Z0-9_{}$]+)"*/\1='\''\2'\''/')
+        done
+        # Extract the arguments to declare_from_tmpl
+        args=$(echo "${line}" | sed -E 's/.*declare_from_tmpl (.*)/\1/')
+        # Extract the flags to declare_from_tmpl (e.g. -rx)
+        flags=$(echo "${args}" | egrep -o -- '-[a-zA-Z]+')
+        # Generate the COM variable declaration using declare_from_tmpl
+        # The prefix arguments need to be in the form of VAR='${var}' (i.e. we want the template to render a literal string with the variable names, not their values)
+        # Use prefix_args to form these prefix assignments and pass them to declare_from_tmpl
+        # Valid prefix args are DUMP, YMD, HH, GRDRESNAME, MEMDIR, RUN, and GRID
+        for var in DUMP YMD HH GRDRESNAME MEMDIR RUN GRID; do
+            for arg in ${prefix_args[@]}; do
+                if [[ "${arg}" =~ ${var} ]]; then
+                    # Get what the variable is assigned to
+                    assignment=$(echo "${arg}" | sed -E "s/${var}='(.*)'/\1/")
+                    declare -x ${var}="${assignment}"
+                fi
+            done
+        done
+
+        # Now render the template
+        COM=$(declare_from_tmpl -rx ${args} | sed 's/declare_from_tmpl :: \(.*\)=\(.*\)/\1=\2/')
+        # Remove duplicate // in the COM path if it exists (e.g. if MEMDIR is empty, we don't want a double slash in the path)
+        COM=$(echo "${COM}" | sed 's/\/\//\//g')
+        # Prepend with declare ${flags} if flags are present
+        if [[ -n "${flags}" ]]; then
+            COM="declare ${flags} ${COM}"
+        fi
+        # And write it to stdout
+        echo "${spaces}${COM}"
+    else
+        # Not a declare_from_tmpl line, just print it as is
+        echo "${line}"
     fi
+
+    # Unset any variables that may have been set in the prefix to avoid unintended consequences in later lines of the jjob
+    unset DUMP YMD HH GRDRESNAME MEMDIR RUN GRID flags args spaces
 done < "${jjob}"
