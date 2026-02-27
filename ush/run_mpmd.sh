b5248522f43e7139022edfcecbcbfb071510d86b
DavidHuber-NOAA <david.huber@noaa.gov>
2026-02-27 22:47:37 +0000

    Rework chunking to work for Slurm and mpiexec

--- ush/run_mpmd.sh
+++ ush/run_mpmd.sh
@@ -6,13 +6,14 @@
 # Script name:         run_mpmd.sh
 # Script description:  Run multiple commands in MPMD mode or serially
 #
-# Author:   Rahul Mahajan
+# Author:   Rahul Mahajan and David Huber
 #
 # Org:      NCEP/EMC
 #
 # Abstract: This script runs multiple commands in MPMD mode. It is used to run
 #           multiple serial commands in parallel using the CFP (Coupled Framework
-#           Parallelism) feature of the workflow.
+#           Parallelism) feature of the workflow. The script handles chunking of the
+#           commands to avoid oversubscription of resources.
 #
 # Environment variables:
 #           USE_CFP: If set to YES, run in MPMD mode, else run in serial mode. Default is 'NO'.
@@ -31,13 +32,22 @@
 #
 ################################################################################
 
-source "${USHgfs}/preamble.sh"
-
 cmdfile=${1:?"run_mpmd requires an input file containing commands to execute in MPMD/serial mode"}
 
+# Determine launcher type
+if [[ "${launcher:-}" =~ ^srun.* ]]; then #  srun-based system e.g. Hera, Orion, etc.
+    _mpmd_launcher=srun
+elif [[ "${launcher:-}" =~ ^mpiexec.* ]]; then # mpiexec-based system e.g. WCOSS2
+    _mpmd_launcher=mpiexec
+else
+    echo "WARNING: Unsupported or empty launcher: '${launcher:-}', using serial mode instead"
+    echo "         Supported launchers are 'srun' and 'mpiexec'"
+    _mpmd_launcher=unsupported
+fi
+
 # Check if we are running a supported launcher
-if [[ "${launcher:-}" =~ ^srun.* || "${launcher:-}" =~ ^mpiexec.* ]]; then
-    echo "INFO: Detected launcher '${launcher:-}', will attempt to run in MPMD mode if USE_CFP is set to YES"
+if [[ "${_mpmd_launcher}" == "srun" || "${_mpmd_launcher}" == "mpiexec" ]]; then
+    echo "INFO: Detected launcher '${_mpmd_launcher}', will attempt to run in MPMD mode if USE_CFP is set to YES"
     if [[ -z "${max_tasks_per_node:-}" || -z "${ntasks:-}" ]]; then
         echo "WARNING: max_tasks_per_node and/or ntasks is not set, disabling MPMD mode."
         USE_CFP=NO
@@ -46,8 +56,6 @@ if [[ "${launcher:-}" =~ ^srun.* || "${launcher:-}" =~ ^mpiexec.* ]]; then
         max_tasks_per_node=$((ntasks < max_tasks_per_node ? ntasks : max_tasks_per_node))
     fi
 else
-    echo "WARNING: Unsupported or empty launcher: '${launcher:-}', using serial mode instead"
-    echo "         Supported launchers are 'srun' and 'mpiexec'"
     USE_CFP="NO"
 fi
 
@@ -67,11 +75,89 @@ export OMP_NUM_THREADS=1
 # Determine the number of MPMD processes from incoming ${cmdfile}
 nprocs=$(wc -l < "${cmdfile}")
 
-# Local MPMD file containing instructions to run in CFP
+# Create a local copy of the mpmd commands
 mpmd_cmdfile="${DATA:-}/mpmd_cmdfile"
 if [[ -s "${mpmd_cmdfile}" ]]; then
     rm -f "${mpmd_cmdfile}"
 fi
+
+# Write all commands to a temporary mpmd_cmdfile. The chunking function will modify this file as needed.
+# Also count the number of commands to determine how many chunks to run in.
+nm=0
+while IFS= read -r line; do
+    echo "${line}" >> "${mpmd_cmdfile}"
+    ((nm = nm + 1))
+done < "${cmdfile}"
+
+chunk_mpmd() {
+    # Usage chunk_mpmd cmdfile chunk_size chunk_num chunk_file
+    # This takes a chunk of the full mpmd command file and creates a new chunk
+    # file with the specified number of lines
+    # Inputs:
+    #   cmdfile: the full mpmd command file to read from and modify
+    #   chunk_size: the number of lines to include in the chunk file
+    #   chunk_num: the chunk number (used to determine which lines from the cmdfile to include in the chunk file)
+    #   chunk_file: the name of the chunk file to create
+    # Use this function when the number of MPMD tasks is greater than the maximum tasks per node.
+    local mpmd_file="${1}"
+    local chunk_sz="${2}"
+    local chunk_num="${3}"
+    local chunk_file="${4}"
+    if [[ ! -s "${mpmd_file}" ]]; then
+        echo "ERROR: MPMD command file '${mpmd_file}' is empty or does not exist."
+        return 1
+    fi
+
+    # Determine which line to start reading from
+    local _start_line=$(( (chunk_num - 1) * chunk_sz + 1 ))
+    local _end_line=$((chunk_num * chunk_sz))
+
+    local _counter=1
+    while IFS= read -r line; do
+        if [[ ${_counter} -ge ${_start_line} && ${_counter} -le ${_end_line} ]]; then
+            # Slurm requires a counter in front of each line in the script
+            if [[ "${_mpmd_launcher}" == "srun" ]]; then
+                echo "$((_counter - _start_line)) ${line}" >> "${chunk_file}"
+            elif [[ "${_mpmd_launcher}" == "mpiexec" ]]; then
+                echo "${line}" >> "${chunk_file}"
+            fi
+            err=$?
+            if [[ ${err} -ne 0 ]]; then
+                echo "ERROR: Failed to write line '${line}' to chunk file '${chunk_file}'"
+                return "${err}"
+            fi
+        fi
+        ((_counter = _counter + 1))
+    done < "${mpmd_file}"
+
+    return 0
+}
+
+cat_outputs() {
+    # This function concatenates the output files from the MPMD job and prints them to stdout.
+    # It also removes the individual output files after concatenation.
+
+    # Optional argument to issue error if no output files are found.
+    _err_on_empty="${1:-false}"
+    out_files=$(find . -name 'mpmd.*.out')
+    if [[ -z "${out_files}" ]]; then
+        if [[ "${_err_on_empty}" == "true" ]]; then
+            echo "ERROR: No output files found for MPMD job"
+            return 1
+        else
+            # Nothing to do, return success.
+            return 0
+        fi
+    fi
+    for file in ${out_files}; do
+        {
+            echo "BEGIN OUTPUT FROM ${file}"
+            cat "${file}"
+            echo "END OUTPUT FROM ${file}"
+        } >> mpmd.out
+        rm -f "${file}"
+    done
+}
 
 cat << EOF
 INFO: Executing MPMD job, STDOUT and STDERR redirected for each process separately
@@ -79,125 +165,58 @@ INFO: On failure, logs for each job will be available in ${DATA}/mpmd.proc_num.o
 INFO: The proc_num corresponds to the line in '${mpmd_cmdfile}'
 EOF
 
-chunk_mpmd() {
-    # Usage chunk_mpmd mpmd_cmdfile chunk_size chunk_file
-    # This takes a chunk of the full mpmd command file and creates a new chunk
-    # file with the specified number of lines, while removing those lines from the
-    # original file to prevent running the same lines multiple times.
-    # Use this function when the number of MPMD tasks is greater than the maximum tasks per node.
-    local mpmd_file="${1}"
-    local chunk_sz="${2}"
-    local chunk_file="${3}"
-    if [[ ! -s "${mpmd_file}" ]]; then
-        echo "ERROR: MPMD command file '${mpmd_file}' is empty or does not exist."
-        return 1
-    fi
-    head -n "${chunk_sz}" "${mpmd_file}" > "${chunk_file}" && true
-    err=$?
-    if [[ ${err} -ne 0 ]]; then
-        echo "ERROR: Failed to create chunk file '${chunk_file}' from '${mpmd_file}'"
-        return "${err}"
-    fi
-    sed -i "1,${chunk_size}d" "${mpmd_file}"
-    err=$?
-    if [[ ${err} -ne 0 ]]; then
-        echo "ERROR: Failed to remove lines from '${mpmd_file}' after creating chunk file '${chunk_file}'"
-        return "${err}"
-    fi
-    return 0
-}
+# Get the number of commands
+nm=$(wc -l < "${cmdfile}")
 
-if [[ "${launcher:-}" =~ ^srun.* ]]; then #  srun-based system e.g. Hera, Orion, etc.
+# For now, keep all MPMD tasks on one node.
+# Test if the number of lines in the cmdfile is greater than the number of tasks per node ($max_tasks_per_node).
+# If needed, split the mpmd_cmdfile and run it in chunks.
+# TODO: consider running the MPMD job across multiple nodes.
 
-    # Slurm requires a counter in front of each line in the script
-    # Read the incoming cmdfile and create srun usable cmdfile
-    nm=0
-    while IFS= read -r line; do
-        echo "${nm} ${line}" >> "${mpmd_cmdfile}"
-        ((nm = nm + 1))
-    done < "${cmdfile}"
-
-    # For now, keep all MPMD tasks on one node.
-    # Test if the number of lines in the cmdfile is greater than the number of tasks per node ($max_tasks_per_node).
-    # If needed, split the mpmd_cmdfile and run it in chunks.
-    # TODO: consider running the MPMD job across multiple nodes.
-
-    if [[ ${nm} -gt ${max_tasks_per_node:-1} ]]; then
-        echo "INFO: Number of MPMD tasks (${nm}) is greater than the maximum tasks per node (${max_tasks_per_node:-1})."
-        echo "      Running MPMD job in chunks of ${max_tasks_per_node:-1} tasks per node."
-        chunk_size=${max_tasks_per_node:-1}
-        # Create a temporary copy of the mpmd_cmdfile
-        tmp_file="${mpmd_cmdfile}.tmp"
-        cp "${mpmd_cmdfile}" "${tmp_file}"
-        for ((i = 0; i < nm; i += chunk_size)); do
-            chunk_file="${mpmd_cmdfile}.chunk${i}"
-            chunk_mpmd "${tmp_file}" "${chunk_size}" "${chunk_file}"
-            err=$?
-            if [[ ${err} -ne 0 ]]; then
-                echo "FATAL ERROR: Failed to create chunk file '${chunk_file}' from '${tmp_file}'"
-                break
-            fi
-            n_mpmd_tasks=$(wc -l < "${chunk_file}")
-            unset_strict
-            # shellcheck disable=SC2086
-            ${launcher:-} ${mpmd_opt:-} -n "${n_mpmd_tasks}" "${chunk_file}"
-            err=$?
-            if [[ ${err} -ne 0 ]]; then
-                echo "ERROR: MPMD job failed for ${chunk_file}"
-                break
-            fi
-            set_strict
-        done
-    else
-
-        unset_strict
-        # shellcheck disable=SC2086
-        ${launcher:-} ${mpmd_opt:-} -n ${nprocs} "${mpmd_cmdfile}"
-        err=$?
-        set_strict
-
-    fi
-
-elif [[ "${launcher:-}" =~ ^mpiexec.* ]]; then # mpiexec
-
-    # Redirect output from each process to its own stdout
-    # Read the incoming cmdfile and create mpiexec usable cmdfile
-    nm=0
-    echo "#!/bin/bash" >> "${mpmd_cmdfile}"
-    while IFS= read -r line; do
-        echo "${line} > mpmd.${nm}.out 2>&1" >> "${mpmd_cmdfile}"
-        ((nm = nm + 1))
-    done < "${cmdfile}"
-    chmod 755 "${mpmd_cmdfile}"
-
-    # Similar to srun, split the cmdfile into chunks if needed
-    if [[ ${nm} -gt ${max_tasks_per_node:-1} ]]; then
-        echo "INFO: Number of MPMD tasks (${nm}) is greater than the maximum tasks per node (${max_tasks_per_node:-1})."
-        echo "      Running MPMD job in chunks of ${max_tasks_per_node:-1} tasks per node."
-        chunk_size=${max_tasks_per_node:-1}
-        for ((i = 0; i < nm; i += chunk_size)); do
-            chunk_file="${mpmd_cmdfile}.chunk${i}"
-            chunk_mpmd "${tmp_file}" "${chunk_size}" "${chunk_file}"
-            n_mpmd_tasks=$(wc -l < "${chunk_file}")
-            unset_strict
-            # shellcheck disable=SC2086
-            ${launcher:-} ${mpmd_opt:-} -np "${n_mpmd_tasks}" "${chunk_file}"
-            err=$?
-            if [[ ${err} -ne 0 ]]; then
-                echo "ERROR: MPMD job failed for ${chunk_file}"
-                break
-            fi
-            set_strict
-        done
-    else
-
-        # shellcheck disable=SC2086
-        ${launcher:-} -np ${nprocs} ${mpmd_opt:-} "${mpmd_cmdfile}"
-        err=$?
-
-    fi
-
+if [[ ${nm} -gt ${max_tasks_per_node:-1} ]]; then
+    echo "INFO: Number of MPMD tasks (${nm}) is greater than the maximum tasks per node (${max_tasks_per_node:-1})."
+    echo "      Running MPMD job in chunks of ${max_tasks_per_node:-1} tasks per node."
+    chunk_size=${max_tasks_per_node:-1}
+else
+    chunk_size=${nm}
 fi
+
+# Disable error checking for MPMD execution.
+in_shellopts=${SHELLOPTS}
+set +e
+# Start chunking through the MPMD command file.
+chunk_num=1
+err=0
+for ((i = 0; i < nm; i += chunk_size)); do
+    chunk_file="${mpmd_cmdfile}.chunk${chunk_num}"
+    chunk_mpmd "${mpmd_cmdfile}" "${chunk_size}" "${chunk_num}" "${chunk_file}"
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+        echo "ERROR: Failed to create chunk file '${chunk_file}' from '${mpmd_cmdfile}'"
+        break
+    fi
+    n_mpmd_tasks=$(wc -l < "${chunk_file}")
+    # shellcheck disable=SC2086
+    if [[ "${_mpmd_launcher}" == "srun" ]]; then
+        ${launcher:-} ${mpmd_opt:-} -n "${n_mpmd_tasks}" "${chunk_file}"
+    elif [[ "${_mpmd_launcher}" == "mpiexec" ]]; then
+        # shellcheck disable=SC2086
+        ${launcher:-} ${mpmd_opt:-} -np "${n_mpmd_tasks}" "${chunk_file}"
+    fi
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+        echo "ERROR: MPMD job failed for ${chunk_file}"
+        break
+    fi
+    # Call cat_outputs and error if no outputs are found.
+    cat_outputs "true"
+    err=$?
+    if [[ ${err} -ne 0 ]]; then
+        echo "ERROR: No output files found for MPMD job for chunk file '${chunk_file}'"
+        break
+    fi
+    (( chunk_num = chunk_num + 1 ))
+done
 
 # On success remove the command file and any chunk files.
 if [[ ${err} -eq 0 ]]; then
@@ -205,16 +224,14 @@ if [[ ${err} -eq 0 ]]; then
     rm -f "${mpmd_cmdfile}.chunk"*
 fi
 
-# Either way, concatenate the output files if they exist
-out_files=$(find . -name 'mpmd.*.out')
-for file in ${out_files}; do
-    {
-        echo "BEGIN OUTPUT FROM ${file}"
-        cat "${file}"
-        echo "END OUTPUT FROM ${file}"
-    } >> mpmd.out
-    rm -f "${file}"
-done
-cat mpmd.out
+# Concatenate any remaining output files if they exist
+cat_outputs
+if [[ -s mpmd.out ]]; then
+    cat mpmd.out
+else
+    echo "WARNING: No output files found for MPMD job"
+fi
+
+export SHELLOPTS="${in_shellopts}"
 
 exit "${err}"
