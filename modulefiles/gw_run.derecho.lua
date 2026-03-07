48f399d4fda614080091dd1ff934f9dde39be567
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2026-03-06 17:24:50 -0700

    Fill in missing Derecho settings and align with existing

--- modulefiles/gw_run.derecho.lua
+++ modulefiles/gw_run.derecho.lua
@@ -2,19 +2,25 @@ help([[
 Load environment to run GFS on Derecho
 ]])
 
+-- Test that HOMEglobal is set.
+-- If not, load_modules.sh was not sourced to load this module.
+local homegfssdir=os.getenv("HOMEglobal") or "None"
+if (homegfssdir == "None") then
+    LmodError("FATAL ERROR HOMEglobal variable is unset.\n" ..
+              "Please \"source dev/ush/load_modules.sh\" rather than loading this module directly.\n")
+end
+
 setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/modulefiles_extra")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
 load("gw_run.common")
 load(pathJoin("wgrib2", (os.getenv("wgrib2_ver") or "None")))
 
--- prepend_path("MODULEPATH", pathJoin("/scratch3/NCEPDEV/global/role.glopara/git/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
--- load(pathJoin("prepobs", (os.getenv("prepobs_run_ver") or "None")))
+prepend_path("MODULEPATH", pathJoin("/glade/work/kolczynski/global_externals/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
+load(pathJoin("prepobs", (os.getenv("prepobs_run_ver") or "None")))
 
--- prepend_path("MODULEPATH", pathJoin("/scratch3/NCEPDEV/global/role.glopara/git/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
--- load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
+prepend_path("MODULEPATH", pathJoin("/glade/work/kolczynski/global_externals/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
+load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
+
+setenv("CRTM_FIX","/gpfs/csfs1/work/huangwei/GW-fix-data/crtm/v2.4.0.2")
 
 whatis("Description: GFS run environment")
-
--- load(pathJoin("imagemagick", (os.getenv("imagemagick_ver") or "None")))
