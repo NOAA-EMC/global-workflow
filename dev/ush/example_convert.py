7a3ed618d08ce53cfe6f6eec962822a24eed875e
Anton Fernando <anton.fernando@noaa.gov>
2026-03-11 20:35:09 +0000

    delete extra files

--- dev/ush/example_convert.py
+++ dev/ush/example_convert.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from variable_name_converter import GlobalToNetConverter, NetToGlobalConverter
 
-REPO_ROOT = '/scratch3/NCEPDEV/global/Anton.Fernando/global-workflow'
-JOBS_PATH = 'dev/jobs'
+REPO_ROOT = '/scratch3/NCEPDEV/global/Anton.Fernando/global-workflow/'
+JOBS_PATH = 'dev/jobs/'
 
-GlobalToNetConverter().convert(REPO_ROOT, JOBS_PATH, 'gfs')
-NetToGlobalConverter().convert(REPO_ROOT, JOBS_PATH, 'gfs')
+GlobalToNetConverter().convert(REPO_ROOT, JOBS_PATH, 'all')
+#NetToGlobalConverter().convert(REPO_ROOT, JOBS_PATH, 'all')
