2e131f900be62374cc7b1799fa0c314b36b489ca
Anton Fernando <anton.fernando@noaa.gov>
2026-03-12 16:55:09 +0000

    remove scripts

--- dev/ush/example_convert.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from variable_name_converter import GlobalToNetConverter, NetToGlobalConverter
-
-REPO_ROOT = '/scratch3/NCEPDEV/global/Anton.Fernando/global-workflow/'
-JOBS_PATH = 'dev/'
-
-# GlobalToNetConverter().convert(REPO_ROOT, JOBS_PATH, 'gfs')
-NetToGlobalConverter().convert(REPO_ROOT, JOBS_PATH, 'gfs')
