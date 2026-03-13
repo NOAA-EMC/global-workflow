45be5060672d8d39da741b3c04452111ac29a069
Cory Martin <Cory.R.Martin@noaa.gov>
2026-03-13 16:05:41 +0000

    change log file

--- dev/scripts/exglobal_atmos_pmgr.sh
+++ dev/scripts/exglobal_atmos_pmgr.sh
@@ -35,7 +35,7 @@ sleep_interval=10
 max_tries=1000
 for fhr in "${posthours[@]}"; do
     fhr3=$(printf "%03d" "${fhr}")
-    log_file="${COMIN_ATMOS_HISTORY}/${RUN}.${cycle}.atm.logf${fhr3}.txt"
+    log_file="${COMIN_ATMOS_HISTORY}/${RUN}.${cycle}.logf${fhr3}.txt"
     if ! wait_for_file "${log_file}" "${sleep_interval}" "${max_tries}"; then
         export err=1
         err_exit "After 2 hours of waiting for GCAFS FCST hour ${fhr3}."
