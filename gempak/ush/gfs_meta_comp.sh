6358617f92f86090df5c9eb19800493163325dd7
Anton Fernando <anton.fernando@noaa.gov>
2026-03-16 20:38:44 +0000

    add else and warning

--- gempak/ush/gfs_meta_comp.sh
+++ gempak/ush/gfs_meta_comp.sh
@@ -27,6 +27,7 @@ for cycle in $(seq -f "%02g" -s ' ' 0 "${INTERVAL_GFS}" "${cyc}"); do
     for file_in in "${gempak_dir}/gfs_1p00_${PDY}${cycle}f"*; do
         # Only copy the file if it exists (it will not if we start on 6, 12, or 18z)
         if [[ ! -f "${file_in}" ]]; then
+            echo "WARNING: ${file_in} does not exist, skipping"
             continue
         fi
 
@@ -34,6 +35,8 @@ for cycle in $(seq -f "%02g" -s ' ' 0 "${INTERVAL_GFS}" "${cyc}"); do
         # Only create new files, do not overwrite existing
         if [[ ! -f "${file_out}" ]]; then
             cpreq "${file_in}" "${file_out}"
+        else
+            echo "WARNING: ${file_out} already exists, skipping"
         fi
     done
 done
