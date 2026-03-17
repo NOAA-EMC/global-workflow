09c72ae89653a5e10a32a60bc49f7804b118992c
AntonMFernando-NOAA <167725623+AntonMFernando-NOAA@users.noreply.github.com>
2026-03-17 08:34:19 -0400

    Remove preamble sourcing in gempak scripts

--- gempak/ush/gfs_meta_mar_atl.sh
+++ gempak/ush/gfs_meta_mar_atl.sh
@@ -5,8 +5,6 @@
 # Set up Local Variables
 #
 
-source "${HOMEgfs}/ush/preamble.sh"
-
 mkdir -p -m 775 "${DATA}/MAR_ATL"
 cd "${DATA}/MAR_ATL" || exit 2
 cpreq "${HOMEgfs}/gempak/fix/datatype.tbl" datatype.tbl
