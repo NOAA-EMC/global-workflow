68423ce1f1c9d008f62ac87993713b3156dda4fc
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2026-03-24 23:06:25 -0600

    Update Derecho paths to permanent location

--- modulefiles/gw_run.derecho.lua
+++ modulefiles/gw_run.derecho.lua
@@ -15,11 +15,16 @@ setenv("LMOD_TMOD_FIND_FIRST","yes")
 load("gw_run.common")
 load(pathJoin("wgrib2", (os.getenv("wgrib2_ver") or "None")))
 
-prepend_path("MODULEPATH", pathJoin("/glade/work/kolczynski/global_externals/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
+prepend_path("MODULEPATH", pathJoin("/lustre/desc1/p/nral0032/global/global_externals/prepobs/v" .. (os.getenv("prepobs_run_ver") or "None"), "modulefiles"))
 load(pathJoin("prepobs", (os.getenv("prepobs_run_ver") or "None")))
 
-prepend_path("MODULEPATH", pathJoin("/glade/work/kolczynski/global_externals/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
+prepend_path("MODULEPATH", pathJoin("/lustre/desc1/p/nral0032/global/global_externals/Fit2Obs/v" .. (os.getenv("fit2obs_ver") or "None"), "modulefiles"))
 load(pathJoin("fit2obs", (os.getenv("fit2obs_ver") or "None")))
+
+-- Derecho's installation or perl doesn't have a needed module
+-- So, it is installed separately and added to the path
+-- Can likely be removed after vminmon is retired
+prepend_path("PERL5LIB", "/lustre/desc1/p/nral0032/global/perl_modules/lib/perl5")
 
 setenv("CRTM_FIX","/gpfs/csfs1/work/huangwei/GW-fix-data/crtm/v2.4.0.2")
 
