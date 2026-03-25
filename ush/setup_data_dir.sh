b00ceaecbe85d7a84ecb474d2f71bab6a2a58459
Cory Martin <Cory.R.Martin@noaa.gov>
2026-03-25 14:32:30 +0000

    update name of var

--- ush/setup_data_dir.sh
+++ ush/setup_data_dir.sh
@@ -4,7 +4,7 @@
 # Creates a working directory and cd's into it.
 #
 # Source this file with a directory argument:
-#   source "${HOMEglobal}/ush/setup_data_dir.sh" <dir>
+#   source "${HOMEglobal}/ush/setup_data_dir.sh" <dir_to_create_>
 #
 # Requires in environment:
 #   err_exit  - (from err_exit.sh)
@@ -12,12 +12,12 @@
 #   WIPE_DATA - whether to delete any existing directory [default: "YES"]
 #######
 
-local dir="${1:?setup_data_dir.sh requires a directory argument}"
+dir_to_create_="${1:?setup_data_dir.sh requires a directory argument}"
 if [[ ${WIPE_DATA:-YES} == "YES" ]]; then
-    rm -rf "${dir}"
+    rm -rf "${dir_to_create_}"
 fi
-mkdir -p "${dir}"
-if ! cd "${dir}"; then
+mkdir -p "${dir_to_create_}"
+if ! cd "${dir_to_create_}"; then
     export err=1
-    err_exit "[${BASH_SOURCE[0]}]: ${dir} does not exist"
+    err_exit "[${BASH_SOURCE[0]}]: ${dir_to_create_} does not exist"
 fi
