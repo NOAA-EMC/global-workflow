93cd8fdcca7153d547461a3d1d6f02e165c7e59d
Anton Fernando <anton.fernando@noaa.gov>
2026-03-28 00:05:14 +0000

    Remove redundant directory creation for COMOUT_ATMOS_TRACK and ensure track_dir_out is created before archiving outputs

--- ush/python/pygfs/task/archive.py
+++ ush/python/pygfs/task/archive.py
@@ -578,11 +578,12 @@ class Archive(Task):
             # Do not attempt to archive the outputs
             return
 
+        mkdir_p(track_dir_out)
+
         out_track_file = track_dir_out + "/atcfunix." + run + "." + to_YMDH(arch_dict.current_cycle)
         out_track_p_file = track_dir_out + "/atcfunixp." + run + "." + to_YMDH(arch_dict.current_cycle)
 
         def replace_string_from_to_file(filename_in, filename_out, search_str, replace_str):
-
             """Write a new file from the contents of an input file while searching
             and replacing ASCII strings.  To prevent partial file creation, a
             temporary file is created and moved to the final location only
