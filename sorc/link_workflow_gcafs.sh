96aa3526fbfeda2a058ce63de7226e73b92a95bf
Cory Martin <Cory.R.Martin@noaa.gov>
2026-03-30 14:31:37 +0000

    copy crtm coeffs to workflow fix

--- sorc/link_workflow_gcafs.sh
+++ sorc/link_workflow_gcafs.sh
@@ -200,6 +200,36 @@ if [[ -d "${HOMEglobal}/sorc/gdas.cd" ]]; then
 fi
 
 #------------------------------
+#--add coefficient files needed for aerosol DA
+#------------------------------
+if [[ "${machine}" == "wcoss2" ]]; then
+    cd "${HOMEglobal}/fix/gdas" || exit 1
+    mkdir -p crtm_aod
+    cd crtm_aod || exit 1
+    gdas_crtm_fix=/lfs/h2/emc/da/noscrub/emc.da/GDASApp/fix/crtm/2.4.0
+    aod_file_list=(
+  "AerosolCoeff.bin"
+  "CloudCoeff.bin"
+  "v.viirs-m_npp.SpcCoeff.bin"
+  "v.viirs-m_npp.TauCoeff.bin"
+  "v.viirs-m_j1.SpcCoeff.bin"
+  "v.viirs-m_j1.TauCoeff.bin"
+  "v.viirs-m_j2.SpcCoeff.bin"
+  "v.viirs-m_j2.TauCoeff.bin"
+  "NPOESS.VISice.EmisCoeff.bin"
+  "NPOESS.VISland.EmisCoeff.bin"
+  "NPOESS.VISsnow.EmisCoeff.bin"
+  "NPOESS.VISwater.EmisCoeff.bin"
+)
+    for file in "${aod_file_list[@]}"; do
+        if [[ -s "${file}" ]]; then
+            rm -f "${file}"
+        fi
+        ${LINK_OR_COPY} "${gdas_crtm_fix}/${file}" .
+    done
+fi
+
+#------------------------------
 #--add NEXUS files
 #------------------------------
 if [[ -d "${HOMEglobal}/sorc/nexus.fd" ]]; then
