683608690f70abb05bd8ad5ad8eda064f71f9d62
Neil Barton <103681022+NeilBarton-NOAA@users.noreply.github.com>
2026-04-08 10:09:02 -0400

    Update ush/process_atmos_daily.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ush/process_atmos_daily.sh
+++ ush/process_atmos_daily.sh
@@ -53,7 +53,10 @@ for ((d = 1; d <= ${month_days}; d++)); do
     list_acc=""
     for hr in $((end_of_day_fhr - 18)) $((end_of_day_fhr - 12)) $((end_of_day_fhr - 6)) ${end_of_day_fhr}; do
         fpath="${COMIN_ATMOS_MASTER}/sfs.t${cyc}z.master.f$(printf "%03d" "${hr}").grib2"
-        if [[ ! -f "${fpath}" ]]; then all_acc_exist=false; break; fi
+        if [[ ! -f "${fpath}" ]]; then
+            all_acc_exist=false
+            break
+        fi
         list_acc+="${fpath} "
     done
 
