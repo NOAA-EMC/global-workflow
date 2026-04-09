4b2f180178a12525108b4e24fe75153ca852773c
DavidHuber-NOAA <david.huber@noaa.gov>
2026-04-09 16:58:38 +0000

    Move load_modules back to dev/ush

--- dev/ush/load_modules.sh
+++ dev/ush/load_modules.sh
@@ -15,7 +15,6 @@ fi
 
 if [[ "${DEBUG_WORKFLOW:-NO}" == "NO" ]]; then
     echo "Loading modules quietly..."
-    set +x
 fi
 
 # Parse module type argument
@@ -184,9 +183,21 @@ case "${MODULE_TYPE}" in
             mod_type="${MODULE_TYPE}"
         fi
 
+        #### Work around for upp module loading issues that is inconsistance with run.ver
         # Source versions file (except for upp)
         if [[ "${mod_type}" != "upp" ]]; then
             source "${HOMEgfs}/versions/run.ver"
+        fi
+        if [[ "${mod_type}" == "upp" ]]; then
+          export hdf5_ver="1.10.6"
+          export netcdf_ver="4.7.4"
+          export g2tmpl_ver="1.16.0"
+          export crtm_ver="2.4.0.1"  #### gfs_goesupp
+          #### Workaround for access ${HOMEgfs}/sorc location
+          export PYTHONPATH=${HOMEgfs}/sorc/wxflow/src:/apps/dev/ve/intel/19.1.3.304/python/3.12.0/gw/1.0/lib/python3.12/site-packages:${HOMEgfs}/ush/python
+        fi
+        if [[ "${mod_type}" == "run" ]]; then
+          export PYTHONPATH=${HOMEgfs}/sorc/wxflow/src:/apps/ops/prod/nco/core/prod_util.v2.0.9/ush:/apps/dev/ve/intel/19.1.3.304/python/3.12.0/gw/1.0/lib/python3.12/site-packages:${HOMEgfs}/ush/python
         fi
 
         if [[ -n "${target_module}" ]]; then
