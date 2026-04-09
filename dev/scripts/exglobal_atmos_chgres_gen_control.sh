baa65d48e6d4da5b5d5146fab32256b300a3418e
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2026-04-09 14:54:01 -0400

    [GFS v17] Remove sorc runtime dependencies (#4742)
    This removes runtime dependencies on sorc from dev/gfs.v17.

--- dev/scripts/exglobal_atmos_chgres_gen_control.sh
+++ dev/scripts/exglobal_atmos_chgres_gen_control.sh
@@ -77,7 +77,7 @@ cat << EOF > ./fort.41
 /
 EOF
 
-${APRUN_CHGRES} "${HOMEgfs}/sorc/ufs_utils.fd/exec/chgres_cube"
+${APRUN_CHGRES} "${EXECgfs}/chgres_cube"
 export err=$?
 if [[ ${err} -ne 0 ]]; then
     err_exit "chgres_cube failed to create cold start ICs, ABORT!"
