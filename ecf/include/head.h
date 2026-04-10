a351f0b2cdc6dd960bede257d8e50971eb7ea410
David Huber <david.huber@noaa.gov>
2026-04-10 15:54:43 +0000

    Remove extra spaces

--- ecf/include/head.h
+++ ecf/include/head.h
@@ -35,7 +35,7 @@ eval "export HOME${model:?"model undefined"}=$modelhome"
 eval "versionfile=\$HOME${model}/versions/run.ver"
 if [ -n "%rrfs_ver:%" ]; then export rrfs_ver=${rrfs_ver:-%rrfs_ver:%}; fi
 if [ -f "$versionfile" ]; then
-  . $versionfile 
+  . $versionfile
 fi
 export envir=%ENVIR%
 export MACHINE_SITE=%MACHINE_SITE%
