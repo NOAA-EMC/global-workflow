5d24ce9ea9dcaeffa56b456f2f8ff3dcf5088592
DavidHuber-NOAA <david.huber@noaa.gov>
2026-04-21 14:44:53 +0000

    Capture error rather than ignore it

--- dev/scripts/exglobal_atmos_products.sh
+++ dev/scripts/exglobal_atmos_products.sh
@@ -89,11 +89,9 @@ for ((nset = 1; nset <= downset; nset++)); do
 
         # if final record of is u-component, add next record v-component
         # if final record is land, add next record icec
-        # grep returns 1 if no match is found, so temporarily turn off exit on non-zero rc
-        unset_strict
-        ${WGRIB2} -d "${last}" "${tmpfile}" | grep -E -i "ugrd|ustm|uflx|u-gwd|land|maxuw"
+        # grep returns 1 if no match is found (not an error)
+        ${WGRIB2} -d "${last}" "${tmpfile}" | grep -E -i "ugrd|ustm|uflx|u-gwd|land|maxuw" && true
         rc=$?
-        source "${USHgfs}/set_strict.sh"
         if [[ ${rc} == 0 ]]; then # Matched the grep
             last=$((last + 1))
         fi
