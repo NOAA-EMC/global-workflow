f9601d8591c523e0253bcd194b8c3d645bc6368f
Clara.Draper-NOAA <clara.draper@noaa.gov>
2026-05-15 14:46:01 +0000

    added missing dependency on regrid of soil increments for forecast job.

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -1029,7 +1029,7 @@ class GFSTasks(Tasks):
         deps = []
         dep_dict = {'type': 'task', 'name': f'{self.run}_sfcanl_gcycle'}
         deps.append(rocoto.add_dependency(dep_dict))
-        if self.options['do_gsisoilda'] and self.run in ['gdas']:
+        if self.options['do_gsisoilda'] and self.run in ['gdas','enkfgdas']:
             dep_dict = {'type': 'task', 'name': f'{self.run}_sfcanl_regrid'}
             deps.append(rocoto.add_dependency(dep_dict))
 
