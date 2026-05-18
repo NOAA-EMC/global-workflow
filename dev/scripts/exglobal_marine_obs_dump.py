01e276b79b050b86a1df7ed45631a49bec666a73
Cory Martin <Cory.R.Martin@noaa.gov>
2026-05-18 16:14:17 +0000

    WIP

--- dev/scripts/exglobal_marine_obs_dump.py
+++ dev/scripts/exglobal_marine_obs_dump.py
@@ -18,16 +18,11 @@ if __name__ == '__main__':
     config_env = cast_strdict_as_dtypedict(os.environ)
 
     # Take configuration from YAML file to augment/append config dict
-    config_yaml = parse_j2yaml(os.path.join(config_env['HOMEobsforge'], 'parm', 'config.yaml'), config_env)
-    # Extract obsforge specific configuration
-    obsforge_dict = {}
-    for key, value in config_yaml['obsforge'].items():
-        if key not in config_env.keys():
-            obsforge_dict[key] = value
+    config_yaml = parse_j2yaml(config_env['marine_obs_provider_dict'], config_env)
 
     # Combine configs together
     config = AttrDict(**config_env, **obsforge_dict)
-    config = AttrDict(**config, **config_yaml['marinedump'])
+    config = AttrDict(**config, **config_yaml)
 
     marineObs = MarineObsPrep(config)
     marineObs.initialize()
