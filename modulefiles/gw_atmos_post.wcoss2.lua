025881efd6bba1fd503f824482dac74f72f48956
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-05-20 21:38:10 +0000

    module versions for atmos_post

--- modulefiles/gw_atmos_post.wcoss2.lua
+++ modulefiles/gw_atmos_post.wcoss2.lua
@@ -1,16 +1,26 @@
 help([[
-  This module loads libraries required for running atmos_post job
-  on Wcoss2 (NOAA Acorn) using ue-oneapi-2024.2.1.
+Load environment for run atmos_post on WCOSS2
 ]])
-prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2024.2.1/modules/Core")
 
-load("stack-intel-oneapi-compilers/2024.2.1")
-load("stack-cray-mpich/8.1.29")
+load(pathJoin("PrgEnv-intel", "8.3.3"))
+load(pathJoin("craype", "2.7.13"))
+load(pathJoin("intel", "19.1.3.304"))
+load(pathJoin("craype", "2.7.17"))
+load(pathJoin("cray-mpich", "8.1.19"))
+load(pathJoin("cray-pals", "1.0.17"))
+load(pathJoin("cfp", "2.0.4"))
+setenv("USE_CFP","YES")
 
-load("wgrib2/3.8.0")
-load("grib-util/1.4.0")
+prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
+
+load(pathJoin("wgrib2", "3.8.0"))
+
+load(pathJoin("g2c", "2.3.0"))
+load(pathJoin("libjpeg-turbo", "2.1.0"))
+load(pathJoin("libaec", "1.1.3"))
+
+load(pathJoin("netcdf-D", "4.9.2"))
 
 setenv("WGRIB2","wgrib2")
 setenv("GMERGE","gmerge")
-
-whatis("Description: atmos_post run environment on Wcoss2 (NOAA Acorn)")
+whatis("Description: atmos_post run environment on WCOSS2")
