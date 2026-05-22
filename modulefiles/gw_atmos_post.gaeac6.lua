bf1c09d61b0b9731e4a6efd8e2444679f7f25df9
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-05-22 13:42:41 +0000

    fix path on c6 modules

--- modulefiles/gw_atmos_post.gaeac6.lua
+++ modulefiles/gw_atmos_post.gaeac6.lua
@@ -4,7 +4,7 @@ help([[
 ]])
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-2.1.0/envs/ue-oneapi-2025.2.1/modules/Core")
 
-load("stack-intel-oneapi-compilers.2025.2.1")
+load("stack-intel-oneapi-compilers/2025.2.1")
 load("stack-cray-mpich/8.1.32")
 
 load("wgrib2/3.8.0")
