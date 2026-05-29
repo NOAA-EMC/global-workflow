08af84b9eff11733a02c30f86ac893700eca7219
David Huber <david.huber@noaa.gov>
2026-05-29 11:54:40 +0000

    Simplify arch_ecf_logs.sh

--- dev/job_cards/rocoto/arch_ecf_logs.sh
+++ dev/job_cards/rocoto/arch_ecf_logs.sh
@@ -5,10 +5,16 @@ set -x
 export job="arch_logs"
 export jobid="${job}.$$"
 
-source "${HOMEglobal}/ush/jjob_header.sh" -e "arch_logs" -c "base"
+# Source config.base to get ATARDIR
+ATARDIR=$(source "${EXPDIR}/config.base" >& /dev/null && echo "${ATARDIR}")
+
+if [[ -z "${ATARDIR:-}" ]]; then
+    echo "FATAL ERROR Could not determine the HPSS archive directory (ATARDIR)!"
+    exit 1
+fi
 
 ###############################################################
-# Tar up the ecflow logs
+# Create the tarball of ecflow logs
 cd "${ECF_LOG_DIR}" || exit 1
 
 htar -cvf "${ATARDIR}/${PDY}${cyc}/${RUN}_logs.tar" "${PDY}${cyc}/${RUN}_"*
