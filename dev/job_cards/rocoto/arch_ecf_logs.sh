314e1a263bf2b0a87c7a92227f344c6462906dac
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2026-06-02 09:58:52 -0400

    (dev/gfs.v17) Introduce the auxiliary verification and archiving workflow (#4950)
    This introduces an auxiliary workflow intended to run alongside the
    ecflow GFS real-time parallel. The workflow shares the same COM space as
    the desired primary workflow and will populate data directly.

---
+++ dev/job_cards/rocoto/arch_ecf_logs.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+set -x
+
+export job="arch_logs"
+export jobid="${job}.$$"
+
+# Source config.base to get ATARDIR
+ATARDIR=$(source "${EXPDIR}/config.base" >&/dev/null && echo "${ATARDIR}")
+
+if [[ -z "${ATARDIR:-}" ]]; then
+    echo "FATAL ERROR Could not determine the HPSS archive directory (ATARDIR)!"
+    exit 1
+fi
+
+###############################################################
+# Create the tarball of ecflow logs
+cd "${ECF_LOG_DIR}" || exit 1
+
+htar -cvf "${ATARDIR}/${PDY}${cyc}/${RUN}_logs.tar" "${PDY}${cyc}/${RUN}_"*
+
+status=$?
+
+exit "${status}"
