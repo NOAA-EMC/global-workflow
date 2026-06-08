4a9086c45596b6d4c9a7eb6ff536c11e05376da2
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-06-08 13:37:50 +0000

    deal with unused variable

--- ecf/setup_ecf_links.sh
+++ ecf/setup_ecf_links.sh
@@ -6,8 +6,9 @@ module load prod_util
 # GFS ecflow workflow resource configuration
 ECF_DIR=$(pwd)
 
-# Create tmp file for git exclude (note need to uncomment lines below to reactivate)
-tmp_exclude="${ECF_DIR}/exclude_list.tmp"
+# Create tmp file for git exclude (note need to uncomment line below 
+# and lines in add_to_tmpfile to reactivate)
+# tmp_exclude="${ECF_DIR}/exclude_list.tmp"
 
 # Function that loops over forecast hours and
 # creates link between the master and target
