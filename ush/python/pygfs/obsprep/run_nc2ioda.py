b4fd1614061aa709efdfc769245ed6b2e09c1e19
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-06-17 18:40:58 +0000

    update run_nc2ioda.py for PARMgfs -> PARMglobal and not return 0 on error

--- ush/python/pygfs/obsprep/run_nc2ioda.py
+++ ush/python/pygfs/obsprep/run_nc2ioda.py
@@ -18,7 +18,7 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
     Returns:
         int: Returns 0 upon successful execution. Logs errors and warnings for failures.
     """
-    jinja_template = join(task_config['PARMgfs'], "gdas", "marine", "nc2ioda.yaml.j2")
+    jinja_template = join(task_config['PARMglobal'], "gdas", "marine", "nc2ioda.yaml.j2")
     yaml_config = parse_j2yaml(jinja_template, context)
     nc2ioda_yaml = join(task_config['DATA'], obs_space, f"{obs_space}_nc2ioda.yaml")
     save_as_yaml(yaml_config, nc2ioda_yaml)
@@ -34,4 +34,3 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
         return 0
     except Exception as e:
         logger.warning(f"ioda converter failed with error {e}")
-        return 0
