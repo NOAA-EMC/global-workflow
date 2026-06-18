f9dee3b0ce7e8c3eda307209d87be384522564e0
Anton Fernando <anton.fernando@noaa.gov>
2026-06-18 20:12:12 +0000

    Remove CDATE variable assignment from various ECF scripts to streamline execution and avoid potential conflicts with date handling. This change affects multiple scripts across analysis, forecast, prep, and product categories in both GDAS and GFS directories.

--- ecf/include/head.h
+++ ecf/include/head.h
@@ -24,7 +24,6 @@ fi
 
 module load prod_util
 export PDY=$(cut -c7-14 /lfs/h1/ops/prod/com/date/t%CYC%z)
-CDATE=${PDY}%CYC:%
 
 if [ -d /apps/ops/prod ]; then # On WCOSS2
   set +x
@@ -107,8 +106,8 @@ ERROR() {
   else
      msg="Killed by signal $1"
   fi
-  #To send email about failure, uncomment next line and update email list: 
-#echo "${ECF_NAME} log file: ${ECF_JOBOUT}" | mail -s "GFSv17 ecflow realtime job failure: ${ECF_NAME}" ${USER}@noaa.gov 
+  #To send email about failure, uncomment next line and update email list:
+#echo "${ECF_NAME} log file: ${ECF_JOBOUT}" | mail -s "GFSv17 ecflow realtime job failure: ${ECF_NAME}" ${USER}@noaa.gov
   ecflow_client --abort="$msg"
   echo $msg
   if [[ " ops.prod ops.para " =~ " $(whoami) " ]]; then
@@ -118,4 +117,3 @@ ERROR() {
 }
 # Trap all error and exit signals
 trap 'ERROR $?' ERR EXIT
-
