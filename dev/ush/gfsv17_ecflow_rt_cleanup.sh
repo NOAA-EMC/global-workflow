1aae4f3f9f9a6e741630c08a06d5b729cb8818ff
David Huber <david.huber@noaa.gov>
2026-06-18 15:37:24 +0000

    Cleanup

--- dev/ush/gfsv17_ecflow_rt_cleanup.sh
+++ dev/ush/gfsv17_ecflow_rt_cleanup.sh
@@ -8,39 +8,38 @@ DATA=/lfs/h2/emc/ptmp/emc.global/gfs_v17_cleanup.$$
 COMROOT=/lfs/h2/emc/gfstemp/emc.global/ecflow/comroot/ops/para/com/gfs/v17.0
 DATAROOT=/lfs/h2/emc/gfstemp/emc.global/ecflow/rundirs
 
-mkdir -p ${DATA} ${DATAROOT}
-export PDY=$($NDATE| cut -c1-8)
+mkdir -p "${DATA}" "${DATAROOT}"
+export PDY=$("${NDATE}" | cut -c1-8)
 export cycle=t00z
 
-cd $DATA
+cd "${DATA}"
 setpdy.sh
 source PDY
 # PDYm1 PDY PDYp1
 
 # Exception handling - if the realtime state is delay
-if [ ! -d ${COMROOT}/enkfgdas.${PDY}/06 ]; then
-  echo "FATAL ERROR: The ${COMROOT}/enkfgdas.${PDY}/06 is not found"
-  exit 9
+if [[ ! -d "${COMROOT}/enkfgdas.${PDY}/06" ]]; then
+    echo "FATAL ERROR: The ${COMROOT}/enkfgdas.${PDY}/06 is not found"
+    exit 9
 fi
 
 # Clean DATA
 # DATA retain current PDY   ~   60TB
-cd $DATAROOT
-excluded_pdy=$PDY
+cd "${DATAROOT}"
+excluded_pdy=${PDY}
 excluded_cdate_m1=${PDYm1}18
-for dir_to_remove in $(ls -d */ | grep -v ${excluded_pdy} | grep -v ${excluded_cdate_m1} | grep -v "DBNLOG" | grep -v "ecflow"); do
-  dir_to_remove="${dir_to_remove%/}"
-  echo "Removing directory ${DATAROOT}/${dir_to_remove}"
-  rm -rf ${DATAROOT}/${dir_to_remove}
+for dir_to_remove in $(ls -d */ | grep -v "${excluded_pdy}" | grep -v "${excluded_cdate_m1}" | grep -v "DBNLOG" | grep -v "ecflow"); do
+    dir_to_remove="${dir_to_remove%/}"
+    echo "Removing directory ${DATAROOT}/${dir_to_remove}"
+    rm -rf "${DATAROOT}/${dir_to_remove}"
 done
 
 
 # Clean COM
 # COM retain 3 full days - 195TB (65T /day on production frequency)
-cd $COMROOT
-for dir_to_remove in $(ls -d */ | grep -v ${PDY} | grep -v ${PDYm1} | grep -v ${PDYm2} | grep -v ${PDYm3} | grep -v "fix"); do
-#### for dir_to_remove in $(ls -d */ | grep -v ${PDY} | grep -v ${PDYm1} | grep -v ${PDYm2} | grep -v "fix"); do
-  dir_to_remove="${dir_to_remove%/}"
-  echo "Removing directory ${COMROOT}/${dir_to_remove}"
-  rm -rf ${COMROOT}/${dir_to_remove}
+cd "${COMROOT}"
+for dir_to_remove in $(ls -d */ | grep -v "${PDY}" | grep -v "${PDYm1}" | grep -v "${PDYm2}" | grep -v "${PDYm3}" | grep -v "fix"); do
+    dir_to_remove="${dir_to_remove%/}"
+    echo "Removing directory ${COMROOT}/${dir_to_remove}"
+    rm -rf "${COMROOT}/${dir_to_remove}"
 done
