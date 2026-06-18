7033fc682e2437c9518dab3574401fe433170afd
Jessica Meixner <jessica.meixner@noaa.gov>
2026-06-18 09:52:29 -0400

    [v17] bug fix and code owner update (#5033)

--- ush/python/pygfs/obsprep/run_nc2ioda.py
+++ ush/python/pygfs/obsprep/run_nc2ioda.py
@@ -18,13 +18,13 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
     Returns:
         int: Returns 0 upon successful execution. Logs errors and warnings for failures.
     """
-    jinja_template = join(task_config['PARMgfs'], "gdas", "marine", "nc2ioda.yaml.j2")
+    jinja_template = join(task_config['PARMglobal'], "gdas", "marine", "nc2ioda.yaml.j2")
     yaml_config = parse_j2yaml(jinja_template, context)
     nc2ioda_yaml = join(task_config['DATA'], obs_space, f"{obs_space}_nc2ioda.yaml")
     save_as_yaml(yaml_config, nc2ioda_yaml)
 
     # Run the ioda converter
-    nc2ioda_exe = join(task_config['EXECgfs'], 'gdas_obsprovider2ioda.x')
+    nc2ioda_exe = join(task_config['EXECglobal'], 'gdas_obsprovider2ioda.x')
     exec_cmd = Executable(nc2ioda_exe)
     exec_cmd.add_default_arg(nc2ioda_yaml)
 
@@ -34,4 +34,3 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
         return 0
     except Exception as e:
         logger.warning(f"ioda converter failed with error {e}")
-        return 0
