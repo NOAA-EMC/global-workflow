a41171ac6e1d646828acc32f2434ea37ad462b98
Travis Elless <travis.j.elless@noaa.gov>
2026-07-15 19:52:07 +0000

    additional version updates

--- dev/ush/load_modules.sh
+++ dev/ush/load_modules.sh
@@ -78,13 +78,14 @@ case "${MODULE_TYPE}" in
             module load prod_util
         fi
         if [[ "${MACHINE_ID}" == "wcoss2" ]]; then
-            module load cray-pals
-            module load cfp
-            module load libjpeg
+            module load "cray-pals/${cray_pals_ver}"
+            module load "cfp/${cfp_ver}"
+            module load "libjpeg/${libjpeg_ver}"
             module load craype-network-ucx
-            module load cray-mpich-ucx
-            module load python/3.8.6
-            module load wgrib2
+            module load "cray-mpich-ucx/${cray_mpich_ver}"
+            module load "python/${python_ver}"
+	    module load "ve/gfs/${ve_gfs_ver}"
+            module load "wgrib2/${wgrib2_ver}"
         else
             export UTILROOT=${prod_util_ROOT}
             source "${HOMEglobal}/versions/run.ver"
