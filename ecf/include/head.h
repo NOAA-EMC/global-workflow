0d4a6b8ef30d10e4d2134deb1190d93ae4bff4b7
David Huber <david.huber@noaa.gov>
2026-07-24 14:42:49 +0000

    Set SENDDBN to YES by default

--- ecf/include/head.h
+++ ecf/include/head.h
@@ -51,12 +51,7 @@ if [ -n "%COMPATH:%" ]; then export COMPATH=${COMPATH:-%COMPATH:%}; fi
 if [ -n "%MAILTO:%" ]; then export MAILTO=${MAILTO:-%MAILTO:%}; fi
 if [ -n "%DBNLOG:%" ]; then export DBNLOG=${DBNLOG:-%DBNLOG:%}; fi
 export KEEPDATA=NO
-#### enkfgdas fcst job failure with SENDDBN="YES" as of 20260306 with expdir:
-####   -rw-r--r-- 1 emc.global global 17K Mar  4 15:57 /lfs/h2/emc/gfstemp/emc.global/expdir/retrov17_01_realtime/config.base
-####   GFS code hash with this issue caab01
-#### Therefore, set the SENDDBN to NO for now
-#### export SENDDBN=${SENDDBN:-%SENDDBN:YES%}
-export SENDDBN="NO"
+export SENDDBN=${SENDDBN:-%SENDDBN:YES%}
 export DBNLOG="NO"
 export SENDDBN_NTC=${SENDDBN_NTC:-%SENDDBN_NTC:YES%}
 
