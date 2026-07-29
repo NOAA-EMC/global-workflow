3eb54c57f23543328217c0f9de31cf0bb990dd2e
jessica.meixner <Jessica.Meixner@noaa.gov>
2026-07-29 17:15:04 -0400

    homegfs -> homeglobal

--- dev/workflow/rocoto/sfs_tasks.py
+++ dev/workflow/rocoto/sfs_tasks.py
@@ -340,7 +340,7 @@ class SFSTasks(Tasks):
                      'dependency': dependencies,
                      'envars': wave_post_envars,
                      'cycledef': self.run,
-                     'command': f'{self.HOMEgfs}/dev/job_cards/rocoto/wavepostgridded.sh',
+                     'command': f'{self.HOMEglobal}/dev/job_cards/rocoto/wavepostgridded.sh',
                      'job_name': f'{self.pslot}_{task_name}_@H',
                      'log': f'{self.rotdir}/logs/@Y@m@d@H/{task_name}.log',
                      'maxtries': '&MAXTRIES;'
