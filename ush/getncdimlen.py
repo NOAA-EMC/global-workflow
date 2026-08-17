cfc4c21e3d0761a3f16cad9a2ecc3fdc7dddfd0c
DavidHuber-NOAA <david.huber@noaa.gov>
2026-08-17 17:48:40 +0000

    More pycodestyle

--- ush/getncdimlen.py
+++ ush/getncdimlen.py
@@ -9,12 +9,12 @@ import argparse
 import gsi_utils
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(
-           description='Get length of dimension specified from a FV3GFS netCDF file')
-  parser.add_argument('ncfile', help='path to input netCDF file', type=str)
-  parser.add_argument('dimname', help='name of dimension (ex: grid_xt)', type=str)
-  args = parser.parse_args()
-  if not os.path.isfile(args.ncfile):
-      parser.error(f"FATAL ERROR: input netcdf file {args.ncfile} is missing")
-  FileDims = gsi_utils.get_ncdims(args.ncfile)
-  print(FileDims[args.dimname])
+    parser = argparse.ArgumentParser(
+             description='Get length of dimension specified from a FV3GFS netCDF file')
+    parser.add_argument('ncfile', help='path to input netCDF file', type=str)
+    parser.add_argument('dimname', help='name of dimension (ex: grid_xt)', type=str)
+    args = parser.parse_args()
+    if not os.path.isfile(args.ncfile):
+        parser.error(f"FATAL ERROR: input netcdf file {args.ncfile} is missing")
+    FileDims = gsi_utils.get_ncdims(args.ncfile)
+    print(FileDims[args.dimname])
