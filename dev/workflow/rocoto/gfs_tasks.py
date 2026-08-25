4b31b8e80cb1dcae216316fbef68f87dd586482e
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-08-25 18:37:41 +0000

    remove gempakmeta

--- dev/workflow/rocoto/gfs_tasks.py
+++ dev/workflow/rocoto/gfs_tasks.py
@@ -1782,29 +1782,6 @@ class GFSTasks(Tasks):
 
         return task
 
-    def gempakmeta(self):
-        deps = []
-        dep_dict = {'type': 'metatask', 'name': f'{self.run}_gempak'}
-        deps.append(rocoto.add_dependency(dep_dict))
-        dependencies = rocoto.create_dependency(dep=deps)
-
-        resources = self.get_resource('gempakmeta')
-        task_name = f'{self.run}_gempakmeta'
-        task_dict = {'task_name': task_name,
-                     'resources': resources,
-                     'dependency': dependencies,
-                     'envars': self.envars,
-                     'cycledef': self.run.replace('enkf', ''),
-                     'command': f'{self.HOMEglobal}/dev/job_cards/rocoto/gempakmeta.sh',
-                     'job_name': f'{self.pslot}_{task_name}_@H',
-                     'log': f'{self.rotdir}/logs/@Y@m@d@H/{task_name}.log',
-                     'maxtries': '&MAXTRIES;'
-                     }
-
-        task = rocoto.create_task(task_dict)
-
-        return task
-
     def gempakmetancdc(self):
         deps = []
         dep_dict = {'type': 'metatask', 'name': f'{self.run}_gempak'}
@@ -2649,8 +2626,6 @@ class GFSTasks(Tasks):
                     dep_dict = {'type': 'task', 'name': f'{self.run}_gempakmetancdc'}
                     deps.append(rocoto.add_dependency(dep_dict))
                 elif self.run in ['gfs']:
-                    dep_dict = {'type': 'task', 'name': f'{self.run}_gempakmeta'}
-                    deps.append(rocoto.add_dependency(dep_dict))
                     if self.app_config.mode in ['cycled']:
                         dep_dict = {'type': 'task', 'name': f'{self.run}_gempakncdcupapgif'}
                         deps.append(rocoto.add_dependency(dep_dict))
