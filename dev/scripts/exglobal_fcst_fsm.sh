d1a24a1cd724efb84e92ea9b9b3819fffe23ae36
Anton Fernando <Anton.Fernando@noaa.gov>
2026-08-25 23:51:27 +0000

    style(exglobal_fcst_fsm.sh): standardize shellcheck directive formatting
    - Remove extra spaces before shellcheck directive comments for consistency
    - Align comment formatting with project style guidelines
    - Improve code readability by standardizing comment spacing

--- dev/scripts/exglobal_fcst_fsm.sh
+++ dev/scripts/exglobal_fcst_fsm.sh
@@ -72,9 +72,9 @@ fi
 scan_and_release() {
     local flag_var="${1}" arr_name="${2}" event="${3}" tmpl="${4}"
     shift 4
-    # shellcheck disable=SC2034  # nameref; assignments propagate to caller
+    # shellcheck disable=SC2034 # nameref; assignments propagate to caller
     local -n scan_flag="${flag_var}"
-    # shellcheck disable=SC2034  # nameref; assignments propagate to caller
+    # shellcheck disable=SC2034 # nameref; assignments propagate to caller
     local -n state="${arr_name}"
 
     # tmpl may hold one or more space-separated templates; split into an array.
