b21e341347ca1da951b1ba1443cc21ede5bd0724
Tseganeh Z. Gichamo <zacctsega@yahoo.com>
2026-08-31 11:23:14 -0400

    Update ush/python/pygfs/task/snow_letkf_analysis.py
    Co-authored-by: Jiarui Dong <Jiarui.Dong@noaa.gov>

--- ush/python/pygfs/task/snow_letkf_analysis.py
+++ ush/python/pygfs/task/snow_letkf_analysis.py
@@ -366,7 +366,7 @@ class SnowLetkfAnalysis(Analysis):
                 'current_cycle': bkgtime,
                 'CASE': self.task_config.CASE,
                 'DATA': self.task_config.DATA,
-                'HOMEgfs': self.task_config.HOMEgfs,
+                'HOMEglobal': self.task_config.HOMEglobal,
                 'OCNRES': self.task_config.OCNRES,
                 'CASE_ENS': self.task_config.CASE_ENS,
                 'ens_size': self.task_config.ens_size,
