b1b6d2039b3b04ef1b9dcbcad6cee88c5d891de7
Jessica Meixner <jessica.meixner@noaa.gov>
2026-09-01 09:09:06 -0400

    (dev/gfs.v17) Update modules for wave jobs & forecasts  (#5232)
    This PR reduces to only the modules needed for forecast and wave jobs in
    the ecf scripts. Also, I noticed we didn't have err_exit calls and added
    those as well.
    Refs #4509

--- dev/scripts/exglobal_wave_post_pnt.sh
+++ dev/scripts/exglobal_wave_post_pnt.sh
@@ -230,6 +230,9 @@ if [[ "${DOSPC_WAV}" == "YES" ]]; then
 
     export pgm="ww3_outp_${NET,,}.x"
     "${EXECglobal}/${pgm}"
+    if [[ ${err} -ne 0 ]]; then
+        err_exit "${WAV_MOD_TAG} post ${PDY} t${cyc}z : did not create spectral point output."
+    fi
 fi
 
 if [[ "${DOBLL_WAV}" == "YES" ]]; then
@@ -243,6 +246,9 @@ if [[ "${DOBLL_WAV}" == "YES" ]]; then
 
     export pgm="ww3_outp_${NET,,}.x"
     "${EXECglobal}/${pgm}"
+    if [[ ${err} -ne 0 ]]; then
+        err_exit "${WAV_MOD_TAG} post ${PDY} t${cyc}z : did not create bll point output."
+    fi
 fi
 # Restore default pgm after override
 pgm=$(basename "${BASH_SOURCE[0]}")
